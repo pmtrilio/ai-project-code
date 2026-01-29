000980becfe96dfd34a814ca0e2df635db4906cf
pmtrilio <pm@trilio.bg>
2026-01-29 09:57:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Маршрутизиране.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Illuminate\Routing;
+
+use ArrayObject;
+use Closure;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Routing\BindingRegistrar;
+use Illuminate\Contracts\Routing\Registrar as RegistrarContract;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Events\PreparingResponse;
+use Illuminate\Routing\Events\ResponsePrepared;
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Routing\Events\Routing;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Traits\Tappable;
+use JsonSerializable;
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
+use ReflectionClass;
+use stdClass;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+/**
+ * @mixin \Illuminate\Routing\RouteRegistrar
+ */
+class Router implements BindingRegistrar, RegistrarContract
+{
+    use Macroable {
+        __call as macroCall;
+    }
+    use Tappable;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The route collection instance.
+?>
