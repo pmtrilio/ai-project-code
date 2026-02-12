71c47721008e8fca6eb256564e76f61b6e76583b
pmtrilio <pm@trilio.bg>
2026-02-12 19:03:45 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Обработка на заявки – Request #397 (30-90).php
@@ -0,0 +1,61 @@
+ * @link      http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5
+ */
+class Request extends AbstractMessage implements RequestInterface
+{
+    /**#@+
+     *
+     * @const string METHOD constant names
+     */
+    public const METHOD_OPTIONS  = 'OPTIONS';
+    public const METHOD_GET      = 'GET';
+    public const METHOD_HEAD     = 'HEAD';
+    public const METHOD_POST     = 'POST';
+    public const METHOD_PUT      = 'PUT';
+    public const METHOD_DELETE   = 'DELETE';
+    public const METHOD_TRACE    = 'TRACE';
+    public const METHOD_CONNECT  = 'CONNECT';
+    public const METHOD_PATCH    = 'PATCH';
+    public const METHOD_PROPFIND = 'PROPFIND';
+    /**#@-*/
+
+    /** @var string */
+    protected $method = self::METHOD_GET;
+
+    /** @var bool */
+    protected $allowCustomMethods = true;
+
+    /** @var string|HttpUri */
+    protected $uri;
+
+    /** @var ParametersInterface */
+    protected $queryParams;
+
+    /** @var ParametersInterface */
+    protected $postParams;
+
+    /** @var ParametersInterface */
+    protected $fileParams;
+
+    /**
+     * A factory that produces a Request object from a well-formed Http Request string
+     *
+     * @param  string $string
+     * @param  bool $allowCustomMethods
+     * @throws Exception\InvalidArgumentException
+     * @return static
+     */
+    public static function fromString($string, $allowCustomMethods = true)
+    {
+        $request = new static();
+        $request->setAllowCustomMethods($allowCustomMethods);
+
+        $lines = explode("\r\n", $string);
+
+        // first line must be Method/Uri/Version string
+        $matches = null;
+        $methods = $allowCustomMethods
+            ? '[\w-]+'
+            : implode(
+                '|',
+                [
+                    self::METHOD_OPTIONS,
