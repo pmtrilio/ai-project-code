39084c6e04382f390916799a32de188aa0d1aa57
pmtrilio <pm@trilio.bg>
2026-02-12 14:42:19 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: View #176 (5-65).php
@@ -0,0 +1,61 @@
+use ArrayAccess;
+use BadMethodCallException;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\Support\MessageProvider;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Contracts\View\Engine;
+use Illuminate\Contracts\View\View as ViewContract;
+use Illuminate\Support\Collection;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\ViewErrorBag;
+use Stringable;
+use Throwable;
+
+class View implements ArrayAccess, Htmlable, Stringable, ViewContract
+{
+    use Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The view factory instance.
+     *
+     * @var \Illuminate\View\Factory
+     */
+    protected $factory;
+
+    /**
+     * The engine implementation.
+     *
+     * @var \Illuminate\Contracts\View\Engine
+     */
+    protected $engine;
+
+    /**
+     * The name of the view.
+     *
+     * @var string
+     */
+    protected $view;
+
+    /**
+     * The array of view data.
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * The path to the view file.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Create a new view instance.
+     *
+     * @param  \Illuminate\View\Factory  $factory
