f2880555c10315ec09b20326cf5d84e1f6bdd604
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:44 +0200

    Upload file via PHP cURL

---
+++ CS/NuGet/NuGet: PackageReaderBase #638 (5-45).cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Frameworks;
+using NuGet.Packaging.Core;
+using NuGet.Packaging.Signing;
+using NuGet.Shared;
+using NuGet.Versioning;
+
+namespace NuGet.Packaging
+{
+    /// <summary>
+    /// Abstract class that both the zip and folder package readers extend
+    /// This class contains the path conventions for both zip and folder readers
+    /// </summary>
+    public abstract class PackageReaderBase : IPackageCoreReader, IPackageContentReader, IAsyncPackageCoreReader, IAsyncPackageContentReader, ISignedPackageReader
+    {
+        private NuspecReader _nuspecReader;
+
+        protected IFrameworkNameProvider FrameworkProvider { get; set; }
+        protected IFrameworkCompatibilityProvider CompatibilityProvider { get; set; }
+
+        /// <summary>
+        /// Instantiates a new <see cref="PackageReaderBase" /> class.
+        /// </summary>
+        /// <param name="frameworkProvider">A framework mapping provider.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="frameworkProvider" /> is <see langword="null" />.</exception>
+        public PackageReaderBase(IFrameworkNameProvider frameworkProvider)
+            : this(frameworkProvider, new CompatibilityProvider(frameworkProvider))
+        {
+        }
+
+        /// <summary>
+        /// Instantiates a new <see cref="PackageReaderBase" /> class.
+        /// </summary>
