be6ca96f2f0bdad5c5e5804cb4f14583852f2298
pmtrilio <pm@trilio.bg>
2026-02-12 14:47:27 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Sessionmanager #462 (5-45).php
@@ -0,0 +1,41 @@
+use Laminas\EventManager\Event;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\Stdlib\ArrayUtils;
+use Traversable;
+
+use function array_key_exists;
+use function array_merge;
+use function assert;
+use function constant;
+use function defined;
+use function headers_sent;
+use function is_array;
+use function is_string;
+use function iterator_to_array;
+use function preg_match;
+use function register_shutdown_function;
+use function session_destroy;
+use function session_id;
+use function session_name;
+use function session_regenerate_id;
+use function session_set_save_handler;
+use function session_start;
+use function session_status;
+use function session_write_close;
+use function setcookie;
+
+use const PHP_SESSION_ACTIVE;
+
+/**
+ * Session ManagerInterface implementation utilizing ext/session
+ *
+ * @final
+ */
+class SessionManager extends AbstractManager
+{
+    /**
+     * Default options when a call to {@link destroy()} is made
+     * - send_expire_cookie: whether or not to send a cookie expiring the current session cookie
+     * - clear_storage: whether or not to empty the storage object of any stored values
+     *
+     * @deprecated This property will be removed in version 3.0
