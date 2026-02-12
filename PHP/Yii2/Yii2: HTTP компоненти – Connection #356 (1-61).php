e966e73f314063e03a33a06f70a75bc550141cf7
pmtrilio <pm@trilio.bg>
2026-02-12 19:02:55 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: HTTP компоненти – Connection #356 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\db;
+
+use PDO;
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\base\NotSupportedException;
+use yii\caching\CacheInterface;
+
+/**
+ * Connection represents a connection to a database via [PDO](https://www.php.net/manual/en/book.pdo.php).
+ *
+ * Connection works together with [[Command]], [[DataReader]] and [[Transaction]]
+ * to provide data access to various DBMS in a common set of APIs. They are a thin wrapper
+ * of the [PDO PHP extension](https://www.php.net/manual/en/book.pdo.php).
+ *
+ * Connection supports database replication and read-write splitting. In particular, a Connection component
+ * can be configured with multiple [[masters]] and [[slaves]]. It will do load balancing and failover by choosing
+ * appropriate servers. It will also automatically direct read operations to the slaves and write operations to
+ * the masters.
+ *
+ * To establish a DB connection, set [[dsn]], [[username]] and [[password]], and then
+ * call [[open()]] to connect to the database server. The current state of the connection can be checked using [[$isActive]].
+ *
+ * The following example shows how to create a Connection instance and establish
+ * the DB connection:
+ *
+ * ```
+ * $connection = new \yii\db\Connection([
+ *     'dsn' => $dsn,
+ *     'username' => $username,
+ *     'password' => $password,
+ * ]);
+ * $connection->open();
+ * ```
+ *
+ * After the DB connection is established, one can execute SQL statements like the following:
+ *
+ * ```
+ * $command = $connection->createCommand('SELECT * FROM post');
+ * $posts = $command->queryAll();
+ * $command = $connection->createCommand('UPDATE post SET status=1');
+ * $command->execute();
+ * ```
+ *
+ * One can also do prepared SQL execution and bind parameters to the prepared SQL.
+ * When the parameters are coming from user input, you should use this approach
+ * to prevent SQL injection attacks. The following is an example:
+ *
+ * ```
+ * $command = $connection->createCommand('SELECT * FROM post WHERE id=:id');
+ * $command->bindValue(':id', $_GET['id']);
+ * $post = $command->query();
