274ce008762d5115e8e12af6471f763b1de63dd1
pmtrilio <pm@trilio.bg>
2026-02-12 14:27:15 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Логика: Time (20-120).php
@@ -0,0 +1,101 @@
+use Closure;
+use IntlDateFormatter;
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+
+/**
+ * Extends time class provided by Chronos.
+ *
+ * Adds handy methods and locale-aware formatting helpers.
+ *
+ * @phpstan-immutable
+ */
+class Time extends ChronosTime implements JsonSerializable, Stringable
+{
+    use DateFormatTrait;
+
+    /**
+     * The format to use when formatting a time using `Cake\I18n\Time::i18nFormat()`
+     * and `__toString`.
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var string|int
+     * @see \Cake\I18n\Time::i18nFormat()
+     */
+    protected static string|int $_toStringFormat = IntlDateFormatter::SHORT;
+
+    /**
+     * The format to use when converting this object to JSON.
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var \Closure|string|int
+     * @see \Cake\I18n\Date::i18nFormat()
+     */
+    protected static Closure|string|int $_jsonEncodeFormat = "HH':'mm':'ss";
+
+    /**
+     * The format to use when formatting a time using `Cake\I18n\Time::nice()`
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var string|int
+     * @see \Cake\I18n\Time::nice()
+     */
+    public static string|int $niceFormat = IntlDateFormatter::MEDIUM;
+
+    /**
+     * Sets the default format used when type converting instances of this type to string
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @param string|int $format Format.
+     * @return void
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     */
+    public static function setToStringFormat($format): void
+    {
+        static::$_toStringFormat = $format;
+    }
+
+    /**
+     * Resets the format used to the default when converting an instance of this type to
+     * a string
+     *
+     * @return void
+     */
+    public static function resetToStringFormat(): void
+    {
+        static::setToStringFormat(IntlDateFormatter::SHORT);
+    }
+
+    /**
+     * Sets the default format used when converting this object to JSON
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (http://www.icu-project.org/apiref/icu4c/classSimpleDateFormat.html#details)
+     *
+     * Alternatively, the format can provide a callback. In this case, the callback
+     * can receive this object and return a formatted string.
+     *
+     * @see \Cake\I18n\Time::i18nFormat()
+     * @param \Closure|string|int $format Format.
+     * @return void
+     */
+    public static function setJsonEncodeFormat(Closure|string|int $format): void
+    {
+        static::$_jsonEncodeFormat = $format;
+    }
+
+    /**
