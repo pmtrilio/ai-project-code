1eb406fee71a2c15b89520c3e6d9d77c8902820f
pmtrilio <pm@trilio.bg>
2026-02-12 19:09:21 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: Маршрутизиране – Route #680 (30-70).php
@@ -0,0 +1,41 @@
+use function array_replace;
+use function array_reverse;
+use function class_implements;
+use function in_array;
+use function is_array;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ */
+class Route implements RouteInterface, RequestHandlerInterface
+{
+    /**
+     * HTTP methods supported by this route
+     *
+     * @var string[]
+     */
+    protected array $methods = [];
+
+    /**
+     * Route identifier
+     */
+    protected string $identifier;
+
+    /**
+     * Route name
+     */
+    protected ?string $name = null;
+
+    /**
+     * Parent route groups
+     *
+     * @var RouteGroupInterface[]
+     */
+    protected array $groups;
+
+    protected InvocationStrategyInterface $invocationStrategy;
+
+    /**
+     * Route parameters
+     *
