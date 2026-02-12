dc1b7790ff759b2a6810f79ecb0f8ec731be54ed
pmtrilio <pm@trilio.bg>
2026-02-12 19:19:26 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Response Compression: ResponseCompressionProvider #507 (1-101).cs
@@ -0,0 +1,101 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.AspNetCore.ResponseCompression;
+
+/// <inheritdoc />
+public class ResponseCompressionProvider : IResponseCompressionProvider
+{
+    private readonly ICompressionProvider[] _providers;
+    private readonly HashSet<string> _mimeTypes;
+    private readonly HashSet<string> _excludedMimeTypes;
+    private readonly bool _enableForHttps;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// If no compression providers are specified then GZip is used by default.
+    /// </summary>
+    /// <param name="services">Services to use when instantiating compression providers.</param>
+    /// <param name="options">The options for this instance.</param>
+    public ResponseCompressionProvider(IServiceProvider services, IOptions<ResponseCompressionOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(options);
+
+        var responseCompressionOptions = options.Value;
+
+        _providers = responseCompressionOptions.Providers.ToArray();
+        if (_providers.Length == 0)
+        {
+            // Use the factory so it can resolve IOptions<GzipCompressionProviderOptions> from DI.
+            _providers = new ICompressionProvider[]
+            {
+                    new CompressionProviderFactory(typeof(BrotliCompressionProvider)),
+                    new CompressionProviderFactory(typeof(GzipCompressionProvider)),
+            };
+        }
+        for (var i = 0; i < _providers.Length; i++)
+        {
+            var factory = _providers[i] as CompressionProviderFactory;
+            if (factory != null)
+            {
+                _providers[i] = factory.CreateInstance(services);
+            }
+        }
+
+        var mimeTypes = responseCompressionOptions.MimeTypes;
+        if (mimeTypes == null || !mimeTypes.Any())
+        {
+            mimeTypes = ResponseCompressionDefaults.MimeTypes;
+        }
+
+        _mimeTypes = new HashSet<string>(mimeTypes, StringComparer.OrdinalIgnoreCase);
+
+        _excludedMimeTypes = new HashSet<string>(
+            responseCompressionOptions.ExcludedMimeTypes ?? Enumerable.Empty<string>(),
+            StringComparer.OrdinalIgnoreCase
+        );
+
+        _enableForHttps = responseCompressionOptions.EnableForHttps;
+
+        _logger = services.GetRequiredService<ILogger<ResponseCompressionProvider>>();
+    }
+
+    /// <inheritdoc />
+    public virtual ICompressionProvider? GetCompressionProvider(HttpContext context)
+    {
+        // e.g. Accept-Encoding: gzip, deflate, sdch
+        var accept = context.Request.Headers.AcceptEncoding;
+
+        // Note this is already checked in CheckRequestAcceptsCompression which _should_ prevent any of these other methods from being called.
+        if (StringValues.IsNullOrEmpty(accept))
+        {
+            Debug.Assert(false, "Duplicate check failed.");
+            _logger.NoAcceptEncoding();
+            return null;
+        }
+
+        if (!StringWithQualityHeaderValue.TryParseList(accept, out var encodings) || encodings.Count == 0)
+        {
+            _logger.NoAcceptEncoding();
+            return null;
+        }
+
+        var candidates = new HashSet<ProviderCandidate>();
+
+        foreach (var encoding in encodings)
+        {
+            var encodingName = encoding.Value;
+            var quality = encoding.Quality.GetValueOrDefault(1);
+
+            if (quality < double.Epsilon)
