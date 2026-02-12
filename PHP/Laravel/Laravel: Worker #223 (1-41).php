08bcdbede3c28a3e32b91ed08f94e3e7cfaf9f7a
pmtrilio <pm@trilio.bg>
2026-02-12 14:43:08 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Worker #223 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Illuminate\Queue;
+
+use Illuminate\Contracts\Cache\Repository as CacheContract;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Queue\Factory as QueueManager;
+use Illuminate\Database\DetectsLostConnections;
+use Illuminate\Queue\Events\JobAttempted;
+use Illuminate\Queue\Events\JobExceptionOccurred;
+use Illuminate\Queue\Events\JobPopped;
+use Illuminate\Queue\Events\JobPopping;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Queue\Events\JobReleasedAfterException;
+use Illuminate\Queue\Events\JobTimedOut;
+use Illuminate\Queue\Events\Looping;
+use Illuminate\Queue\Events\WorkerStarting;
+use Illuminate\Queue\Events\WorkerStopping;
+use Illuminate\Support\Carbon;
+use Throwable;
+
+class Worker
+{
+    use DetectsLostConnections;
+
+    const EXIT_SUCCESS = 0;
+    const EXIT_ERROR = 1;
+    const EXIT_MEMORY_LIMIT = 12;
+
+    /**
+     * The name of the worker.
+     *
+     * @var string|null
+     */
+    protected $name;
+
+    /**
+     * The queue manager instance.
+     *
