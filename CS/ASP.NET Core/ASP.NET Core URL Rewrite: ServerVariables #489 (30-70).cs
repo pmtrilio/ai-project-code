d957ce9f19174c2a99367d1b692175b1c3e99276
pmtrilio <pm@trilio.bg>
2026-02-12 19:19:10 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: ServerVariables #489 (30-70).cs
@@ -0,0 +1,41 @@
+            case "APP_POOL_ID":
+                managedVariableThunk = () => throw new NotSupportedException(Resources.FormatError_UnsupportedServerVariable(serverVariable));
+                break;
+            case "CONTENT_LENGTH":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.ContentLength);
+                break;
+            case "CONTENT_TYPE":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.ContentType);
+                break;
+            case "HTTP_ACCEPT":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.Accept);
+                break;
+            case "HTTP_COOKIE":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.Cookie);
+                break;
+            case "HTTP_HOST":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.Host);
+                break;
+            case "HTTP_REFERER":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.Referer);
+                break;
+            case "HTTP_USER_AGENT":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.UserAgent);
+                break;
+            case "HTTP_CONNECTION":
+                managedVariableThunk = () => new HeaderSegment(HeaderNames.Connection);
+                break;
+            case "HTTP_URL":
+                managedVariableThunk = () => new UrlSegment(uriMatchPart);
+                break;
+            case "HTTPS":
+                managedVariableThunk = () => new IsHttpsUrlSegment();
+                break;
+            case "LOCAL_ADDR":
+                managedVariableThunk = () => new LocalAddressSegment();
+                break;
+            case "HTTP_PROXY_CONNECTION":
+                managedVariableThunk = () => throw new NotSupportedException(Resources.FormatError_UnsupportedServerVariable(serverVariable));
+                break;
+            case "QUERY_STRING":
+                managedVariableThunk = () => new QueryStringSegment();
