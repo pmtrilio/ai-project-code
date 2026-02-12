62646c38047636049b8daba648332a1153c0f65c
pmtrilio <pm@trilio.bg>
2026-02-12 14:45:40 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Env #365 (20-100).php
@@ -0,0 +1,81 @@
+    /**
+     * The environment repository instance.
+     *
+     * @var \Dotenv\Repository\RepositoryInterface|null
+     */
+    protected static $repository;
+
+    /**
+     * The list of custom adapters for loading environment variables.
+     *
+     * @var array<Closure>
+     */
+    protected static $customAdapters = [];
+
+    /**
+     * Enable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function enablePutenv()
+    {
+        static::$putenv = true;
+        static::$repository = null;
+    }
+
+    /**
+     * Disable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function disablePutenv()
+    {
+        static::$putenv = false;
+        static::$repository = null;
+    }
+
+    /**
+     * Register a custom adapter creator Closure.
+     */
+    public static function extend(Closure $callback, ?string $name = null): void
+    {
+        if (! is_null($name)) {
+            static::$customAdapters[$name] = $callback;
+        } else {
+            static::$customAdapters[] = $callback;
+        }
+    }
+
+    /**
+     * Get the environment repository instance.
+     *
+     * @return \Dotenv\Repository\RepositoryInterface
+     */
+    public static function getRepository()
+    {
+        if (static::$repository === null) {
+            $builder = RepositoryBuilder::createWithDefaultAdapters();
+
+            if (static::$putenv) {
+                $builder = $builder->addAdapter(PutenvAdapter::class);
+            }
+
+            foreach (static::$customAdapters as $adapter) {
+                $builder = $builder->addAdapter($adapter());
+            }
+
+            static::$repository = $builder->immutable()->make();
+        }
+
+        return static::$repository;
+    }
+
+    /**
+     * Get the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public static function get($key, $default = null)
+    {
