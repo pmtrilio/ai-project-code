f5ceaba06072c6e78bc4d23b9f75a0b8ea68311c
pmtrilio <pm@trilio.bg>
2026-02-12 19:09:04 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: HTTP компоненти – Eventdispatcher #666 (30-90).php
@@ -0,0 +1,60 @@
+ * @author Nicolas Grekas <p@tchwork.com>
+ */
+class EventDispatcher implements EventDispatcherInterface
+{
+    private array $listeners = [];
+    private array $sorted = [];
+    private array $optimized;
+
+    public function __construct()
+    {
+        if (__CLASS__ === static::class) {
+            $this->optimized = [];
+        }
+    }
+
+    public function dispatch(object $event, ?string $eventName = null): object
+    {
+        $eventName ??= $event::class;
+
+        if (isset($this->optimized)) {
+            $listeners = $this->optimized[$eventName] ?? (empty($this->listeners[$eventName]) ? [] : $this->optimizeListeners($eventName));
+        } else {
+            $listeners = $this->getListeners($eventName);
+        }
+
+        if ($listeners) {
+            $this->callListeners($listeners, $eventName, $event);
+        }
+
+        return $event;
+    }
+
+    public function getListeners(?string $eventName = null): array
+    {
+        if (null !== $eventName) {
+            if (empty($this->listeners[$eventName])) {
+                return [];
+            }
+
+            if (!isset($this->sorted[$eventName])) {
+                $this->sortListeners($eventName);
+            }
+
+            return $this->sorted[$eventName];
+        }
+
+        foreach ($this->listeners as $eventName => $eventListeners) {
+            if (!isset($this->sorted[$eventName])) {
+                $this->sortListeners($eventName);
+            }
+        }
+
+        return array_filter($this->sorted);
+    }
+
+    public function getListenerPriority(string $eventName, callable|array $listener): ?int
+    {
+        if (empty($this->listeners[$eventName])) {
+            return null;
+        }
