c25d89c9131936ab6e4315140cc240dc9d038c7f
pmtrilio <pm@trilio.bg>
2026-02-12 21:25:47 +0200

    Upload file via PHP cURL

---
+++ PHP/General/The PHP League: AuthorizationServer #295 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @author      Alex Bilbie <hello@alexbilbie.com>
+ * @copyright   Copyright (c) Alex Bilbie
+ * @license     http://mit-license.org/
+ *
+ * @link        https://github.com/thephpleague/oauth2-server
+ */
+
+declare(strict_types=1);
+
+namespace League\OAuth2\Server;
+
+use DateInterval;
+use Defuse\Crypto\Key;
+use League\OAuth2\Server\EventEmitting\EmitterAwareInterface;
+use League\OAuth2\Server\EventEmitting\EmitterAwarePolyfill;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\Grant\GrantTypeInterface;
+use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+use League\OAuth2\Server\RequestTypes\AuthorizationRequestInterface;
+use League\OAuth2\Server\ResponseTypes\AbstractResponseType;
+use League\OAuth2\Server\ResponseTypes\BearerTokenResponse;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use SensitiveParameter;
+
+class AuthorizationServer implements EmitterAwareInterface
+{
+    use EmitterAwarePolyfill;
+
+    /**
+     * @var GrantTypeInterface[]
+     */
+    protected array $enabledGrantTypes = [];
+
+    /**
