4802d7a2b260ba6fc802dc8c36e4b22466f613fc
pmtrilio <pm@trilio.bg>
2026-02-12 14:43:13 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Factory #227 (30-90).php
@@ -0,0 +1,61 @@
+    protected $engines;
+
+    /**
+     * The view finder implementation.
+     *
+     * @var \Illuminate\View\ViewFinderInterface
+     */
+    protected $finder;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * Data that should be available to all templates.
+     *
+     * @var array
+     */
+    protected $shared = [];
+
+    /**
+     * The extension to engine bindings.
+     *
+     * @var array
+     */
+    protected $extensions = [
+        'blade.php' => 'blade',
+        'php' => 'php',
+        'css' => 'file',
+        'html' => 'file',
+    ];
+
+    /**
+     * The view composer events.
+     *
+     * @var array
+     */
+    protected $composers = [];
+
+    /**
+     * The number of active rendering operations.
+     *
+     * @var int
+     */
+    protected $renderCount = 0;
+
+    /**
+     * The "once" block IDs that have been rendered.
+     *
+     * @var array
+     */
