bcf97d96356a3b094e71702c67ad7bac9a78499c
pmtrilio <pm@trilio.bg>
2026-02-12 21:28:33 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: RateLimiter #447 (20-80).php
@@ -0,0 +1,61 @@
+     * @var \Illuminate\Contracts\Cache\Repository
+     */
+    protected $cache;
+
+    /**
+     * The configured limit object resolvers.
+     *
+     * @var array
+     */
+    protected $limiters = [];
+
+    /**
+     * Create a new rate limiter instance.
+     *
+     * @param  \Illuminate\Contracts\Cache\Repository  $cache
+     */
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Register a named limiter configuration.
+     *
+     * @param  \BackedEnum|\UnitEnum|string  $name
+     * @param  \Closure  $callback
+     * @return $this
+     */
+    public function for($name, Closure $callback)
+    {
+        $resolvedName = $this->resolveLimiterName($name);
+
+        $this->limiters[$resolvedName] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Get the given named rate limiter.
+     *
+     * @param  \BackedEnum|\UnitEnum|string  $name
+     * @return \Closure|null
+     */
+    public function limiter($name)
+    {
+        $resolvedName = $this->resolveLimiterName($name);
+
+        $limiter = $this->limiters[$resolvedName] ?? null;
+
+        if (! is_callable($limiter)) {
+            return;
+        }
+
+        return function (...$args) use ($limiter) {
+            $result = $limiter(...$args);
+
+            if (! is_array($result)) {
+                return $result;
+            }
+
+            $duplicates = (new Collection($result))->duplicates('key');
