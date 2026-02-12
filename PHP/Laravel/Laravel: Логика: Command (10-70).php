7e65e6a8e619a162d9c0702669f4f64fa7f5a983
pmtrilio <pm@trilio.bg>
2026-02-12 14:26:09 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Логика: Command (10-70).php
@@ -0,0 +1,61 @@
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class Command extends SymfonyCommand
+{
+    use Concerns\CallsCommands,
+        Concerns\ConfiguresPrompts,
+        Concerns\HasParameters,
+        Concerns\InteractsWithIO,
+        Concerns\InteractsWithSignals,
+        Concerns\PromptsForMissingInput,
+        Macroable;
+
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $laravel;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * The console command help text.
+     *
+     * @var string
+     */
+    protected $help = '';
+
+    /**
+     * Indicates whether the command should be shown in the Artisan command list.
+     *
+     * @var bool
+     */
+    protected $hidden = false;
+
+    /**
+     * Indicates whether only one instance of the command can run at any given time.
+     *
+     * @var bool
+     */
