e7656193192cd443107f06c8ea06da26637808f3
pmtrilio <pm@trilio.bg>
2026-02-12 14:59:02 +0200

    Upload file via PHP cURL

---
+++ CS/.NET SDK/.NET SDK | MSBuild: GenerateRuntimeConfigurationFiles #508 (30-70).cs
@@ -0,0 +1,40 @@
+        [Required]
+        public string RuntimeConfigPath { get; set; }
+
+        public string RuntimeConfigDevPath { get; set; }
+
+        public string RuntimeIdentifier { get; set; }
+
+        public string PlatformLibraryName { get; set; }
+
+        public ITaskItem[] RuntimeFrameworks { get; set; }
+
+        public string RollForward { get; set; }
+
+        public string UserRuntimeConfig { get; set; }
+
+        public ITaskItem[] HostConfigurationOptions { get; set; }
+
+        public ITaskItem[] AdditionalProbingPaths { get; set; }
+
+        public bool IsSelfContained { get; set; }
+
+        public bool WriteAdditionalProbingPathsToMainConfig { get; set; }
+
+        public bool WriteIncludedFrameworks { get; set; }
+
+        public bool GenerateRuntimeConfigDevFile { get; set; }
+
+        public bool AlwaysIncludeCoreFramework { get; set; }
+
+        List<ITaskItem> _filesWritten = new();
+
+        private static readonly string[] RollForwardValues = new string[]
+        {
+            "Disable",
+            "LatestPatch",
+            "Minor",
+            "LatestMinor",
+            "Major",
+            "LatestMajor"
+        };
