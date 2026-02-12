083276c85f835398d781dd83a25543eb6fddab17
pmtrilio <pm@trilio.bg>
2026-02-12 14:50:21 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Env #625 (20-60).php
@@ -0,0 +1,41 @@
+    /**
+     * The environment repository instance.
+     *
+     * @var \Dotenv\Repository\RepositoryInterface|null
+     */
+    protected static $repository;
+
+    /**
+     * The list of custom adapters for loading environment variables.
+     *
+     * @var array<Closure>
+     */
+    protected static $customAdapters = [];
+
+    /**
+     * Enable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function enablePutenv()
+    {
+        static::$putenv = true;
+        static::$repository = null;
+    }
+
+    /**
+     * Disable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function disablePutenv()
+    {
+        static::$putenv = false;
+        static::$repository = null;
+    }
+
+    /**
+     * Register a custom adapter creator Closure.
+     */
+    public static function extend(Closure $callback, ?string $name = null): void
+    {
