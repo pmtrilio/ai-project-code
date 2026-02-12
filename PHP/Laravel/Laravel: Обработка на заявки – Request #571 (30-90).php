1fa2c455a71ad0c43029c44afe558b398d3c6583
pmtrilio <pm@trilio.bg>
2026-02-12 19:07:09 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Обработка на заявки – Request #571 (30-90).php
@@ -0,0 +1,61 @@
+{
+    use Concerns\CanBePrecognitive,
+        Concerns\InteractsWithContentTypes,
+        Concerns\InteractsWithFlashData,
+        Concerns\InteractsWithInput,
+        Conditionable,
+        Macroable;
+
+    /**
+     * The decoded JSON content for the request.
+     *
+     * @var \Symfony\Component\HttpFoundation\InputBag|null
+     */
+    protected $json;
+
+    /**
+     * All of the converted files for the request.
+     *
+     * @var array<int, \Illuminate\Http\UploadedFile|\Illuminate\Http\UploadedFile[]>
+     */
+    protected $convertedFiles;
+
+    /**
+     * The user resolver callback.
+     *
+     * @var \Closure
+     */
+    protected $userResolver;
+
+    /**
+     * The route resolver callback.
+     *
+     * @var \Closure
+     */
+    protected $routeResolver;
+
+    /**
+     * The cached "Accept" header value.
+     *
+     * @var string|null
+     */
+    protected $cachedAcceptHeader;
+
+    /**
+     * Create a new Illuminate HTTP request from server variables.
+     *
+     * @return static
+     */
+    public static function capture()
+    {
+        static::enableHttpMethodParameterOverride();
+
+        return static::createFromBase(SymfonyRequest::createFromGlobals());
+    }
+
+    /**
+     * Return the Request instance.
+     *
+     * @return $this
+     */
+    public function instance()
