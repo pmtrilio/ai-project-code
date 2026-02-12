a28a9cd355106b937160abc70a333d771416fe30
pmtrilio <pm@trilio.bg>
2026-02-12 19:17:27 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteRule #388 (1-61).cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Rewrite.Logging;
+
+namespace Microsoft.AspNetCore.Rewrite;
+
+internal sealed class RewriteRule : IRule
+{
+    private readonly TimeSpan _regexTimeout = TimeSpan.FromSeconds(1);
+    public Regex InitialMatch { get; }
+    public string Replacement { get; }
+    public bool StopProcessing { get; }
+    public RewriteRule(string regex, string replacement, bool stopProcessing)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(regex);
+        ArgumentException.ThrowIfNullOrEmpty(replacement);
+
+        InitialMatch = new Regex(regex, RegexOptions.Compiled | RegexOptions.CultureInvariant, _regexTimeout);
+        Replacement = replacement;
+        StopProcessing = stopProcessing;
+    }
+
+    public void ApplyRule(RewriteContext context)
+    {
+        var path = context.HttpContext.Request.Path;
+        Match initMatchResults;
+        if (path == PathString.Empty)
+        {
+            initMatchResults = InitialMatch.Match(path.ToString());
+        }
+        else
+        {
+            initMatchResults = InitialMatch.Match(path.ToString().Substring(1));
+        }
+
+        if (initMatchResults.Success)
+        {
+            var result = initMatchResults.Result(Replacement);
+            var request = context.HttpContext.Request;
+
+            if (StopProcessing)
+            {
+                context.Result = RuleResult.SkipRemainingRules;
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "/";
+            }
+
+            if (result.Contains(Uri.SchemeDelimiter, StringComparison.Ordinal))
+            {
+                string scheme;
+                HostString host;
+                PathString pathString;
+                QueryString query;
+                UriHelper.FromAbsolute(result, out scheme, out host, out pathString, out query, out _);
