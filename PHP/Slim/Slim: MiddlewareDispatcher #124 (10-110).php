2832509d24b9a7b048f6d1b7bffdfced8058151e
pmtrilio <pm@trilio.bg>
2026-02-12 20:27:46 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: MiddlewareDispatcher #124 (10-110).php
@@ -0,0 +1,101 @@
+
+namespace Slim;
+
+use Closure;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
+use Slim\Interfaces\AdvancedCallableResolverInterface;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\MiddlewareDispatcherInterface;
+
+use function class_exists;
+use function function_exists;
+use function is_callable;
+use function is_string;
+use function preg_match;
+use function sprintf;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ */
+class MiddlewareDispatcher implements MiddlewareDispatcherInterface
+{
+    /**
+     * Tip of the middleware call stack
+     */
+    protected RequestHandlerInterface $tip;
+
+    protected ?CallableResolverInterface $callableResolver;
+
+    /** @var TContainerInterface $container */
+    protected ?ContainerInterface $container;
+
+    /**
+     * @param TContainerInterface $container
+     */
+    public function __construct(
+        RequestHandlerInterface $kernel,
+        ?CallableResolverInterface $callableResolver = null,
+        ?ContainerInterface $container = null
+    ) {
+        $this->seedMiddlewareStack($kernel);
+        $this->callableResolver = $callableResolver;
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function seedMiddlewareStack(RequestHandlerInterface $kernel): void
+    {
+        $this->tip = $kernel;
+    }
+
+    /**
+     * Invoke the middleware stack
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->tip->handle($request);
+    }
+
+    /**
+     * Add a new middleware to the stack
+     *
+     * Middleware are organized as a stack. That means middleware
+     * that have been added before will be executed after the newly
+     * added one (last in, first out).
+     *
+     * @param MiddlewareInterface|string|callable $middleware
+     */
+    public function add($middleware): MiddlewareDispatcherInterface
+    {
+        if ($middleware instanceof MiddlewareInterface) {
+            return $this->addMiddleware($middleware);
+        }
+
+        if (is_string($middleware)) {
+            return $this->addDeferred($middleware);
+        }
+
+        if (is_callable($middleware)) {
+            return $this->addCallable($middleware);
+        }
+
+        /** @phpstan-ignore-next-line */
+        throw new RuntimeException(
+            'A middleware must be an object/class name referencing an implementation of ' .
+            'MiddlewareInterface or a callable with a matching signature.'
+        );
+    }
+
+    /**
+     * Add a new middleware to the stack
+     *
+     * Middleware are organized as a stack. That means middleware
+     * that have been added before will be executed after the newly
