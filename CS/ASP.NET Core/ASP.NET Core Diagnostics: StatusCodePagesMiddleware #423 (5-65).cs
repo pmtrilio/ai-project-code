a9fd4f6fe891f5bdd47cee3cf9526e8b5a1e7064
pmtrilio <pm@trilio.bg>
2026-02-12 19:17:59 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Diagnostics: StatusCodePagesMiddleware #423 (5-65).cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Diagnostics;
+
+/// <summary>
+/// A middleware for generating the response body of error status codes with no body.
+/// </summary>
+public class StatusCodePagesMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly StatusCodePagesOptions _options;
+
+    /// <summary>
+    /// Creates a new <see cref="StatusCodePagesMiddleware"/>
+    /// </summary>
+    /// <param name="next">The <see cref="RequestDelegate"/> representing the next middleware in the pipeline.</param>
+    /// <param name="options">The options for configuring the middleware.</param>
+    public StatusCodePagesMiddleware(RequestDelegate next, IOptions<StatusCodePagesOptions> options)
+    {
+        _next = next;
+        _options = options.Value;
+        if (_options.HandleAsync == null)
+        {
+            throw new ArgumentException("Missing options.HandleAsync implementation.");
+        }
+    }
+
+    /// <summary>
+    /// Executes the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+    /// <returns>A task that represents the execution of this middleware.</returns>
+    public async Task Invoke(HttpContext context)
+    {
+        var statusCodeFeature = new StatusCodePagesFeature();
+        context.Features.Set<IStatusCodePagesFeature>(statusCodeFeature);
+        var endpoint = context.GetEndpoint();
+        var shouldCheckEndpointAgain = endpoint is null;
+
+        if (HasSkipStatusCodePagesMetadata(endpoint))
+        {
+            statusCodeFeature.Enabled = false;
+        }
+
+        // Attach pathFormat to HttpContext.Items early in the pipeline
+        context.Items[nameof(StatusCodePagesOptions)] = _options.PathFormat;
+
+        await _next(context);
+
+        // Remove pathFormat from HttpContext.Items after handler execution
+        context.Items.Remove(nameof(StatusCodePagesOptions));
+
+        if (!statusCodeFeature.Enabled)
+        {
+            // Check if the feature is still available because other middleware (such as a web API written in MVC) could
+            // have disabled the feature to prevent HTML status code responses from showing up to an API client.
+            return;
+        }
