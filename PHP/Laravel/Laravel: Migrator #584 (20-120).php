5d74f065f1775f292755b4eeef9fede093bf59e4
pmtrilio <pm@trilio.bg>
2026-02-12 14:49:38 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Migrator #584 (20-120).php
@@ -0,0 +1,101 @@
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Migrator
+{
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The migration repository implementation.
+     *
+     * @var \Illuminate\Database\Migrations\MigrationRepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * The connection resolver instance.
+     *
+     * @var \Illuminate\Database\ConnectionResolverInterface
+     */
+    protected $resolver;
+
+    /**
+     * The custom connection resolver callback.
+     *
+     * @var \Closure|null
+     */
+    protected static $connectionResolverCallback;
+
+    /**
+     * The name of the default connection.
+     *
+     * @var string
+     */
+    protected $connection;
+
+    /**
+     * The paths to all of the migration files.
+     *
+     * @var string[]
+     */
+    protected $paths = [];
+
+    /**
+     * The paths that have already been required.
+     *
+     * @var array<string, \Illuminate\Database\Migrations\Migration|null>
+     */
+    protected static $requiredPathCache = [];
+
+    /**
+     * The output interface implementation.
+     *
+     * @var \Symfony\Component\Console\Output\OutputInterface
+     */
+    protected $output;
+
+    /**
+     * The pending migrations to skip.
+     *
+     * @var list<string>
+     */
+    protected static $withoutMigrations = [];
+
+    /**
+     * Create a new migrator instance.
+     *
+     * @param  \Illuminate\Database\Migrations\MigrationRepositoryInterface  $repository
+     * @param  \Illuminate\Database\ConnectionResolverInterface  $resolver
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @param  \Illuminate\Contracts\Events\Dispatcher|null  $dispatcher
+     */
+    public function __construct(
+        MigrationRepositoryInterface $repository,
+        Resolver $resolver,
+        Filesystem $files,
+        ?Dispatcher $dispatcher = null,
+    ) {
+        $this->files = $files;
+        $this->events = $dispatcher;
+        $this->resolver = $resolver;
+        $this->repository = $repository;
+    }
+
+    /**
+     * Run the pending migrations at a given path.
+     *
+     * @param  string[]|string  $paths
