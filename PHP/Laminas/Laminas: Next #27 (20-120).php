e2196297d7fcf00048e26f2be76632c2b16d727c
pmtrilio <pm@trilio.bg>
2026-02-12 15:40:27 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Next #27 (20-120).php
@@ -0,0 +1,32 @@
+    private ?SplQueue $queue;
+
+    /**
+     * Clones the queue provided to allow re-use.
+     *
+     * @param SplQueue<MiddlewareInterface> $queue
+     * @param RequestHandlerInterface $fallbackHandler Fallback handler to
+     *     invoke when the queue is exhausted.
+     */
+    public function __construct(SplQueue $queue, private RequestHandlerInterface $fallbackHandler)
+    {
+        $this->queue = clone $queue;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if ($this->queue === null) {
+            throw MiddlewarePipeNextHandlerAlreadyCalledException::create();
+        }
+
+        if ($this->queue->isEmpty()) {
+            $this->queue = null;
+            return $this->fallbackHandler->handle($request);
+        }
+
+        $middleware  = $this->queue->dequeue();
+        $next        = clone $this; // deep clone is not used intentionally
+        $this->queue = null; // mark queue as processed at this nesting level
+
+        return $middleware->process($request, $next);
+    }
+}
