8702fce8eb041d81faca917ab5732e08b109820d
pmtrilio <pm@trilio.bg>
2026-02-12 14:42:15 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Store #173 (5-45).php
@@ -0,0 +1,41 @@
+use Closure;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Uri;
+use Illuminate\Support\ViewErrorBag;
+use RuntimeException;
+use SessionHandlerInterface;
+use stdClass;
+
+class Store implements Session
+{
+    use Macroable;
+
+    /**
+     * The session ID.
+     *
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * The session name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The session attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * The session handler implementation.
