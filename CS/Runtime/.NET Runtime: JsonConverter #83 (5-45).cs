4401c03693f3a3523f4f435f9f9a4d67e567ee1c
pmtrilio <pm@trilio.bg>
2026-02-12 15:04:19 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: JsonConverter #83 (5-45).cs
@@ -0,0 +1,41 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Json.Schema;
+using System.Text.Json.Serialization.Converters;
+using System.Text.Json.Serialization.Metadata;
+
+namespace System.Text.Json.Serialization
+{
+    /// <summary>
+    /// Converts an object or value to or from JSON.
+    /// </summary>
+    public abstract partial class JsonConverter
+    {
+        internal JsonConverter()
+        {
+            IsInternalConverter = GetType().Assembly == typeof(JsonConverter).Assembly;
+            ConverterStrategy = GetDefaultConverterStrategy();
+        }
+
+        /// <summary>
+        /// Gets the type being converted by the current converter instance.
+        /// </summary>
+        /// <remarks>
+        /// For instances of type <see cref="JsonConverter{T}"/> returns typeof(T),
+        /// and for instances of type <see cref="JsonConverterFactory"/> returns <see langword="null" />.
+        /// </remarks>
+        public abstract Type? Type { get; }
+
+        /// <summary>
+        /// Determines whether the type can be converted.
+        /// </summary>
+        /// <param name="typeToConvert">The type is checked as to whether it can be converted.</param>
+        /// <returns>True if the type can be converted, false otherwise.</returns>
+        public abstract bool CanConvert(Type typeToConvert);
+
+        internal ConverterStrategy ConverterStrategy
+        {
+            get => _converterStrategy;
+            init
+            {
+                CanUseDirectReadOrWrite = value == ConverterStrategy.Value && IsInternalConverter;
