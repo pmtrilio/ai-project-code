f40686127bfe89a55e9339b5eb83b52fac2823c7
pmtrilio <pm@trilio.bg>
2026-02-12 20:29:52 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: CacheEngine #226 (20-80).php
@@ -0,0 +1,61 @@
+use Cake\Cache\Event\CacheBeforeAddEvent;
+use Cake\Cache\Exception\InvalidArgumentException;
+use Cake\Core\InstanceConfigTrait;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
+use DateInterval;
+use DateTime;
+use Psr\SimpleCache\CacheInterface;
+use function Cake\Core\triggerWarning;
+
+/**
+ * Storage engine for CakePHP caching
+ *
+ * @template TSubject of \Cake\Cache\CacheEngine
+ * @implements \Cake\Event\EventDispatcherInterface<TSubject>
+ */
+abstract class CacheEngine implements CacheInterface, CacheEngineInterface, EventDispatcherInterface
+{
+    /**
+     * @use \Cake\Event\EventDispatcherTrait<TSubject>
+     */
+    use EventDispatcherTrait;
+    use InstanceConfigTrait;
+
+    /**
+     * @var string
+     */
+    protected const CHECK_KEY = 'key';
+
+    /**
+     * @var string
+     */
+    protected const CHECK_VALUE = 'value';
+
+    /**
+     * The default cache configuration is overridden in most cache adapters. These are
+     * the keys that are common to all adapters. If overridden, this property is not used.
+     *
+     * - `duration` Specify how long items in this cache configuration last.
+     * - `groups` List of groups or 'tags' associated to every key stored in this config.
+     *    handy for deleting a complete group from cache.
+     * - `prefix` Prefix appended to all entries. Good for when you need to share a keyspace
+     *    with either another cache config or another application.
+     * - `warnOnWriteFailures` Some engines, such as ApcuEngine, may raise warnings on
+     *    write failures.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [
+        'duration' => 3600,
+        'groups' => [],
+        'prefix' => 'cake_',
+        'warnOnWriteFailures' => true,
+    ];
+
+    /**
+     * Contains the compiled string with all group
+     * prefixes to be prepended to every key in this cache engine
+     *
+     * @var string
+     */
