dbc155a645b8b3137a5b923efe603d96b7812ce1
pmtrilio <pm@trilio.bg>
2026-02-12 19:16:21 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Diagnostics: ExceptionHandlerMiddleware #328 (20-120).cs
@@ -0,0 +1,30 @@
+    /// <summary>
+    /// Creates a new <see cref="ExceptionHandlerMiddleware"/>
+    /// </summary>
+    /// <param name="next">The <see cref="RequestDelegate"/> representing the next middleware in the pipeline.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> used for logging.</param>
+    /// <param name="options">The options for configuring the middleware.</param>
+    /// <param name="diagnosticListener">The <see cref="DiagnosticListener"/> used for writing diagnostic messages.</param>
+    public ExceptionHandlerMiddleware(
+        RequestDelegate next,
+        ILoggerFactory loggerFactory,
+        IOptions<ExceptionHandlerOptions> options,
+        DiagnosticListener diagnosticListener)
+    {
+        _innerMiddlewareImpl = new(
+            next,
+            loggerFactory,
+            options,
+            diagnosticListener,
+            Enumerable.Empty<IExceptionHandler>(),
+            new DummyMeterFactory(),
+            problemDetailsService: null);
+    }
+
+    /// <summary>
+    /// Executes the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+    public Task Invoke(HttpContext context)
+        => _innerMiddlewareImpl.Invoke(context);
+}
