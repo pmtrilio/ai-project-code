c8b894741b263e91c200982e8ea4cb9dee13da8c
pmtrilio <pm@trilio.bg>
2026-02-12 14:08:53 +0200

    Upload file via PHP cURL

---
+++ CS/Roslyn/Roslyn (C# compiler): Вход|изход (I|O) – Compilation (20-60).cs
@@ -0,0 +1,41 @@
+using System.Threading;
+using Microsoft.CodeAnalysis.CodeGen;
+using Microsoft.CodeAnalysis.Collections;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Symbols;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.DiaSymReader;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis
+{
+    /// <summary>
+    /// The compilation object is an immutable representation of a single invocation of the
+    /// compiler. Although immutable, a compilation is also on-demand, and will realize and cache
+    /// data as necessary. A compilation can produce a new compilation from existing compilation
+    /// with the application of small deltas. In many cases, it is more efficient than creating a
+    /// new compilation from scratch, as the new compilation can reuse information from the old
+    /// compilation.
+    /// </summary>
+    public abstract partial class Compilation
+    {
+        /// <summary>
+        /// Optional data collected during testing only.
+        /// Used for instance for nullable analysis (NullableWalker.NullableAnalysisData)
+        /// and inferred delegate types (InferredDelegateTypeData).
+        /// </summary>
+        internal object? TestOnlyCompilationData;
+
+        /// <summary>
+        /// Returns true if this is a case sensitive compilation, false otherwise.  Case sensitivity
+        /// affects compilation features such as name lookup as well as choosing what names to emit
+        /// when there are multiple different choices (for example between a virtual method and an
+        /// override).
+        /// </summary>
+        public abstract bool IsCaseSensitive { get; }
+
+        /// <summary>
+        /// Used for test purposes only to emulate missing members.
