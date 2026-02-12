775ec9cd37021fb889cfebeb7552c67be7c7f0fc
pmtrilio <pm@trilio.bg>
2026-02-12 15:04:06 +0200

    Upload file via PHP cURL

---
+++ CS/General/Newtonsoft.Json: JsonSerializer #69 (10-70).cs
@@ -0,0 +1,61 @@
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Utilities;
+using System.Runtime.Serialization;
+using ErrorEventArgs = Newtonsoft.Json.Serialization.ErrorEventArgs;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Newtonsoft.Json
+{
+    /// <summary>
+    /// Serializes and deserializes objects into and from the JSON format.
+    /// The <see cref="JsonSerializer"/> enables you to control how objects are encoded into JSON.
+    /// </summary>
+    [RequiresUnreferencedCode(MiscellaneousUtils.TrimWarning)]
+    [RequiresDynamicCode(MiscellaneousUtils.AotWarning)]
+    public class JsonSerializer
+    {
+        internal TypeNameHandling _typeNameHandling;
+        internal TypeNameAssemblyFormatHandling _typeNameAssemblyFormatHandling;
+        internal PreserveReferencesHandling _preserveReferencesHandling;
+        internal ReferenceLoopHandling _referenceLoopHandling;
+        internal MissingMemberHandling _missingMemberHandling;
+        internal ObjectCreationHandling _objectCreationHandling;
+        internal NullValueHandling _nullValueHandling;
+        internal DefaultValueHandling _defaultValueHandling;
+        internal ConstructorHandling _constructorHandling;
+        internal MetadataPropertyHandling _metadataPropertyHandling;
+        internal JsonConverterCollection? _converters;
+        internal IContractResolver _contractResolver;
+        internal ITraceWriter? _traceWriter;
+        internal IEqualityComparer? _equalityComparer;
+        internal ISerializationBinder _serializationBinder;
+        internal StreamingContext _context;
+        private IReferenceResolver? _referenceResolver;
+
+        private Formatting? _formatting;
+        private DateFormatHandling? _dateFormatHandling;
