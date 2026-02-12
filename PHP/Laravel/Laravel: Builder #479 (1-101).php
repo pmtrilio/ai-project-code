cd60b65f170b6796dc521307c7c9da9abf2f45b5
pmtrilio <pm@trilio.bg>
2026-02-12 14:47:45 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Builder #479 (1-101).php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Illuminate\Database\Schema;
+
+use Closure;
+use Illuminate\Container\Container;
+use Illuminate\Database\Connection;
+use Illuminate\Database\PostgresConnection;
+use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
+use LogicException;
+use RuntimeException;
+
+class Builder
+{
+    use Macroable;
+
+    /**
+     * The database connection instance.
+     *
+     * @var \Illuminate\Database\Connection
+     */
+    protected $connection;
+
+    /**
+     * The schema grammar instance.
+     *
+     * @var \Illuminate\Database\Schema\Grammars\Grammar
+     */
+    protected $grammar;
+
+    /**
+     * The Blueprint resolver callback.
+     *
+     * @var \Closure(\Illuminate\Database\Connection, string, \Closure|null): \Illuminate\Database\Schema\Blueprint
+     */
+    protected $resolver;
+
+    /**
+     * The default string length for migrations.
+     *
+     * @var int|null
+     */
+    public static $defaultStringLength = 255;
+
+    /**
+     * The default time precision for migrations.
+     */
+    public static ?int $defaultTimePrecision = 0;
+
+    /**
+     * The default relationship morph key type.
+     *
+     * @var string
+     */
+    public static $defaultMorphKeyType = 'int';
+
+    /**
+     * Create a new database Schema manager.
+     *
+     * @param  \Illuminate\Database\Connection  $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+        $this->grammar = $connection->getSchemaGrammar();
+    }
+
+    /**
+     * Set the default string length for migrations.
+     *
+     * @param  int  $length
+     * @return void
+     */
+    public static function defaultStringLength($length)
+    {
+        static::$defaultStringLength = $length;
+    }
+
+    /**
+     * Set the default time precision for migrations.
+     */
+    public static function defaultTimePrecision(?int $precision): void
+    {
+        static::$defaultTimePrecision = $precision;
+    }
+
+    /**
+     * Set the default morph key type for migrations.
+     *
+     * @param  string  $type
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function defaultMorphKeyType(string $type)
+    {
+        if (! in_array($type, ['int', 'uuid', 'ulid'])) {
+            throw new InvalidArgumentException("Morph key type must be 'int', 'uuid', or 'ulid'.");
+        }
