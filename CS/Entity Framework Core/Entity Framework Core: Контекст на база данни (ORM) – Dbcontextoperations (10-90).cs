0ffbb0068d62fcc34ca59a42e1d45ce0349e9a14
pmtrilio <pm@trilio.bg>
2026-02-12 14:07:53 +0200

    Upload file via PHP cURL

---
+++ CS/Entity Framework Core/Entity Framework Core: Контекст на база данни (ORM) – Dbcontextoperations (10-90).cs
@@ -0,0 +1,81 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+
+namespace Microsoft.EntityFrameworkCore.Design.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class DbContextOperations
+{
+    private readonly IOperationReporter _reporter;
+    private readonly Assembly _assembly;
+    private readonly Assembly _startupAssembly;
+    private readonly string _project;
+    private readonly string _projectDir;
+    private readonly string? _rootNamespace;
+    private readonly string? _language;
+    private readonly bool _nullable;
+    private readonly string[] _args;
+    private readonly AppServiceProviderFactory _appServicesFactory;
+    private readonly DesignTimeServicesBuilder _servicesBuilder;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public DbContextOperations(
+        IOperationReporter reporter,
+        Assembly assembly,
+        Assembly startupAssembly,
+        string project,
+        string projectDir,
+        string? rootNamespace,
+        string? language,
+        bool nullable,
+        string[]? args)
+        : this(
+            reporter,
+            assembly,
+            startupAssembly,
+            project,
+            projectDir,
+            rootNamespace,
+            language,
+            nullable,
+            args,
+            new AppServiceProviderFactory(startupAssembly, reporter))
+    {
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected DbContextOperations(
+        IOperationReporter reporter,
+        Assembly assembly,
+        Assembly startupAssembly,
+        string project,
+        string projectDir,
+        string? rootNamespace,
+        string? language,
+        bool nullable,
+        string[]? args,
+        AppServiceProviderFactory appServicesFactory)
+    {
+        _reporter = reporter;
+        _assembly = assembly;
+        _startupAssembly = startupAssembly;
+        _project = project;
+        _projectDir = projectDir;
+        _rootNamespace = rootNamespace;
+        _language = language;
