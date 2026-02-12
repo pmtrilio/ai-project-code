c9e9f2e8ded360b4515811f00361da637a5846ae
pmtrilio <pm@trilio.bg>
2026-02-12 19:11:30 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Twig Loader (267-334).php
@@ -0,0 +1,24 @@
+        foreach ($parts as $part) {
+            if ('..' === $part) {
+                --$level;
+            } elseif ('.' !== $part) {
+                ++$level;
+            }
+
+            if ($level < 0) {
+                throw new LoaderError(\sprintf('Looks like you try to load a template outside configured directories (%s).', $name));
+            }
+        }
+    }
+
+    private function isAbsolutePath(string $file): bool
+    {
+        return strspn($file, '/\\', 0, 1)
+            || (\strlen($file) > 3 && ctype_alpha($file[0])
+                && ':' === $file[1]
+                && strspn($file, '/\\', 2, 1)
+            )
+            || null !== parse_url($file, \PHP_URL_SCHEME)
+        ;
+    }
+}
