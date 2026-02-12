85294b3947201662162e5968beddba80d97ab20d
pmtrilio <pm@trilio.bg>
2026-02-12 15:42:27 +0200

    Upload file via PHP cURL

---
+++ PHP/General/PHP-FIG (PSR): ContainerInterface #142 (20-100).php
@@ -0,0 +1,17 @@
+     * @return mixed Entry.
+     */
+    public function get(string $id);
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
+     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
+     */
+    public function has(string $id): bool;
+}
