fd540eef94502f5411628b0753859f20513aa687
pmtrilio <pm@trilio.bg>
2026-02-12 15:05:51 +0200

    Upload file via PHP cURL

---
+++ CS/Entity Framework Core/Entity Framework Core: SqlServerQuerySqlGenerator #199 (10-90).cs
@@ -0,0 +1,81 @@
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+namespace Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class SqlServerQuerySqlGenerator : QuerySqlGenerator
+{
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+    private readonly ISqlGenerationHelper _sqlGenerationHelper;
+    private readonly ISqlServerSingletonOptions _sqlServerSingletonOptions;
+
+    private bool _withinTable;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqlServerQuerySqlGenerator(
+        QuerySqlGeneratorDependencies dependencies,
+        IRelationalTypeMappingSource typeMappingSource,
+        ISqlServerSingletonOptions sqlServerSingletonOptions)
+        : base(dependencies)
+    {
+        _typeMappingSource = typeMappingSource;
+        _sqlGenerationHelper = dependencies.SqlGenerationHelper;
+        _sqlServerSingletonOptions = sqlServerSingletonOptions;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected override bool TryGenerateWithoutWrappingSelect(SelectExpression selectExpression)
+        // SQL Server doesn't support VALUES as a top-level statement, so we need to wrap the VALUES in a SELECT:
+        // SELECT 1 AS x UNION VALUES (2), (3) -- simple
+        // SELECT 1 AS x UNION SELECT * FROM (VALUES (2), (3)) AS f(x) -- SQL Server
+        => selectExpression.Tables is not [ValuesExpression]
+            && base.TryGenerateWithoutWrappingSelect(selectExpression);
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected override Expression VisitCollate(CollateExpression collateExpression)
+    {
+        Visit(collateExpression.Operand);
+
+        // SQL Server collation docs: https://learn.microsoft.com/sql/relational-databases/collations/collation-and-unicode-support
+
+        // The default behavior in QuerySqlGenerator is to quote collation names, but SQL Server does not support that.
+        // Instead, make sure the collation name only contains a restricted set of characters.
+        foreach (var c in collateExpression.Collation)
+        {
+            if (!char.IsLetterOrDigit(c) && c != '_')
+            {
+                throw new InvalidOperationException(SqlServerStrings.InvalidCollationName(collateExpression.Collation));
+            }
+        }
+
+        Sql
+            .Append(" COLLATE ")
+            .Append(collateExpression.Collation);
+
+        return collateExpression;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
