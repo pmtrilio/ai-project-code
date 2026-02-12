326ca419490d1f6148e2afd0b86e32c125f71891
pmtrilio <pm@trilio.bg>
2026-02-12 20:32:18 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: OptionsResolver #346 (20-60).php
@@ -0,0 +1,41 @@
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
+
+/**
+ * Validates options and merges them with default values.
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ * @author Tobias Schultze <http://tobion.de>
+ */
+class OptionsResolver implements Options
+{
+    private const VALIDATION_FUNCTIONS = [
+        'bool' => 'is_bool',
+        'boolean' => 'is_bool',
+        'int' => 'is_int',
+        'integer' => 'is_int',
+        'long' => 'is_int',
+        'float' => 'is_float',
+        'double' => 'is_float',
+        'real' => 'is_float',
+        'numeric' => 'is_numeric',
+        'string' => 'is_string',
+        'scalar' => 'is_scalar',
+        'array' => 'is_array',
+        'iterable' => 'is_iterable',
+        'countable' => 'is_countable',
+        'callable' => 'is_callable',
+        'object' => 'is_object',
+        'resource' => 'is_resource',
+    ];
+
+    /**
+     * The names of all defined options.
+     */
+    private array $defined = [];
+
+    /**
+     * The default option values.
+     */
+    private array $defaults = [];
+
+    /**
