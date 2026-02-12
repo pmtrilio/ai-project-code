805b3ccaf3dea3ed9874d4f8e18c167c976aa83b
pmtrilio <pm@trilio.bg>
2026-02-12 15:38:48 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Pipeline #644 (30-70).php
@@ -0,0 +1,41 @@
+    protected $passable;
+
+    /**
+     * The array of class pipes.
+     *
+     * @var array
+     */
+    protected $pipes = [];
+
+    /**
+     * The method to call on each pipe.
+     *
+     * @var string
+     */
+    protected $method = 'handle';
+
+    /**
+     * The final callback to be executed after the pipeline ends regardless of the outcome.
+     *
+     * @var \Closure|null
+     */
+    protected $finally;
+
+    /**
+     * Indicates whether to wrap the pipeline in a database transaction.
+     *
+     * @var string|null|\UnitEnum|false
+     */
+    protected $withinTransaction = false;
+
+    /**
+     * Create a new class instance.
+     *
+     * @param  \Illuminate\Contracts\Container\Container|null  $container
+     */
+    public function __construct(?Container $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
