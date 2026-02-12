9b26e3686885f87916c8be45f49e1e22bb2d72ad
pmtrilio <pm@trilio.bg>
2026-02-12 14:42:47 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Authenticationservice #203 (30-70).php
@@ -0,0 +1,41 @@
+    public function __construct(?Storage\StorageInterface $storage = null, ?Adapter\AdapterInterface $adapter = null)
+    {
+        if (null !== $storage) {
+            $this->setStorage($storage);
+        }
+        if (null !== $adapter) {
+            $this->setAdapter($adapter);
+        }
+    }
+
+    /**
+     * Returns the authentication adapter
+     *
+     * The adapter does not have a default if the storage adapter has not been set.
+     *
+     * @return Adapter\AdapterInterface|null
+     */
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
+
+    /**
+     * Sets the authentication adapter
+     *
+     * @return self Provides a fluent interface
+     */
+    public function setAdapter(Adapter\AdapterInterface $adapter)
+    {
+        $this->adapter = $adapter;
+        return $this;
+    }
+
+    /**
+     * Returns the persistent storage handler
+     *
+     * Session storage is used by default unless a different storage adapter has been set.
+     *
+     * @return Storage\StorageInterface
+     */
+    public function getStorage()
