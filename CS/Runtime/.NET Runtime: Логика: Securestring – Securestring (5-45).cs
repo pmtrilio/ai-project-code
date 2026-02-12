ecada90faabb32bd194aa584532f5e4f1d87bb04
pmtrilio <pm@trilio.bg>
2026-02-12 14:09:03 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Логика: Securestring – Securestring (5-45).cs
@@ -0,0 +1,41 @@
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace System.Security
+{
+    public sealed partial class SecureString : IDisposable
+    {
+        private const int MaxLength = 65536;
+        private readonly object _methodLock = new object();
+        private UnmanagedBuffer? _buffer;
+        private int _decryptedLength;
+        private bool _encrypted;
+        private bool _readOnly;
+
+        public SecureString()
+        {
+            Initialize(ReadOnlySpan<char>.Empty);
+        }
+
+        [CLSCompliant(false)]
+        public unsafe SecureString(char* value, int length)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            ArgumentOutOfRangeException.ThrowIfNegative(length);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(length, MaxLength);
+
+            Initialize(new ReadOnlySpan<char>(value, length));
+        }
+
+        private void Initialize(ReadOnlySpan<char> value)
+        {
+            _buffer = UnmanagedBuffer.Allocate(GetAlignedByteSize(value.Length));
+            _decryptedLength = value.Length;
+
+            SafeBuffer? bufferToRelease = null;
+            try
+            {
+                Span<char> span = AcquireSpan(ref bufferToRelease);
+                value.CopyTo(span);
+            }
