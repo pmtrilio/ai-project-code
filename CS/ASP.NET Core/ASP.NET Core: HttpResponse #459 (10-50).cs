c1b11c0f84dab7dd67fff463d65fdf1553d7e413
pmtrilio <pm@trilio.bg>
2026-02-12 14:58:23 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: HttpResponse #459 (10-50).cs
@@ -0,0 +1,41 @@
+namespace Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// Represents the outgoing side of an individual HTTP request.
+/// </summary>
+[DebuggerDisplay("{DebuggerToString(),nq}")]
+[DebuggerTypeProxy(typeof(HttpResponseDebugView))]
+public abstract class HttpResponse
+{
+    private static readonly Func<object, Task> _callbackDelegate = callback => ((Func<Task>)callback)();
+    private static readonly Func<object, Task> _disposeDelegate = state =>
+    {
+        // Prefer async dispose over dispose
+        if (state is IAsyncDisposable asyncDisposable)
+        {
+            return asyncDisposable.DisposeAsync().AsTask();
+        }
+        else if (state is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+        return Task.CompletedTask;
+    };
+
+    /// <summary>
+    /// Gets the <see cref="HttpContext"/> for this response.
+    /// </summary>
+    public abstract HttpContext HttpContext { get; }
+
+    /// <summary>
+    /// Gets or sets the HTTP response code.
+    /// </summary>
+    public abstract int StatusCode { get; set; }
+
+    /// <summary>
+    /// Gets the response headers.
+    /// </summary>
+    public abstract IHeaderDictionary Headers { get; }
+
+    /// <summary>
+    /// Gets or sets the response body <see cref="Stream"/>.
