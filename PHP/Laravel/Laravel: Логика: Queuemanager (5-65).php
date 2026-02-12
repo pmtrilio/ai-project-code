a26971323dcfb1f831ea18cb3fa72b1a77c4268c
pmtrilio <pm@trilio.bg>
2026-02-12 14:25:59 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Логика: Queuemanager (5-65).php
@@ -0,0 +1,61 @@
+use Closure;
+use Illuminate\Contracts\Queue\Factory as FactoryContract;
+use Illuminate\Contracts\Queue\Monitor as MonitorContract;
+use Illuminate\Support\Queue\Concerns\ResolvesQueueRoutes;
+use InvalidArgumentException;
+
+/**
+ * @mixin \Illuminate\Contracts\Queue\Queue
+ */
+class QueueManager implements FactoryContract, MonitorContract
+{
+    use ResolvesQueueRoutes;
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The array of resolved queue connections.
+     *
+     * @var array
+     */
+    protected $connections = [];
+
+    /**
+     * The array of resolved queue connectors.
+     *
+     * @var array
+     */
+    protected $connectors = [];
+
+    /**
+     * Create a new queue manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register an event listener for the before job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function before($callback)
+    {
+        $this->app['events']->listen(Events\JobProcessing::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the after job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
