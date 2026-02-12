01a483934240533e7b8c03d6bd3c632104dd689a
pmtrilio <pm@trilio.bg>
2026-02-12 20:31:05 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: UnicodeString #287 (10-50).php
@@ -0,0 +1,41 @@
+ */
+
+namespace Symfony\Component\String;
+
+use Symfony\Component\String\Exception\ExceptionInterface;
+use Symfony\Component\String\Exception\InvalidArgumentException;
+
+/**
+ * Represents a string of Unicode grapheme clusters encoded as UTF-8.
+ *
+ * A letter followed by combining characters (accents typically) form what Unicode defines
+ * as a grapheme cluster: a character as humans mean it in written texts. This class knows
+ * about the concept and won't split a letter apart from its combining accents. It also
+ * ensures all string comparisons happen on their canonically-composed representation,
+ * ignoring e.g. the order in which accents are listed when a letter has many of them.
+ *
+ * @see https://unicode.org/reports/tr15/
+ *
+ * @author Nicolas Grekas <p@tchwork.com>
+ * @author Hugo Hamon <hugohamon@neuf.fr>
+ *
+ * @throws ExceptionInterface
+ */
+class UnicodeString extends AbstractUnicodeString
+{
+    public function __construct(string $string = '')
+    {
+        if ('' === $string || normalizer_is_normalized($this->string = $string)) {
+            return;
+        }
+
+        if (false === $string = normalizer_normalize($string)) {
+            throw new InvalidArgumentException('Invalid UTF-8 string.');
+        }
+
+        $this->string = $string;
+    }
+
+    public function append(string ...$suffix): static
+    {
+        $str = clone $this;
