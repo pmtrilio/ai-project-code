e884b7080ea84156a27e686c67c4e312bce01e5a
pmtrilio <pm@trilio.bg>
2026-02-12 19:15:48 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteMiddleware #293 (5-65).cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Rewrite.Logging;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Rewrite;
+
+/// <summary>
+/// Represents a middleware that rewrites urls
+/// </summary>
+public class RewriteMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly RewriteOptions _options;
+    private readonly IFileProvider _fileProvider;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="RewriteMiddleware"/>
+    /// </summary>
+    /// <param name="next">The delegate representing the next middleware in the request pipeline.</param>
+    /// <param name="hostingEnvironment">The Hosting Environment.</param>
+    /// <param name="loggerFactory">The Logger Factory.</param>
+    /// <param name="options">The middleware options, containing the rules to apply.</param>
+    public RewriteMiddleware(
+        RequestDelegate next,
+        IWebHostEnvironment hostingEnvironment,
+        ILoggerFactory loggerFactory,
+        IOptions<RewriteOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next;
+        _options = options.Value;
+        _fileProvider = _options.StaticFileProvider ?? hostingEnvironment.WebRootFileProvider;
+        _logger = loggerFactory.CreateLogger<RewriteMiddleware>();
+    }
+
+    /// <summary>
+    /// Executes the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+    /// <returns>A task that represents the execution of this middleware.</returns>
+    public Task Invoke(HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var rewriteContext = new RewriteContext
+        {
+            HttpContext = context,
+            StaticFileProvider = _fileProvider,
+            Logger = _logger,
+            Result = RuleResult.ContinueRules
+        };
+
+        var originalPath = context.Request.Path;
