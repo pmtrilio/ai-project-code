077a9bce156c2c6ac8d368f7a1e4e734049e3fec
pmtrilio <pm@trilio.bg>
2026-02-12 19:09:48 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Кеширане на данни (201-245).php
@@ -0,0 +1,44 @@
+        return $value;
+    }
+
+    /**
+     * Retrieve an item from the cache and delete it.
+     *
+     * @param  \BackedEnum|\UnitEnum|array|string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function pull($key, $default = null)
+    {
+        return tap($this->get($key, $default), function () use ($key) {
+            $this->forget($key);
+        });
+    }
+
+    /**
+     * Store an item in the cache.
+     *
+     * @param  \BackedEnum|\UnitEnum|array|string  $key
+     * @param  mixed  $value
+     * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
+     * @return bool
+     */
+    public function put($key, $value, $ttl = null)
+    {
+        if (is_array($key)) {
+            return $this->putMany($key, $value);
+        }
+
+        $key = enum_value($key);
+
+        if ($ttl === null) {
+            return $this->forever($key, $value);
+        }
+
+        $seconds = $this->getSeconds($ttl);
+
+        if ($seconds <= 0) {
+            return $this->forget($key);
+        }
+
+        $this->event(new WritingKey($this->getName(), $key, $value, $seconds));
