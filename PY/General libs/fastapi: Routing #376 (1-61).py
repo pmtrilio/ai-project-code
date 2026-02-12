c6bbb6fa8fbb2645b1b341dce8aed7a905dd7a36
pmtrilio <pm@trilio.bg>
2026-02-12 19:38:07 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/fastapi: Routing #376 (1-61).py
@@ -0,0 +1,61 @@
+import email.message
+import functools
+import inspect
+import json
+from collections.abc import (
+    AsyncIterator,
+    Awaitable,
+    Collection,
+    Coroutine,
+    Mapping,
+    Sequence,
+)
+from contextlib import AsyncExitStack, asynccontextmanager
+from enum import Enum, IntEnum
+from typing import (
+    Annotated,
+    Any,
+    Callable,
+    Optional,
+    Union,
+)
+
+from annotated_doc import Doc
+from fastapi import params
+from fastapi._compat import (
+    ModelField,
+    Undefined,
+    annotation_is_pydantic_v1,
+    lenient_issubclass,
+)
+from fastapi.datastructures import Default, DefaultPlaceholder
+from fastapi.dependencies.models import Dependant
+from fastapi.dependencies.utils import (
+    _should_embed_body_fields,
+    get_body_field,
+    get_dependant,
+    get_flat_dependant,
+    get_parameterless_sub_dependant,
+    get_typed_return_annotation,
+    solve_dependencies,
+)
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import (
+    EndpointContext,
+    FastAPIError,
+    PydanticV1NotSupportedError,
+    RequestValidationError,
+    ResponseValidationError,
+    WebSocketRequestValidationError,
+)
+from fastapi.types import DecoratedCallable, IncEx
+from fastapi.utils import (
+    create_cloned_field,
+    create_model_field,
+    generate_unique_id,
+    get_value_or_default,
+    is_body_allowed_for_status_code,
+)
+from starlette import routing
+from starlette._exception_handler import wrap_app_handling_exceptions
+from starlette._utils import is_async_callable
