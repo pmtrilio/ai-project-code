b7ebeed0f4f1de0a37ef435ccef301c2f6708f4b
pmtrilio <pm@trilio.bg>
2026-02-12 14:09:48 +0200

    Upload file via PHP cURL

---
+++ CS/Roslyn/Roslyn (C# compiler): Компилатор – Csharpcompiler (10-50).cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Collections;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    internal abstract class CSharpCompiler : CommonCompiler
+    {
+        internal const string ResponseFileName = "csc.rsp";
+
+        private readonly CommandLineDiagnosticFormatter _diagnosticFormatter;
+        private readonly string? _tempDirectory;
+
+        protected CSharpCompiler(CSharpCommandLineParser parser, string? responseFile, string[] args, BuildPaths buildPaths, string? additionalReferenceDirectories, IAnalyzerAssemblyLoader assemblyLoader, GeneratorDriverCache? driverCache = null, ICommonCompilerFileSystem? fileSystem = null)
+            : base(parser, responseFile, args, buildPaths, additionalReferenceDirectories, assemblyLoader, driverCache, fileSystem)
+        {
+            _diagnosticFormatter = new CommandLineDiagnosticFormatter(buildPaths.WorkingDirectory, Arguments.PrintFullPaths, Arguments.ShouldIncludeErrorEndLocation);
+            _tempDirectory = buildPaths.TempDirectory;
+        }
+
+        public override DiagnosticFormatter DiagnosticFormatter { get { return _diagnosticFormatter; } }
+        protected internal new CSharpCommandLineArguments Arguments { get { return (CSharpCommandLineArguments)base.Arguments; } }
+
+        public override Compilation? CreateCompilation(
+            TextWriter consoleOutput,
+            TouchedFileLogger? touchedFilesLogger,
+            ErrorLogger? errorLogger,
+            ImmutableArray<AnalyzerConfigOptionsResult> analyzerConfigOptions,
+            AnalyzerConfigOptionsResult globalConfigOptions)
+        {
+            var parseOptions = Arguments.ParseOptions;
+
+            // We compute script parse options once so we don't have to do it repeatedly in
+            // case there are many script files.
+            var scriptParseOptions = parseOptions.WithKind(SourceCodeKind.Script);
