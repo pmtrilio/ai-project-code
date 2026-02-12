6f2230c57a1d85a9ca1f0e79eb12069537710b73
pmtrilio <pm@trilio.bg>
2026-02-12 20:10:47 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Regex #631 (20-60).cs
@@ -0,0 +1,41 @@
+    /// allow use of regular expressions without instantiating a Regex explicitly.
+    /// </summary>
+    public partial class Regex : ISerializable
+    {
+        [StringSyntax(StringSyntaxAttribute.Regex)]
+        protected internal string? pattern;                   // The string pattern provided
+        protected internal RegexOptions roptions;             // the top-level options from the options string
+        protected internal RegexRunnerFactory? factory;       // Factory used to create runner instances for executing the regex
+        protected internal Hashtable? caps;                   // if captures are sparse, this is the hashtable capnum->index
+        protected internal Hashtable? capnames;               // if named captures are used, this maps names->index
+        protected internal string[]? capslist;                // if captures are sparse or named captures are used, this is the sorted list of names
+        protected internal int capsize;                       // the size of the capture array
+
+        private volatile RegexRunner? _runner;                // cached runner
+
+#if DEBUG
+        // These members aren't used from Regex(), but we want to keep them in debug builds for now,
+        // so this is a convenient place to include them rather than needing a debug-only illink file.
+        [DynamicDependency(nameof(SaveDGML))]
+        [DynamicDependency(nameof(GenerateUnicodeTables))]
+        [DynamicDependency(nameof(SampleMatches))]
+        [DynamicDependency(nameof(Explore))]
+#endif
+        protected Regex()
+        {
+            internalMatchTimeout = s_defaultMatchTimeout;
+        }
+
+        /// <summary>
+        /// Creates a regular expression object for the specified regular expression.
+        /// </summary>
+        public Regex([StringSyntax(StringSyntaxAttribute.Regex)] string pattern) :
+            this(pattern, culture: null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a regular expression object for the specified regular expression, with options that modify the pattern.
+        /// </summary>
+        public Regex([StringSyntax(StringSyntaxAttribute.Regex, nameof(options))] string pattern, RegexOptions options) :
+            this(pattern, options, s_defaultMatchTimeout, culture: null)
