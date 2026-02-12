a55b2e14bc7fb00b82444dfceeed3bd21b80d602
pmtrilio <pm@trilio.bg>
2026-02-12 19:17:44 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: DirectoryBrowserOptions #408 (5-45).cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+
+namespace Microsoft.AspNetCore.Builder;
+
+/// <summary>
+/// Directory browsing options
+/// </summary>
+public class DirectoryBrowserOptions : SharedOptionsBase
+{
+    /// <summary>
+    /// Enabled directory browsing for all request paths
+    /// </summary>
+    public DirectoryBrowserOptions()
+        : this(new SharedOptions())
+    {
+    }
+
+    /// <summary>
+    /// Enabled directory browsing all request paths
+    /// </summary>
+    /// <param name="sharedOptions"></param>
+    public DirectoryBrowserOptions(SharedOptions sharedOptions)
+        : base(sharedOptions)
+    {
+    }
+
+    /// <summary>
+    /// The component that generates the view.
+    /// </summary>
+    public IDirectoryFormatter? Formatter { get; set; }
+}
