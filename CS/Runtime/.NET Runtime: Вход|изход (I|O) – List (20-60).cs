db557eda8cd8012a7a6207796b27c0e142663bf2
pmtrilio <pm@trilio.bg>
2026-02-12 14:12:41 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Вход|изход (I|O) – List (20-60).cs
@@ -0,0 +1,41 @@
+    [TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+    public class List<T> : IList<T>, IList, IReadOnlyList<T>
+    {
+        private const int DefaultCapacity = 4;
+
+        internal T[] _items; // Do not rename (binary serialization)
+        internal int _size; // Do not rename (binary serialization)
+        internal int _version; // Do not rename (binary serialization)
+
+#pragma warning disable CA1825, IDE0300 // avoid the extra generic instantiation for Array.Empty<T>()
+        private static readonly T[] s_emptyArray = new T[0];
+#pragma warning restore CA1825, IDE0300
+
+        // Constructs a List. The list is initially empty and has a capacity
+        // of zero. Upon adding the first element to the list the capacity is
+        // increased to DefaultCapacity, and then increased in multiples of two
+        // as required.
+        public List()
+        {
+            _items = s_emptyArray;
+        }
+
+        // Constructs a List with a given initial capacity. The list is
+        // initially empty, but will have room for the given number of elements
+        // before any reallocations are required.
+        //
+        public List(int capacity)
+        {
+            if (capacity < 0)
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.capacity, ExceptionResource.ArgumentOutOfRange_NeedNonNegNum);
+
+            if (capacity == 0)
+                _items = s_emptyArray;
+            else
+                _items = new T[capacity];
+        }
+
+        // Constructs a List, copying the contents of the given collection. The
+        // size and capacity of the new list will both be equal to the size of the
+        // given collection.
+        //
