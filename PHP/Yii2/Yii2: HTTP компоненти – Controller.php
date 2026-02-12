4571604ab9b0f6f3f3f3da65ed553131aef479df
pmtrilio <pm@trilio.bg>
2026-02-12 12:56:53 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: HTTP компоненти – Controller.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+use Yii;
+use yii\base\Exception;
+use yii\base\InlineAction;
+use yii\helpers\Url;
+use yii\base\Action;
+use yii\base\Controller as BaseController;
+use yii\base\Module;
+
+/**
+ * Controller is the base class of web controllers.
+ *
+ * For more details and usage information on Controller, see the [guide article on controllers](guide:structure-controllers).
+ *
+ * @property Request $request The request object.
+ * @property Response $response The response object.
+ * @property View $view The view object that can be used to render views or view files.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ *
+ * @template T of Module
+ * @extends BaseController<T>
+ */
+class Controller extends BaseController
+{
+    /**
+     * @var bool whether to enable CSRF validation for the actions in this controller.
+     * CSRF validation is enabled only when both this property and [[\yii\web\Request::enableCsrfValidation]] are true.
+     */
+    public $enableCsrfValidation = true;
+    /**
+     * @var array the parameters bound to the current action.
+     */
+    public $actionParams = [];
+
+
+    /**
+     * Renders a view in response to an AJAX request.
+     *
+     * This method is similar to [[renderPartial()]] except that it will inject into
+     * the rendering result with JS/CSS scripts and files which are registered with the view.
+     * For this reason, you should use this method instead of [[renderPartial()]] to render
+     * a view to respond to an AJAX request.
+     *
+     * @param string $view the view name. Please refer to [[render()]] on how to specify a view name.
+     * @param array $params the parameters (name-value pairs) that should be made available in the view.
+     * @return string the rendering result.
+     */
+    public function renderAjax($view, $params = [])
+    {
