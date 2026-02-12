9b80ac25d342bf350ca23639897b29b6652b3067
pmtrilio <pm@trilio.bg>
2026-02-12 14:39:16 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Eventserviceprovider #5 (10-70).php
@@ -0,0 +1,18 @@
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('events', function ($app) {
+            return (new Dispatcher($app))->setQueueResolver(function () {
+                return app(QueueFactoryContract::class);
+            })->setTransactionManagerResolver(function () {
+                return app()->bound('db.transactions')
+                    ? app('db.transactions')
+                    : null;
+            });
+        });
+    }
+}
