26638ad7f955852b055c7bc759d7d1598b6b51bc
pmtrilio <pm@trilio.bg>
2026-02-12 15:39:28 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Dispatcher #680 (20-120).php
@@ -0,0 +1,101 @@
+ *
+ * An instance of Dispatcher is registered as a core application component and can be accessed using `Yii::$app->log`.
+ *
+ * You may configure the targets in application configuration, like the following:
+ *
+ * ```
+ * [
+ *     'components' => [
+ *         'log' => [
+ *             'targets' => [
+ *                 'file' => [
+ *                     'class' => 'yii\log\FileTarget',
+ *                     'levels' => ['trace', 'info'],
+ *                     'categories' => ['yii\*'],
+ *                 ],
+ *                 'email' => [
+ *                     'class' => 'yii\log\EmailTarget',
+ *                     'levels' => ['error', 'warning'],
+ *                     'message' => [
+ *                         'to' => 'admin@example.com',
+ *                     ],
+ *                 ],
+ *             ],
+ *         ],
+ *     ],
+ * ]
+ * ```
+ *
+ * Each log target can have a name and can be referenced via the [[targets]] property as follows:
+ *
+ * ```
+ * Yii::$app->log->targets['file']->enabled = false;
+ * ```
+ *
+ * @property int $flushInterval How many messages should be logged before they are sent to targets. See
+ * [[getFlushInterval()]] and [[setFlushInterval()]] for details.
+ * @property Logger $logger The logger. If not set, [[Yii::getLogger()]] will be used. Note that the type of
+ * this property differs in getter and setter. See [[getLogger()]] and [[setLogger()]] for details.
+ * @property int $traceLevel How many application call stacks should be logged together with each message.
+ * This method returns the value of [[Logger::traceLevel]]. Defaults to 0.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Dispatcher extends Component
+{
+    /**
+     * @var array|Target[] the log targets. Each array element represents a single [[Target|log target]] instance
+     * or the configuration for creating the log target instance.
+     */
+    public $targets = [];
+
+    /**
+     * @var Logger|null the logger.
+     */
+    private $_logger;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($config = [])
+    {
+        // ensure logger gets set before any other config option
+        if (isset($config['logger'])) {
+            $this->setLogger($config['logger']);
+            unset($config['logger']);
+        }
+        // connect logger and dispatcher
+        $this->getLogger();
+
+        parent::__construct($config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        foreach ($this->targets as $name => $target) {
+            if (!$target instanceof Target) {
+                $this->targets[$name] = Yii::createObject($target);
+            }
+        }
+    }
+
+    /**
+     * Gets the connected logger.
+     * If not set, [[Yii::getLogger()]] will be used.
+     * @return Logger the logger.
+     */
+    public function getLogger()
+    {
+        if ($this->_logger === null) {
+            $this->setLogger(Yii::getLogger());
+        }
+
+        return $this->_logger;
+    }
