ba8ad2ec9ff79921db11ef1c85f7ad907a6bfd28
pmtrilio <pm@trilio.bg>
2026-02-12 21:24:27 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: RateLimiter #225 (30-110).php
@@ -0,0 +1,80 @@
+
+    /**
+     * Create a new rate limiter instance.
+     *
+     * @param  \Illuminate\Contracts\Cache\Repository  $cache
+     */
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Register a named limiter configuration.
+     *
+     * @param  \BackedEnum|\UnitEnum|string  $name
+     * @param  \Closure  $callback
+     * @return $this
+     */
+    public function for($name, Closure $callback)
+    {
+        $resolvedName = $this->resolveLimiterName($name);
+
+        $this->limiters[$resolvedName] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Get the given named rate limiter.
+     *
+     * @param  \BackedEnum|\UnitEnum|string  $name
+     * @return \Closure|null
+     */
+    public function limiter($name)
+    {
+        $resolvedName = $this->resolveLimiterName($name);
+
+        $limiter = $this->limiters[$resolvedName] ?? null;
+
+        if (! is_callable($limiter)) {
+            return;
+        }
+
+        return function (...$args) use ($limiter) {
+            $result = $limiter(...$args);
+
+            if (! is_array($result)) {
+                return $result;
+            }
+
+            $duplicates = (new Collection($result))->duplicates('key');
+
+            if ($duplicates->isEmpty()) {
+                return $result;
+            }
+
+            foreach ($result as $limit) {
+                if ($duplicates->contains($limit->key)) {
+                    $limit->key = $limit->fallbackKey();
+                }
+            }
+
+            return $result;
+        };
+    }
+
+    /**
+     * Attempts to execute a callback if it's not limited.
+     *
+     * @param  string  $key
+     * @param  int  $maxAttempts
+     * @param  \Closure  $callback
+     * @param  \DateTimeInterface|\DateInterval|int  $decaySeconds
+     * @return mixed
+     */
+    public function attempt($key, $maxAttempts, Closure $callback, $decaySeconds = 60)
+    {
+        if ($this->tooManyAttempts($key, $maxAttempts)) {
+            return false;
+        }
