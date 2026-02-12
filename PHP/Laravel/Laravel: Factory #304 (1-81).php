28c2297a3cf1bfa9618d2a4be4e8ac5494fda73f
pmtrilio <pm@trilio.bg>
2026-02-12 21:25:59 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Factory #304 (1-81).php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Illuminate\Process;
+
+use Closure;
+use Illuminate\Contracts\Process\ProcessResult as ProcessResultContract;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Traits\Macroable;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+class Factory
+{
+    use Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * Indicates if the process factory has faked process handlers.
+     *
+     * @var bool
+     */
+    protected $recording = false;
+
+    /**
+     * All of the recorded processes.
+     *
+     * @var array
+     */
+    protected $recorded = [];
+
+    /**
+     * The registered fake handler callbacks.
+     *
+     * @var array
+     */
+    protected $fakeHandlers = [];
+
+    /**
+     * Indicates that an exception should be thrown if any process is not faked.
+     *
+     * @var bool
+     */
+    protected $preventStrayProcesses = false;
+
+    /**
+     * Create a new fake process response for testing purposes.
+     *
+     * @param  array|string  $output
+     * @param  array|string  $errorOutput
+     * @param  int  $exitCode
+     * @return \Illuminate\Process\FakeProcessResult
+     */
+    public function result(array|string $output = '', array|string $errorOutput = '', int $exitCode = 0)
+    {
+        return new FakeProcessResult(
+            output: $output,
+            errorOutput: $errorOutput,
+            exitCode: $exitCode,
+        );
+    }
+
+    /**
+     * Begin describing a fake process lifecycle.
+     *
+     * @return \Illuminate\Process\FakeProcessDescription
+     */
+    public function describe()
+    {
+        return new FakeProcessDescription;
+    }
+
+    /**
+     * Begin describing a fake process sequence.
+     *
+     * @param  array  $processes
+     * @return \Illuminate\Process\FakeProcessSequence
+     */
+    public function sequence(array $processes = [])
+    {
+        return new FakeProcessSequence($processes);
+    }
