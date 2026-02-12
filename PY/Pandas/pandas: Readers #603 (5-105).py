184417c5b649c3a65fafc2841f559ca123c7ab5f
pmtrilio <pm@trilio.bg>
2026-02-12 19:42:34 +0200

    Upload file via PHP cURL

---
+++ PY/Pandas/pandas: Readers #603 (5-105).py
@@ -0,0 +1,101 @@
+"""
+
+from __future__ import annotations
+
+from collections import (
+    abc,
+    defaultdict,
+)
+import csv
+import sys
+from typing import (
+    IO,
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    Literal,
+    Self,
+    TypedDict,
+    Unpack,
+    cast,
+    overload,
+)
+import warnings
+
+import numpy as np
+
+from pandas._libs import lib
+from pandas._libs.parsers import STR_NA_VALUES
+from pandas.errors import (
+    AbstractMethodError,
+    ParserWarning,
+)
+from pandas.util._decorators import (
+    set_module,
+)
+from pandas.util._exceptions import find_stack_level
+from pandas.util._validators import check_dtype_backend
+
+from pandas.core.dtypes.common import (
+    is_file_like,
+    is_float,
+    is_integer,
+    is_list_like,
+    pandas_dtype,
+)
+
+from pandas import Series
+from pandas.core.frame import DataFrame
+from pandas.core.indexes.api import RangeIndex
+
+from pandas.io.common import (
+    IOHandles,
+    get_handle,
+    stringify_path,
+    validate_header_arg,
+)
+from pandas.io.parsers.arrow_parser_wrapper import ArrowParserWrapper
+from pandas.io.parsers.base_parser import (
+    ParserBase,
+    is_index_col,
+    parser_defaults,
+)
+from pandas.io.parsers.c_parser_wrapper import CParserWrapper
+from pandas.io.parsers.python_parser import (
+    FixedWidthFieldParser,
+    PythonParser,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import (
+        Callable,
+        Hashable,
+        Iterable,
+        Mapping,
+        Sequence,
+    )
+    from types import TracebackType
+
+    from pandas._typing import (
+        CompressionOptions,
+        CSVEngine,
+        DtypeArg,
+        DtypeBackend,
+        FilePath,
+        HashableT,
+        IndexLabel,
+        ReadCsvBuffer,
+        StorageOptions,
+        UsecolsArgType,
+    )
+
+    class _read_shared(TypedDict, Generic[HashableT], total=False):
+        # annotations shared between read_csv/fwf/table's overloads
+        # NOTE: Keep in sync with the annotations of the implementation
+        sep: str | None | lib.NoDefault
+        delimiter: str | None | lib.NoDefault
+        header: int | Sequence[int] | None | Literal["infer"]
+        names: Sequence[Hashable] | None | lib.NoDefault
+        index_col: IndexLabel | Literal[False] | None
+        usecols: UsecolsArgType
+        dtype: DtypeArg | None
