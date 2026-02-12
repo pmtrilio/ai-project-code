a42146801486b4a5daab8b015815002f2fd730e1
pmtrilio <pm@trilio.bg>
2026-02-12 15:42:10 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Validator #120 (5-105).php
@@ -0,0 +1,101 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\validators;
+
+use Yii;
+use yii\base\Component;
+use yii\base\NotSupportedException;
+
+/**
+ * Validator is the base class for all validators.
+ *
+ * Child classes should override the [[validateValue()]] and/or [[validateAttribute()]] methods to provide the actual
+ * logic of performing data validation. Child classes may also override [[clientValidateAttribute()]]
+ * to provide client-side validation support.
+ *
+ * Validator declares a set of [[builtInValidators|built-in validators]] which can
+ * be referenced using short names. They are listed as follows:
+ *
+ * - `boolean`: [[BooleanValidator]]
+ * - `captcha`: [[\yii\captcha\CaptchaValidator]]
+ * - `compare`: [[CompareValidator]]
+ * - `date`: [[DateValidator]]
+ * - `datetime`: [[DateValidator]]
+ * - `time`: [[DateValidator]]
+ * - `default`: [[DefaultValueValidator]]
+ * - `double`: [[NumberValidator]]
+ * - `each`: [[EachValidator]]
+ * - `email`: [[EmailValidator]]
+ * - `exist`: [[ExistValidator]]
+ * - `file`: [[FileValidator]]
+ * - `filter`: [[FilterValidator]]
+ * - `image`: [[ImageValidator]]
+ * - `in`: [[RangeValidator]]
+ * - `integer`: [[NumberValidator]]
+ * - `match`: [[RegularExpressionValidator]]
+ * - `required`: [[RequiredValidator]]
+ * - `safe`: [[SafeValidator]]
+ * - `string`: [[StringValidator]]
+ * - `trim`: [[TrimValidator]]
+ * - `unique`: [[UniqueValidator]]
+ * - `url`: [[UrlValidator]]
+ * - `ip`: [[IpValidator]]
+ *
+ * For more details and usage information on Validator, see the [guide article on validators](guide:input-validation).
+ *
+ * @property-read array $attributeNames Attribute names.
+ * @property-read array|null $validationAttributes List of attribute names.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Validator extends Component
+{
+    /**
+     * @var array list of built-in validators (name => class or configuration)
+     */
+    public static $builtInValidators = [
+        'boolean' => 'yii\validators\BooleanValidator',
+        'captcha' => 'yii\captcha\CaptchaValidator',
+        'compare' => 'yii\validators\CompareValidator',
+        'date' => 'yii\validators\DateValidator',
+        'datetime' => [
+            'class' => 'yii\validators\DateValidator',
+            'type' => DateValidator::TYPE_DATETIME,
+        ],
+        'time' => [
+            'class' => 'yii\validators\DateValidator',
+            'type' => DateValidator::TYPE_TIME,
+        ],
+        'default' => 'yii\validators\DefaultValueValidator',
+        'double' => 'yii\validators\NumberValidator',
+        'each' => 'yii\validators\EachValidator',
+        'email' => 'yii\validators\EmailValidator',
+        'exist' => 'yii\validators\ExistValidator',
+        'file' => 'yii\validators\FileValidator',
+        'filter' => 'yii\validators\FilterValidator',
+        'image' => 'yii\validators\ImageValidator',
+        'in' => 'yii\validators\RangeValidator',
+        'integer' => [
+            'class' => 'yii\validators\NumberValidator',
+            'integerOnly' => true,
+        ],
+        'match' => 'yii\validators\RegularExpressionValidator',
+        'number' => 'yii\validators\NumberValidator',
+        'required' => 'yii\validators\RequiredValidator',
+        'safe' => 'yii\validators\SafeValidator',
+        'string' => 'yii\validators\StringValidator',
+        'trim' => [
+            'class' => 'yii\validators\TrimValidator',
+            'skipOnArray' => true,
+        ],
+        'unique' => 'yii\validators\UniqueValidator',
+        'url' => 'yii\validators\UrlValidator',
+        'ip' => 'yii\validators\IpValidator',
+    ];
+    /**
+     * @var array|string attributes to be validated by this validator. For multiple attributes,
+     * please specify them as an array; for single attribute, you may use either a string or an array.
+     */
