0949e28bf3cfe9c997f3f97841bb131c29a26b19
pmtrilio <pm@trilio.bg>
2026-02-12 19:19:44 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Localization: AcceptLanguageHeaderRequestCultureProvider #523 (20-80).cs
@@ -0,0 +1,34 @@
+    public int MaximumAcceptLanguageHeaderValuesToTry { get; set; } = 3;
+
+    /// <inheritdoc />
+    public override Task<ProviderCultureResult?> DetermineProviderCultureResult(HttpContext httpContext)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        var acceptLanguageHeader = httpContext.Request.GetTypedHeaders().AcceptLanguage;
+
+        if (acceptLanguageHeader == null || acceptLanguageHeader.Count == 0)
+        {
+            return NullProviderCultureResult;
+        }
+
+        var languages = acceptLanguageHeader.AsEnumerable();
+
+        if (MaximumAcceptLanguageHeaderValuesToTry > 0)
+        {
+            // We take only the first configured number of languages from the header and then order those that we
+            // attempt to parse as a CultureInfo to mitigate potentially spinning CPU on lots of parse attempts.
+            languages = languages.Take(MaximumAcceptLanguageHeaderValuesToTry);
+        }
+
+        var orderedLanguages = languages.OrderByDescending(h => h, StringWithQualityHeaderValueComparer.QualityComparer)
+            .Select(x => x.Value).ToList();
+
+        if (orderedLanguages.Count > 0)
+        {
+            return Task.FromResult<ProviderCultureResult?>(new ProviderCultureResult(orderedLanguages));
+        }
+
+        return NullProviderCultureResult;
+    }
+}
