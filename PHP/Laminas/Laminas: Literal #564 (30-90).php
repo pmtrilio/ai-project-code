951409126637044281d5c40a39bdc4ec18979f39
pmtrilio <pm@trilio.bg>
2026-02-12 20:36:55 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Literal #564 (30-90).php
@@ -0,0 +1,61 @@
+    /**
+     * @internal
+     * @deprecated Since 3.9.0 This property will be removed or made private in version 4.0
+     *
+     * @var int|null
+     */
+    public $priority;
+
+    /**
+     * Create a new literal route.
+     *
+     * @param  string $route
+     */
+    public function __construct(
+        /**
+         * RouteInterface to match.
+         */
+        protected $route,
+        array $defaults = []
+    ) {
+        $this->defaults = $defaults;
+    }
+
+    /**
+     * factory(): defined by RouteInterface interface.
+     *
+     * @see    \Laminas\Router\RouteInterface::factory()
+     *
+     * @param  iterable $options
+     * @return Literal
+     * @throws Exception\InvalidArgumentException
+     */
+    public static function factory($options = [])
+    {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        } elseif (! is_array($options)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s expects an array or Traversable set of options',
+                __METHOD__
+            ));
+        }
+
+        if (! isset($options['route'])) {
+            throw new Exception\InvalidArgumentException('Missing "route" in options array');
+        }
+
+        if (! isset($options['defaults'])) {
+            $options['defaults'] = [];
+        }
+
+        return new static($options['route'], $options['defaults']);
+    }
+
+    /**
+     * match(): defined by RouteInterface interface.
+     *
+     * @see    \Laminas\Router\RouteInterface::match()
+     *
+     * @param  integer|null $pathOffset
+     * @return RouteMatch|null
