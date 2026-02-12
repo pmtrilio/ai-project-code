5dcc7ec85604b911247bcd327bda76cacce68544
pmtrilio <pm@trilio.bg>
2026-02-12 20:31:45 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Application #320 (10-110).php
@@ -0,0 +1,101 @@
+use Illuminate\Support\ProcessUtils;
+use ReflectionClass;
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+use function Illuminate\Support\artisan_binary;
+use function Illuminate\Support\php_binary;
+
+class Application extends SymfonyApplication implements ApplicationContract
+{
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $laravel;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The output from the previous command.
+     *
+     * @var \Symfony\Component\Console\Output\BufferedOutput
+     */
+    protected $lastOutput;
+
+    /**
+     * The console application bootstrappers.
+     *
+     * @var array<array-key, \Closure($this): void>
+     */
+    protected static $bootstrappers = [];
+
+    /**
+     * A map of command names to classes.
+     *
+     * @var array
+     */
+    protected $commandMap = [];
+
+    /**
+     * Create a new Artisan console application.
+     *
+     * @param  \Illuminate\Contracts\Container\Container  $laravel
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @param  string  $version
+     */
+    public function __construct(Container $laravel, Dispatcher $events, $version)
+    {
+        parent::__construct('Laravel Framework', $version);
+
+        $this->laravel = $laravel;
+        $this->events = $events;
+        $this->setAutoExit(false);
+        $this->setCatchExceptions(false);
+
+        $this->events->dispatch(new ArtisanStarting($this));
+
+        $this->bootstrap();
+    }
+
+    /**
+     * Determine the proper PHP executable.
+     *
+     * @return string
+     */
+    public static function phpBinary()
+    {
+        return ProcessUtils::escapeArgument(php_binary());
+    }
+
+    /**
+     * Determine the proper Artisan executable.
+     *
+     * @return string
+     */
+    public static function artisanBinary()
+    {
+        return ProcessUtils::escapeArgument(artisan_binary());
+    }
+
+    /**
+     * Format the given command as a fully-qualified executable command.
+     *
+     * @param  string  $string
+     * @return string
+     */
+    public static function formatCommandString($string)
+    {
