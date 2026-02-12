f8bf2f4bd0fe49f7346faba95eb95e7c57fb1b1c
pmtrilio <pm@trilio.bg>
2026-02-12 14:14:57 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Вход|изход (I|O) – Stream (5-65).cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    public abstract partial class Stream : MarshalByRefObject, IDisposable, IAsyncDisposable
+    {
+        public static readonly Stream Null = new NullStream();
+
+        /// <summary>To serialize async operations on streams that don't implement their own.</summary>
+        private protected SemaphoreSlim? _asyncActiveSemaphore;
+
+        [MemberNotNull(nameof(_asyncActiveSemaphore))]
+        private protected SemaphoreSlim EnsureAsyncActiveSemaphoreInitialized() =>
+            // Lazily-initialize _asyncActiveSemaphore.  As we're never accessing the SemaphoreSlim's
+            // WaitHandle, we don't need to worry about Disposing it in the case of a race condition.
+            _asyncActiveSemaphore ??
+            Interlocked.CompareExchange(ref _asyncActiveSemaphore, new SemaphoreSlim(1, 1), null) ??
+            _asyncActiveSemaphore;
+
+        public abstract bool CanRead { get; }
+        public abstract bool CanWrite { get; }
+        public abstract bool CanSeek { get; }
+        public virtual bool CanTimeout => false;
+
+        public abstract long Length { get; }
+        public abstract long Position { get; set; }
+
+        public virtual int ReadTimeout
+        {
+            get => throw new InvalidOperationException(SR.InvalidOperation_TimeoutsNotSupported);
+            set => throw new InvalidOperationException(SR.InvalidOperation_TimeoutsNotSupported);
+        }
+
+        public virtual int WriteTimeout
+        {
+            get => throw new InvalidOperationException(SR.InvalidOperation_TimeoutsNotSupported);
+            set => throw new InvalidOperationException(SR.InvalidOperation_TimeoutsNotSupported);
+        }
+
+        public void CopyTo(Stream destination) => CopyTo(destination, GetCopyBufferSize());
+
+        public virtual void CopyTo(Stream destination, int bufferSize)
+        {
+            ValidateCopyToArguments(destination, bufferSize);
+            if (!CanRead)
+            {
+                if (CanWrite)
+                {
+                    ThrowHelper.ThrowNotSupportedException_UnreadableStream();
+                }
+
+                ThrowHelper.ThrowObjectDisposedException_StreamClosed(GetType().Name);
+            }
+
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+            try
