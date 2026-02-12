d39c9185953a9a82f1eee7532678e21d31d8bd7d
pmtrilio <pm@trilio.bg>
2026-02-12 14:45:52 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Blueprint #374 (20-80).php
@@ -0,0 +1,60 @@
+    use Macroable;
+
+    /**
+     * The database connection instance.
+     */
+    protected Connection $connection;
+
+    /**
+     * The schema grammar instance.
+     */
+    protected Grammar $grammar;
+
+    /**
+     * The table the blueprint describes.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * The columns that should be added to the table.
+     *
+     * @var \Illuminate\Database\Schema\ColumnDefinition[]
+     */
+    protected $columns = [];
+
+    /**
+     * The commands that should be run for the table.
+     *
+     * @var \Illuminate\Support\Fluent[]
+     */
+    protected $commands = [];
+
+    /**
+     * The storage engine that should be used for the table.
+     *
+     * @var string
+     */
+    public $engine;
+
+    /**
+     * The default character set that should be used for the table.
+     *
+     * @var string
+     */
+    public $charset;
+
+    /**
+     * The collation that should be used for the table.
+     *
+     * @var string
+     */
+    public $collation;
+
+    /**
+     * Whether to make the table temporary.
+     *
+     * @var bool
+     */
+    public $temporary = false;
