01b0a2b044fad1314068ddd16aa5ad1aa0525b15
pmtrilio <pm@trilio.bg>
2026-02-12 19:03:06 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Container #368 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Illuminate\Container;
+
+use ArrayAccess;
+use Closure;
+use Exception;
+use Illuminate\Container\Attributes\Bind;
+use Illuminate\Container\Attributes\Scoped;
+use Illuminate\Container\Attributes\Singleton;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Container\CircularDependencyException;
+use Illuminate\Contracts\Container\Container as ContainerContract;
+use Illuminate\Contracts\Container\ContextualAttribute;
+use Illuminate\Contracts\Container\SelfBuilding;
+use Illuminate\Support\Traits\ReflectsClosures;
+use LogicException;
+use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionFunction;
+use ReflectionParameter;
+use TypeError;
+
+class Container implements ArrayAccess, ContainerContract
+{
+    use ReflectsClosures;
+
+    /**
+     * The current globally available container (if any).
+     *
+     * @var static
+     */
+    protected static $instance;
+
+    /**
+     * An array of the types that have been resolved.
+     *
+     * @var bool[]
+     */
+    protected $resolved = [];
+
+    /**
+     * The container's bindings.
+     *
+     * @var array[]
+     */
+    protected $bindings = [];
+
+    /**
+     * The container's method bindings.
+     *
+     * @var \Closure[]
+     */
+    protected $methodBindings = [];
+
+    /**
+     * The container's shared instances.
+     *
+     * @var object[]
+     */
