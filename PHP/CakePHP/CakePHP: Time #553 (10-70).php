06d158cd2cd70a575eeffe4cc22f57846ea8b889
pmtrilio <pm@trilio.bg>
2026-02-12 14:49:07 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Time #553 (10-70).php
@@ -0,0 +1,61 @@
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         5.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\I18n;
+
+use Cake\Chronos\ChronosTime;
+use Closure;
+use IntlDateFormatter;
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+
+/**
+ * Extends time class provided by Chronos.
+ *
+ * Adds handy methods and locale-aware formatting helpers.
+ *
+ * @phpstan-immutable
+ */
+class Time extends ChronosTime implements JsonSerializable, Stringable
+{
+    use DateFormatTrait;
+
+    /**
+     * The format to use when formatting a time using `Cake\I18n\Time::i18nFormat()`
+     * and `__toString`.
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var string|int
+     * @see \Cake\I18n\Time::i18nFormat()
+     */
+    protected static string|int $_toStringFormat = IntlDateFormatter::SHORT;
+
+    /**
+     * The format to use when converting this object to JSON.
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var \Closure|string|int
+     * @see \Cake\I18n\Date::i18nFormat()
+     */
+    protected static Closure|string|int $_jsonEncodeFormat = "HH':'mm':'ss";
+
+    /**
+     * The format to use when formatting a time using `Cake\I18n\Time::nice()`
+     *
+     * The format should be either the formatting constants from IntlDateFormatter as
+     * described in (https://secure.php.net/manual/en/class.intldateformatter.php) or a pattern
+     * as specified in (https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classSimpleDateFormat.html#details)
+     *
+     * @var string|int
+     * @see \Cake\I18n\Time::nice()
