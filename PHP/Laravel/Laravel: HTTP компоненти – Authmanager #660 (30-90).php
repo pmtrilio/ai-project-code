d34e4f38063ff013546efbf45a29530027736c56
pmtrilio <pm@trilio.bg>
2026-02-12 19:08:56 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Authmanager #660 (30-90).php
@@ -0,0 +1,61 @@
+
+    /**
+     * The array of created "drivers".
+     *
+     * @var array
+     */
+    protected $guards = [];
+
+    /**
+     * The user resolver shared by various services.
+     *
+     * Determines the default user for Gate, Request, and the Authenticatable contract.
+     *
+     * @var \Closure
+     */
+    protected $userResolver;
+
+    /**
+     * Create a new Auth manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+
+        $this->userResolver = fn ($guard = null) => $this->guard($guard)->user();
+    }
+
+    /**
+     * Attempt to get the guard from the local cache.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard
+     */
+    public function guard($name = null)
+    {
+        $name = $name ?: $this->getDefaultDriver();
+
+        return $this->guards[$name] ??= $this->resolve($name);
+    }
+
+    /**
+     * Resolve the given guard.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function resolve($name)
+    {
+        $config = $this->getConfig($name);
+
+        if (is_null($config)) {
+            throw new InvalidArgumentException("Auth guard [{$name}] is not defined.");
+        }
+
+        if (isset($this->customCreators[$config['driver']])) {
+            return $this->callCustomCreator($name, $config);
+        }
