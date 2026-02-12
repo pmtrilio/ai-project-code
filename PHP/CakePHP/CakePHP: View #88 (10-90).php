e8dc7d2109a1dd77d6c8d8c4ed9233ae61b72e9b
pmtrilio <pm@trilio.bg>
2026-02-12 14:40:48 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: View #88 (10-90).php
@@ -0,0 +1,81 @@
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\View;
+
+use Cake\Cache\Cache;
+use Cake\Core\App;
+use Cake\Core\Exception\CakeException;
+use Cake\Core\InstanceConfigTrait;
+use Cake\Core\Plugin;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
+use Cake\Event\EventManagerInterface;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use Cake\Log\LogTrait;
+use Cake\Routing\Router;
+use Cake\Utility\Inflector;
+use Cake\View\Exception\MissingElementException;
+use Cake\View\Exception\MissingLayoutException;
+use Cake\View\Exception\MissingTemplateException;
+use Generator;
+use InvalidArgumentException;
+use LogicException;
+use Throwable;
+use function Cake\Core\pluginSplit;
+
+/**
+ * View, the V in the MVC triad. View interacts with Helpers and view variables passed
+ * in from the controller to render the results of the controller action. Often this is HTML,
+ * but can also take the form of JSON, XML, PDF's or streaming files.
+ *
+ * CakePHP uses a two-step-view pattern. This means that the template content is rendered first,
+ * and then inserted into the selected layout. This also means you can pass data from the template to the
+ * layout using `$this->set()`
+ *
+ * View class supports using plugins as themes. You can set
+ *
+ * ```
+ * public function beforeRender(\Cake\Event\EventInterface $event)
+ * {
+ *      $this->viewBuilder()->setTheme('SuperHot');
+ * }
+ * ```
+ *
+ * in your Controller to use plugin `SuperHot` as a theme. Eg. If current action
+ * is PostsController::index() then View class will look for template file
+ * `plugins/SuperHot/templates/Posts/index.php`. If a theme template
+ * is not found for the current action the default app template file is used.
+ *
+ * @property \Cake\View\Helper\BreadcrumbsHelper $Breadcrumbs
+ * @property \Cake\View\Helper\FlashHelper $Flash
+ * @property \Cake\View\Helper\FormHelper $Form
+ * @property \Cake\View\Helper\HtmlHelper $Html
+ * @property \Cake\View\Helper\NumberHelper $Number
+ * @property \Cake\View\Helper\PaginatorHelper $Paginator
+ * @property \Cake\View\Helper\TextHelper $Text
+ * @property \Cake\View\Helper\TimeHelper $Time
+ * @property \Cake\View\Helper\UrlHelper $Url
+ * @property \Cake\View\ViewBlock $Blocks
+ * @template TSubject of \Cake\View\View
+ * @implements \Cake\Event\EventDispatcherInterface<TSubject>
+ */
+class View implements EventDispatcherInterface
+{
+    use CellTrait {
+        cell as public;
+    }
+    /**
+     * @use \Cake\Event\EventDispatcherTrait<TSubject>
+     */
+    use EventDispatcherTrait;
+    use InstanceConfigTrait;
+    use LogTrait;
+
+    /**
+     * Helpers collection
