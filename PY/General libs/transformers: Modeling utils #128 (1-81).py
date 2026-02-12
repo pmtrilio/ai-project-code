4c9560be4611c3dec495a878dea9a385fbf8454c
pmtrilio <pm@trilio.bg>
2026-02-12 19:32:45 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/transformers: Modeling utils #128 (1-81).py
@@ -0,0 +1,81 @@
+# Copyright 2018 The Google AI Language Team Authors, Facebook AI Research authors and The HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import collections
+import copy
+import functools
+import importlib.metadata
+import inspect
+import json
+import os
+import re
+import sys
+import warnings
+from abc import abstractmethod
+from collections import defaultdict
+from collections.abc import Callable, Iterator
+from contextlib import contextmanager
+from dataclasses import dataclass, field, replace
+from enum import Enum
+from functools import partial, wraps
+from itertools import cycle
+from threading import Thread
+from typing import Optional, TypeVar, get_type_hints
+from zipfile import is_zipfile
+
+import torch
+from huggingface_hub import create_repo, is_offline_mode, split_torch_state_dict_into_shards
+from packaging import version
+from safetensors import safe_open
+from safetensors.torch import save_file as safe_save_file
+from torch import Tensor, nn
+from torch.distributions import constraints
+from torch.utils.checkpoint import checkpoint
+
+from . import initialization as init
+from .configuration_utils import PreTrainedConfig
+from .conversion_mapping import get_model_conversion_mapping
+from .core_model_loading import (
+    WeightConverter,
+    WeightRenaming,
+    convert_and_load_state_dict_in_model,
+    revert_weight_conversion,
+)
+from .distributed import DistributedConfig
+from .dynamic_module_utils import custom_object_save
+from .generation import CompileConfig, GenerationConfig
+from .integrations import PeftAdapterMixin, deepspeed_config, is_deepspeed_zero3_enabled, is_fsdp_enabled
+from .integrations.accelerate import (
+    _get_device_map,
+    accelerate_disk_offload,
+    accelerate_dispatch,
+    check_and_set_device_map,
+    expand_device_map,
+    get_device,
+    load_offloaded_parameter,
+)
+from .integrations.deepspeed import _load_state_dict_into_zero3_model
+from .integrations.eager_paged import eager_paged_attention_forward
+from .integrations.flash_attention import flash_attention_forward
+from .integrations.flash_paged import paged_attention_forward
+from .integrations.flex_attention import flex_attention_forward
+from .integrations.hub_kernels import is_kernel
+from .integrations.peft import maybe_load_adapters
+from .integrations.sdpa_attention import sdpa_attention_forward
+from .integrations.sdpa_paged import sdpa_attention_paged_forward
+from .integrations.tensor_parallel import (
+    ALL_PARALLEL_STYLES,
+    _get_parameter_tp_plan,
+    distribute_model,
+    initialize_tensor_parallelism,
