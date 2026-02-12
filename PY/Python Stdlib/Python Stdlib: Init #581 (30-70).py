8ad93767cc61ce024326672260ff592433c70a4a
pmtrilio <pm@trilio.bg>
2026-02-12 20:23:03 +0200

    Upload file via PHP cURL

---
+++ PY/Python Stdlib/Python Stdlib: Init #581 (30-70).py
@@ -0,0 +1,40 @@
+import sys as _sys
+
+_sys.modules['collections.abc'] = _collections_abc
+abc = _collections_abc
+
+from itertools import chain as _chain
+from itertools import repeat as _repeat
+from itertools import starmap as _starmap
+from keyword import iskeyword as _iskeyword
+from operator import eq as _eq
+from operator import itemgetter as _itemgetter
+from reprlib import recursive_repr as _recursive_repr
+from _weakref import proxy as _proxy
+
+try:
+    from _collections import deque
+except ImportError:
+    pass
+else:
+    _collections_abc.MutableSequence.register(deque)
+
+try:
+    # Expose _deque_iterator to support pickling deque iterators
+    from _collections import _deque_iterator  # noqa: F401
+except ImportError:
+    pass
+
+try:
+    from _collections import defaultdict
+except ImportError:
+    pass
+
+heapq = None  # Lazily imported
+
+
+################################################################################
+### OrderedDict
+################################################################################
+
+class _OrderedDictKeysView(_collections_abc.KeysView):
