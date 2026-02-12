049fbfa324d109f7c03b894123786c9e9a4fdc57
pmtrilio <pm@trilio.bg>
2026-02-12 20:33:14 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Pipeline #392 (20-100).php
@@ -0,0 +1,81 @@
+     *
+     * @var \Illuminate\Contracts\Container\Container|null
+     */
+    protected $container;
+
+    /**
+     * The object being passed through the pipeline.
+     *
+     * @var mixed
+     */
+    protected $passable;
+
+    /**
+     * The array of class pipes.
+     *
+     * @var array
+     */
+    protected $pipes = [];
+
+    /**
+     * The method to call on each pipe.
+     *
+     * @var string
+     */
+    protected $method = 'handle';
+
+    /**
+     * The final callback to be executed after the pipeline ends regardless of the outcome.
+     *
+     * @var \Closure|null
+     */
+    protected $finally;
+
+    /**
+     * Indicates whether to wrap the pipeline in a database transaction.
+     *
+     * @var string|null|\UnitEnum|false
+     */
+    protected $withinTransaction = false;
+
+    /**
+     * Create a new class instance.
+     *
+     * @param  \Illuminate\Contracts\Container\Container|null  $container
+     */
+    public function __construct(?Container $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Set the object being sent through the pipeline.
+     *
+     * @param  mixed  $passable
+     * @return $this
+     */
+    public function send($passable)
+    {
+        $this->passable = $passable;
+
+        return $this;
+    }
+
+    /**
+     * Set the array of pipes.
+     *
+     * @param  mixed  $pipes
+     * @return $this
+     */
+    public function through($pipes)
+    {
+        $this->pipes = is_array($pipes) ? $pipes : func_get_args();
+
+        return $this;
+    }
+
+    /**
+     * Push additional pipes onto the pipeline.
+     *
+     * @param  mixed  $pipes
+     * @return $this
