43fb2fe6cc36f7ea8fc9c0edf484573a00597afb
pmtrilio <pm@trilio.bg>
2026-02-12 20:30:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: ArgvInput #275 (20-60).php
@@ -0,0 +1,41 @@
+ *
+ *     $input = new ArgvInput();
+ *
+ * By default, the `$_SERVER['argv']` array is used for the input values.
+ *
+ * This can be overridden by explicitly passing the input values in the constructor:
+ *
+ *     $input = new ArgvInput($_SERVER['argv']);
+ *
+ * If you pass it yourself, don't forget that the first element of the array
+ * is the name of the running application.
+ *
+ * When passing an argument to the constructor, be sure that it respects
+ * the same rules as the argv one. It's almost always better to use the
+ * `StringInput` when you want to provide your own input.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ *
+ * @see http://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html
+ * @see http://www.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap12.html#tag_12_02
+ */
+class ArgvInput extends Input
+{
+    private array $tokens;
+    private array $parsed;
+
+    public function __construct(?array $argv = null, ?InputDefinition $definition = null)
+    {
+        $argv ??= $_SERVER['argv'] ?? [];
+
+        // strip the application name
+        array_shift($argv);
+
+        $this->tokens = $argv;
+
+        parent::__construct($definition);
+    }
+
+    /**
+     * @return void
+     */
