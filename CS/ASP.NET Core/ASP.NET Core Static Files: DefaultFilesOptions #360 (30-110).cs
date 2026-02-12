eefd6390a4353008935ba06a711a85f94198d4d2
pmtrilio <pm@trilio.bg>
2026-02-12 19:16:54 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: DefaultFilesOptions #360 (30-110).cs
@@ -0,0 +1,13 @@
+            {
+                "default.htm",
+                "default.html",
+                "index.htm",
+                "index.html",
+            };
+    }
+
+    /// <summary>
+    /// An ordered list of file names to select by default. List length and ordering may affect performance.
+    /// </summary>
+    public IList<string> DefaultFileNames { get; set; }
+}
