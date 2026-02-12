bbb8433594afa93a16b6e17d9a66529f51671bff
pmtrilio <pm@trilio.bg>
2026-02-12 20:15:26 +0200

    Upload file via PHP cURL

---
+++ PY/Matplotlib/matplotlib: Pyplot #157 (5-45).py
@@ -0,0 +1,40 @@
+# because all changes will be overwritten by the next run of the script.
+# For more information see the description in boilerplate.py.
+
+"""
+`matplotlib.pyplot` is a state-based interface to matplotlib. It provides
+an implicit,  MATLAB-like, way of plotting.  It also opens figures on your
+screen, and acts as the figure GUI manager.
+
+pyplot is mainly intended for interactive plots and simple cases of
+programmatic plot generation::
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    x = np.arange(0, 5, 0.1)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.show()
+
+The explicit object-oriented API is recommended for complex plots, though
+pyplot is still usually used to create the figure and often the Axes in the
+figure. See `.pyplot.figure`, `.pyplot.subplots`, and
+`.pyplot.subplot_mosaic` to create figures, and
+:doc:`Axes API </api/axes_api>` for the plotting methods on an Axes::
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    x = np.arange(0, 5, 0.1)
+    y = np.sin(x)
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    plt.show()
+
+
+See :ref:`api_interfaces` for an explanation of the tradeoffs between the
+implicit and explicit interfaces.
+"""
+
+# fmt: off
