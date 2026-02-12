8992bec96831d680ca29bf0f840453296f892960
pmtrilio <pm@trilio.bg>
2026-02-12 14:44:59 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Queuemanager #326 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Illuminate\Queue;
+
+use Closure;
+use Illuminate\Contracts\Queue\Factory as FactoryContract;
+use Illuminate\Contracts\Queue\Monitor as MonitorContract;
+use Illuminate\Support\Queue\Concerns\ResolvesQueueRoutes;
+use InvalidArgumentException;
+
+/**
+ * @mixin \Illuminate\Contracts\Queue\Queue
+ */
+class QueueManager implements FactoryContract, MonitorContract
+{
+    use ResolvesQueueRoutes;
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The array of resolved queue connections.
+     *
+     * @var array
+     */
+    protected $connections = [];
+
+    /**
+     * The array of resolved queue connectors.
+     *
+     * @var array
+     */
+    protected $connectors = [];
+
+    /**
+     * Create a new queue manager instance.
+     *
