b16c7031d77ae9e52ff9ebbf5eb44c3f11274e81
pmtrilio <pm@trilio.bg>
2026-02-12 20:32:39 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Dispatcher #365 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\log;
+
+use Yii;
+use yii\base\Component;
+use yii\base\ErrorHandler;
+
+/**
+ * Dispatcher manages a set of [[Target|log targets]].
+ *
+ * Dispatcher implements the [[dispatch()]]-method that forwards the log messages from a [[Logger]] to
+ * the registered log [[targets]].
+ *
+ * An instance of Dispatcher is registered as a core application component and can be accessed using `Yii::$app->log`.
+ *
+ * You may configure the targets in application configuration, like the following:
+ *
+ * ```
+ * [
+ *     'components' => [
+ *         'log' => [
+ *             'targets' => [
+ *                 'file' => [
+ *                     'class' => 'yii\log\FileTarget',
+ *                     'levels' => ['trace', 'info'],
+ *                     'categories' => ['yii\*'],
+ *                 ],
+ *                 'email' => [
+ *                     'class' => 'yii\log\EmailTarget',
+ *                     'levels' => ['error', 'warning'],
+ *                     'message' => [
+ *                         'to' => 'admin@example.com',
+ *                     ],
+ *                 ],
+ *             ],
+ *         ],
+ *     ],
+ * ]
+ * ```
+ *
+ * Each log target can have a name and can be referenced via the [[targets]] property as follows:
+ *
+ * ```
+ * Yii::$app->log->targets['file']->enabled = false;
+ * ```
+ *
+ * @property int $flushInterval How many messages should be logged before they are sent to targets. See
+ * [[getFlushInterval()]] and [[setFlushInterval()]] for details.
+ * @property Logger $logger The logger. If not set, [[Yii::getLogger()]] will be used. Note that the type of
+ * this property differs in getter and setter. See [[getLogger()]] and [[setLogger()]] for details.
+ * @property int $traceLevel How many application call stacks should be logged together with each message.
+ * This method returns the value of [[Logger::traceLevel]]. Defaults to 0.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
