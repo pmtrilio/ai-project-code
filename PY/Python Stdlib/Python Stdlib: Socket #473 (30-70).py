2543d3b9b3c6458069f8f4990a14b91d6626d268
pmtrilio <pm@trilio.bg>
2026-02-12 20:21:10 +0200

    Upload file via PHP cURL

---
+++ PY/Python Stdlib/Python Stdlib: Socket #473 (30-70).py
@@ -0,0 +1,41 @@
+                       optional source address.
+create_server() -- create a TCP socket and bind it to a specified address.
+
+ [*] not available on all platforms!
+
+Special objects:
+
+SocketType -- type object for socket objects
+error -- exception raised for I/O errors
+has_ipv6 -- boolean value indicating if IPv6 is supported
+
+IntEnum constants:
+
+AF_INET, AF_UNIX -- socket domains (first argument to socket() call)
+SOCK_STREAM, SOCK_DGRAM, SOCK_RAW -- socket types (second argument)
+
+Integer constants:
+
+Many other constants may be defined; these may be used in calls to
+the setsockopt() and getsockopt() methods.
+"""
+
+import _socket
+from _socket import *
+
+import io
+import os
+import sys
+from enum import IntEnum, IntFlag
+from functools import partial
+
+try:
+    import errno
+except ImportError:
+    errno = None
+EBADF = getattr(errno, 'EBADF', 9)
+EAGAIN = getattr(errno, 'EAGAIN', 11)
+EWOULDBLOCK = getattr(errno, 'EWOULDBLOCK', 11)
+
+__all__ = ["fromfd", "getfqdn", "create_connection", "create_server",
+           "has_dualstack_ipv6", "AddressFamily", "SocketKind"]
