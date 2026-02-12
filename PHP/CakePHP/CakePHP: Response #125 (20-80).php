766336f04edef425bba0d161e929bd8ef47641c6
pmtrilio <pm@trilio.bg>
2026-02-12 15:42:14 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Response #125 (20-80).php
@@ -0,0 +1,61 @@
+use Laminas\Diactoros\MessageTrait;
+use Laminas\Diactoros\Stream;
+use Psr\Http\Message\ResponseInterface;
+use SimpleXMLElement;
+
+/**
+ * Implements methods for HTTP responses.
+ *
+ * All the following examples assume that `$response` is an
+ * instance of this class.
+ *
+ * ### Get header values
+ *
+ * Header names are case-insensitive, but normalized to Title-Case
+ * when the response is parsed.
+ *
+ * ```
+ * $val = $response->getHeaderLine('content-type');
+ * ```
+ *
+ * Will read the Content-Type header. You can get all set
+ * headers using:
+ *
+ * ```
+ * $response->getHeaders();
+ * ```
+ *
+ * ### Get the response body
+ *
+ * You can access the response body stream using:
+ *
+ * ```
+ * $content = $response->getBody();
+ * ```
+ *
+ * You can get the body string using:
+ *
+ * ```
+ * $content = $response->getStringBody();
+ * ```
+ *
+ * If your response body is in XML or JSON you can use
+ * special content type specific accessors to read the decoded data.
+ * JSON data will be returned as arrays, while XML data will be returned
+ * as SimpleXML nodes:
+ *
+ * ```
+ * // Get as XML
+ * $content = $response->getXml()
+ * // Get as JSON
+ * $content = $response->getJson()
+ * ```
+ *
+ * If the response cannot be decoded, null will be returned.
+ *
+ * ### Check the status code
+ *
+ * You can access the response status code using:
+ *
+ * ```
+ * $content = $response->getStatusCode();
