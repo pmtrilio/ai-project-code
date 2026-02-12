1e862b60a4e7436913c45ce162517918ef88125d
pmtrilio <pm@trilio.bg>
2026-02-12 20:02:42 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: View #633 (5-65).php
@@ -0,0 +1,61 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\base;
+
+use Yii;
+use yii\helpers\FileHelper;
+use yii\widgets\Block;
+use yii\widgets\ContentDecorator;
+use yii\widgets\FragmentCache;
+
+/**
+ * View represents a view object in the MVC pattern.
+ *
+ * View provides a set of methods (e.g. [[render()]]) for rendering purpose.
+ *
+ * For more details and usage information on View, see the [guide article on views](guide:structure-views).
+ *
+ * @property-read DynamicContentAwareInterface[] $dynamicContents Class instances supporting dynamic contents.
+ * @property-read string|bool $viewFile The view file currently being rendered. False if no view file is being
+ * rendered.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class View extends Component implements DynamicContentAwareInterface
+{
+    /**
+     * @event Event an event that is triggered by [[beginPage()]].
+     */
+    public const EVENT_BEGIN_PAGE = 'beginPage';
+    /**
+     * @event Event an event that is triggered by [[endPage()]].
+     */
+    public const EVENT_END_PAGE = 'endPage';
+    /**
+     * @event ViewEvent an event that is triggered by [[renderFile()]] right before it renders a view file.
+     */
+    public const EVENT_BEFORE_RENDER = 'beforeRender';
+    /**
+     * @event ViewEvent an event that is triggered by [[renderFile()]] right after it renders a view file.
+     */
+    public const EVENT_AFTER_RENDER = 'afterRender';
+    /**
+     * @var ViewContextInterface the context under which the [[renderFile()]] method is being invoked.
+     */
+    public $context;
+    /**
+     * @var array custom parameters that are shared among view templates.
+     */
+    public $params = [];
+    /**
+     * @var array|null a list of available renderers indexed by their corresponding supported file extensions.
+     * Each renderer may be a view renderer object or the configuration for creating the renderer object.
+     * For example, the following configuration enables both Smarty and Twig view renderers:
+     *
+     * ```
+     * [
+     *     'tpl' => ['class' => 'yii\smarty\ViewRenderer'],
+     *     'twig' => ['class' => 'yii\twig\ViewRenderer'],
