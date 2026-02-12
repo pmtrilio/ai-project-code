894d0c2e9c6e9f8d4f84035f8558081a350ee4ac
pmtrilio <pm@trilio.bg>
2026-02-12 14:28:54 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Validator #19 (20-100).php
@@ -0,0 +1,81 @@
+use RuntimeException;
+use stdClass;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class Validator implements ValidatorContract
+{
+    use Concerns\FormatsMessages,
+        Concerns\ValidatesAttributes;
+
+    /**
+     * The Translator implementation.
+     *
+     * @var \Illuminate\Contracts\Translation\Translator
+     */
+    protected $translator;
+
+    /**
+     * The container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The Presence Verifier implementation.
+     *
+     * @var \Illuminate\Validation\PresenceVerifierInterface
+     */
+    protected $presenceVerifier;
+
+    /**
+     * The failed validation rules.
+     *
+     * @var array
+     */
+    protected $failedRules = [];
+
+    /**
+     * Attributes that should be excluded from the validated data.
+     *
+     * @var array
+     */
+    protected $excludeAttributes = [];
+
+    /**
+     * The message bag instance.
+     *
+     * @var \Illuminate\Support\MessageBag
+     */
+    protected $messages;
+
+    /**
+     * The data under validation.
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * The initial rules provided.
+     *
+     * @var array
+     */
+    protected $initialRules;
+
+    /**
+     * The rules to be applied to the data.
+     *
+     * @var array
+     */
+    protected $rules;
+
+    /**
+     * The current rule that is validating.
+     *
+     * @var string
+     */
+    protected $currentRule;
+
+    /**
+     * The array of wildcard attributes with their asterisks expanded.
