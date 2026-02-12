4968dadc318a534decdb55265b4123e4b22c2167
pmtrilio <pm@trilio.bg>
2026-02-12 15:02:29 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel Scout Model (178-245).php
@@ -0,0 +1,15 @@
+
+        return $result;
+    }
+
+    /**
+     * Determine if the given model uses soft deletes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return bool
+     */
+    protected function usesSoftDelete($model)
+    {
+        return in_array(SoftDeletes::class, class_uses_recursive($model));
+    }
+}
