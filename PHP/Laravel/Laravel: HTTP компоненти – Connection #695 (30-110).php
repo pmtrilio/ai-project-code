1b85cb3d514967062e3ebe1937f8e35e1610fd70
pmtrilio <pm@trilio.bg>
2026-02-12 19:09:38 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Connection #695 (30-110).php
@@ -0,0 +1,81 @@
+class Connection implements ConnectionInterface
+{
+    use DetectsConcurrencyErrors,
+        DetectsLostConnections,
+        Concerns\ManagesTransactions,
+        InteractsWithTime,
+        Macroable;
+
+    /**
+     * The active PDO connection.
+     *
+     * @var \PDO|(\Closure(): \PDO)
+     */
+    protected $pdo;
+
+    /**
+     * The active PDO connection used for reads.
+     *
+     * @var \PDO|(\Closure(): \PDO)
+     */
+    protected $readPdo;
+
+    /**
+     * The database connection configuration options for reading.
+     *
+     * @var array
+     */
+    protected $readPdoConfig = [];
+
+    /**
+     * The name of the connected database.
+     *
+     * @var string
+     */
+    protected $database;
+
+    /**
+     * The type of the connection.
+     *
+     * @var string|null
+     */
+    protected $readWriteType;
+
+    /**
+     * The table prefix for the connection.
+     *
+     * @var string
+     */
+    protected $tablePrefix = '';
+
+    /**
+     * The database connection configuration options.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * The reconnector instance for the connection.
+     *
+     * @var (callable(\Illuminate\Database\Connection): mixed)
+     */
+    protected $reconnector;
+
+    /**
+     * The query grammar implementation.
+     *
+     * @var \Illuminate\Database\Query\Grammars\Grammar
+     */
+    protected $queryGrammar;
+
+    /**
+     * The schema grammar implementation.
+     *
+     * @var \Illuminate\Database\Schema\Grammars\Grammar
+     */
+    protected $schemaGrammar;
+
+    /**
+     * The query post processor implementation.
+     *
