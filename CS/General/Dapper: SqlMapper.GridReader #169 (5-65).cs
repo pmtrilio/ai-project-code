9b3948b296b3d5537667bf1b253c8e9ad5e2998a
pmtrilio <pm@trilio.bg>
2026-02-12 20:05:46 +0200

    Upload file via PHP cURL

---
+++ CS/General/Dapper: SqlMapper.GridReader #169 (5-65).cs
@@ -0,0 +1,61 @@
+using System.Data.Common;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Dapper
+{
+    public static partial class SqlMapper
+    {
+        /// <summary>
+        /// The grid reader provides interfaces for reading multiple result sets from a Dapper query
+        /// </summary>
+        public partial class GridReader : IDisposable
+        {
+            private DbDataReader reader;
+            private Identity? _identity;
+            private readonly bool addToCache;
+            private readonly Action<object?>? onCompleted;
+            private readonly object? state;
+            private readonly CancellationToken cancel;
+
+            /// <summary>
+            /// Creates a grid reader over an existing command and reader
+            /// </summary>
+            [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+            protected GridReader(IDbCommand command, DbDataReader reader, Identity? identity, Action<object?>? onCompleted = null, object? state = null, bool addToCache = false, CancellationToken cancellationToken = default)
+            {
+                Command = command;
+                this.reader = reader;
+                _identity = identity;
+                this.onCompleted = onCompleted;
+                this.state = state;
+                this.addToCache = addToCache;
+                cancel = cancellationToken;
+            }
+
+            internal GridReader(IDbCommand command, DbDataReader reader, Identity identity, IParameterCallbacks? callbacks, bool addToCache,
+                CancellationToken cancellationToken = default)
+                : this(command, reader, identity, callbacks is null ? null : static state => ((IParameterCallbacks)state!).OnCompleted(),
+                      callbacks, addToCache, cancellationToken)
+            { }
+
+            private Identity Identity => _identity ??= CreateIdentity();
+
+            private Identity CreateIdentity()
+            {
+                var cmd = Command;
+                if (cmd is not null && cmd.Connection is not null)
+                {
+                    return new Identity(cmd.CommandText, cmd.CommandType, cmd.Connection, null, null);
+                }
+                throw new InvalidOperationException("This operation requires an identity or a connected command");
+            }
+
+            /// <summary>
+            /// Read the next grid of results, returned as a dynamic object.
+            /// </summary>
+            /// <param name="buffered">Whether the results should be buffered in memory.</param>
+            /// <remarks>Note: each row can be accessed via "dynamic", or by casting to an IDictionary&lt;string,object&gt;</remarks>
+            public IEnumerable<dynamic> Read(bool buffered = true) => ReadImpl<dynamic>(typeof(DapperRow), buffered);
