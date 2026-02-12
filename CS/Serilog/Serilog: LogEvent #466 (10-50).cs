3a1e7765c5c39827c70a4f7ea9015940bce15853
pmtrilio <pm@trilio.bg>
2026-02-12 20:08:54 +0200

    Upload file via PHP cURL

---
+++ CS/Serilog/Serilog: LogEvent #466 (10-50).cs
@@ -0,0 +1,41 @@
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+// ReSharper disable IntroduceOptionalParameters.Global
+
+namespace Serilog.Events;
+
+/// <summary>
+/// A log event.
+/// </summary>
+public class LogEvent
+{
+    readonly Dictionary<string, LogEventPropertyValue> _properties;
+    ActivityTraceId _traceId;
+    ActivitySpanId _spanId;
+
+    LogEvent(
+        DateTimeOffset timestamp,
+        LogEventLevel level,
+        Exception? exception,
+        MessageTemplate messageTemplate,
+        Dictionary<string, LogEventPropertyValue> properties,
+        ActivityTraceId traceId,
+        ActivitySpanId spanId)
+    {
+        Timestamp = timestamp;
+        Level = level;
+        Exception = exception;
+        _traceId = traceId;
+        _spanId = spanId;
+        MessageTemplate = Guard.AgainstNull(messageTemplate);
+        _properties = Guard.AgainstNull(properties);
+    }
+
+    /// <summary>
+    /// Construct a new <seealso cref="LogEvent"/>.
+    /// </summary>
+    /// <param name="timestamp">The time at which the event occurred.</param>
