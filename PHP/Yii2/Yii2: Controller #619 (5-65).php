fd6491ba4bc6be9146a4c4c1685f57f5f64899e6
pmtrilio <pm@trilio.bg>
2026-02-12 21:24:08 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Controller #619 (5-65).php
@@ -0,0 +1,61 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\rest;
+
+use Yii;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\RateLimiter;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+use yii\web\Controller as WebController;
+use yii\base\Module;
+
+/**
+ * Controller is the base class for RESTful API controller classes.
+ *
+ * Controller implements the following steps in a RESTful API request handling cycle:
+ *
+ * 1. Resolving response format (see [[ContentNegotiator]]);
+ * 2. Validating request method (see [[verbs()]]).
+ * 3. Authenticating user (see [[\yii\filters\auth\AuthInterface]]);
+ * 4. Rate limiting (see [[RateLimiter]]);
+ * 5. Formatting response data (see [[serializeData()]]).
+ *
+ * For more details and usage information on Controller, see the [guide article on rest controllers](guide:rest-controllers).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ *
+ * @template T of Module
+ * @extends WebController<T>
+ */
+class Controller extends WebController
+{
+    /**
+     * @var string|array the configuration for creating the serializer that formats the response data.
+     */
+    public $serializer = 'yii\rest\Serializer';
+    /**
+     * {@inheritdoc}
+     */
+    public $enableCsrfValidation = false;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::className(),
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
+            'verbFilter' => [
+                'class' => VerbFilter::className(),
