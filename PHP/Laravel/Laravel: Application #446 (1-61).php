3956628a6f18b857f36b778502498f72949b3da2
pmtrilio <pm@trilio.bg>
2026-02-12 20:34:20 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Application #446 (1-61).php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Illuminate\Console;
+
+use Closure;
+use Illuminate\Console\Events\ArtisanStarting;
+use Illuminate\Contracts\Console\Application as ApplicationContract;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\ProcessUtils;
+use ReflectionClass;
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+use function Illuminate\Support\artisan_binary;
+use function Illuminate\Support\php_binary;
+
+class Application extends SymfonyApplication implements ApplicationContract
+{
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $laravel;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The output from the previous command.
+     *
+     * @var \Symfony\Component\Console\Output\BufferedOutput
+     */
+    protected $lastOutput;
+
+    /**
+     * The console application bootstrappers.
+     *
+     * @var array<array-key, \Closure($this): void>
+     */
+    protected static $bootstrappers = [];
+
+    /**
+     * A map of command names to classes.
+     *
+     * @var array
+     */
+    protected $commandMap = [];
