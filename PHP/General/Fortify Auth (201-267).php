9c898b5c143f3c77f14d58a7fe58e7ffd17c951c
pmtrilio <pm@trilio.bg>
2026-02-12 19:11:23 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Fortify Auth (201-267).php
@@ -0,0 +1,67 @@
+        app()->singleton(RequestPasswordResetLinkViewResponse::class, function () use ($view) {
+            return new SimpleViewResponse($view);
+        });
+    }
+
+    /**
+     * Register a callback that is responsible for building the authentication pipeline array.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public static function loginThrough(callable $callback)
+    {
+        static::authenticateThrough($callback);
+    }
+
+    /**
+     * Register a callback that is responsible for building the authentication pipeline array.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public static function authenticateThrough(callable $callback)
+    {
+        static::$authenticateThroughCallback = $callback;
+    }
+
+    /**
+     * Register a callback that is responsible for validating incoming authentication credentials.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public static function authenticateUsing(callable $callback)
+    {
+        static::$authenticateUsingCallback = $callback;
+    }
+
+    /**
+     * Register a callback that is responsible for confirming existing user passwords as valid.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public static function confirmPasswordsUsing(callable $callback)
+    {
+        static::$confirmPasswordsUsingCallback = $callback;
+    }
+
+    /**
+     * Register a class / callback that should be used to create new users.
+     *
+     * @param  string  $callback
+     * @return void
+     */
+    public static function createUsersUsing(string $callback)
+    {
+        app()->singleton(CreatesNewUsers::class, $callback);
+    }
+
+    /**
+     * Register a class / callback that should be used to update user profile information.
+     *
+     * @param  string  $callback
+     * @return void
+     */
+    public static function updateUserProfileInformationUsing(string $callback)
