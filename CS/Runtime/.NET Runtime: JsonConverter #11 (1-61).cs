e185ae9a5a63419a0756a3a8fe6368df3038db07
pmtrilio <pm@trilio.bg>
2026-02-12 15:03:20 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: JsonConverter #11 (1-61).cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Json.Schema;
+using System.Text.Json.Serialization.Converters;
+using System.Text.Json.Serialization.Metadata;
+
+namespace System.Text.Json.Serialization
+{
+    /// <summary>
+    /// Converts an object or value to or from JSON.
+    /// </summary>
+    public abstract partial class JsonConverter
+    {
+        internal JsonConverter()
+        {
+            IsInternalConverter = GetType().Assembly == typeof(JsonConverter).Assembly;
+            ConverterStrategy = GetDefaultConverterStrategy();
+        }
+
+        /// <summary>
+        /// Gets the type being converted by the current converter instance.
+        /// </summary>
+        /// <remarks>
+        /// For instances of type <see cref="JsonConverter{T}"/> returns typeof(T),
+        /// and for instances of type <see cref="JsonConverterFactory"/> returns <see langword="null" />.
+        /// </remarks>
+        public abstract Type? Type { get; }
+
+        /// <summary>
+        /// Determines whether the type can be converted.
+        /// </summary>
+        /// <param name="typeToConvert">The type is checked as to whether it can be converted.</param>
+        /// <returns>True if the type can be converted, false otherwise.</returns>
+        public abstract bool CanConvert(Type typeToConvert);
+
+        internal ConverterStrategy ConverterStrategy
+        {
+            get => _converterStrategy;
+            init
+            {
+                CanUseDirectReadOrWrite = value == ConverterStrategy.Value && IsInternalConverter;
+                RequiresReadAhead = value == ConverterStrategy.Value;
+                _converterStrategy = value;
+            }
+        }
+
+        private ConverterStrategy _converterStrategy;
+
+        /// <summary>
+        /// Invoked by the base contructor to populate the initial value of the <see cref="ConverterStrategy"/> property.
+        /// Used for declaring the default strategy for specific converter hierarchies without explicitly setting in a constructor.
+        /// </summary>
+        private protected abstract ConverterStrategy GetDefaultConverterStrategy();
+
+        /// <summary>
+        /// Indicates that the converter can consume the <see cref="JsonTypeInfo.CreateObject"/> delegate.
+        /// Needed because certain collection converters cannot support arbitrary delegates.
