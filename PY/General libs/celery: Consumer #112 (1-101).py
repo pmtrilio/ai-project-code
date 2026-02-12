bf856b8d80036af33780a268112e1ca72f853185
pmtrilio <pm@trilio.bg>
2026-02-12 19:32:20 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/celery: Consumer #112 (1-101).py
@@ -0,0 +1,101 @@
+"""Worker Consumer Blueprint.
+
+This module contains the components responsible for consuming messages
+from the broker, processing the messages and keeping the broker connections
+up and running.
+"""
+import errno
+import logging
+import os
+import warnings
+from collections import defaultdict
+from time import sleep
+
+from billiard.common import restart_state
+from billiard.exceptions import RestartFreqExceeded
+from kombu.asynchronous.semaphore import DummyLock
+from kombu.exceptions import ContentDisallowed, DecodeError
+from kombu.utils.compat import _detect_environment
+from kombu.utils.encoding import safe_repr
+from kombu.utils.limits import TokenBucket
+from vine import ppartial, promise
+
+from celery import bootsteps, signals
+from celery.app.trace import build_tracer
+from celery.exceptions import (CPendingDeprecationWarning, InvalidTaskError, NotRegistered, WorkerShutdown,
+                               WorkerTerminate)
+from celery.utils.functional import noop
+from celery.utils.log import get_logger
+from celery.utils.nodenames import gethostname
+from celery.utils.objects import Bunch
+from celery.utils.text import truncate
+from celery.utils.time import humanize_seconds, rate
+from celery.worker import loops
+from celery.worker.state import (active_requests, maybe_shutdown, requests, reserved_requests, successful_requests,
+                                 task_reserved)
+
+__all__ = ('Consumer', 'Evloop', 'dump_body')
+
+CLOSE = bootsteps.CLOSE
+TERMINATE = bootsteps.TERMINATE
+STOP_CONDITIONS = {CLOSE, TERMINATE}
+logger = get_logger(__name__)
+debug, info, warn, error, crit = (logger.debug, logger.info, logger.warning,
+                                  logger.error, logger.critical)
+
+CONNECTION_RETRY = """\
+consumer: Connection to broker lost. \
+Trying to re-establish the connection...\
+"""
+
+CONNECTION_RETRY_STEP = """\
+Trying again {when}... ({retries}/{max_retries})\
+"""
+
+CONNECTION_ERROR = """\
+consumer: Cannot connect to %s: %s.
+%s
+"""
+
+CONNECTION_FAILOVER = """\
+Will retry using next failover.\
+"""
+
+UNKNOWN_FORMAT = """\
+Received and deleted unknown message.  Wrong destination?!?
+
+The full contents of the message body was: %s
+"""
+
+#: Error message for when an unregistered task is received.
+UNKNOWN_TASK_ERROR = """\
+Received unregistered task of type %s.
+The message has been ignored and discarded.
+
+Did you remember to import the module containing this task?
+Or maybe you're using relative imports?
+
+Please see
+https://docs.celeryq.dev/en/latest/internals/protocol.html
+for more information.
+
+The full contents of the message body was:
+%s
+
+The full contents of the message headers:
+%s
+
+The delivery info for this task is:
+%s
+"""
+
+#: Error message for when an invalid task message is received.
+INVALID_TASK_ERROR = """\
+Received invalid task message: %s
+The message has been ignored and discarded.
+
+Please ensure your message conforms to the task
+message protocol as described here:
+https://docs.celeryq.dev/en/latest/internals/protocol.html
+
+The full contents of the message body was:
