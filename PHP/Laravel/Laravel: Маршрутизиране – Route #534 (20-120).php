89ec65cd46ada96156759327ce9c7a5cf5851205
pmtrilio <pm@trilio.bg>
2026-02-12 19:06:25 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Маршрутизиране – Route #534 (20-120).php
@@ -0,0 +1,101 @@
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
+use Laravel\SerializableClosure\SerializableClosure;
+use LogicException;
+use Symfony\Component\Routing\Route as SymfonyRoute;
+
+use function Illuminate\Support\enum_value;
+
+class Route
+{
+    use Conditionable, CreatesRegularExpressionRouteConstraints, FiltersControllerMiddleware, Macroable, ResolvesRouteDependencies;
+
+    /**
+     * The URI pattern the route responds to.
+     *
+     * @var string
+     */
+    public $uri;
+
+    /**
+     * The HTTP methods the route responds to.
+     *
+     * @var array
+     */
+    public $methods;
+
+    /**
+     * The route action array.
+     *
+     * @var array
+     */
+    public $action;
+
+    /**
+     * Indicates whether the route is a fallback route.
+     *
+     * @var bool
+     */
+    public $isFallback = false;
+
+    /**
+     * The controller instance.
+     *
+     * @var mixed
+     */
+    public $controller;
+
+    /**
+     * The default values for the route.
+     *
+     * @var array
+     */
+    public $defaults = [];
+
+    /**
+     * The regular expression requirements.
+     *
+     * @var array
+     */
+    public $wheres = [];
+
+    /**
+     * The array of matched parameters.
+     *
+     * @var array|null
+     */
+    public $parameters;
+
+    /**
+     * The parameter names for the route.
+     *
+     * @var array|null
+     */
+    public $parameterNames;
+
+    /**
+     * The array of the matched parameters' original values.
+     *
+     * @var array
+     */
+    protected $originalParameters;
+
+    /**
+     * Indicates "trashed" models can be retrieved when resolving implicit model bindings for this route.
+     *
+     * @var bool
+     */
+    protected $withTrashedBindings = false;
+
+    /**
+     * Indicates the maximum number of seconds the route should acquire a session lock for.
+     *
+     * @var int|null
+     */
+    protected $lockSeconds;
+
+    /**
+     * Indicates the maximum number of seconds the route should wait while attempting to acquire a session lock.
+     *
