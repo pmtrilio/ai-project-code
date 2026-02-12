296094aaff3f087f32430081acfea2a6c068e735
pmtrilio <pm@trilio.bg>
2026-02-12 15:06:10 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: RazorView #222 (10-50).cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Mvc.Razor;
+
+/// <summary>
+/// Default implementation for <see cref="IView"/> that executes one or more <see cref="IRazorPage"/>
+/// as parts of its execution.
+/// </summary>
+[DebuggerDisplay("{Path,nq}")]
+public class RazorView : IView
+{
+    private readonly IRazorViewEngine _viewEngine;
+    private readonly IRazorPageActivator _pageActivator;
+    private readonly HtmlEncoder _htmlEncoder;
+    private readonly DiagnosticListener _diagnosticListener;
+    private IViewBufferScope? _bufferScope;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="RazorView"/>
+    /// </summary>
+    /// <param name="viewEngine">The <see cref="IRazorViewEngine"/> used to locate Layout pages.</param>
+    /// <param name="pageActivator">The <see cref="IRazorPageActivator"/> used to activate pages.</param>
+    /// <param name="viewStartPages">The sequence of <see cref="IRazorPage" /> instances executed as _ViewStarts.
+    /// </param>
+    /// <param name="razorPage">The <see cref="IRazorPage"/> instance to execute.</param>
+    /// <param name="htmlEncoder">The HTML encoder.</param>
+    /// <param name="diagnosticListener">The <see cref="DiagnosticListener"/>.</param>
+    public RazorView(
+        IRazorViewEngine viewEngine,
+        IRazorPageActivator pageActivator,
+        IReadOnlyList<IRazorPage> viewStartPages,
+        IRazorPage razorPage,
+        HtmlEncoder htmlEncoder,
+        DiagnosticListener diagnosticListener)
+    {
+        ArgumentNullException.ThrowIfNull(viewEngine);
+        ArgumentNullException.ThrowIfNull(pageActivator);
+        ArgumentNullException.ThrowIfNull(viewStartPages);
+        ArgumentNullException.ThrowIfNull(razorPage);
+        ArgumentNullException.ThrowIfNull(htmlEncoder);
+        ArgumentNullException.ThrowIfNull(diagnosticListener);
