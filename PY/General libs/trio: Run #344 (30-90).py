0a787e33c46464b8d6ac5ea80c964ca8a0b5ee43
pmtrilio <pm@trilio.bg>
2026-02-12 19:37:25 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/trio: Run #344 (30-90).py
@@ -0,0 +1,61 @@
+from sortedcontainers import SortedDict
+
+from .. import _core
+from .._abc import Clock, Instrument
+from .._deprecate import warn_deprecated
+from .._util import NoPublicConstructor, coroutine_or_error, final
+from ._asyncgens import AsyncGenerators
+from ._concat_tb import concat_tb
+from ._entry_queue import EntryQueue, TrioToken
+from ._exceptions import (
+    Cancelled,
+    CancelReasonLiteral,
+    RunFinishedError,
+    TrioInternalError,
+)
+from ._instrumentation import Instruments
+from ._ki import KIManager, enable_ki_protection
+from ._parking_lot import GLOBAL_PARKING_LOT_BREAKER
+from ._run_context import GLOBAL_RUN_CONTEXT as GLOBAL_RUN_CONTEXT
+from ._thread_cache import start_thread_soon
+from ._traps import (
+    Abort,
+    CancelShieldedCheckpoint,
+    PermanentlyDetachCoroutineObject,
+    WaitTaskRescheduled,
+    cancel_shielded_checkpoint,
+    wait_task_rescheduled,
+)
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
+
+
+if TYPE_CHECKING:
+    import contextvars
+    import types
+    from collections.abc import (
+        Awaitable,
+        Callable,
+        Generator,
+        Iterator,
+        Sequence,
+    )
+    from types import TracebackType
+
+    # for some strange reason Sphinx works with outcome.Outcome, but not Outcome, in
+    # start_guest_run. Same with types.FrameType in iter_await_frames
+    import outcome
+    from typing_extensions import Self, TypeVar, TypeVarTuple, Unpack
+
+    PosArgT = TypeVarTuple("PosArgT")
+    StatusT = TypeVar("StatusT", default=None)
+    StatusT_contra = TypeVar("StatusT_contra", contravariant=True, default=None)
+    BaseExcT = TypeVar("BaseExcT", bound=BaseException)
+else:
+    from typing import TypeVar
+
+    StatusT = TypeVar("StatusT")
+    StatusT_contra = TypeVar("StatusT_contra", contravariant=True)
+
+RetT = TypeVar("RetT")
