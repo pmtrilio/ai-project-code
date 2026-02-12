44239aea18dd834a7871ad0d3d3d550b48e5a76c
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:32 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: RequestResponse #504 (10-70).php
@@ -0,0 +1,32 @@
+
+namespace Slim\Handlers\Strategies;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Interfaces\InvocationStrategyInterface;
+
+/**
+ * Default route callback strategy with route parameters as an array of arguments.
+ */
+class RequestResponse implements InvocationStrategyInterface
+{
+    /**
+     * Invoke a route callable with request, response, and all route parameters
+     * as an array of arguments.
+     *
+     * @param array<string, string>  $routeArguments
+     */
+    public function __invoke(
+        callable $callable,
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $routeArguments
+    ): ResponseInterface {
+        foreach ($routeArguments as $k => $v) {
+            $request = $request->withAttribute($k, $v);
+        }
+
+        /** @var ResponseInterface */
+        return $callable($request, $response, $routeArguments);
+    }
+}
