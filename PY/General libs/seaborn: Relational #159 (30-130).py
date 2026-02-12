9ec52361a041a7f672e64f20fc1de1ef1be912cc
pmtrilio <pm@trilio.bg>
2026-02-12 20:15:29 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/seaborn: Relational #159 (30-130).py
@@ -0,0 +1,101 @@
+
+    # ---  Introductory prose
+    main_api="""
+The relationship between `x` and `y` can be shown for different subsets
+of the data using the `hue`, `size`, and `style` parameters. These
+parameters control what visual semantics are used to identify the different
+subsets. It is possible to show up to three dimensions independently by
+using all three semantic types, but this style of plot can be hard to
+interpret and is often ineffective. Using redundant semantics (i.e. both
+`hue` and `style` for the same variable) can be helpful for making
+graphics more accessible.
+
+See the :ref:`tutorial <relational_tutorial>` for more information.
+    """,
+
+    relational_semantic="""
+The default treatment of the `hue` (and to a lesser extent, `size`)
+semantic, if present, depends on whether the variable is inferred to
+represent "numeric" or "categorical" data. In particular, numeric variables
+are represented with a sequential colormap by default, and the legend
+entries show regular "ticks" with values that may or may not exist in the
+data. This behavior can be controlled through various parameters, as
+described and illustrated below.
+    """,
+))
+
+_relational_docs = dict(
+
+    # --- Shared function parameters
+    data_vars="""
+x, y : names of variables in `data` or vector data
+    Input data variables; must be numeric. Can pass data directly or
+    reference columns in `data`.
+    """,
+    data="""
+data : DataFrame, array, or list of arrays
+    Input data structure. If `x` and `y` are specified as names, this
+    should be a "long-form" DataFrame containing those columns. Otherwise
+    it is treated as "wide-form" data and grouping variables are ignored.
+    See the examples for the various ways this parameter can be specified
+    and the different effects of each.
+    """,
+    palette="""
+palette : string, list, dict, or matplotlib colormap
+    An object that determines how colors are chosen when `hue` is used.
+    It can be the name of a seaborn palette or matplotlib colormap, a list
+    of colors (anything matplotlib understands), a dict mapping levels
+    of the `hue` variable to colors, or a matplotlib colormap object.
+    """,
+    hue_order="""
+hue_order : list
+    Specified order for the appearance of the `hue` variable levels,
+    otherwise they are determined from the data. Not relevant when the
+    `hue` variable is numeric.
+    """,
+    hue_norm="""
+hue_norm : tuple or :class:`matplotlib.colors.Normalize` object
+    Normalization in data units for colormap applied to the `hue`
+    variable when it is numeric. Not relevant if `hue` is categorical.
+    """,
+    sizes="""
+sizes : list, dict, or tuple
+    An object that determines how sizes are chosen when `size` is used.
+    List or dict arguments should provide a size for each unique data value,
+    which forces a categorical interpretation. The argument may also be a
+    min, max tuple.
+    """,
+    size_order="""
+size_order : list
+    Specified order for appearance of the `size` variable levels,
+    otherwise they are determined from the data. Not relevant when the
+    `size` variable is numeric.
+    """,
+    size_norm="""
+size_norm : tuple or Normalize object
+    Normalization in data units for scaling plot objects when the
+    `size` variable is numeric.
+    """,
+    dashes="""
+dashes : boolean, list, or dictionary
+    Object determining how to draw the lines for different levels of the
+    `style` variable. Setting to `True` will use default dash codes, or
+    you can pass a list of dash codes or a dictionary mapping levels of the
+    `style` variable to dash codes. Setting to `False` will use solid
+    lines for all subsets. Dashes are specified as in matplotlib: a tuple
+    of `(segment, gap)` lengths, or an empty string to draw a solid line.
+    """,
+    markers="""
+markers : boolean, list, or dictionary
+    Object determining how to draw the markers for different levels of the
+    `style` variable. Setting to `True` will use default markers, or
+    you can pass a list of markers or a dictionary mapping levels of the
+    `style` variable to markers. Setting to `False` will draw
+    marker-less lines.  Markers are specified as in matplotlib.
+    """,
+    style_order="""
+style_order : list
+    Specified order for appearance of the `style` variable levels
+    otherwise they are determined from the data. Not relevant when the
+    `style` variable is numeric.
+    """,
