b8e9c1b361b0830ff0aebb1d0c1686ed7ef37e06
pmtrilio <pm@trilio.bg>
2026-02-12 15:08:27 +0200

    Upload file via PHP cURL

---
+++ CS/General/Dapper: SqlMapper #391 (1-81).cs
@@ -0,0 +1,81 @@
+﻿/*
+ License: http://www.apache.org/licenses/LICENSE-2.0
+ Home page: https://github.com/DapperLib/Dapper-dot-net
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Dapper
+{
+    /// <summary>
+    /// Dapper, a light weight object mapper for ADO.NET
+    /// </summary>
+    public static partial class SqlMapper
+    {
+        private class PropertyInfoByNameComparer : IComparer<PropertyInfo>
+        {
+            public int Compare(PropertyInfo? x, PropertyInfo? y) => string.CompareOrdinal(x?.Name, y?.Name);
+        }
+        private static int GetColumnHash(DbDataReader reader, int startBound = 0, int length = -1)
+        {
+            unchecked
+            {
+                int max = length < 0 ? reader.FieldCount : startBound + length;
+                int hash = (-37 * startBound) + max;
+                for (int i = startBound; i < max; i++)
+                {
+                    object tmp = reader.GetName(i);
+                    hash = (-79 * ((hash * 31) + (tmp?.GetHashCode() ?? 0))) + (reader.GetFieldType(i)?.GetHashCode() ?? 0);
+                }
+                return hash;
+            }
+        }
+
+        /// <summary>
+        /// Called if the query cache is purged via PurgeQueryCache
+        /// </summary>
+        public static event EventHandler? QueryCachePurged;
+        private static void OnQueryCachePurged()
+        {
+            var handler = QueryCachePurged;
+            handler?.Invoke(null, EventArgs.Empty);
+        }
+
+        private static readonly System.Collections.Concurrent.ConcurrentDictionary<Identity, CacheInfo> _queryCache = new();
+        private static void SetQueryCache(Identity key, CacheInfo value)
+        {
+            if (Interlocked.Increment(ref collect) == COLLECT_PER_ITEMS)
+            {
+                CollectCacheGarbage();
+            }
+            _queryCache[key] = value;
+        }
+
+        private static void CollectCacheGarbage()
+        {
+            try
+            {
+                foreach (var pair in _queryCache)
+                {
+                    if (pair.Value.GetHitCount() <= COLLECT_HIT_COUNT_MIN)
+                    {
+                        _queryCache.TryRemove(pair.Key, out var _);
+                    }
+                }
