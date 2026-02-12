75de69b180cd0528dee035244bdbcb8895cce7bc
pmtrilio <pm@trilio.bg>
2026-02-12 15:40:44 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: AssetBundle #42 (5-105).php
@@ -0,0 +1,101 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+use Yii;
+use yii\base\BaseObject;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+
+/**
+ * AssetBundle represents a collection of asset files, such as CSS, JS, images.
+ *
+ * Each asset bundle has a unique name that globally identifies it among all asset bundles used in an application.
+ * The name is the [fully qualified class name](https://www.php.net/manual/en/language.namespaces.rules.php)
+ * of the class representing it.
+ *
+ * An asset bundle can depend on other asset bundles. When registering an asset bundle
+ * with a view, all its dependent asset bundles will be automatically registered.
+ *
+ * For more details and usage information on AssetBundle, see the [guide article on assets](guide:structure-assets).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ *
+ * @phpstan-import-type RegisterJsFileOptions from View
+ * @phpstan-import-type RegisterCssFileOptions from View
+ * @phpstan-import-type PublishOptions from AssetManager
+ */
+class AssetBundle extends BaseObject
+{
+    /**
+     * @var string|null the directory that contains the source asset files for this asset bundle.
+     * A source asset file is a file that is part of your source code repository of your Web application.
+     *
+     * You must set this property if the directory containing the source asset files is not Web accessible.
+     * By setting this property, [[AssetManager]] will publish the source asset files
+     * to a Web-accessible directory automatically when the asset bundle is registered on a page.
+     *
+     * If you do not set this property, it means the source asset files are located under [[basePath]].
+     *
+     * You can use either a directory or an alias of the directory.
+     * @see publishOptions
+     */
+    public $sourcePath;
+    /**
+     * @var string the Web-accessible directory that contains the asset files in this bundle.
+     *
+     * If [[sourcePath]] is set, this property will be *overwritten* by [[AssetManager]]
+     * when it publishes the asset files from [[sourcePath]].
+     *
+     * You can use either a directory or an alias of the directory.
+     */
+    public $basePath;
+    /**
+     * @var string the base URL for the relative asset files listed in [[js]] and [[css]].
+     *
+     * If [[sourcePath]] is set, this property will be *overwritten* by [[AssetManager]]
+     * when it publishes the asset files from [[sourcePath]].
+     *
+     * You can use either a URL or an alias of the URL.
+     */
+    public $baseUrl;
+    /**
+     * @var class-string[] list of bundle class names that this bundle depends on.
+     *
+     * For example:
+     *
+     * ```
+     * public $depends = [
+     *    'yii\web\YiiAsset',
+     *    'yii\bootstrap\BootstrapAsset',
+     * ];
+     * ```
+     */
+    public $depends = [];
+    /**
+     * @var (string|array<array-key, mixed>)[] list of JavaScript files that this bundle contains. Each JavaScript file can be
+     * specified in one of the following formats:
+     *
+     * - an absolute URL representing an external asset. For example,
+     *   `https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js` or
+     *   `//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js`.
+     * - a relative path representing a local asset (e.g. `js/main.js`). The actual file path of a local
+     *   asset can be determined by prefixing [[basePath]] to the relative path, and the actual URL
+     *   of the asset can be determined by prefixing [[baseUrl]] to the relative path.
+     * - an array, with the first entry being the URL or relative path as described before, and a list of key => value pairs
+     *   that will be used to overwrite [[jsOptions]] settings for this entry.
+     *   This functionality is available since version 2.0.7.
+     *
+     * Note that only a forward slash "/" should be used as directory separator.
+     */
+    public $js = [];
+    /**
+     * @var (string|array<array-key, mixed>)[] list of CSS files that this bundle contains. Each CSS file can be specified
+     * in one of the three formats as explained in [[js]].
+     *
+     * Note that only a forward slash "/" should be used as directory separator.
+     */
+    public $css = [];
