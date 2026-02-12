70659a1e55d192311558cad75c693d3b3ef82207
pmtrilio <pm@trilio.bg>
2026-02-12 19:02:18 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Container #328 (20-60).php
@@ -0,0 +1,41 @@
+use ReflectionException;
+use ReflectionFunction;
+use ReflectionParameter;
+use TypeError;
+
+class Container implements ArrayAccess, ContainerContract
+{
+    use ReflectsClosures;
+
+    /**
+     * The current globally available container (if any).
+     *
+     * @var static
+     */
+    protected static $instance;
+
+    /**
+     * An array of the types that have been resolved.
+     *
+     * @var bool[]
+     */
+    protected $resolved = [];
+
+    /**
+     * The container's bindings.
+     *
+     * @var array[]
+     */
+    protected $bindings = [];
+
+    /**
+     * The container's method bindings.
+     *
+     * @var \Closure[]
+     */
+    protected $methodBindings = [];
+
+    /**
+     * The container's shared instances.
+     *
+     * @var object[]
