b6bf9f9538a73e65392f983380b44d955f473011
pmtrilio <pm@trilio.bg>
2026-02-12 14:43:07 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Queuemanager #222 (30-130).php
@@ -0,0 +1,101 @@
+    protected $connections = [];
+
+    /**
+     * The array of resolved queue connectors.
+     *
+     * @var array
+     */
+    protected $connectors = [];
+
+    /**
+     * Create a new queue manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register an event listener for the before job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function before($callback)
+    {
+        $this->app['events']->listen(Events\JobProcessing::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the after job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function after($callback)
+    {
+        $this->app['events']->listen(Events\JobProcessed::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the exception occurred job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function exceptionOccurred($callback)
+    {
+        $this->app['events']->listen(Events\JobExceptionOccurred::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the daemon queue loop.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function looping($callback)
+    {
+        $this->app['events']->listen(Events\Looping::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the failed job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function failing($callback)
+    {
+        $this->app['events']->listen(Events\JobFailed::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the daemon queue starting.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function starting($callback)
+    {
+        $this->app['events']->listen(Events\WorkerStarting::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the daemon queue stopping.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function stopping($callback)
+    {
+        $this->app['events']->listen(Events\WorkerStopping::class, $callback);
+    }
+
+    /**
+     * Set the queue route for the given class.
+     *
+     * @param  array|class-string  $class
+     * @param  string|null  $queue
