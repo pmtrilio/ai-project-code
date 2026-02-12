b879afb422d7917cc06b0ea8c3b0ef11375acd7b
pmtrilio <pm@trilio.bg>
2026-02-12 20:25:26 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: LogManager #11 (30-70).php
@@ -0,0 +1,41 @@
+{
+    use ParsesLogConfiguration;
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The array of resolved channels.
+     *
+     * @var array
+     */
+    protected $channels = [];
+
+    /**
+     * The context shared across channels and stacks.
+     *
+     * @var array
+     */
+    protected $sharedContext = [];
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * The standard date format to use when writing logs.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Create a new Log manager instance.
+     *
