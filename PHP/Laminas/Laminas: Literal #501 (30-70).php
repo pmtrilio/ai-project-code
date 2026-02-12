224e6edf3ea1e09e196a393163f481ce08f7a0ae
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:28 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Literal #501 (30-70).php
@@ -0,0 +1,41 @@
+    /**
+     * @internal
+     * @deprecated Since 3.9.0 This property will be removed or made private in version 4.0
+     *
+     * @var int|null
+     */
+    public $priority;
+
+    /**
+     * Create a new literal route.
+     *
+     * @param  string $route
+     */
+    public function __construct(
+        /**
+         * RouteInterface to match.
+         */
+        protected $route,
+        array $defaults = []
+    ) {
+        $this->defaults = $defaults;
+    }
+
+    /**
+     * factory(): defined by RouteInterface interface.
+     *
+     * @see    \Laminas\Router\RouteInterface::factory()
+     *
+     * @param  iterable $options
+     * @return Literal
+     * @throws Exception\InvalidArgumentException
+     */
+    public static function factory($options = [])
+    {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        } elseif (! is_array($options)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s expects an array or Traversable set of options',
+                __METHOD__
+            ));
