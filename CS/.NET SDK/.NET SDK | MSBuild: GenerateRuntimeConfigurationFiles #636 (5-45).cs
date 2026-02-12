30b3fae357f0a8800b1ab3864a63b07f530c7749
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:42 +0200

    Upload file via PHP cURL

---
+++ CS/.NET SDK/.NET SDK | MSBuild: GenerateRuntimeConfigurationFiles #636 (5-45).cs
@@ -0,0 +1,41 @@
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using NuGet.Frameworks;
+using NuGet.ProjectModel;
+
+namespace Microsoft.NET.Build.Tasks
+{
+    /// <summary>
+    /// Generates the $(project).runtimeconfig.json and optionally $(project).runtimeconfig.dev.json files
+    /// for a project.
+    /// </summary>
+    public class GenerateRuntimeConfigurationFiles : TaskBase
+    {
+        public string AssetsFilePath { get; set; }
+
+        [Required]
+        public string TargetFramework { get; set; }
+
+        [Required]
+        public string TargetFrameworkMoniker { get; set; }
+
+        [Required]
+        public string RuntimeConfigPath { get; set; }
+
+        public string RuntimeConfigDevPath { get; set; }
+
+        public string RuntimeIdentifier { get; set; }
+
+        public string PlatformLibraryName { get; set; }
+
+        public ITaskItem[] RuntimeFrameworks { get; set; }
+
+        public string RollForward { get; set; }
+
+        public string UserRuntimeConfig { get; set; }
+
+        public ITaskItem[] HostConfigurationOptions { get; set; }
