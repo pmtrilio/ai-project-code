59da76c45d02d5e8d46417c1ea508a2e6a187a23
pmtrilio <pm@trilio.bg>
2026-02-12 14:24:38 +0200

    Upload file via PHP cURL

---
+++ PY/HTTP комуникация/HTTP комуникация –  split (10-50).py
@@ -0,0 +1,41 @@
+import warnings
+from abc import ABCMeta, abstractmethod
+from collections import defaultdict
+from collections.abc import Iterable
+from inspect import signature
+from itertools import chain, combinations
+from math import ceil, floor
+
+import numpy as np
+from scipy.special import comb
+
+from sklearn.utils import (
+    _safe_indexing,
+    check_random_state,
+    indexable,
+    metadata_routing,
+)
+from sklearn.utils._array_api import (
+    _convert_to_numpy,
+    get_namespace,
+    get_namespace_and_device,
+    move_to,
+)
+from sklearn.utils._param_validation import Interval, RealNotInt, validate_params
+from sklearn.utils.extmath import _approximate_mode
+from sklearn.utils.metadata_routing import _MetadataRequester
+from sklearn.utils.multiclass import type_of_target
+from sklearn.utils.validation import _num_samples, check_array, column_or_1d
+
+__all__ = [
+    "BaseCrossValidator",
+    "GroupKFold",
+    "GroupShuffleSplit",
+    "KFold",
+    "LeaveOneGroupOut",
+    "LeaveOneOut",
+    "LeavePGroupsOut",
+    "LeavePOut",
+    "PredefinedSplit",
+    "RepeatedKFold",
+    "RepeatedStratifiedKFold",
