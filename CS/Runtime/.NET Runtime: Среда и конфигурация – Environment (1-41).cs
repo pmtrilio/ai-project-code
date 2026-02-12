7499f7165e0e07f885121ec24e16bca427126b45
pmtrilio <pm@trilio.bg>
2026-02-12 14:08:14 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Среда и конфигурация – Environment (1-41).cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace System
+{
+    public static partial class Environment
+    {
+        /// <summary>
+        /// Represents the CPU usage statistics of a process.
+        /// </summary>
+        /// <remarks>
+        /// The CPU usage statistics include information about the time spent by the process in the application code (user mode) and the operating system code (kernel mode),
+        /// as well as the total time spent by the process in both user mode and kernel mode.
+        /// </remarks>
+        public readonly struct ProcessCpuUsage
+        {
+            /// <summary>
+            /// Gets the amount of time the associated process has spent running code inside the application portion of the process (not the operating system code).
+            /// </summary>
+            public TimeSpan UserTime { get; internal init; }
+
+            /// <summary>
+            /// Gets the amount of time the process has spent running code inside the operating system code.
+            /// </summary>
+            public TimeSpan PrivilegedTime { get; internal init; }
+
+            /// <summary>
+            /// Gets the amount of time the process has spent utilizing the CPU including the process time spent in the application code and the process time spent in the operating system code.
+            /// </summary>
+            public TimeSpan TotalTime => UserTime + PrivilegedTime;
+        }
+
+        /// <summary>
+        /// Gets whether the current machine has only a single processor.
+        /// </summary>
+#if FEATURE_SINGLE_THREADED
