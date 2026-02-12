855489b9f797cf9fecc643d2a37723ee5da9a0cd
pmtrilio <pm@trilio.bg>
2026-02-12 14:12:12 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: Middleware обработка – Endpointroutingmiddleware (20-60).cs
@@ -0,0 +1,41 @@
+
+internal sealed partial class EndpointRoutingMiddleware
+{
+    private const string DiagnosticsEndpointMatchedKey = "Microsoft.AspNetCore.Routing.EndpointMatched";
+
+    private readonly MatcherFactory _matcherFactory;
+    private readonly ILogger _logger;
+    private readonly EndpointDataSource _endpointDataSource;
+    private readonly DiagnosticListener _diagnosticListener;
+    private readonly RoutingMetrics _metrics;
+    private readonly RequestDelegate _next;
+    private readonly RouteOptions _routeOptions;
+    private Task<Matcher>? _initializationTask;
+
+    public EndpointRoutingMiddleware(
+        MatcherFactory matcherFactory,
+        ILogger<EndpointRoutingMiddleware> logger,
+        IEndpointRouteBuilder endpointRouteBuilder,
+        EndpointDataSource rootCompositeEndpointDataSource,
+        DiagnosticListener diagnosticListener,
+        IOptions<RouteOptions> routeOptions,
+        RoutingMetrics metrics,
+        RequestDelegate next)
+    {
+        ArgumentNullException.ThrowIfNull(endpointRouteBuilder);
+
+        _matcherFactory = matcherFactory ?? throw new ArgumentNullException(nameof(matcherFactory));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _diagnosticListener = diagnosticListener ?? throw new ArgumentNullException(nameof(diagnosticListener));
+        _metrics = metrics;
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+        _routeOptions = routeOptions.Value;
+
+        // rootCompositeEndpointDataSource is a constructor parameter only so it always gets disposed by DI. This ensures that any
+        // disposable EndpointDataSources also get disposed. _endpointDataSource is a component of rootCompositeEndpointDataSource.
+        _ = rootCompositeEndpointDataSource;
+        _endpointDataSource = new CompositeEndpointDataSource(endpointRouteBuilder.DataSources);
+    }
+
+    public Task Invoke(HttpContext httpContext)
+    {
