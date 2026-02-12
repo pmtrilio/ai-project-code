f5d4d3edb70947053a420049c3e501732b84419d
pmtrilio <pm@trilio.bg>
2026-02-12 20:03:37 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: TranslationExtension #684 (5-65).php
@@ -0,0 +1,61 @@
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bridge\Twig\Extension;
+
+use Symfony\Bridge\Twig\NodeVisitor\TranslationDefaultDomainNodeVisitor;
+use Symfony\Bridge\Twig\NodeVisitor\TranslationNodeVisitor;
+use Symfony\Bridge\Twig\TokenParser\TransDefaultDomainTokenParser;
+use Symfony\Bridge\Twig\TokenParser\TransTokenParser;
+use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorTrait;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+// Help opcache.preload discover always-needed symbols
+class_exists(TranslatorInterface::class);
+class_exists(TranslatorTrait::class);
+
+/**
+ * Provides integration of the Translation component with Twig.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+final class TranslationExtension extends AbstractExtension
+{
+    private ?TranslatorInterface $translator;
+    private ?TranslationNodeVisitor $translationNodeVisitor;
+
+    public function __construct(?TranslatorInterface $translator = null, ?TranslationNodeVisitor $translationNodeVisitor = null)
+    {
+        $this->translator = $translator;
+        $this->translationNodeVisitor = $translationNodeVisitor;
+    }
+
+    public function getTranslator(): TranslatorInterface
+    {
+        if (null === $this->translator) {
+            if (!interface_exists(TranslatorInterface::class)) {
+                throw new \LogicException(\sprintf('You cannot use the "%s" if the Translation Contracts are not available. Try running "composer require symfony/translation".', __CLASS__));
+            }
+
+            $this->translator = new class implements TranslatorInterface {
+                use TranslatorTrait;
+            };
+        }
+
+        return $this->translator;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('t', $this->createTranslatable(...)),
+        ];
