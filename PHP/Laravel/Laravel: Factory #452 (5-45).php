fdcfacb6906d782212198fa86999674ea9b5a212
pmtrilio <pm@trilio.bg>
2026-02-12 21:28:40 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Factory #452 (5-45).php
@@ -0,0 +1,41 @@
+use Closure;
+use Illuminate\Contracts\Process\ProcessResult as ProcessResultContract;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Traits\Macroable;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+class Factory
+{
+    use Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * Indicates if the process factory has faked process handlers.
+     *
+     * @var bool
+     */
+    protected $recording = false;
+
+    /**
+     * All of the recorded processes.
+     *
+     * @var array
+     */
+    protected $recorded = [];
+
+    /**
+     * The registered fake handler callbacks.
+     *
+     * @var array
+     */
+    protected $fakeHandlers = [];
+
+    /**
+     * Indicates that an exception should be thrown if any process is not faked.
+     *
+     * @var bool
+     */
+    protected $preventStrayProcesses = false;
+
+    /**
