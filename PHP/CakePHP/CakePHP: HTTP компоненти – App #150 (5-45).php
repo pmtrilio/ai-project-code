abd0cd9d3af7511a9af8a4a695ff9b246d96761e
pmtrilio <pm@trilio.bg>
2026-02-12 14:31:24 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: HTTP компоненти – App #150 (5-45).php
@@ -0,0 +1,41 @@
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Core;
+
+use Cake\Core\Exception\CakeException;
+
+/**
+ * App is responsible for resource location, and path management.
+ *
+ * ### Adding paths
+ *
+ * Additional paths for Templates and Plugins are configured with Configure now. See config/app.php for an
+ * example. The `App.paths.plugins` and `App.paths.templates` variables are used to configure paths for plugins
+ * and templates respectively. All class based resources should be mapped using your application's autoloader.
+ *
+ * ### Inspecting loaded paths
+ *
+ * You can inspect the currently loaded paths using `App::classPath('Controller')` for example to see loaded
+ * controller paths.
+ *
+ * It is also possible to inspect paths for plugin classes, for instance, to get
+ * the path to a plugin's helpers you would call `App::classPath('View/Helper', 'MyPlugin')`
+ *
+ * ### Locating plugins
+ *
+ * Plugins can be located with App as well. Using Plugin::path('DebugKit') for example, will
+ * give you the full path to the DebugKit plugin.
+ *
+ * @link https://book.cakephp.org/5/en/core-libraries/app.html
+ */
+class App
