152ecfefa0ce78796a36f64e2902345ea3e44000
pmtrilio <pm@trilio.bg>
2026-02-12 15:11:30 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: StaticFileMiddleware #615 (1-61).cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.StaticFiles;
+
+/// <summary>
+/// Enables serving static files for a given request path
+/// </summary>
+public class StaticFileMiddleware
+{
+    private readonly StaticFileOptions _options;
+    private readonly PathString _matchUrl;
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+    private readonly IFileProvider _fileProvider;
+    private readonly IContentTypeProvider _contentTypeProvider;
+
+    /// <summary>
+    /// Creates a new instance of the StaticFileMiddleware.
+    /// </summary>
+    /// <param name="next">The next middleware in the pipeline.</param>
+    /// <param name="hostingEnv">The <see cref="IWebHostEnvironment"/> used by this middleware.</param>
+    /// <param name="options">The configuration options.</param>
+    /// <param name="loggerFactory">An <see cref="ILoggerFactory"/> instance used to create loggers.</param>
+    public StaticFileMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<StaticFileOptions> options, ILoggerFactory loggerFactory)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(hostingEnv);
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+
+        _next = next;
+        _options = options.Value;
+        _contentTypeProvider = _options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+        _fileProvider = _options.FileProvider ?? Helpers.ResolveFileProvider(hostingEnv);
+        _matchUrl = _options.RequestPath;
+        _logger = loggerFactory.CreateLogger<StaticFileMiddleware>();
+
+        // See HostingEnvironmentExtensions.Initialize
+        if (_fileProvider is NullFileProvider && _fileProvider == hostingEnv.WebRootFileProvider)
+        {
+            _logger.WebRootPathNotFound(Path.GetFullPath(Path.Combine(hostingEnv.ContentRootPath, hostingEnv.WebRootPath ?? "wwwroot")));
+        }
+    }
+
+    /// <summary>
+    /// Processes a request to determine if it matches a known file, and if so, serves it.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public Task Invoke(HttpContext context)
+    {
+        if (!ValidateNoEndpointDelegate(context))
+        {
