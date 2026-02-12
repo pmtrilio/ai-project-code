6ee0e8fda9c53821c3f1f29c65399f54e35de047
pmtrilio <pm@trilio.bg>
2026-02-12 14:39:32 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Factory #19 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Illuminate\View;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\View\Factory as FactoryContract;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\View\Engines\EngineResolver;
+use InvalidArgumentException;
+
+class Factory implements FactoryContract
+{
+    use Macroable,
+        Concerns\ManagesComponents,
+        Concerns\ManagesEvents,
+        Concerns\ManagesFragments,
+        Concerns\ManagesLayouts,
+        Concerns\ManagesLoops,
+        Concerns\ManagesStacks,
+        Concerns\ManagesTranslations;
+
+    /**
+     * The engine implementation.
+     *
+     * @var \Illuminate\View\Engines\EngineResolver
+     */
+    protected $engines;
+
+    /**
+     * The view finder implementation.
+     *
+     * @var \Illuminate\View\ViewFinderInterface
+     */
+    protected $finder;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * Data that should be available to all templates.
+     *
+     * @var array
+     */
+    protected $shared = [];
+
+    /**
+     * The extension to engine bindings.
