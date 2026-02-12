c1eb69634a4960dc6de0ca0bd947edb5dfb5fc04
pmtrilio <pm@trilio.bg>
2026-02-12 14:30:02 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: HTTP компоненти – App #79 (20-80).php
@@ -0,0 +1,61 @@
+use Slim\Factory\ServerRequestCreatorFactory;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\MiddlewareDispatcherInterface;
+use Slim\Interfaces\RouteCollectorInterface;
+use Slim\Interfaces\RouteResolverInterface;
+use Slim\Middleware\BodyParsingMiddleware;
+use Slim\Middleware\ErrorMiddleware;
+use Slim\Middleware\RoutingMiddleware;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteResolver;
+use Slim\Routing\RouteRunner;
+
+use function strtoupper;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ * @template-extends RouteCollectorProxy<TContainerInterface>
+ */
+class App extends RouteCollectorProxy implements RequestHandlerInterface
+{
+    /**
+     * Current version
+     *
+     * @var string
+     */
+    public const VERSION = '4.15.1';
+
+    protected RouteResolverInterface $routeResolver;
+
+    protected MiddlewareDispatcherInterface $middlewareDispatcher;
+
+    /**
+     * @param TContainerInterface $container
+     */
+    public function __construct(
+        ResponseFactoryInterface $responseFactory,
+        ?ContainerInterface $container = null,
+        ?CallableResolverInterface $callableResolver = null,
+        ?RouteCollectorInterface $routeCollector = null,
+        ?RouteResolverInterface $routeResolver = null,
+        ?MiddlewareDispatcherInterface $middlewareDispatcher = null
+    ) {
+        parent::__construct(
+            $responseFactory,
+            $callableResolver ?? new CallableResolver($container),
+            $container,
+            $routeCollector
+        );
+
+        $this->routeResolver = $routeResolver ?? new RouteResolver($this->routeCollector);
+        $routeRunner = new RouteRunner($this->routeResolver, $this->routeCollector->getRouteParser(), $this);
+
+        if (!$middlewareDispatcher) {
+            $middlewareDispatcher = new MiddlewareDispatcher($routeRunner, $this->callableResolver, $container);
+        } else {
+            $middlewareDispatcher->seedMiddlewareStack($routeRunner);
+        }
+
+        $this->middlewareDispatcher = $middlewareDispatcher;
+    }
