259d0d4641a036a62d224f13d3a0e17fb2e4835a
pmtrilio <pm@trilio.bg>
2026-02-12 15:42:13 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Client #124 (10-50).php
@@ -0,0 +1,41 @@
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Http;
+
+use Cake\Core\App;
+use Cake\Core\Exception\CakeException;
+use Cake\Core\InstanceConfigTrait;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
+use Cake\Http\Client\Adapter\Curl;
+use Cake\Http\Client\Adapter\Mock as MockAdapter;
+use Cake\Http\Client\Adapter\Stream;
+use Cake\Http\Client\AdapterInterface;
+use Cake\Http\Client\ClientEvent;
+use Cake\Http\Client\Request;
+use Cake\Http\Client\Response;
+use Cake\Http\Cookie\CookieCollection;
+use Cake\Http\Cookie\CookieInterface;
+use Cake\Utility\Hash;
+use InvalidArgumentException;
+use Laminas\Diactoros\Uri;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * The end user interface for doing HTTP requests.
+ *
+ * ### Scoped clients
+ *
+ * If you're doing multiple requests to the same hostname it's often convenient
+ * to use the constructor arguments to create a scoped client. This allows you
+ * to keep your code DRY and not repeat hostnames, authentication, and other options.
+ *
+ * ### Doing requests
+ *
+ * Once you've created an instance of Client you can do requests
