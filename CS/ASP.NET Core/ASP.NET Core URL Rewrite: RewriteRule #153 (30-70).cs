dbc734621a9581a93abe0b68f69144d12ea0591a
pmtrilio <pm@trilio.bg>
2026-02-12 19:13:22 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteRule #153 (30-70).cs
@@ -0,0 +1,41 @@
+        Match initMatchResults;
+        if (path == PathString.Empty)
+        {
+            initMatchResults = InitialMatch.Match(path.ToString());
+        }
+        else
+        {
+            initMatchResults = InitialMatch.Match(path.ToString().Substring(1));
+        }
+
+        if (initMatchResults.Success)
+        {
+            var result = initMatchResults.Result(Replacement);
+            var request = context.HttpContext.Request;
+
+            if (StopProcessing)
+            {
+                context.Result = RuleResult.SkipRemainingRules;
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = "/";
+            }
+
+            if (result.Contains(Uri.SchemeDelimiter, StringComparison.Ordinal))
+            {
+                string scheme;
+                HostString host;
+                PathString pathString;
+                QueryString query;
+                UriHelper.FromAbsolute(result, out scheme, out host, out pathString, out query, out _);
+
+                request.Scheme = scheme;
+                request.Host = host;
+                request.Path = pathString;
+                request.QueryString = query.Add(request.QueryString);
+            }
+            else
+            {
+                var split = result.IndexOf('?');
