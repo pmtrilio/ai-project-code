eb6cb2f38d4a19c4724653d48cb417ac50575c39
pmtrilio <pm@trilio.bg>
2026-02-12 14:58:36 +0200

    Upload file via PHP cURL

---
+++ CS/.NET SDK/.NET SDK | MSBuild: Program #475 (1-41).cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable disable
+
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.DotNet.Cli.CommandFactory;
+using Microsoft.DotNet.Cli.CommandFactory.CommandResolution;
+using Microsoft.DotNet.Cli.CommandLine;
+using Microsoft.DotNet.Cli.Commands.Run;
+using Microsoft.DotNet.Cli.Commands.Workload;
+using Microsoft.DotNet.Cli.Extensions;
+using Microsoft.DotNet.Cli.ShellShim;
+using Microsoft.DotNet.Cli.Telemetry;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.Cli.Utils.Extensions;
+using Microsoft.DotNet.Configurer;
+using Microsoft.DotNet.ProjectTools;
+using Microsoft.DotNet.Utilities;
+using Microsoft.Extensions.EnvironmentAbstractions;
+using NuGet.Frameworks;
+using CommandResult = System.CommandLine.Parsing.CommandResult;
+
+namespace Microsoft.DotNet.Cli;
+
+public class Program
+{
+    private static readonly string ToolPathSentinelFileName = $"{Product.Version}.toolpath.sentinel";
+
+    public static ITelemetry TelemetryClient;
+    public static int Main(string[] args)
+    {
+        // Register a handler for SIGTERM to allow graceful shutdown of the application on Unix.
+        // See https://github.com/dotnet/docs/issues/46226.
+        using var termSignalRegistration = PosixSignalRegistration.Create(PosixSignal.SIGTERM, _ => Environment.Exit(0));
+
+        using AutomaticEncodingRestorer _ = new();
