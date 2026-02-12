8430393b50fa8cfcdc467e20eec21798795ebf01
pmtrilio <pm@trilio.bg>
2026-02-12 14:40:27 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Factory #71 (20-60).php
@@ -0,0 +1,41 @@
+        Concerns\ManagesLayouts,
+        Concerns\ManagesLoops,
+        Concerns\ManagesStacks,
+        Concerns\ManagesTranslations;
+
+    /**
+     * The engine implementation.
+     *
+     * @var \Illuminate\View\Engines\EngineResolver
+     */
+    protected $engines;
+
+    /**
+     * The view finder implementation.
+     *
+     * @var \Illuminate\View\ViewFinderInterface
+     */
+    protected $finder;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * Data that should be available to all templates.
+     *
+     * @var array
+     */
+    protected $shared = [];
+
+    /**
