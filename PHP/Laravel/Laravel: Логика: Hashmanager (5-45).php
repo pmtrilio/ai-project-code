85b33ec5cae38dc5d89fd2d324efabfc72d53bcc
pmtrilio <pm@trilio.bg>
2026-02-12 14:26:18 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Логика: Hashmanager (5-45).php
@@ -0,0 +1,41 @@
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Support\Manager;
+
+/**
+ * @mixin \Illuminate\Contracts\Hashing\Hasher
+ */
+class HashManager extends Manager implements Hasher
+{
+    /**
+     * Create an instance of the Bcrypt hash Driver.
+     *
+     * @return \Illuminate\Hashing\BcryptHasher
+     */
+    public function createBcryptDriver()
+    {
+        return new BcryptHasher($this->config->get('hashing.bcrypt') ?? []);
+    }
+
+    /**
+     * Create an instance of the Argon2i hash Driver.
+     *
+     * @return \Illuminate\Hashing\ArgonHasher
+     */
+    public function createArgonDriver()
+    {
+        return new ArgonHasher($this->config->get('hashing.argon') ?? []);
+    }
+
+    /**
+     * Create an instance of the Argon2id hash Driver.
+     *
+     * @return \Illuminate\Hashing\Argon2IdHasher
+     */
+    public function createArgon2idDriver()
+    {
+        return new Argon2IdHasher($this->config->get('hashing.argon') ?? []);
+    }
+
+    /**
+     * Get information about the given hashed value.
+     *
