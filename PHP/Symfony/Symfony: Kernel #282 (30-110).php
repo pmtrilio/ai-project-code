bc8ca13bd71b2ab1b1f9a616fcd9a6610f1f8da6
pmtrilio <pm@trilio.bg>
2026-02-12 14:44:13 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: Kernel #282 (30-110).php
@@ -0,0 +1,81 @@
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\ErrorHandler\DebugClassLoader;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\CacheWarmer\WarmableInterface;
+use Symfony\Component\HttpKernel\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\AddAnnotatedClassesToCachePass;
+use Symfony\Component\HttpKernel\DependencyInjection\MergeExtensionConfigurationPass;
+
+// Help opcache.preload discover always-needed symbols
+class_exists(ConfigCache::class);
+
+/**
+ * The Kernel is the heart of the Symfony system.
+ *
+ * It manages an environment made of bundles.
+ *
+ * Environment names must always start with a letter and
+ * they must only contain letters and numbers.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+abstract class Kernel implements KernelInterface, RebootableInterface, TerminableInterface
+{
+    /**
+     * @var array<string, BundleInterface>
+     */
+    protected $bundles = [];
+
+    protected $container;
+    protected $environment;
+    protected $debug;
+    protected $booted = false;
+    protected $startTime;
+
+    private string $projectDir;
+    private ?string $warmupDir = null;
+    private int $requestStackSize = 0;
+    private bool $resetServices = false;
+    private bool $handlingHttpCache = false;
+
+    /**
+     * @var array<string, bool>
+     */
+    private static array $freshCache = [];
+
+    public const VERSION = '6.4.34-DEV';
+    public const VERSION_ID = 60434;
+    public const MAJOR_VERSION = 6;
+    public const MINOR_VERSION = 4;
+    public const RELEASE_VERSION = 34;
+    public const EXTRA_VERSION = 'DEV';
+
+    public const END_OF_MAINTENANCE = '11/2026';
+    public const END_OF_LIFE = '11/2027';
+
+    public function __construct(string $environment, bool $debug)
+    {
+        if (!$this->environment = $environment) {
+            throw new \InvalidArgumentException(\sprintf('Invalid environment provided to "%s": the environment cannot be empty.', get_debug_type($this)));
+        }
+
+        $this->debug = $debug;
+    }
+
+    public function __clone()
+    {
+        $this->booted = false;
+        $this->container = null;
+        $this->requestStackSize = 0;
+        $this->resetServices = false;
+        $this->handlingHttpCache = false;
+    }
+
+    /**
+     * @return void
+     */
