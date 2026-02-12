32bd825d72527d30e51e4f25b84304f2b696f4c0
pmtrilio <pm@trilio.bg>
2026-02-12 20:36:11 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: HttpClient #532 (10-50).php
@@ -0,0 +1,41 @@
+ */
+
+namespace Symfony\Component\HttpClient;
+
+use Amp\Http\Client\Connection\ConnectionLimitingPool;
+use Amp\Promise;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * A factory to instantiate the best possible HTTP client for the runtime.
+ *
+ * @author Nicolas Grekas <p@tchwork.com>
+ */
+final class HttpClient
+{
+    /**
+     * @param array $defaultOptions     Default request's options
+     * @param int   $maxHostConnections The maximum number of connections to a single host
+     * @param int   $maxPendingPushes   The maximum number of pushed responses to accept in the queue
+     *
+     * @see HttpClientInterface::OPTIONS_DEFAULTS for available options
+     */
+    public static function create(array $defaultOptions = [], int $maxHostConnections = 6, int $maxPendingPushes = 50): HttpClientInterface
+    {
+        if ($amp = class_exists(ConnectionLimitingPool::class) && interface_exists(Promise::class)) {
+            if (!\extension_loaded('curl')) {
+                return new AmpHttpClient($defaultOptions, null, $maxHostConnections, $maxPendingPushes);
+            }
+
+            // Skip curl when HTTP/2 push is unsupported or buggy, see https://bugs.php.net/77535
+            if (!\defined('CURLMOPT_PUSHFUNCTION')) {
+                return new AmpHttpClient($defaultOptions, null, $maxHostConnections, $maxPendingPushes);
+            }
+
+            static $curlVersion = null;
+            $curlVersion ??= curl_version();
+
+            // HTTP/2 push crashes before curl 7.61
+            if (0x073D00 > $curlVersion['version_number'] || !(\CURL_VERSION_HTTP2 & $curlVersion['features'])) {
+                return new AmpHttpClient($defaultOptions, null, $maxHostConnections, $maxPendingPushes);
+            }
