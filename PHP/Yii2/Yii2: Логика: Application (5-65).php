f4b26990ffb21a08085ddbf26d9dc0165e4a721b
pmtrilio <pm@trilio.bg>
2026-02-12 14:27:21 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Логика: Application (5-65).php
@@ -0,0 +1,61 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+use Yii;
+use yii\base\InvalidRouteException;
+use yii\base\Module;
+use yii\helpers\Url;
+
+/**
+ * Application is the base class for all web application classes.
+ *
+ * For more details and usage information on Application, see the [guide article on applications](guide:structure-applications).
+ *
+ * @template TUserIdentity of IdentityInterface
+ *
+ * @property-read ErrorHandler $errorHandler The error handler application component.
+ * @property string $homeUrl The homepage URL.
+ * @property-read Request $request The request component.
+ * @property-read Response $response The response component.
+ * @property-read Session $session The session component.
+ * @property-read User<TUserIdentity> $user The user component.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Application extends \yii\base\Application
+{
+    /**
+     * @var string the default route of this application. Defaults to 'site'.
+     */
+    public $defaultRoute = 'site';
+    /**
+     * @var array|null the configuration specifying a controller action which should handle
+     * all user requests. This is mainly used when the application is in maintenance mode
+     * and needs to handle all incoming requests via a single action.
+     * The configuration is an array whose first element specifies the route of the action.
+     * The rest of the array elements (key-value pairs) specify the parameters to be bound
+     * to the action. For example,
+     *
+     * ```
+     * [
+     *     'offline/notice',
+     *     'param1' => 'value1',
+     *     'param2' => 'value2',
+     * ]
+     * ```
+     *
+     * Defaults to null, meaning catch-all is not used.
+     */
+    public $catchAll;
+    /**
+     * @var Controller<Module>|null the currently active controller instance
+     */
+    public $controller;
+
+
+    /**
+     * {@inheritdoc}
