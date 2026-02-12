2a8fac899139a77cad3818892de08a7b9fcbe48a
pmtrilio <pm@trilio.bg>
2026-02-12 21:25:06 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Acl #255 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Permissions\Acl;
+
+use Laminas\Permissions\Acl\Assertion\AssertionInterface;
+use Laminas\Permissions\Acl\Exception\ExceptionInterface;
+use Laminas\Permissions\Acl\Exception\InvalidArgumentException;
+use Laminas\Permissions\Acl\Exception\RuntimeException;
+use Laminas\Permissions\Acl\Resource\ResourceInterface;
+use Throwable;
+
+use function array_key_exists;
+use function array_keys;
+use function array_pop;
+use function is_array;
+use function is_string;
+use function sprintf;
+use function strtoupper;
+
+class Acl implements AclInterface
+{
+    /**
+     * Rule type: allow
+     */
+    public const TYPE_ALLOW = 'TYPE_ALLOW';
+
+    /**
+     * Rule type: deny
+     */
+    public const TYPE_DENY = 'TYPE_DENY';
+
+    /**
+     * Rule operation: add
+     */
+    public const OP_ADD = 'OP_ADD';
+
+    /**
+     * Rule operation: remove
+     */
+    public const OP_REMOVE = 'OP_REMOVE';
+
+    /**
+     * Role registry
+     *
+     * @var Role\Registry|null
+     */
+    protected $roleRegistry;
+
+    /**
+     * Resource tree
+     *
+     * @var array
+     */
+    protected $resources = [];
+
+    /**
+     * Resources by resourceId plus a null element
+     * Used to speed up setRule()
+     *
