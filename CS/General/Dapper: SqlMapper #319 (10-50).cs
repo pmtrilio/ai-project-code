9a0093a21b5e26bec6bfd92848e888a8788a70c5
pmtrilio <pm@trilio.bg>
2026-02-12 15:07:26 +0200

    Upload file via PHP cURL

---
+++ CS/General/Dapper: SqlMapper #319 (10-50).cs
@@ -0,0 +1,41 @@
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Dapper
+{
+    /// <summary>
+    /// Dapper, a light weight object mapper for ADO.NET
+    /// </summary>
+    public static partial class SqlMapper
+    {
+        private class PropertyInfoByNameComparer : IComparer<PropertyInfo>
+        {
+            public int Compare(PropertyInfo? x, PropertyInfo? y) => string.CompareOrdinal(x?.Name, y?.Name);
+        }
+        private static int GetColumnHash(DbDataReader reader, int startBound = 0, int length = -1)
+        {
+            unchecked
+            {
+                int max = length < 0 ? reader.FieldCount : startBound + length;
+                int hash = (-37 * startBound) + max;
+                for (int i = startBound; i < max; i++)
+                {
+                    object tmp = reader.GetName(i);
+                    hash = (-79 * ((hash * 31) + (tmp?.GetHashCode() ?? 0))) + (reader.GetFieldType(i)?.GetHashCode() ?? 0);
+                }
+                return hash;
+            }
+        }
+
