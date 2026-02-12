217b57f869d8aafdaa21db474cfd537cd8500867
pmtrilio <pm@trilio.bg>
2026-02-12 14:39:29 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Sessionmanager #16 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Illuminate\Session;
+
+use Illuminate\Support\Manager;
+
+/**
+ * @mixin \Illuminate\Session\Store
+ */
+class SessionManager extends Manager
+{
+    /**
+     * Call a custom driver creator.
+     *
+     * @param  string  $driver
+     * @return \Illuminate\Session\Store
+     */
+    protected function callCustomCreator($driver)
+    {
+        return $this->buildSession(parent::callCustomCreator($driver));
+    }
+
+    /**
+     * Create an instance of the "null" session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createNullDriver()
+    {
+        return $this->buildSession(new NullSessionHandler);
+    }
+
+    /**
+     * Create an instance of the "array" session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createArrayDriver()
+    {
+        return $this->buildSession(new ArraySessionHandler(
+            $this->config->get('session.lifetime')
