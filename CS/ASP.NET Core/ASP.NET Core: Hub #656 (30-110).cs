0ed9323bf683860f05acb5175d98215059ee0a13
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:55 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: Hub #656 (30-110).cs
@@ -0,0 +1,80 @@
+        set
+        {
+            CheckDisposed();
+            _clients = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the hub caller context.
+    /// </summary>
+    public HubCallerContext Context
+    {
+        get
+        {
+            CheckDisposed();
+            return _context;
+        }
+        set
+        {
+            CheckDisposed();
+            _context = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the group manager.
+    /// </summary>
+    public IGroupManager Groups
+    {
+        get
+        {
+            CheckDisposed();
+            return _groups;
+        }
+        set
+        {
+            CheckDisposed();
+            _groups = value;
+        }
+    }
+
+    /// <summary>
+    /// Called when a new connection is established with the hub.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> that represents the asynchronous connect.</returns>
+    public virtual Task OnConnectedAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Called when a connection with the hub is terminated.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> that represents the asynchronous disconnect.</returns>
+    public virtual Task OnDisconnectedAsync(Exception? exception)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Releases all resources currently used by this <see cref="Hub"/> instance.
+    /// </summary>
+    /// <param name="disposing"><c>true</c> if this method is being invoked by the <see cref="Dispose()"/> method,
+    /// otherwise <c>false</c>.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        Dispose(true);
+
+        _disposed = true;
+    }
