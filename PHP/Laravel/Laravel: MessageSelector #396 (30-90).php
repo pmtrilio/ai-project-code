e0a4c031c3c5b251c815c925fcab6a19ba639bd1
pmtrilio <pm@trilio.bg>
2026-02-12 20:33:19 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: MessageSelector #396 (30-90).php
@@ -0,0 +1,61 @@
+            return $segments[0];
+        }
+
+        return $segments[$pluralIndex];
+    }
+
+    /**
+     * Extract a translation string using inline conditions.
+     *
+     * @param  array  $segments
+     * @param  int  $number
+     * @return mixed
+     */
+    private function extract($segments, $number)
+    {
+        foreach ($segments as $part) {
+            if (! is_null($line = $this->extractFromString($part, $number))) {
+                return $line;
+            }
+        }
+    }
+
+    /**
+     * Get the translation string if the condition matches.
+     *
+     * @param  string  $part
+     * @param  int  $number
+     * @return mixed
+     */
+    private function extractFromString($part, $number)
+    {
+        preg_match('/^[\{\[]([^\[\]\{\}]*)[\}\]](.*)/s', $part, $matches);
+
+        if (count($matches) !== 3) {
+            return null;
+        }
+
+        $condition = $matches[1];
+
+        $value = $matches[2];
+
+        if (str_contains($condition, ',')) {
+            [$from, $to] = explode(',', $condition, 2);
+
+            if ($to === '*' && $number >= $from) {
+                return $value;
+            } elseif ($from === '*' && $number <= $to) {
+                return $value;
+            } elseif ($number >= $from && $number <= $to) {
+                return $value;
+            }
+        }
+
+        return $condition == $number ? $value : null;
+    }
+
+    /**
+     * Strip the inline conditions from each segment, just leaving the text.
+     *
+     * @param  array  $segments
+     * @return array
