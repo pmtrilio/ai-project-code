a9eeec11734d16f012a469ec229c3d9c3154d483
pmtrilio <pm@trilio.bg>
2026-02-12 14:28:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Builder #16 (30-90).php
@@ -0,0 +1,60 @@
+use LogicException;
+use RuntimeException;
+use UnitEnum;
+
+use function Illuminate\Support\enum_value;
+
+class Builder implements BuilderContract
+{
+    /** @use \Illuminate\Database\Concerns\BuildsQueries<\stdClass> */
+    use BuildsWhereDateClauses, BuildsQueries, ExplainsQueries, ForwardsCalls, Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The database connection instance.
+     *
+     * @var \Illuminate\Database\ConnectionInterface
+     */
+    public $connection;
+
+    /**
+     * The database query grammar instance.
+     *
+     * @var \Illuminate\Database\Query\Grammars\Grammar
+     */
+    public $grammar;
+
+    /**
+     * The database query post processor instance.
+     *
+     * @var \Illuminate\Database\Query\Processors\Processor
+     */
+    public $processor;
+
+    /**
+     * The current query value bindings.
+     *
+     * @var array{
+     *     select: list<mixed>,
+     *     from: list<mixed>,
+     *     join: list<mixed>,
+     *     where: list<mixed>,
+     *     groupBy: list<mixed>,
+     *     having: list<mixed>,
+     *     order: list<mixed>,
+     *     union: list<mixed>,
+     *     unionOrder: list<mixed>,
+     * }
+     */
+    public $bindings = [
+        'select' => [],
+        'from' => [],
+        'join' => [],
+        'where' => [],
+        'groupBy' => [],
+        'having' => [],
+        'order' => [],
+        'union' => [],
+        'unionOrder' => [],
+    ];
