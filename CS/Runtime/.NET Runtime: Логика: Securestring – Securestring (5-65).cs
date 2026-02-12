eb18a7162c492a2fc5d12651421d7ce54f5413bf
pmtrilio <pm@trilio.bg>
2026-02-12 14:11:46 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Логика: Securestring – Securestring (5-65).cs
@@ -0,0 +1,60 @@
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace System.Security
+{
+    public sealed partial class SecureString : IDisposable
+    {
+        private const int MaxLength = 65536;
+        private readonly object _methodLock = new object();
+        private UnmanagedBuffer? _buffer;
+        private int _decryptedLength;
+        private bool _encrypted;
+        private bool _readOnly;
+
+        public SecureString()
+        {
+            Initialize(ReadOnlySpan<char>.Empty);
+        }
+
+        [CLSCompliant(false)]
+        public unsafe SecureString(char* value, int length)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            ArgumentOutOfRangeException.ThrowIfNegative(length);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(length, MaxLength);
+
+            Initialize(new ReadOnlySpan<char>(value, length));
+        }
+
+        private void Initialize(ReadOnlySpan<char> value)
+        {
+            _buffer = UnmanagedBuffer.Allocate(GetAlignedByteSize(value.Length));
+            _decryptedLength = value.Length;
+
+            SafeBuffer? bufferToRelease = null;
+            try
+            {
+                Span<char> span = AcquireSpan(ref bufferToRelease);
+                value.CopyTo(span);
+            }
+            finally
+            {
+                ProtectMemory();
+                bufferToRelease?.DangerousRelease();
+            }
+        }
+
+        private SecureString(SecureString str)
+        {
+            Debug.Assert(str._buffer != null, "Expected other SecureString's buffer to be non-null");
+            Debug.Assert(str._encrypted, "Expected to be used only on encrypted SecureStrings");
+
+            _buffer = UnmanagedBuffer.Allocate((int)str._buffer.ByteLength);
+            Debug.Assert(_buffer != null);
+            UnmanagedBuffer.Copy(str._buffer, _buffer, str._buffer.ByteLength);
+
+            _decryptedLength = str._decryptedLength;
+            _encrypted = str._encrypted;
+        }
