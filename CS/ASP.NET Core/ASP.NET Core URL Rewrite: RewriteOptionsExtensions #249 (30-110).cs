6e1a2c6e981cae3aed000fb2af6cc0de299cab83
pmtrilio <pm@trilio.bg>
2026-02-12 19:15:00 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteOptionsExtensions #249 (30-110).cs
@@ -0,0 +1,81 @@
+    /// <param name="applyRule">A Func that checks and applies the rule.</param>
+    /// <returns></returns>
+    public static RewriteOptions Add(this RewriteOptions options, Action<RewriteContext> applyRule)
+    {
+        options.Rules.Add(new DelegateRule(applyRule));
+        return options;
+    }
+
+    /// <summary>
+    /// Adds a rule that rewrites the path if the regex matches the HttpContext's PathString.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <param name="skipRemainingRules">If the regex matches, conditionally stop processing other rules.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRewrite(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement, bool skipRemainingRules)
+    {
+        options.Rules.Add(new RewriteRule(regex, replacement, skipRemainingRules));
+        return options;
+    }
+
+    /// <summary>
+    /// Redirect the request if the regex matches the HttpContext's PathString, with returning a 302
+    /// status code for found.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRedirect(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement)
+    {
+        return AddRedirect(options, regex, replacement, statusCode: StatusCodes.Status302Found);
+    }
+
+    /// <summary>
+    /// Redirect the request if the regex matches the HttpContext's PathString
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <param name="statusCode">The status code to add to the response.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRedirect(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement, int statusCode)
+    {
+        options.Rules.Add(new RedirectRule(regex, replacement, statusCode));
+        return options;
+    }
+
+    /// <summary>
+    /// Redirect a request to https if the incoming request is http, with returning a 301
+    /// status code for permanently redirected.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <returns></returns>
+    public static RewriteOptions AddRedirectToHttpsPermanent(this RewriteOptions options)
+    {
+        return AddRedirectToHttps(options, statusCode: StatusCodes.Status301MovedPermanently, sslPort: null);
+    }
+
+    /// <summary>
+    /// Redirect a request to https if the incoming request is http
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    public static RewriteOptions AddRedirectToHttps(this RewriteOptions options)
+    {
+        return AddRedirectToHttps(options, statusCode: StatusCodes.Status302Found, sslPort: null);
+    }
+
+    /// <summary>
+    /// Redirect a request to https if the incoming request is http
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="statusCode">The status code to add to the response.</param>
+    public static RewriteOptions AddRedirectToHttps(this RewriteOptions options, int statusCode)
+    {
+        return AddRedirectToHttps(options, statusCode, sslPort: null);
+    }
+
+    /// <summary>
+    /// Redirect a request to https if the incoming request is http
