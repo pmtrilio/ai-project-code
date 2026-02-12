9e2ed8d372856ecf26bbdcf4d2d9e6a3df06ef2b
pmtrilio <pm@trilio.bg>
2026-02-12 14:19:07 +0200

    Upload file via PHP cURL

---
+++ PY/HTTP комуникация/HTTP комуникация –  forest (10-50).py
@@ -0,0 +1,41 @@
+  class calls the ``fit`` method of each sub-estimator on random samples
+  (with replacement, a.k.a. bootstrap) of the training set.
+
+  The init of the sub-estimator is further delegated to the
+  ``BaseEnsemble`` constructor.
+
+- The ``ForestClassifier`` and ``ForestRegressor`` base classes further
+  implement the prediction logic by computing an average of the predicted
+  outcomes of the sub-estimators.
+
+- The ``RandomForestClassifier`` and ``RandomForestRegressor`` derived
+  classes provide the user with concrete implementations of
+  the forest ensemble method using classical, deterministic
+  ``DecisionTreeClassifier`` and ``DecisionTreeRegressor`` as
+  sub-estimator implementations.
+
+- The ``ExtraTreesClassifier`` and ``ExtraTreesRegressor`` derived
+  classes provide the user with concrete implementations of the
+  forest ensemble method using the extremely randomized trees
+  ``ExtraTreeClassifier`` and ``ExtraTreeRegressor`` as
+  sub-estimator implementations.
+
+Single and multi-output problems are both handled.
+"""
+
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+import threading
+from abc import ABCMeta, abstractmethod
+from numbers import Integral
+from warnings import warn
+
+import numpy as np
+from scipy.sparse import hstack as sparse_hstack
+from scipy.sparse import issparse
+
+from sklearn.base import (
+    ClassifierMixin,
+    MultiOutputMixin,
+    RegressorMixin,
