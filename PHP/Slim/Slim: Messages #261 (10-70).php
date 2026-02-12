517f7e87652d060ec2b73012260dee717e5f45bd
pmtrilio <pm@trilio.bg>
2026-02-12 21:25:12 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: Messages #261 (10-70).php
@@ -0,0 +1,61 @@
+ */
+class Messages
+{
+    /**
+     * Messages from previous request
+     *
+     * @var string[]
+     */
+    protected $fromPrevious = [];
+
+    /**
+     * Messages for current request
+     *
+     * @var string[]
+     */
+    protected $forNow = [];
+
+    /**
+     * Messages for next request
+     *
+     * @var string[]
+     */
+    protected $forNext = [];
+
+    /**
+     * Message storage
+     *
+     * @var null|array|ArrayAccess
+     */
+    protected $storage;
+
+    /**
+     * Message storage key
+     *
+     * @var string
+     */
+    protected $storageKey = 'slimFlash';
+
+    /**
+     * Create new Flash messages service provider
+     *
+     * @param null|array|ArrayAccess $storage
+     * @throws RuntimeException if the session cannot be found
+     * @throws InvalidArgumentException if the store is not array-like
+     */
+    public function __construct(&$storage = null, $storageKey = null)
+    {
+        if (is_string($storageKey) && $storageKey) {
+            $this->storageKey = $storageKey;
+        }
+
+        // Set storage
+        if (is_array($storage) || $storage instanceof ArrayAccess) {
+            $this->storage = &$storage;
+        } elseif (is_null($storage)) {
+            if (!isset($_SESSION)) {
+                throw new RuntimeException('Flash messages middleware failed. Session not found.');
+            }
+            $this->storage = &$_SESSION;
+        } else {
+            throw new InvalidArgumentException('Flash messages storage must be an array or implement \ArrayAccess');
