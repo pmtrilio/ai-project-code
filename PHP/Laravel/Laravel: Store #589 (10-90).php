5b5cadb876237f1920220b804d3bc3f0d16ad2ed
pmtrilio <pm@trilio.bg>
2026-02-12 14:49:44 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Store #589 (10-90).php
@@ -0,0 +1,81 @@
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Uri;
+use Illuminate\Support\ViewErrorBag;
+use RuntimeException;
+use SessionHandlerInterface;
+use stdClass;
+
+class Store implements Session
+{
+    use Macroable;
+
+    /**
+     * The session ID.
+     *
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * The session name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The session attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * The session handler implementation.
+     *
+     * @var \SessionHandlerInterface
+     */
+    protected $handler;
+
+    /**
+     * The session store's serialization strategy.
+     *
+     * @var string
+     */
+    protected $serialization = 'php';
+
+    /**
+     * Session store started status.
+     *
+     * @var bool
+     */
+    protected $started = false;
+
+    /**
+     * Create a new session instance.
+     *
+     * @param  string  $name
+     * @param  \SessionHandlerInterface  $handler
+     * @param  string|null  $id
+     * @param  string  $serialization
+     */
+    public function __construct($name, SessionHandlerInterface $handler, $id = null, $serialization = 'php')
+    {
+        $this->setId($id);
+        $this->name = $name;
+        $this->handler = $handler;
+        $this->serialization = $serialization;
+    }
+
+    /**
+     * Start the session, reading the data from a handler.
+     *
+     * @return bool
+     */
+    public function start()
+    {
+        $this->loadSession();
+
+        if (! $this->has('_token')) {
