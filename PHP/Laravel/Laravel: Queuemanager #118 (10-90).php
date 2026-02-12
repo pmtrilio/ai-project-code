a5bb101ad4f775bd46a4f449cdf74763963008f6
pmtrilio <pm@trilio.bg>
2026-02-12 14:41:16 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Queuemanager #118 (10-90).php
@@ -0,0 +1,81 @@
+
+/**
+ * @mixin \Illuminate\Contracts\Queue\Queue
+ */
+class QueueManager implements FactoryContract, MonitorContract
+{
+    use ResolvesQueueRoutes;
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The array of resolved queue connections.
+     *
+     * @var array
+     */
+    protected $connections = [];
+
+    /**
+     * The array of resolved queue connectors.
+     *
+     * @var array
+     */
+    protected $connectors = [];
+
+    /**
+     * Create a new queue manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register an event listener for the before job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function before($callback)
+    {
+        $this->app['events']->listen(Events\JobProcessing::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the after job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function after($callback)
+    {
+        $this->app['events']->listen(Events\JobProcessed::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the exception occurred job event.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function exceptionOccurred($callback)
+    {
+        $this->app['events']->listen(Events\JobExceptionOccurred::class, $callback);
+    }
+
+    /**
+     * Register an event listener for the daemon queue loop.
+     *
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function looping($callback)
+    {
+        $this->app['events']->listen(Events\Looping::class, $callback);
