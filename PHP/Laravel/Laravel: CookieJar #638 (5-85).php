0876be6624a4fa331cad897e30f68b6ff64ce5ed
pmtrilio <pm@trilio.bg>
2026-02-12 15:38:42 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: CookieJar #638 (5-85).php
@@ -0,0 +1,81 @@
+use Illuminate\Contracts\Cookie\QueueingFactory as JarContract;
+use Illuminate\Support\Arr;
+use Illuminate\Support\InteractsWithTime;
+use Illuminate\Support\Traits\Macroable;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class CookieJar implements JarContract
+{
+    use InteractsWithTime, Macroable;
+
+    /**
+     * The default path (if specified).
+     *
+     * @var string
+     */
+    protected $path = '/';
+
+    /**
+     * The default domain (if specified).
+     *
+     * @var string|null
+     */
+    protected $domain;
+
+    /**
+     * The default secure setting (defaults to null).
+     *
+     * @var bool|null
+     */
+    protected $secure;
+
+    /**
+     * The default SameSite option (defaults to lax).
+     *
+     * @var string
+     */
+    protected $sameSite = 'lax';
+
+    /**
+     * All of the cookies queued for sending.
+     *
+     * @var \Symfony\Component\HttpFoundation\Cookie[]
+     */
+    protected $queued = [];
+
+    /**
+     * Create a new cookie instance.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  int  $minutes
+     * @param  string|null  $path
+     * @param  string|null  $domain
+     * @param  bool|null  $secure
+     * @param  bool  $httpOnly
+     * @param  bool  $raw
+     * @param  string|null  $sameSite
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
+    public function make($name, $value, $minutes = 0, $path = null, $domain = null, $secure = null, $httpOnly = true, $raw = false, $sameSite = null)
+    {
+        [$path, $domain, $secure, $sameSite] = $this->getPathAndDomain($path, $domain, $secure, $sameSite);
+
+        $time = ($minutes == 0) ? 0 : $this->availableAt($minutes * 60);
+
+        return new Cookie($name, $value, $time, $path, $domain, $secure, $httpOnly, $raw, $sameSite);
+    }
+
+    /**
+     * Create a cookie that lasts "forever" (400 days).
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  string|null  $path
+     * @param  string|null  $domain
+     * @param  bool|null  $secure
+     * @param  bool  $httpOnly
+     * @param  bool  $raw
+     * @param  string|null  $sameSite
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
