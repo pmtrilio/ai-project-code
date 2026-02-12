874f90b37241e5089839db2fd69c289c955013c5
pmtrilio <pm@trilio.bg>
2026-02-12 14:50:30 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Substitutebindings #633 (20-60).php
@@ -0,0 +1,33 @@
+     *
+     * @param  \Illuminate\Contracts\Routing\Registrar  $router
+     */
+    public function __construct(Registrar $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $route = $request->route();
+
+        try {
+            $this->router->substituteBindings($route);
+            $this->router->substituteImplicitBindings($route);
+        } catch (ModelNotFoundException $exception) {
+            if ($route->getMissing()) {
+                return $route->getMissing()($request, $exception);
+            }
+
+            throw $exception;
+        }
+
+        return $next($request);
+    }
+}
