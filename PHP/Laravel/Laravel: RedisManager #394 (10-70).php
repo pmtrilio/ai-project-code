48ade844b1b86b4e36d5037ba0eefa124c3cbd5a
pmtrilio <pm@trilio.bg>
2026-02-12 20:33:17 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: RedisManager #394 (10-70).php
@@ -0,0 +1,61 @@
+use Illuminate\Support\Arr;
+use Illuminate\Support\ConfigurationUrlParser;
+use InvalidArgumentException;
+
+/**
+ * @mixin \Illuminate\Redis\Connections\Connection
+ */
+class RedisManager implements Factory
+{
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The name of the default driver.
+     *
+     * @var string
+     */
+    protected $driver;
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * The Redis server configurations.
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * The Redis connections.
+     *
+     * @var mixed
+     */
+    protected $connections;
+
+    /**
+     * Indicates whether event dispatcher is set on connections.
+     *
+     * @var bool
+     */
+    protected $events = false;
+
+    /**
+     * Create a new Redis manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  string  $driver
+     * @param  array  $config
+     */
+    public function __construct($app, $driver, array $config)
+    {
+        $this->app = $app;
