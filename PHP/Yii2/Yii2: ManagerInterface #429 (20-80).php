ef241d61bce0cb4ae4ab8b77e695f7e68916be5e
pmtrilio <pm@trilio.bg>
2026-02-12 20:33:58 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: ManagerInterface #429 (20-80).php
@@ -0,0 +1,61 @@
+     * Creates a new Role object.
+     * Note that the newly created role is not added to the RBAC system yet.
+     * You must fill in the needed data and call [[add()]] to add it to the system.
+     * @param string $name the role name
+     * @return Role the new Role object
+     */
+    public function createRole($name);
+
+    /**
+     * Creates a new Permission object.
+     * Note that the newly created permission is not added to the RBAC system yet.
+     * You must fill in the needed data and call [[add()]] to add it to the system.
+     * @param string $name the permission name
+     * @return Permission the new Permission object
+     */
+    public function createPermission($name);
+
+    /**
+     * Adds a role, permission or rule to the RBAC system.
+     * @param Role|Permission|Rule $object
+     * @return bool whether the role, permission or rule is successfully added to the system
+     * @throws \Exception if data validation or saving fails (such as the name of the role or permission is not unique)
+     */
+    public function add($object);
+
+    /**
+     * Removes a role, permission or rule from the RBAC system.
+     * @param Role|Permission|Rule $object
+     * @return bool whether the role, permission or rule is successfully removed
+     */
+    public function remove($object);
+
+    /**
+     * Updates the specified role, permission or rule in the system.
+     * @param string $name the old name of the role, permission or rule
+     * @param Role|Permission|Rule $object
+     * @return bool whether the update is successful
+     * @throws \Exception if data validation or saving fails (such as the name of the role or permission is not unique)
+     */
+    public function update($name, $object);
+
+    /**
+     * Returns the named role.
+     * @param string $name the role name.
+     * @return Role|null the role corresponding to the specified name. Null is returned if no such role.
+     */
+    public function getRole($name);
+
+    /**
+     * Returns all roles in the system.
+     * @return Role[] all roles in the system. The array is indexed by the role names.
+     */
+    public function getRoles();
+
+    /**
+     * Returns the roles that are assigned to the user via [[assign()]].
+     * Note that child roles that are not assigned directly to the user will not be returned.
+     * @param string|int $userId the user ID (see [[\yii\web\User::id]])
+     * @return Role[] all roles directly assigned to the user. The array is indexed by the role names.
+     */
+    public function getRolesByUser($userId);
