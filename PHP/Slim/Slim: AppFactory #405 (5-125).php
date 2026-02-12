2f42aa4da3cf11a86b34b2979dd3c357a286c304
pmtrilio <pm@trilio.bg>
2026-02-12 21:27:54 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: AppFactory #405 (5-125).php
@@ -0,0 +1,121 @@
+ *
+ * @license https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+
+declare(strict_types=1);
+
+namespace Slim\Factory;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use RuntimeException;
+use Slim\App;
+use Slim\Factory\Psr17\Psr17Factory;
+use Slim\Factory\Psr17\Psr17FactoryProvider;
+use Slim\Factory\Psr17\SlimHttpPsr17Factory;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\MiddlewareDispatcherInterface;
+use Slim\Interfaces\Psr17FactoryProviderInterface;
+use Slim\Interfaces\RouteCollectorInterface;
+use Slim\Interfaces\RouteResolverInterface;
+
+/** @api */
+class AppFactory
+{
+    protected static ?Psr17FactoryProviderInterface $psr17FactoryProvider = null;
+
+    protected static ?ResponseFactoryInterface $responseFactory = null;
+
+    protected static ?StreamFactoryInterface $streamFactory = null;
+
+    protected static ?ContainerInterface $container = null;
+
+    protected static ?CallableResolverInterface $callableResolver = null;
+
+    protected static ?RouteCollectorInterface $routeCollector = null;
+
+    protected static ?RouteResolverInterface $routeResolver = null;
+
+    protected static ?MiddlewareDispatcherInterface $middlewareDispatcher = null;
+
+    protected static bool $slimHttpDecoratorsAutomaticDetectionEnabled = true;
+
+    /**
+     * @template TContainerInterface of (ContainerInterface|null)
+     * @param TContainerInterface $container
+     * @return (TContainerInterface is ContainerInterface ? App<TContainerInterface> : App<ContainerInterface|null>)
+     */
+    public static function create(
+        ?ResponseFactoryInterface $responseFactory = null,
+        ?ContainerInterface $container = null,
+        ?CallableResolverInterface $callableResolver = null,
+        ?RouteCollectorInterface $routeCollector = null,
+        ?RouteResolverInterface $routeResolver = null,
+        ?MiddlewareDispatcherInterface $middlewareDispatcher = null
+    ): App {
+        static::$responseFactory = $responseFactory ?? static::$responseFactory;
+        return new App(
+            self::determineResponseFactory(),
+            $container ?? static::$container,
+            $callableResolver ?? static::$callableResolver,
+            $routeCollector ?? static::$routeCollector,
+            $routeResolver ?? static::$routeResolver,
+            $middlewareDispatcher ?? static::$middlewareDispatcher
+        );
+    }
+
+    /**
+     * @template TContainerInterface of (ContainerInterface)
+     * @param TContainerInterface $container
+     * @return App<TContainerInterface>
+     */
+    public static function createFromContainer(ContainerInterface $container): App
+    {
+        $responseFactory = $container->has(ResponseFactoryInterface::class)
+        && (
+            $responseFactoryFromContainer = $container->get(ResponseFactoryInterface::class)
+        ) instanceof ResponseFactoryInterface
+            ? $responseFactoryFromContainer
+            : self::determineResponseFactory();
+
+        $callableResolver = $container->has(CallableResolverInterface::class)
+        && (
+            $callableResolverFromContainer = $container->get(CallableResolverInterface::class)
+        ) instanceof CallableResolverInterface
+            ? $callableResolverFromContainer
+            : null;
+
+        $routeCollector = $container->has(RouteCollectorInterface::class)
+        && (
+            $routeCollectorFromContainer = $container->get(RouteCollectorInterface::class)
+        ) instanceof RouteCollectorInterface
+            ? $routeCollectorFromContainer
+            : null;
+
+        $routeResolver = $container->has(RouteResolverInterface::class)
+        && (
+            $routeResolverFromContainer = $container->get(RouteResolverInterface::class)
+        ) instanceof RouteResolverInterface
+            ? $routeResolverFromContainer
+            : null;
+
+        $middlewareDispatcher = $container->has(MiddlewareDispatcherInterface::class)
+        && (
+            $middlewareDispatcherFromContainer = $container->get(MiddlewareDispatcherInterface::class)
+        ) instanceof MiddlewareDispatcherInterface
+            ? $middlewareDispatcherFromContainer
+            : null;
+
+        return new App(
+            $responseFactory,
+            $container,
+            $callableResolver,
+            $routeCollector,
+            $routeResolver,
+            $middlewareDispatcher
+        );
+    }
+
+    /**
+     * @throws RuntimeException
