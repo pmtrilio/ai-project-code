91c4358e939acb6a8f4d5262aceaaa22fa4bb0fe
pmtrilio <pm@trilio.bg>
2026-02-12 13:38:57 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Обработка на заявки (1-60).php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         2.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Http;
+
+use BadMethodCallException;
+use Cake\Core\Configure;
+use Cake\Core\Exception\CakeException;
+use Cake\Http\Cookie\CookieCollection;
+use Cake\Http\Exception\MethodNotAllowedException;
+use Cake\Utility\Hash;
+use Closure;
+use InvalidArgumentException;
+use Laminas\Diactoros\Stream;
+use Laminas\Diactoros\UploadedFile;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
+use function Cake\Core\deprecationWarning;
+use function Cake\Core\env;
+
+/**
+ * A class that helps wrap Request information and particulars about a single request.
+ * Provides methods commonly used to introspect on the request headers and request body.
+ */
+class ServerRequest implements ServerRequestInterface
+{
+    /**
+     * Array of parameters parsed from the URL.
+     *
+     * @var array
+     */
+    protected array $params = [
+        'plugin' => null,
+        'controller' => null,
+        'action' => null,
+        '_ext' => null,
+        'pass' => [],
+    ];
+
+    /**
+     * Array of POST data. Will contain form data as well as uploaded files.
+     * In PUT/PATCH/DELETE requests this property will contain the form-urlencoded
+     * data.
+     *
+     * @var object|array|null
