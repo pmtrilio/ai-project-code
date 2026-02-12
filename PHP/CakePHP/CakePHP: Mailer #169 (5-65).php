3016b01ab99362bf24e9344b0e8f2a04ef43940e
pmtrilio <pm@trilio.bg>
2026-02-12 20:28:40 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Mailer #169 (5-65).php
@@ -0,0 +1,61 @@
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @since         3.1.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Mailer;
+
+use BadMethodCallException;
+use Cake\Core\StaticConfigTrait;
+use Cake\Event\EventListenerInterface;
+use Cake\Log\Log;
+use Cake\Mailer\Exception\MissingActionException;
+use Cake\ORM\Locator\LocatorAwareTrait;
+use Cake\View\ViewBuilder;
+use InvalidArgumentException;
+use function Cake\Core\deprecationWarning;
+
+/**
+ * Mailer base class.
+ *
+ * Mailer classes let you encapsulate related Email logic into a reusable
+ * and testable class.
+ *
+ * ## Defining Messages
+ *
+ * Mailers make it easy for you to define methods that handle email formatting
+ * logic. For example:
+ *
+ * ```
+ * class UserMailer extends Mailer
+ * {
+ *     public function resetPassword($user)
+ *     {
+ *         $this
+ *             ->setSubject('Reset Password')
+ *             ->setTo($user->email)
+ *             ->set(['token' => $user->token]);
+ *     }
+ * }
+ * ```
+ *
+ * Is a trivial example but shows how a mailer could be declared.
+ *
+ * ## Sending Messages
+ *
+ * After you have defined some messages you will want to send them:
+ *
+ * ```
+ * $mailer = new UserMailer();
+ * $mailer->send('resetPassword', $user);
+ * ```
+ *
+ * ## Event Listener
+ *
+ * Mailers can also subscribe to application event allowing you to
+ * decouple email delivery from your application code. By re-declaring the
