c03dc519767f4fa1f92d5d0250e9534cb140f39a
pmtrilio <pm@trilio.bg>
2026-02-12 14:41:51 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Sessionmanager #150 (5-105).php
@@ -0,0 +1,100 @@
+use Laminas\EventManager\Event;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\Stdlib\ArrayUtils;
+use Traversable;
+
+use function array_key_exists;
+use function array_merge;
+use function assert;
+use function constant;
+use function defined;
+use function headers_sent;
+use function is_array;
+use function is_string;
+use function iterator_to_array;
+use function preg_match;
+use function register_shutdown_function;
+use function session_destroy;
+use function session_id;
+use function session_name;
+use function session_regenerate_id;
+use function session_set_save_handler;
+use function session_start;
+use function session_status;
+use function session_write_close;
+use function setcookie;
+
+use const PHP_SESSION_ACTIVE;
+
+/**
+ * Session ManagerInterface implementation utilizing ext/session
+ *
+ * @final
+ */
+class SessionManager extends AbstractManager
+{
+    /**
+     * Default options when a call to {@link destroy()} is made
+     * - send_expire_cookie: whether or not to send a cookie expiring the current session cookie
+     * - clear_storage: whether or not to empty the storage object of any stored values
+     *
+     * @deprecated This property will be removed in version 3.0
+     *
+     * @var array
+     */
+    protected $defaultDestroyOptions = [
+        'send_expire_cookie' => true,
+        'clear_storage'      => false,
+    ];
+
+    /**
+     * @deprecated This property will be removed in version 3.0
+     *
+     * @var array Default session manager options
+     */
+    protected $defaultOptions = [
+        'attach_default_validators' => true,
+    ];
+
+    /** @var array Default validators */
+    protected $defaultValidators = [
+        Validator\Id::class,
+    ];
+
+    /** @var string value returned by session_name() */
+    protected $name;
+
+    /** @var EventManagerInterface Validation chain to determine if session is valid */
+    protected $validatorChain;
+
+    /**
+     * Constructor
+     *
+     * @throws Exception\RuntimeException
+     */
+    public function __construct(
+        ?Config\ConfigInterface $config = null,
+        ?Storage\StorageInterface $storage = null,
+        ?SaveHandler\SaveHandlerInterface $saveHandler = null,
+        array $validators = [],
+        array $options = []
+    ) {
+        $options = array_merge($this->defaultOptions, $options);
+        if ($options['attach_default_validators']) {
+            $validators = array_merge($this->defaultValidators, $validators);
+        }
+
+        parent::__construct($config, $storage, $saveHandler, $validators);
+        register_shutdown_function([$this, 'writeClose']);
+    }
+
+    /**
+     * Does a session exist and is it currently active?
+     *
+     * @return bool
+     */
+    public function sessionExists()
+    {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            return true;
+        }
