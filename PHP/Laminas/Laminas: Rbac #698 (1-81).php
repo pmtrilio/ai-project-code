b7ffba6e6d08553eb0a407f3e2667d2fa30d3736
pmtrilio <pm@trilio.bg>
2026-02-12 20:03:55 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Rbac #698 (1-81).php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Permissions\Rbac;
+
+use function array_values;
+use function is_array;
+use function is_callable;
+use function is_object;
+use function is_string;
+use function sprintf;
+
+/**
+ * @final
+ */
+class Rbac
+{
+    /** @var array<string, RoleInterface> */
+    protected $roles = [];
+
+    /**
+     * flag: whether or not to create roles automatically if
+     * they do not exist.
+     *
+     * @var bool
+     */
+    protected $createMissingRoles = false;
+
+    public function setCreateMissingRoles(bool $createMissingRoles): void
+    {
+        $this->createMissingRoles = $createMissingRoles;
+    }
+
+    public function getCreateMissingRoles(): bool
+    {
+        return $this->createMissingRoles;
+    }
+
+    /**
+     * Add a child.
+     *
+     * @param  string|RoleInterface $role
+     * @param  null|array|RoleInterface $parents
+     * @throws Exception\InvalidArgumentException If $role is not a string or RoleInterface.
+     */
+    public function addRole($role, $parents = null): void
+    {
+        if (is_string($role)) {
+            $role = new Role($role);
+        }
+        if (! $role instanceof RoleInterface) {
+            throw new Exception\InvalidArgumentException(
+                'Role must be a string or implement Laminas\Permissions\Rbac\RoleInterface'
+            );
+        }
+
+        if ($parents !== null) {
+            $parents = is_array($parents) ? $parents : [$parents];
+            /** @var RoleInterface|string $parent */
+            foreach ($parents as $parent) {
+                if ($this->createMissingRoles && ! $this->hasRole($parent)) {
+                    $this->addRole($parent);
+                }
+                if (is_string($parent)) {
+                    $parent = $this->getRole($parent);
+                }
+                $parent->addChild($role);
+            }
+        }
+
+        $this->roles[$role->getName()] = $role;
+    }
+
+    /**
+     * Is a role registered?
+     *
+     * @param  RoleInterface|string $role
+     */
+    public function hasRole($role): bool
+    {
