538d1c890ebe20ce3f7c8d5401e4b1ff589c71ba
pmtrilio <pm@trilio.bg>
2026-02-12 14:42:54 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Eventfake #211 (1-61).php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Illuminate\Support\Testing\Fakes;
+
+use Closure;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\ForwardsCalls;
+use Illuminate\Support\Traits\ReflectsClosures;
+use PHPUnit\Framework\Assert as PHPUnit;
+use ReflectionFunction;
+
+class EventFake implements Dispatcher, Fake
+{
+    use ForwardsCalls, ReflectsClosures;
+
+    /**
+     * The original event dispatcher.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    public $dispatcher;
+
+    /**
+     * The event types that should be intercepted instead of dispatched.
+     *
+     * @var array
+     */
+    protected $eventsToFake = [];
+
+    /**
+     * The event types that should be dispatched instead of intercepted.
+     *
+     * @var array
+     */
+    protected $eventsToDispatch = [];
+
+    /**
+     * All of the events that have been intercepted keyed by type.
+     *
+     * @var array
+     */
+    protected $events = [];
+
+    /**
+     * Create a new event fake instance.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $dispatcher
+     * @param  array|string  $eventsToFake
+     */
+    public function __construct(Dispatcher $dispatcher, $eventsToFake = [])
+    {
+        $this->dispatcher = $dispatcher;
+
+        $this->eventsToFake = Arr::wrap($eventsToFake);
+    }
