14dcd778509d0e48d06fbc2d39533b819712dfb7
pmtrilio <pm@trilio.bg>
2026-02-12 14:09:58 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Интервали от време – Timespan (20-60).cs
@@ -0,0 +1,41 @@
+    // we do not provide Years() or Months().
+    //
+    [Serializable]
+    public readonly struct TimeSpan
+        : IComparable,
+          IComparable<TimeSpan>,
+          IEquatable<TimeSpan>,
+          ISpanFormattable,
+          ISpanParsable<TimeSpan>,
+          IUtf8SpanFormattable
+    {
+        /// <summary>
+        /// Represents the number of nanoseconds per tick. This field is constant.
+        /// </summary>
+        /// <remarks>
+        /// The value of this constant is 100.
+        /// </remarks>
+        public const long NanosecondsPerTick = 100;                                                 //             100
+
+        /// <summary>
+        /// Represents the number of ticks in 1 microsecond. This field is constant.
+        /// </summary>
+        /// <remarks>
+        /// The value of this constant is 10.
+        /// </remarks>
+        public const long TicksPerMicrosecond = 10;                                                 //              10
+
+        /// <summary>
+        /// Represents the number of ticks in 1 millisecond. This field is constant.
+        /// </summary>
+        /// <remarks>
+        /// The value of this constant is 10 thousand; that is, 10,000.
+        /// </remarks>
+        public const long TicksPerMillisecond = TicksPerMicrosecond * 1000;                         //          10,000
+
+        /// <summary>
+        /// Represents the number of ticks in 1 second. This field is constant.
+        /// </summary>
+        /// <remarks>
+        /// The value of this constant is 10 million; that is, 10,000,000.
+        /// </remarks>
