1b98a543bd2b9762fe5bb6639d6dc3c7e232dd25
pmtrilio <pm@trilio.bg>
2026-02-12 19:30:48 +0200

    Upload file via PHP cURL

---
+++ PY/Pandas/pandas: Readers #51 (10-50).py
@@ -0,0 +1,40 @@
+    abc,
+    defaultdict,
+)
+import csv
+import sys
+from typing import (
+    IO,
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    Literal,
+    Self,
+    TypedDict,
+    Unpack,
+    cast,
+    overload,
+)
+import warnings
+
+import numpy as np
+
+from pandas._libs import lib
+from pandas._libs.parsers import STR_NA_VALUES
+from pandas.errors import (
+    AbstractMethodError,
+    ParserWarning,
+)
+from pandas.util._decorators import (
+    set_module,
+)
+from pandas.util._exceptions import find_stack_level
+from pandas.util._validators import check_dtype_backend
+
+from pandas.core.dtypes.common import (
+    is_file_like,
+    is_float,
+    is_integer,
+    is_list_like,
+    pandas_dtype,
+)
