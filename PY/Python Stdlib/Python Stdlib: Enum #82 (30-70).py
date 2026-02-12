c6a2ddbc9406fc6274faf04277949a35aeb0db3f
pmtrilio <pm@trilio.bg>
2026-02-12 19:31:30 +0200

    Upload file via PHP cURL

---
+++ PY/Python Stdlib/Python Stdlib: Enum #82 (30-70).py
@@ -0,0 +1,41 @@
+class member(object):
+    """
+    Forces item to become an Enum member during class creation.
+    """
+    def __init__(self, value):
+        self.value = value
+
+def _is_descriptor(obj):
+    """
+    Returns True if obj is a descriptor, False otherwise.
+    """
+    return (
+            hasattr(obj, '__get__') or
+            hasattr(obj, '__set__') or
+            hasattr(obj, '__delete__')
+            )
+
+def _is_dunder(name):
+    """
+    Returns True if a __dunder__ name, False otherwise.
+    """
+    return (
+            len(name) > 4 and
+            name[:2] == name[-2:] == '__' and
+            name[2] != '_' and
+            name[-3] != '_'
+            )
+
+def _is_sunder(name):
+    """
+    Returns True if a _sunder_ name, False otherwise.
+    """
+    return (
+            len(name) > 2 and
+            name[0] == name[-1] == '_' and
+            name[1] != '_' and
+            name[-2] != '_'
+            )
+
+def _is_internal_class(cls_name, obj):
+    # do not use `re` as `re` imports `enum`
