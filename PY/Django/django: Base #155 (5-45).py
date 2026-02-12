aa83077300921780a552e72ba2b31f4eda74bfbf
pmtrilio <pm@trilio.bg>
2026-02-12 19:33:22 +0200

    Upload file via PHP cURL

---
+++ PY/Django/django: Base #155 (5-45).py
@@ -0,0 +1,41 @@
+from functools import partialmethod
+from itertools import chain
+
+from asgiref.sync import sync_to_async
+
+import django
+from django.apps import apps
+from django.conf import settings
+from django.core import checks
+from django.core.exceptions import (
+    NON_FIELD_ERRORS,
+    FieldDoesNotExist,
+    FieldError,
+    MultipleObjectsReturned,
+    ObjectDoesNotExist,
+    ObjectNotUpdated,
+    ValidationError,
+)
+from django.db import (
+    DJANGO_VERSION_PICKLE_KEY,
+    DatabaseError,
+    connection,
+    connections,
+    router,
+    transaction,
+)
+from django.db.models import NOT_PROVIDED, ExpressionWrapper, IntegerField, Max, Value
+from django.db.models.constants import LOOKUP_SEP
+from django.db.models.deletion import CASCADE, DO_NOTHING, Collector, DatabaseOnDelete
+from django.db.models.expressions import DatabaseDefault
+from django.db.models.fetch_modes import FETCH_ONE
+from django.db.models.fields.composite import CompositePrimaryKey
+from django.db.models.fields.related import (
+    ForeignObjectRel,
+    OneToOneField,
+    lazy_related_operation,
+    resolve_relation,
+)
+from django.db.models.functions import Coalesce
+from django.db.models.manager import Manager
+from django.db.models.options import Options
