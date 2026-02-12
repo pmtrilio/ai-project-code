731e74ddea95282d4b5dd49479f4d68593eb1169
pmtrilio <pm@trilio.bg>
2026-02-12 20:08:20 +0200

    Upload file via PHP cURL

---
+++ CS/General/AutoMapper: Extensions #408 (20-60).cs
@@ -0,0 +1,41 @@
+    /// <param name="parameters">Optional parameter object for parameterized mapping expressions</param>
+    /// <param name="membersToExpand">Explicit members to expand</param>
+    /// <returns>Expression to project into</returns>
+    public static IQueryable<TDestination> ProjectTo<TDestination>(this IQueryable source, IConfigurationProvider configuration, object parameters, params Expression<Func<TDestination, object>>[] membersToExpand) =>
+        source.ToCore<TDestination>(configuration, parameters, membersToExpand.Select(MemberVisitor.GetMemberPath));
+    /// <summary>
+    /// Extension method to project from a queryable using the provided mapping engine
+    /// </summary>
+    /// <remarks>Projections are only calculated once and cached</remarks>
+    /// <typeparam name="TDestination">Destination type</typeparam>
+    /// <param name="source">Queryable source</param>
+    /// <param name="configuration">Mapper configuration</param>
+    /// <param name="membersToExpand">Explicit members to expand</param>
+    /// <returns>Expression to project into</returns>
+    public static IQueryable<TDestination> ProjectTo<TDestination>(this IQueryable source, IConfigurationProvider configuration,
+        params Expression<Func<TDestination, object>>[] membersToExpand) => 
+        source.ProjectTo(configuration, null, membersToExpand);
+    /// <summary>
+    /// Projects the source type to the destination type given the mapping configuration
+    /// </summary>
+    /// <typeparam name="TDestination">Destination type to map to</typeparam>
+    /// <param name="source">Queryable source</param>
+    /// <param name="configuration">Mapper configuration</param>
+    /// <param name="parameters">Optional parameter object for parameterized mapping expressions</param>
+    /// <param name="membersToExpand">Explicit members to expand</param>
+    /// <returns>Queryable result, use queryable extension methods to project and execute result</returns>
+    public static IQueryable<TDestination> ProjectTo<TDestination>(this IQueryable source, IConfigurationProvider configuration, ParameterBag parameters, params string[] membersToExpand) =>
+        source.ToCore<TDestination>(configuration, parameters, membersToExpand.Select(memberName => ReflectionHelper.GetMemberPath(typeof(TDestination), memberName)));
+    /// <summary>
+    /// Extension method to project from a queryable using the provided mapping engine
+    /// </summary>
+    /// <remarks>Projections are only calculated once and cached</remarks>
+    /// <param name="source">Queryable source</param>
+    /// <param name="destinationType">Destination type</param>
+    /// <param name="configuration">Mapper configuration</param>
+    /// <returns>Expression to project into</returns>
+    public static IQueryable ProjectTo(this IQueryable source, Type destinationType, IConfigurationProvider configuration) => 
+        source.ProjectTo(destinationType, configuration, null);
+    /// <summary>
+    /// Projects the source type to the destination type given the mapping configuration
+    /// </summary>
