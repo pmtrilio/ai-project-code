5ee818e57dd5a033bc2eebb2e9299fee25b4dbe9
pmtrilio <pm@trilio.bg>
2026-02-12 20:29:30 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: CompilerEngine #208 (10-70).php
@@ -0,0 +1,61 @@
+use Illuminate\View\Compilers\CompilerInterface;
+use Illuminate\View\ViewException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+
+class CompilerEngine extends PhpEngine
+{
+    /**
+     * The Blade compiler instance.
+     *
+     * @var \Illuminate\View\Compilers\CompilerInterface
+     */
+    protected $compiler;
+
+    /**
+     * A stack of the last compiled templates.
+     *
+     * @var array
+     */
+    protected $lastCompiled = [];
+
+    /**
+     * The view paths that were compiled or are not expired, keyed by the path.
+     *
+     * @var array<string, true>
+     */
+    protected $compiledOrNotExpired = [];
+
+    /**
+     * Create a new compiler engine instance.
+     *
+     * @param  \Illuminate\View\Compilers\CompilerInterface  $compiler
+     * @param  \Illuminate\Filesystem\Filesystem|null  $files
+     */
+    public function __construct(CompilerInterface $compiler, ?Filesystem $files = null)
+    {
+        parent::__construct($files ?: new Filesystem);
+
+        $this->compiler = $compiler;
+    }
+
+    /**
+     * Get the evaluated contents of the view.
+     *
+     * @param  string  $path
+     * @param  array  $data
+     * @return string
+     */
+    public function get($path, array $data = [])
+    {
+        $this->lastCompiled[] = $path;
+
+        // If this given view has expired, which means it has simply been edited since
+        // it was last compiled, we will re-compile the views so we can evaluate a
+        // fresh copy of the view. We'll pass the compiler the path of the view.
+        if (! isset($this->compiledOrNotExpired[$path]) && $this->compiler->isExpired($path)) {
+            $this->compiler->compile($path);
+        }
+
+        // Once we have the path to the compiled file, we will evaluate the paths with
+        // typical PHP just like any other templates. We also keep a stack of views
