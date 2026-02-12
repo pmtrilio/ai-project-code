8316ba3bb8df425d1401e545429064caad0331ca
pmtrilio <pm@trilio.bg>
2026-02-12 14:56:58 +0200

    Upload file via PHP cURL

---
+++ CS/.NET SDK/.NET SDK | MSBuild: GenerateRuntimeConfigurationFiles #348 (1-61).cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable disable
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using NuGet.Frameworks;
+using NuGet.ProjectModel;
+
+namespace Microsoft.NET.Build.Tasks
+{
+    /// <summary>
+    /// Generates the $(project).runtimeconfig.json and optionally $(project).runtimeconfig.dev.json files
+    /// for a project.
+    /// </summary>
+    public class GenerateRuntimeConfigurationFiles : TaskBase
+    {
+        public string AssetsFilePath { get; set; }
+
+        [Required]
+        public string TargetFramework { get; set; }
+
+        [Required]
+        public string TargetFrameworkMoniker { get; set; }
+
+        [Required]
+        public string RuntimeConfigPath { get; set; }
+
+        public string RuntimeConfigDevPath { get; set; }
+
+        public string RuntimeIdentifier { get; set; }
+
+        public string PlatformLibraryName { get; set; }
+
+        public ITaskItem[] RuntimeFrameworks { get; set; }
+
+        public string RollForward { get; set; }
+
+        public string UserRuntimeConfig { get; set; }
+
+        public ITaskItem[] HostConfigurationOptions { get; set; }
+
+        public ITaskItem[] AdditionalProbingPaths { get; set; }
+
+        public bool IsSelfContained { get; set; }
+
+        public bool WriteAdditionalProbingPathsToMainConfig { get; set; }
+
+        public bool WriteIncludedFrameworks { get; set; }
+
+        public bool GenerateRuntimeConfigDevFile { get; set; }
+
+        public bool AlwaysIncludeCoreFramework { get; set; }
+
+        List<ITaskItem> _filesWritten = new();
+
+        private static readonly string[] RollForwardValues = new string[]
