a68952f2bf20855b39eca57e6015c93116ab7987
pmtrilio <pm@trilio.bg>
2026-02-12 20:28:44 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Container #172 (5-45).php
@@ -0,0 +1,41 @@
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\di;
+
+use ReflectionClass;
+use ReflectionException;
+use ReflectionNamedType;
+use ReflectionParameter;
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Container implements a [dependency injection](https://en.wikipedia.org/wiki/Dependency_injection) container.
+ *
+ * A dependency injection (DI) container is an object that knows how to instantiate and configure objects and
+ * all their dependent objects. For more information about DI, please refer to
+ * [Martin Fowler's article](https://martinfowler.com/articles/injection.html).
+ *
+ * Container supports constructor injection as well as property injection.
+ *
+ * To use Container, you first need to set up the class dependencies by calling [[set()]].
+ * You then call [[get()]] to create a new class object. The Container will automatically instantiate
+ * dependent objects, inject them into the object being created, configure, and finally return the newly created object.
+ *
+ * By default, [[\Yii::$container]] refers to a Container instance which is used by [[\Yii::createObject()]]
+ * to create new object instances. You may use this method to replace the `new` operator
+ * when creating a new object, which gives you the benefit of automatic dependency resolution and default
+ * property configuration.
+ *
+ * Below is an example of using Container:
+ *
+ * ```
+ * namespace app\models;
+ *
+ * use yii\base\BaseObject;
+ * use yii\db\Connection;
+ * use yii\di\Container;
