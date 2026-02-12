7678c20bc9b4998af7ca5d7595afd061258ad561
pmtrilio <pm@trilio.bg>
2026-02-12 12:54:07 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Builder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Illuminate\Database\Query;
+
+use BackedEnum;
+use Carbon\CarbonPeriod;
+use Closure;
+use DateTimeInterface;
+use Illuminate\Contracts\Database\Query\Builder as BuilderContract;
+use Illuminate\Contracts\Database\Query\ConditionExpression;
+use Illuminate\Contracts\Database\Query\Expression as ExpressionContract;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Concerns\BuildsQueries;
+use Illuminate\Database\Concerns\BuildsWhereDateClauses;
+use Illuminate\Database\Concerns\ExplainsQueries;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\PostgresConnection;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\ForwardsCalls;
+use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
+use LogicException;
+use RuntimeException;
+use UnitEnum;
+
+use function Illuminate\Support\enum_value;
+
+class Builder implements BuilderContract
+{
+    /** @use \Illuminate\Database\Concerns\BuildsQueries<\stdClass> */
+    use BuildsWhereDateClauses, BuildsQueries, ExplainsQueries, ForwardsCalls, Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The database connection instance.
+     *
+     * @var \Illuminate\Database\ConnectionInterface
+     */
+    public $connection;
+
+    /**
+     * The database query grammar instance.
+     *
+     * @var \Illuminate\Database\Query\Grammars\Grammar
+     */
+    public $grammar;
+
+    /**
+     * The database query post processor instance.
+     *
+     * @var \Illuminate\Database\Query\Processors\Processor
