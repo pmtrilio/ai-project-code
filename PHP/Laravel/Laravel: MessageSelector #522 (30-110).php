5fb5dd092c00eaed5c80d4429de03a91408eeac7
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:56 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: MessageSelector #522 (30-110).php
@@ -0,0 +1,81 @@
+            return $segments[0];
+        }
+
+        return $segments[$pluralIndex];
+    }
+
+    /**
+     * Extract a translation string using inline conditions.
+     *
+     * @param  array  $segments
+     * @param  int  $number
+     * @return mixed
+     */
+    private function extract($segments, $number)
+    {
+        foreach ($segments as $part) {
+            if (! is_null($line = $this->extractFromString($part, $number))) {
+                return $line;
+            }
+        }
+    }
+
+    /**
+     * Get the translation string if the condition matches.
+     *
+     * @param  string  $part
+     * @param  int  $number
+     * @return mixed
+     */
+    private function extractFromString($part, $number)
+    {
+        preg_match('/^[\{\[]([^\[\]\{\}]*)[\}\]](.*)/s', $part, $matches);
+
+        if (count($matches) !== 3) {
+            return null;
+        }
+
+        $condition = $matches[1];
+
+        $value = $matches[2];
+
+        if (str_contains($condition, ',')) {
+            [$from, $to] = explode(',', $condition, 2);
+
+            if ($to === '*' && $number >= $from) {
+                return $value;
+            } elseif ($from === '*' && $number <= $to) {
+                return $value;
+            } elseif ($number >= $from && $number <= $to) {
+                return $value;
+            }
+        }
+
+        return $condition == $number ? $value : null;
+    }
+
+    /**
+     * Strip the inline conditions from each segment, just leaving the text.
+     *
+     * @param  array  $segments
+     * @return array
+     */
+    private function stripConditions($segments)
+    {
+        return (new Collection($segments))
+            ->map(fn ($part) => preg_replace('/^[\{\[]([^\[\]\{\}]*)[\}\]]/', '', $part))
+            ->all();
+    }
+
+    /**
+     * Get the index to use for pluralization.
+     *
+     * The plural rules are derived from code of the Zend Framework (2010-09-25), which
+     * is subject to the new BSD license (https://framework.zend.com/license)
+     * Copyright (c) 2005-2010 - Zend Technologies USA Inc. (http://www.zend.com)
+     *
+     * @param  string  $locale
+     * @param  int  $number
+     * @return int
+     */
+    public function getPluralIndex($locale, $number)
