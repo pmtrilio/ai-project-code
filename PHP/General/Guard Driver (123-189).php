8d2a922fe52a183160110ef2cfda2fecbd19f1af
pmtrilio <pm@trilio.bg>
2026-02-12 19:11:51 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Guard Driver (123-189).php
@@ -0,0 +1,27 @@
+    {
+        if (empty($credentials[$this->inputKey])) {
+            return false;
+        }
+
+        $credentials = [$this->storageKey => $credentials[$this->inputKey]];
+
+        if ($this->provider->retrieveByCredentials($credentials)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Set the current request instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return $this
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+}
