fabe5de4f89e746a974725e29b75fca046bddc8f
pmtrilio <pm@trilio.bg>
2026-02-12 19:34:08 +0200

    Upload file via PHP cURL

---
+++ PY/Scikit-learn/scikit-learn: Forest #191 (1-41).py
@@ -0,0 +1,41 @@
+"""
+Forest of trees-based ensemble methods.
+
+Those methods include random forests and extremely randomized trees.
+
+The module structure is the following:
+
+- The ``BaseForest`` base class implements a common ``fit`` method for all
+  the estimators in the module. The ``fit`` method of the base ``Forest``
+  class calls the ``fit`` method of each sub-estimator on random samples
+  (with replacement, a.k.a. bootstrap) of the training set.
+
+  The init of the sub-estimator is further delegated to the
+  ``BaseEnsemble`` constructor.
+
+- The ``ForestClassifier`` and ``ForestRegressor`` base classes further
+  implement the prediction logic by computing an average of the predicted
+  outcomes of the sub-estimators.
+
+- The ``RandomForestClassifier`` and ``RandomForestRegressor`` derived
+  classes provide the user with concrete implementations of
+  the forest ensemble method using classical, deterministic
+  ``DecisionTreeClassifier`` and ``DecisionTreeRegressor`` as
+  sub-estimator implementations.
+
+- The ``ExtraTreesClassifier`` and ``ExtraTreesRegressor`` derived
+  classes provide the user with concrete implementations of the
+  forest ensemble method using the extremely randomized trees
+  ``ExtraTreeClassifier`` and ``ExtraTreeRegressor`` as
+  sub-estimator implementations.
+
+Single and multi-output problems are both handled.
+"""
+
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+import threading
+from abc import ABCMeta, abstractmethod
+from numbers import Integral
+from warnings import warn
