aa3bc993a6d3aea9ee28dc4b3235e2d1df792ce7
pmtrilio <pm@trilio.bg>
2026-02-12 19:04:49 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Маршрутизиране – Router #453 (20-120).php
@@ -0,0 +1,101 @@
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Routing\Events\Routing;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Traits\Tappable;
+use JsonSerializable;
+use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
+use ReflectionClass;
+use stdClass;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+/**
+ * @mixin \Illuminate\Routing\RouteRegistrar
+ */
+class Router implements BindingRegistrar, RegistrarContract
+{
+    use Macroable {
+        __call as macroCall;
+    }
+    use Tappable;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The route collection instance.
+     *
+     * @var \Illuminate\Routing\RouteCollectionInterface
+     */
+    protected $routes;
+
+    /**
+     * The currently dispatched route instance.
+     *
+     * @var \Illuminate\Routing\Route|null
+     */
+    protected $current;
+
+    /**
+     * The request currently being dispatched.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $currentRequest;
+
+    /**
+     * All of the short-hand keys for middlewares.
+     *
+     * @var array
+     */
+    protected $middleware = [];
+
+    /**
+     * All of the middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [];
+
+    /**
+     * The priority-sorted list of middleware.
+     *
+     * Forces the listed middleware to always be in the given order.
+     *
+     * @var array
+     */
+    public $middlewarePriority = [];
+
+    /**
+     * The registered route value binders.
+     *
+     * @var array
+     */
+    protected $binders = [];
+
+    /**
+     * The globally available parameter patterns.
+     *
+     * @var array
+     */
+    protected $patterns = [];
+
+    /**
+     * The route group attribute stack.
+     *
+     * @var array
