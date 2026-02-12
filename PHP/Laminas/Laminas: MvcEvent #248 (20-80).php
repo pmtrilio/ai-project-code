5da687a5c2b14861ceb9453153df3bb5c1a5ad16
pmtrilio <pm@trilio.bg>
2026-02-12 20:30:17 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: MvcEvent #248 (20-80).php
@@ -0,0 +1,61 @@
+    public const EVENT_DISPATCH_ERROR = 'dispatch.error';
+    public const EVENT_FINISH         = 'finish';
+    public const EVENT_RENDER         = 'render';
+    public const EVENT_RENDER_ERROR   = 'render.error';
+    public const EVENT_ROUTE          = 'route';
+    /** @var ApplicationInterface|null */
+    protected $application;
+
+    /** @var Request */
+    protected $request;
+
+    /** @var Response */
+    protected $response;
+
+    /** @var mixed */
+    protected $result;
+
+    /** @var RouteStackInterface */
+    protected $router;
+
+    /** @var null|RouteMatch */
+    protected $routeMatch;
+
+    /** @var Model */
+    protected $viewModel;
+
+    /**
+     * Set application instance
+     *
+     * @return MvcEvent
+     */
+    public function setApplication(ApplicationInterface $application)
+    {
+        $this->setParam('application', $application);
+        $this->application = $application;
+        return $this;
+    }
+
+    /**
+     * Get application instance
+     *
+     * @return ApplicationInterface
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    /**
+     * Get router
+     *
+     * @return RouteStackInterface
+     */
+    public function getRouter()
+    {
+        return $this->router;
+    }
+
+    /**
+     * Set router
+     *
