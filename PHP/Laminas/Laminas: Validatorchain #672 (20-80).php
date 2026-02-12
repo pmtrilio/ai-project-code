92f9e0a8c75c1775f3112ca8e3335578c66389da
pmtrilio <pm@trilio.bg>
2026-02-12 14:51:21 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Validatorchain #672 (20-80).php
@@ -0,0 +1,61 @@
+use function is_string;
+use function rsort;
+
+use const SORT_NUMERIC;
+
+/**
+ * @psalm-type QueueElement = array{instance: ValidatorInterface, breakChainOnFailure: bool}
+ * @implements IteratorAggregate<array-key, QueueElement>
+ * @psalm-type ValidatorSpecification = array{
+ *     name: non-empty-string|class-string<ValidatorInterface>,
+ *     options?: array<string, mixed>,
+ *     break_chain_on_failure?: bool,
+ *     priority?: int,
+ * }
+ * @psalm-type ValidatorChainSpecification = array<array-key, ValidatorSpecification|ValidatorInterface>
+ */
+final class ValidatorChain implements Countable, IteratorAggregate, ValidatorChainInterface
+{
+    /**
+     * Default priority at which validators are added
+     *
+     * @deprecated Use ValidatorChainInterface::DEFAULT_PRIORITY instead.
+     */
+    public const DEFAULT_PRIORITY = ValidatorChainInterface::DEFAULT_PRIORITY;
+
+    /**
+     * Validator chain
+     *
+     * @var PriorityQueue<QueueElement, int>
+     */
+    private PriorityQueue $validators;
+
+    /**
+     * Array of validation failure messages
+     *
+     * @var array<string, string>
+     */
+    private array $messages = [];
+
+    /**
+     * Initialize validator chain
+     */
+    public function __construct(
+        private ValidatorPluginManager|null $pluginManager = null,
+    ) {
+        /** @var PriorityQueue<QueueElement, int> $queue */
+        $queue            = new PriorityQueue();
+        $this->validators = $queue;
+    }
+
+    /**
+     * Return the count of attached validators
+     */
+    public function count(): int
+    {
+        return count($this->validators);
+    }
+
+    /**
+     * Retrieve the Validator Plugin Manager used by this instance
+     *
