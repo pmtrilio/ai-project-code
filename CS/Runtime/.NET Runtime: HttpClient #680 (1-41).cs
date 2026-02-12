6599894e99983ef73eba509981ce3101e2a164d7
pmtrilio <pm@trilio.bg>
2026-02-12 15:01:14 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: HttpClient #680 (1-41).cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Runtime.Versioning;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public partial class HttpClient : HttpMessageInvoker
+    {
+        #region Fields
+
+        private static IWebProxy? s_defaultProxy;
+        private static readonly TimeSpan s_defaultTimeout = TimeSpan.FromSeconds(100);
+        private static readonly TimeSpan s_maxTimeout = TimeSpan.FromMilliseconds(int.MaxValue);
+        private static readonly TimeSpan s_infiniteTimeout = Threading.Timeout.InfiniteTimeSpan;
+        private const HttpCompletionOption DefaultCompletionOption = HttpCompletionOption.ResponseContentRead;
+
+        private volatile bool _operationStarted;
+        private volatile bool _disposed;
+
+        private CancellationTokenSource _pendingRequestsCts;
+        private HttpRequestHeaders? _defaultRequestHeaders;
+        private Version _defaultRequestVersion = HttpRequestMessage.DefaultRequestVersion;
+        private HttpVersionPolicy _defaultVersionPolicy = HttpRequestMessage.DefaultVersionPolicy;
+
+        private Uri? _baseAddress;
+        private TimeSpan _timeout;
+        private int _maxResponseContentBufferSize;
+
+        #endregion Fields
+
+        #region Properties
+        public static IWebProxy DefaultProxy
+        {
+            get => LazyInitializer.EnsureInitialized(ref s_defaultProxy, () => SystemProxyInfo.Proxy);
