112f531a47fb7221f0671ef337705b273bd0f90c
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:22 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: HttpClient #616 (5-65).cs
@@ -0,0 +1,61 @@
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Runtime.Versioning;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public partial class HttpClient : HttpMessageInvoker
+    {
+        #region Fields
+
+        private static IWebProxy? s_defaultProxy;
+        private static readonly TimeSpan s_defaultTimeout = TimeSpan.FromSeconds(100);
+        private static readonly TimeSpan s_maxTimeout = TimeSpan.FromMilliseconds(int.MaxValue);
+        private static readonly TimeSpan s_infiniteTimeout = Threading.Timeout.InfiniteTimeSpan;
+        private const HttpCompletionOption DefaultCompletionOption = HttpCompletionOption.ResponseContentRead;
+
+        private volatile bool _operationStarted;
+        private volatile bool _disposed;
+
+        private CancellationTokenSource _pendingRequestsCts;
+        private HttpRequestHeaders? _defaultRequestHeaders;
+        private Version _defaultRequestVersion = HttpRequestMessage.DefaultRequestVersion;
+        private HttpVersionPolicy _defaultVersionPolicy = HttpRequestMessage.DefaultVersionPolicy;
+
+        private Uri? _baseAddress;
+        private TimeSpan _timeout;
+        private int _maxResponseContentBufferSize;
+
+        #endregion Fields
+
+        #region Properties
+        public static IWebProxy DefaultProxy
+        {
+            get => LazyInitializer.EnsureInitialized(ref s_defaultProxy, () => SystemProxyInfo.Proxy);
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                s_defaultProxy = value;
+            }
+        }
+
+        public HttpRequestHeaders DefaultRequestHeaders =>
+            _defaultRequestHeaders ??= new HttpRequestHeaders();
+
+        public Version DefaultRequestVersion
+        {
+            get => _defaultRequestVersion;
+            set
+            {
+                CheckDisposedOrStarted();
+                ArgumentNullException.ThrowIfNull(value);
+                _defaultRequestVersion = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the default value of <see cref="HttpRequestMessage.VersionPolicy" /> for implicitly created requests in convenience methods,
+        /// e.g.: <see cref="GetAsync(string?)" />, <see cref="PostAsync(string?, HttpContent)" />.
