f62907e062741ebdc843922de9b1a42f14b7fd64
pmtrilio <pm@trilio.bg>
2026-02-12 15:10:07 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: KestrelServer #511 (20-80).cs
@@ -0,0 +1,61 @@
+/// </summary>
+public class KestrelServer : IServer
+{
+    private readonly KestrelServerImpl _innerKestrelServer;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="KestrelServer"/>.
+    /// </summary>
+    /// <param name="options">The Kestrel <see cref="IOptions{TOptions}"/>.</param>
+    /// <param name="transportFactory">The <see cref="IConnectionListenerFactory"/>.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
+    public KestrelServer(IOptions<KestrelServerOptions> options, IConnectionListenerFactory transportFactory, ILoggerFactory loggerFactory)
+    {
+        _innerKestrelServer = new KestrelServerImpl(
+            options,
+            new[] { transportFactory ?? throw new ArgumentNullException(nameof(transportFactory)) },
+            Array.Empty<IMultiplexedConnectionListenerFactory>(),
+            new SimpleHttpsConfigurationService(),
+            loggerFactory,
+            diagnosticSource: null,
+            new KestrelMetrics(new DummyMeterFactory()),
+            heartbeatHandlers: []);
+    }
+
+    /// <inheritdoc />
+    public IFeatureCollection Features => _innerKestrelServer.Features;
+
+    /// <summary>
+    /// Gets the <see cref="KestrelServerOptions"/>.
+    /// </summary>
+    public KestrelServerOptions Options => _innerKestrelServer.Options;
+
+    /// <inheritdoc />
+    public Task StartAsync<TContext>(IHttpApplication<TContext> application, CancellationToken cancellationToken) where TContext : notnull
+    {
+        return _innerKestrelServer.StartAsync(application, cancellationToken);
+    }
+
+    // Graceful shutdown if possible
+    /// <inheritdoc />
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return _innerKestrelServer.StopAsync(cancellationToken);
+    }
+
+    // Ungraceful shutdown
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _innerKestrelServer.Dispose();
+    }
+
+    // This factory used when type is created without DI. For example, via KestrelServer.
+    private sealed class DummyMeterFactory : IMeterFactory
+    {
+        public Meter Create(MeterOptions options) => new Meter(options);
+
+        public void Dispose() { }
+    }
+
+    private sealed class SimpleHttpsConfigurationService : IHttpsConfigurationService
