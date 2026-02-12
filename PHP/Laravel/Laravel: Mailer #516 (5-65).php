fc24873f2fad3f68c0de148b62ea79476989d0a1
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:47 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Mailer #516 (5-65).php
@@ -0,0 +1,61 @@
+use Closure;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Mail\Mailable as MailableContract;
+use Illuminate\Contracts\Mail\Mailer as MailerContract;
+use Illuminate\Contracts\Mail\MailQueue as MailQueueContract;
+use Illuminate\Contracts\Queue\Factory as QueueContract;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
+
+class Mailer implements MailerContract, MailQueueContract
+{
+    use Macroable;
+
+    /**
+     * The name that is configured for the mailer.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The view factory instance.
+     *
+     * @var \Illuminate\Contracts\View\Factory
+     */
+    protected $views;
+
+    /**
+     * The Symfony Transport instance.
+     *
+     * @var \Symfony\Component\Mailer\Transport\TransportInterface
+     */
+    protected $transport;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher|null
+     */
+    protected $events;
+
+    /**
+     * The global from address and name.
+     *
+     * @var array
+     */
+    protected $from;
+
+    /**
+     * The global reply-to address and name.
+     *
