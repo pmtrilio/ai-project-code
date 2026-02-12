f6ab5a35cde961cfbcbc33fed58e6cc23e4f96f8
pmtrilio <pm@trilio.bg>
2026-02-12 19:08:14 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: HTTP компоненти – Containerbuilder #625 (30-110).php
@@ -0,0 +1,81 @@
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\DependencyInjection\Compiler\Compiler;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\Compiler\ResolveEnvPlaceholdersPass;
+use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\LazyProxy\Instantiator\InstantiatorInterface;
+use Symfony\Component\DependencyInjection\LazyProxy\Instantiator\LazyServiceInstantiator;
+use Symfony\Component\DependencyInjection\LazyProxy\Instantiator\RealServiceInstantiator;
+use Symfony\Component\DependencyInjection\ParameterBag\EnvPlaceholderParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
+
+/**
+ * ContainerBuilder is a DI container that provides an API to easily describe services.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class ContainerBuilder extends Container implements TaggedContainerInterface
+{
+    /**
+     * @var array<string, ExtensionInterface>
+     */
+    private array $extensions = [];
+
+    /**
+     * @var array<string, ExtensionInterface>
+     */
+    private array $extensionsByNs = [];
+
+    /**
+     * @var array<string, Definition>
+     */
+    private array $definitions = [];
+
+    /**
+     * @var array<string, Alias>
+     */
+    private array $aliasDefinitions = [];
+
+    /**
+     * @var array<string, ResourceInterface>
+     */
+    private array $resources = [];
+
+    /**
+     * @var array<string, array<array<string, mixed>>>
+     */
+    private array $extensionConfigs = [];
+
+    private Compiler $compiler;
+    private bool $trackResources;
+    private InstantiatorInterface $proxyInstantiator;
+    private ExpressionLanguage $expressionLanguage;
+
+    /**
+     * @var ExpressionFunctionProviderInterface[]
+     */
+    private array $expressionLanguageProviders = [];
+
+    /**
+     * @var string[] with tag names used by findTaggedServiceIds
+     */
+    private array $usedTags = [];
+
+    /**
+     * @var string[][] a map of env var names to their placeholders
+     */
+    private array $envPlaceholders = [];
+
+    /**
+     * @var int[] a map of env vars to their resolution counter
