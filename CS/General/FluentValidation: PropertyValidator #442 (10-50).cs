0b2b4717e04ba2d884ae0890ba0d6edd779afb80
pmtrilio <pm@trilio.bg>
2026-02-12 20:08:43 +0200

    Upload file via PHP cURL

---
+++ CS/General/FluentValidation: PropertyValidator #442 (10-50).cs
@@ -0,0 +1,41 @@
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The latest version of this file can be found at https://github.com/FluentValidation/FluentValidation
+#endregion
+
+namespace FluentValidation.Validators;
+
+using Internal;
+
+public abstract class PropertyValidator<T, TProperty> : IPropertyValidator<T,TProperty> {
+
+	string IPropertyValidator.GetDefaultMessageTemplate(string errorCode)
+		=> GetDefaultMessageTemplate(errorCode);
+
+	/// <summary>
+	/// Returns the default error message template for this validator, when not overridden.
+	/// </summary>
+	/// <param name="errorCode">The currently configured error code for the validator.</param>
+	/// <returns></returns>
+	protected virtual string GetDefaultMessageTemplate(string errorCode) => "No default error message has been specified";
+
+	/// <inheritdoc />
+	public abstract string Name { get; }
+
+	/// <summary>
+	/// Retrieves a localized string from the LanguageManager.
+	/// If an ErrorCode is defined for this validator, the error code is used as the key.
+	/// If no ErrorCode is defined (or the language manager doesn't have a translation for the error code)
+	/// then the fallback key is used instead.
+	/// </summary>
+	/// <param name="errorCode">The currently configured error code for the validator.</param>
+	/// <param name="fallbackKey">The fallback key to use for translation, if no ErrorCode is available.</param>
+	/// <returns>The translated error message template.</returns>
+	protected string Localized(string errorCode, string fallbackKey) {
+		return ValidatorOptions.Global.LanguageManager.ResolveErrorMessageUsingErrorCode(errorCode, fallbackKey);
+	}
+
