fed8a91065fc26e7e52f4d5f9a58da35c0f2dd45
pmtrilio <pm@trilio.bg>
2026-02-12 15:09:44 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: IPAddress #482 (5-105).cs
@@ -0,0 +1,101 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace System.Net
+{
+    /// <devdoc>
+    ///   <para>
+    ///     Provides an Internet Protocol (IP) address.
+    ///   </para>
+    /// </devdoc>
+    public class IPAddress : ISpanFormattable, ISpanParsable<IPAddress>, IUtf8SpanFormattable, IUtf8SpanParsable<IPAddress>
+    {
+        public static readonly IPAddress Any = new ReadOnlyIPAddress([0, 0, 0, 0]);
+        public static readonly IPAddress Loopback = new ReadOnlyIPAddress([127, 0, 0, 1]);
+        public static readonly IPAddress Broadcast = new ReadOnlyIPAddress([255, 255, 255, 255]);
+        public static readonly IPAddress None = Broadcast;
+
+        internal const uint LoopbackMaskHostOrder = 0xFF000000;
+
+        public static readonly IPAddress IPv6Any = new ReadOnlyIPAddress([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0);
+        public static readonly IPAddress IPv6Loopback = new ReadOnlyIPAddress([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 0);
+        public static readonly IPAddress IPv6None = IPv6Any;
+
+        private static readonly IPAddress s_loopbackMappedToIPv6 = new ReadOnlyIPAddress([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 127, 0, 0, 1], 0);
+
+        /// <summary>
+        /// For IPv4 addresses, this field stores the Address.
+        /// For IPv6 addresses, this field stores the ScopeId.
+        /// Instead of accessing this field directly, use the <see cref="PrivateAddress"/> or <see cref="PrivateScopeId"/> properties.
+        /// </summary>
+        private uint _addressOrScopeId;
+
+        /// <summary>
+        /// This field is only used for IPv6 addresses. A null value indicates that this instance is an IPv4 address.
+        /// </summary>
+        private readonly ushort[]? _numbers;
+
+        /// <summary>
+        /// A lazily initialized cache of the result of calling <see cref="ToString"/>.
+        /// </summary>
+        private string? _toString;
+
+        /// <summary>
+        /// A lazily initialized cache of the <see cref="GetHashCode"/> value.
+        /// </summary>
+        private int _hashCode;
+
+        internal const int NumberOfLabels = IPAddressParserStatics.IPv6AddressBytes / 2;
+
+        [MemberNotNullWhen(false, nameof(_numbers))]
+        private bool IsIPv4
+        {
+            get { return _numbers == null; }
+        }
+
+        [MemberNotNullWhen(true, nameof(_numbers))]
+        private bool IsIPv6
+        {
+            get { return _numbers != null; }
+        }
+
+        internal uint PrivateAddress
+        {
+            get
+            {
+                Debug.Assert(IsIPv4);
+                return _addressOrScopeId;
+            }
+            private set
+            {
+                Debug.Assert(IsIPv4);
+                _toString = null;
+                _hashCode = 0;
+                _addressOrScopeId = value;
+            }
+        }
+
+        internal uint PrivateIPv4Address
+        {
+            get
+            {
+                Debug.Assert(IsIPv4 || IsIPv4MappedToIPv6);
+                if (IsIPv4)
+                {
+                    return _addressOrScopeId;
+                }
+                uint address = (uint)_numbers[6] << 16 | (uint)_numbers[7];
+                return (uint)HostToNetworkOrder(unchecked((int)address));
+            }
+        }
+
+        private uint PrivateScopeId
+        {
+            get
+            {
+                Debug.Assert(IsIPv6);
