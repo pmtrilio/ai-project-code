998d7317b11fc01ec39eb5aa1a75d7262b2a420b
pmtrilio <pm@trilio.bg>
2026-02-12 19:13:38 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: DefaultFilesOptions #172 (5-125).cs
@@ -0,0 +1,38 @@
+
+namespace Microsoft.AspNetCore.Builder;
+
+/// <summary>
+/// Options for selecting default file names.
+/// </summary>
+public class DefaultFilesOptions : SharedOptionsBase
+{
+    /// <summary>
+    /// Configuration for the DefaultFilesMiddleware.
+    /// </summary>
+    public DefaultFilesOptions()
+        : this(new SharedOptions())
+    {
+    }
+
+    /// <summary>
+    /// Configuration for the DefaultFilesMiddleware.
+    /// </summary>
+    /// <param name="sharedOptions"></param>
+    public DefaultFilesOptions(SharedOptions sharedOptions)
+        : base(sharedOptions)
+    {
+        // Prioritized list
+        DefaultFileNames = new List<string>
+            {
+                "default.htm",
+                "default.html",
+                "index.htm",
+                "index.html",
+            };
+    }
+
+    /// <summary>
+    /// An ordered list of file names to select by default. List length and ordering may affect performance.
+    /// </summary>
+    public IList<string> DefaultFileNames { get; set; }
+}
