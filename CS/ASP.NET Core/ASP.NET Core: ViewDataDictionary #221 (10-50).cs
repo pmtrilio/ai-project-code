e21507ec061fb7cae0885809002f92a8b2a3a947
pmtrilio <pm@trilio.bg>
2026-02-12 15:06:08 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: ViewDataDictionary #221 (10-50).cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Shared;
+using Microsoft.Extensions.Internal;
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+/// <summary>
+/// A <see cref="IDictionary{TKey, TValue}"/> for view data.
+/// </summary>
+[DebuggerDisplay("Count = {Count}")]
+[DebuggerTypeProxy(typeof(DictionaryDebugView<string, object?>))]
+public class ViewDataDictionary : IDictionary<string, object?>
+{
+    private readonly IDictionary<string, object?> _data;
+    private readonly Type _declaredModelType;
+    private readonly IModelMetadataProvider _metadataProvider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class.
+    /// </summary>
+    /// <param name="metadataProvider">
+    /// <see cref="IModelMetadataProvider"/> instance used to create <see cref="ViewFeatures.ModelExplorer"/>
+    /// instances.
+    /// </param>
+    /// <param name="modelState"><see cref="ModelStateDictionary"/> instance for this scope.</param>
+    /// <remarks>For use when creating a <see cref="ViewDataDictionary"/> for a new top-level scope.</remarks>
+    public ViewDataDictionary(
+        IModelMetadataProvider metadataProvider,
+        ModelStateDictionary modelState)
+        : this(metadataProvider, modelState, declaredModelType: typeof(object))
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class based entirely on an existing
+    /// instance.
+    /// </summary>
+    /// <param name="source"><see cref="ViewDataDictionary"/> instance to copy initial values from.</param>
+    /// <remarks>
+    /// <para>
+    /// For use when copying a <see cref="ViewDataDictionary"/> instance and the declared <see cref="Model"/>
