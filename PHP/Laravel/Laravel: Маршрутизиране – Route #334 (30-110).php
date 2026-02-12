ee086912b0a60da171b5e3b4f56f677216638c2c
pmtrilio <pm@trilio.bg>
2026-02-12 19:02:25 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Маршрутизиране – Route #334 (30-110).php
@@ -0,0 +1,80 @@
+class Route
+{
+    use Conditionable, CreatesRegularExpressionRouteConstraints, FiltersControllerMiddleware, Macroable, ResolvesRouteDependencies;
+
+    /**
+     * The URI pattern the route responds to.
+     *
+     * @var string
+     */
+    public $uri;
+
+    /**
+     * The HTTP methods the route responds to.
+     *
+     * @var array
+     */
+    public $methods;
+
+    /**
+     * The route action array.
+     *
+     * @var array
+     */
+    public $action;
+
+    /**
+     * Indicates whether the route is a fallback route.
+     *
+     * @var bool
+     */
+    public $isFallback = false;
+
+    /**
+     * The controller instance.
+     *
+     * @var mixed
+     */
+    public $controller;
+
+    /**
+     * The default values for the route.
+     *
+     * @var array
+     */
+    public $defaults = [];
+
+    /**
+     * The regular expression requirements.
+     *
+     * @var array
+     */
+    public $wheres = [];
+
+    /**
+     * The array of matched parameters.
+     *
+     * @var array|null
+     */
+    public $parameters;
+
+    /**
+     * The parameter names for the route.
+     *
+     * @var array|null
+     */
+    public $parameterNames;
+
+    /**
+     * The array of the matched parameters' original values.
+     *
+     * @var array
+     */
+    protected $originalParameters;
+
+    /**
+     * Indicates "trashed" models can be retrieved when resolving implicit model bindings for this route.
+     *
+     * @var bool
+     */
+    protected $withTrashedBindings = false;
