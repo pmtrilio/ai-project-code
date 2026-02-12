63482a0b1a660efab4433eae01d6b879e0849f37
pmtrilio <pm@trilio.bg>
2026-02-12 20:25:51 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: OptionsResolver #31 (20-120).php
@@ -0,0 +1,100 @@
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
+
+/**
+ * Validates options and merges them with default values.
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ * @author Tobias Schultze <http://tobion.de>
+ */
+class OptionsResolver implements Options
+{
+    private const VALIDATION_FUNCTIONS = [
+        'bool' => 'is_bool',
+        'boolean' => 'is_bool',
+        'int' => 'is_int',
+        'integer' => 'is_int',
+        'long' => 'is_int',
+        'float' => 'is_float',
+        'double' => 'is_float',
+        'real' => 'is_float',
+        'numeric' => 'is_numeric',
+        'string' => 'is_string',
+        'scalar' => 'is_scalar',
+        'array' => 'is_array',
+        'iterable' => 'is_iterable',
+        'countable' => 'is_countable',
+        'callable' => 'is_callable',
+        'object' => 'is_object',
+        'resource' => 'is_resource',
+    ];
+
+    /**
+     * The names of all defined options.
+     */
+    private array $defined = [];
+
+    /**
+     * The default option values.
+     */
+    private array $defaults = [];
+
+    /**
+     * A list of closure for nested options.
+     *
+     * @var \Closure[][]
+     */
+    private array $nested = [];
+
+    /**
+     * The names of required options.
+     */
+    private array $required = [];
+
+    /**
+     * The resolved option values.
+     */
+    private array $resolved = [];
+
+    /**
+     * A list of normalizer closures.
+     *
+     * @var \Closure[][]
+     */
+    private array $normalizers = [];
+
+    /**
+     * A list of accepted values for each option.
+     */
+    private array $allowedValues = [];
+
+    /**
+     * A list of accepted types for each option.
+     */
+    private array $allowedTypes = [];
+
+    /**
+     * A list of info messages for each option.
+     */
+    private array $info = [];
+
+    /**
+     * A list of closures for evaluating lazy options.
+     */
+    private array $lazy = [];
+
+    /**
+     * A list of lazy options whose closure is currently being called.
+     *
+     * This list helps detecting circular dependencies between lazy options.
+     */
+    private array $calling = [];
+
+    /**
+     * A list of deprecated options.
+     */
+    private array $deprecated = [];
+
+    /**
+     * The list of options provided by the user.
+     */
+    private array $given = [];
