4d6d07c0df3491c76ce7c2d533ba375f667b9d27
pmtrilio <pm@trilio.bg>
2026-02-12 20:27:56 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Command #132 (10-50).php
@@ -0,0 +1,41 @@
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class Command extends SymfonyCommand
+{
+    use Concerns\CallsCommands,
+        Concerns\ConfiguresPrompts,
+        Concerns\HasParameters,
+        Concerns\InteractsWithIO,
+        Concerns\InteractsWithSignals,
+        Concerns\PromptsForMissingInput,
+        Macroable;
+
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $laravel;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '';
