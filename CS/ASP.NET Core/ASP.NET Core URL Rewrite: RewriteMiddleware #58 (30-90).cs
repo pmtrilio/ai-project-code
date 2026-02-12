71f16c7c9c5b8d473dc8b98dfd08673a17bc4ded
pmtrilio <pm@trilio.bg>
2026-02-12 15:13:42 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteMiddleware #58 (30-90).cs
@@ -0,0 +1,61 @@
+    /// <param name="loggerFactory">The Logger Factory.</param>
+    /// <param name="options">The middleware options, containing the rules to apply.</param>
+    public RewriteMiddleware(
+        RequestDelegate next,
+        IWebHostEnvironment hostingEnvironment,
+        ILoggerFactory loggerFactory,
+        IOptions<RewriteOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next;
+        _options = options.Value;
+        _fileProvider = _options.StaticFileProvider ?? hostingEnvironment.WebRootFileProvider;
+        _logger = loggerFactory.CreateLogger<RewriteMiddleware>();
+    }
+
+    /// <summary>
+    /// Executes the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+    /// <returns>A task that represents the execution of this middleware.</returns>
+    public Task Invoke(HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var rewriteContext = new RewriteContext
+        {
+            HttpContext = context,
+            StaticFileProvider = _fileProvider,
+            Logger = _logger,
+            Result = RuleResult.ContinueRules
+        };
+
+        var originalPath = context.Request.Path;
+
+        RunRules(rewriteContext, _options, context, _logger);
+        if (rewriteContext.Result == RuleResult.EndResponse)
+        {
+            return Task.CompletedTask;
+        }
+
+        // If a rule changed the path we want routing to find a new endpoint
+        if (originalPath != context.Request.Path)
+        {
+            if (_options.BranchedNext is not null)
+            {
+                // An endpoint may have already been set. Since we're going to re-invoke the middleware pipeline we need to reset
+                // the endpoint and route values to ensure things are re-calculated.
+                context.SetEndpoint(endpoint: null);
+                context.Features.Get<IRouteValuesFeature>()?.RouteValues = null!;
+                return _options.BranchedNext(context);
+            }
+        }
+
+        return _next(context);
+    }
+
+    static void RunRules(RewriteContext rewriteContext, RewriteOptions options, HttpContext httpContext, ILogger logger)
+    {
+        foreach (var rule in options.Rules)
