686f23a8d2685b28cee0a000b9a06ce91cab6c7c
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:27 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: MvcEvent #500 (10-110).php
@@ -0,0 +1,101 @@
+use Laminas\View\Model\ModelInterface as Model;
+use Laminas\View\Model\ViewModel;
+
+class MvcEvent extends Event
+{
+    /**#@+
+     * Mvc events triggered by eventmanager
+     */
+    public const EVENT_BOOTSTRAP      = 'bootstrap';
+    public const EVENT_DISPATCH       = 'dispatch';
+    public const EVENT_DISPATCH_ERROR = 'dispatch.error';
+    public const EVENT_FINISH         = 'finish';
+    public const EVENT_RENDER         = 'render';
+    public const EVENT_RENDER_ERROR   = 'render.error';
+    public const EVENT_ROUTE          = 'route';
+    /** @var ApplicationInterface|null */
+    protected $application;
+
+    /** @var Request */
+    protected $request;
+
+    /** @var Response */
+    protected $response;
+
+    /** @var mixed */
+    protected $result;
+
+    /** @var RouteStackInterface */
+    protected $router;
+
+    /** @var null|RouteMatch */
+    protected $routeMatch;
+
+    /** @var Model */
+    protected $viewModel;
+
+    /**
+     * Set application instance
+     *
+     * @return MvcEvent
+     */
+    public function setApplication(ApplicationInterface $application)
+    {
+        $this->setParam('application', $application);
+        $this->application = $application;
+        return $this;
+    }
+
+    /**
+     * Get application instance
+     *
+     * @return ApplicationInterface
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    /**
+     * Get router
+     *
+     * @return RouteStackInterface
+     */
+    public function getRouter()
+    {
+        return $this->router;
+    }
+
+    /**
+     * Set router
+     *
+     * @return MvcEvent
+     */
+    public function setRouter(RouteStackInterface $router)
+    {
+        $this->setParam('router', $router);
+        $this->router = $router;
+        return $this;
+    }
+
+    /**
+     * Get route match
+     *
+     * @return null|RouteMatch
+     */
+    public function getRouteMatch()
+    {
+        return $this->routeMatch;
+    }
+
+    /**
+     * Set route match
+     *
+     * @return MvcEvent
+     */
+    public function setRouteMatch(RouteMatch $matches)
+    {
+        $this->setParam('route-match', $matches);
+        $this->routeMatch = $matches;
+        return $this;
+    }
