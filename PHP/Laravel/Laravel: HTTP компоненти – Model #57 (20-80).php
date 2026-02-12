5ede1cc6fe3ac7730a94f02c8d6cf81419f98530
pmtrilio <pm@trilio.bg>
2026-02-12 14:29:36 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Model #57 (20-80).php
@@ -0,0 +1,61 @@
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\Concerns\AsPivot;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection as BaseCollection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable as SupportStringable;
+use Illuminate\Support\Traits\ForwardsCalls;
+use JsonException;
+use JsonSerializable;
+use LogicException;
+use ReflectionClass;
+use ReflectionMethod;
+use Stringable;
+
+use function Illuminate\Support\enum_value;
+
+abstract class Model implements Arrayable, ArrayAccess, CanBeEscapedWhenCastToString, HasBroadcastChannel, Jsonable, JsonSerializable, QueueableEntity, Stringable, UrlRoutable
+{
+    use Concerns\HasAttributes,
+        Concerns\HasEvents,
+        Concerns\HasGlobalScopes,
+        Concerns\HasRelationships,
+        Concerns\HasTimestamps,
+        Concerns\HasUniqueIds,
+        Concerns\HidesAttributes,
+        Concerns\GuardsAttributes,
+        Concerns\PreventsCircularRecursion,
+        Concerns\TransformsToResource,
+        ForwardsCalls;
+    /** @use HasCollection<\Illuminate\Database\Eloquent\Collection<array-key, static & self>> */
+    use HasCollection;
+
+    /**
+     * The connection name for the model.
+     *
+     * @var \UnitEnum|string|null
+     */
+    protected $connection;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string|null
+     */
+    protected $table;
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
