d29465f2d0fedf25cacb139e9ff7a1434597b969
pmtrilio <pm@trilio.bg>
2026-02-12 19:06:20 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Dispatcher #530 (20-60).php
@@ -0,0 +1,41 @@
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Traits\ReflectsClosures;
+use ReflectionClass;
+
+use function Illuminate\Support\enum_value;
+
+class Dispatcher implements DispatcherContract
+{
+    use Macroable, ReflectsClosures, ResolvesQueueRoutes;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The registered event listeners.
+     *
+     * @var array<string, callable|array|class-string|null>
+     */
+    protected $listeners = [];
+
+    /**
+     * The wildcard listeners.
+     *
+     * @var array<string, \Closure|string>
+     */
+    protected $wildcards = [];
+
+    /**
+     * The cached wildcard listeners.
+     *
+     * @var array<string, \Closure|string>
+     */
+    protected $wildcardsCache = [];
+
+    /**
+     * The queue resolver instance.
