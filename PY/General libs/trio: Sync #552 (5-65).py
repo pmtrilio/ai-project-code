33287ce96eb8aafc1b47cd0846d6e3e9545bb641
pmtrilio <pm@trilio.bg>
2026-02-12 19:41:35 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/trio: Sync #552 (5-65).py
@@ -0,0 +1,60 @@
+
+import attrs
+
+import trio
+
+from . import _core
+from ._core import (
+    Abort,
+    ParkingLot,
+    RaiseCancelT,
+    add_parking_lot_breaker,
+    enable_ki_protection,
+    remove_parking_lot_breaker,
+)
+from ._deprecate import warn_deprecated
+from ._util import final
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from types import TracebackType
+
+    from typing_extensions import deprecated
+
+    from ._core import Task
+    from ._core._parking_lot import ParkingLotStatistics
+else:
+    T = TypeVar("T")
+
+    def deprecated(
+        message: str,
+        /,
+        *,
+        category: type[Warning] | None = DeprecationWarning,
+        stacklevel: int = 1,
+    ) -> Callable[[T], T]:
+        def wrapper(f: T) -> T:
+            return f
+
+        return wrapper
+
+
+@attrs.frozen
+class EventStatistics:
+    """An object containing debugging information.
+
+    Currently the following fields are defined:
+
+    * ``tasks_waiting``: The number of tasks blocked on this event's
+      :meth:`trio.Event.wait` method.
+
+    """
+
+    tasks_waiting: int
+
+
+@final
+@attrs.define(repr=False, eq=False)
+class Event:
+    """A waitable boolean value useful for inter-task synchronization,
+    inspired by :class:`threading.Event`.
