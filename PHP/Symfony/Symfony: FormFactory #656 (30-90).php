a4473c24cfbf4dfb12106c2ab8955a52e72439ba
pmtrilio <pm@trilio.bg>
2026-02-12 15:39:02 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: FormFactory #656 (30-90).php
@@ -0,0 +1,60 @@
+
+    public function createNamed(string $name, string $type = FormType::class, mixed $data = null, array $options = []): FormInterface
+    {
+        return $this->createNamedBuilder($name, $type, $data, $options)->getForm();
+    }
+
+    public function createForProperty(string $class, string $property, mixed $data = null, array $options = []): FormInterface
+    {
+        return $this->createBuilderForProperty($class, $property, $data, $options)->getForm();
+    }
+
+    public function createBuilder(string $type = FormType::class, mixed $data = null, array $options = []): FormBuilderInterface
+    {
+        return $this->createNamedBuilder($this->registry->getType($type)->getBlockPrefix(), $type, $data, $options);
+    }
+
+    public function createNamedBuilder(string $name, string $type = FormType::class, mixed $data = null, array $options = []): FormBuilderInterface
+    {
+        if (null !== $data && !\array_key_exists('data', $options)) {
+            $options['data'] = $data;
+        }
+
+        $type = $this->registry->getType($type);
+
+        $builder = $type->createBuilder($this, $name, $options);
+
+        // Explicitly call buildForm() in order to be able to override either
+        // createBuilder() or buildForm() in the resolved form type
+        $type->buildForm($builder, $builder->getOptions());
+
+        return $builder;
+    }
+
+    public function createBuilderForProperty(string $class, string $property, mixed $data = null, array $options = []): FormBuilderInterface
+    {
+        if (null === $guesser = $this->registry->getTypeGuesser()) {
+            return $this->createNamedBuilder($property, TextType::class, $data, $options);
+        }
+
+        $typeGuess = $guesser->guessType($class, $property);
+        $maxLengthGuess = $guesser->guessMaxLength($class, $property);
+        $requiredGuess = $guesser->guessRequired($class, $property);
+        $patternGuess = $guesser->guessPattern($class, $property);
+
+        $type = $typeGuess ? $typeGuess->getType() : TextType::class;
+
+        $maxLength = $maxLengthGuess?->getValue();
+        $pattern = $patternGuess?->getValue();
+
+        if (null !== $pattern) {
+            $options = array_replace_recursive(['attr' => ['pattern' => $pattern]], $options);
+        }
+
+        if (null !== $maxLength) {
+            $options = array_replace_recursive(['attr' => ['maxlength' => $maxLength]], $options);
+        }
+
+        if ($requiredGuess) {
+            $options = array_merge(['required' => $requiredGuess->getValue()], $options);
+        }
