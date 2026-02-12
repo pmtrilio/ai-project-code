d5115b257342f2a52893f16675c77a1348a50ce9
pmtrilio <pm@trilio.bg>
2026-02-12 20:01:51 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: BroadcastManager #593 (30-90).php
@@ -0,0 +1,61 @@
+ */
+class BroadcastManager implements FactoryContract
+{
+    use ResolvesQueueRoutes;
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $app;
+
+    /**
+     * The array of resolved broadcast drivers.
+     *
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * Create a new manager instance.
+     *
+     * @param  \Illuminate\Contracts\Container\Container  $app
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register the routes for handling broadcast channel authentication and sockets.
+     *
+     * @param  array|null  $attributes
+     * @return void
+     */
+    public function routes(?array $attributes = null)
+    {
+        if ($this->app instanceof CachesRoutes && $this->app->routesAreCached()) {
+            return;
+        }
+
+        $attributes = $attributes ?: ['middleware' => ['web']];
+
+        $this->app['router']->group($attributes, function ($router) {
+            $router->match(
+                ['get', 'post'], '/broadcasting/auth',
+                '\\'.BroadcastController::class.'@authenticate'
+            )->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class]);
+        });
+    }
+
+    /**
+     * Register the routes for handling broadcast user authentication.
+     *
