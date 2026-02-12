ae01d031eb93a7ba7f66909365f678b5ac792726
pmtrilio <pm@trilio.bg>
2026-02-12 15:42:24 +0200

    Upload file via PHP cURL

---
+++ PHP/Monolog/Monolog: UidProcessor #139 (20-60).php
@@ -0,0 +1,41 @@
+ * @author Simon Mönch <sm@webfactory.de>
+ */
+class UidProcessor implements ProcessorInterface, ResettableInterface
+{
+    /** @var non-empty-string */
+    private string $uid;
+
+    /**
+     * @param int<1, 32> $length
+     */
+    public function __construct(int $length = 7)
+    {
+        if ($length > 32 || $length < 1) {
+            throw new \InvalidArgumentException('The uid length must be an integer between 1 and 32');
+        }
+
+        $this->uid = $this->generateUid($length);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(LogRecord $record): LogRecord
+    {
+        $record->extra['uid'] = $this->uid;
+
+        return $record;
+    }
+
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    public function reset(): void
+    {
+        $this->uid = $this->generateUid(\strlen($this->uid));
+    }
+
+    /**
+     * @param  positive-int     $length
