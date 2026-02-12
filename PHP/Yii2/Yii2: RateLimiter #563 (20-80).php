a3c172edb81a5de5835e2e94cd3d3cd1047d2518
pmtrilio <pm@trilio.bg>
2026-02-12 20:01:24 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: RateLimiter #563 (20-80).php
@@ -0,0 +1,60 @@
+use yii\web\Response;
+use yii\web\TooManyRequestsHttpException;
+
+/**
+ * RateLimiter implements a rate limiting algorithm based on the [leaky bucket algorithm](https://en.wikipedia.org/wiki/Leaky_bucket).
+ *
+ * You may use RateLimiter by attaching it as a behavior to a controller or module, like the following,
+ *
+ * ```
+ * public function behaviors()
+ * {
+ *     return [
+ *         'rateLimiter' => [
+ *             'class' => \yii\filters\RateLimiter::class,
+ *         ],
+ *     ];
+ * }
+ * ```
+ *
+ * When the user has exceeded his rate limit, RateLimiter will throw a [[TooManyRequestsHttpException]] exception.
+ *
+ * Note that RateLimiter requires [[user]] to implement the [[RateLimitInterface]]. RateLimiter will
+ * do nothing if [[user]] is not set or does not implement [[RateLimitInterface]].
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ *
+ * @template T of Component
+ * @extends ActionFilter<T>
+ */
+class RateLimiter extends ActionFilter
+{
+    /**
+     * @var bool whether to include rate limit headers in the response
+     */
+    public $enableRateLimitHeaders = true;
+    /**
+     * @var string the message to be displayed when rate limit exceeds
+     */
+    public $errorMessage = 'Rate limit exceeded.';
+    /**
+     * @var RateLimitInterface|IdentityInterface|Closure|null the user object that implements the RateLimitInterface. If not set, it will take the value of `Yii::$app->user->getIdentity(false)`.
+     * {@since 2.0.38} It's possible to provide a closure function in order to assign the user identity on runtime. Using a closure to assign the user identity is recommend
+     * when you are **not** using the standard `Yii::$app->user` component. See the example below:
+     * ```
+     * 'user' => function() {
+     *     return Yii::$app->apiUser->identity;
+     * }
+     * ```
+     */
+    public $user;
+    /**
+     * @var Request|null the current request. If not set, the `request` application component will be used.
+     */
+    public $request;
+    /**
+     * @var Response|null the response to be sent. If not set, the `response` application component will be used.
+     */
+    public $response;
+
