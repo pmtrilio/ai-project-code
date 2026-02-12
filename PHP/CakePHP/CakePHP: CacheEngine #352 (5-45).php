0d304fdeccf06a97566dd0d734f14c70529aa37a
pmtrilio <pm@trilio.bg>
2026-02-12 20:32:25 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: CacheEngine #352 (5-45).php
@@ -0,0 +1,41 @@
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Cache;
+
+use Cake\Cache\Event\CacheAfterAddEvent;
+use Cake\Cache\Event\CacheBeforeAddEvent;
+use Cake\Cache\Exception\InvalidArgumentException;
+use Cake\Core\InstanceConfigTrait;
+use Cake\Event\EventDispatcherInterface;
+use Cake\Event\EventDispatcherTrait;
+use DateInterval;
+use DateTime;
+use Psr\SimpleCache\CacheInterface;
+use function Cake\Core\triggerWarning;
+
+/**
+ * Storage engine for CakePHP caching
+ *
+ * @template TSubject of \Cake\Cache\CacheEngine
+ * @implements \Cake\Event\EventDispatcherInterface<TSubject>
+ */
+abstract class CacheEngine implements CacheInterface, CacheEngineInterface, EventDispatcherInterface
+{
+    /**
+     * @use \Cake\Event\EventDispatcherTrait<TSubject>
+     */
+    use EventDispatcherTrait;
+    use InstanceConfigTrait;
+
+    /**
+     * @var string
