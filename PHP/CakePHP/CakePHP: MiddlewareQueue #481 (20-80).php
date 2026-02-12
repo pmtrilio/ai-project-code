f05c9f4e10af101d764cf27b25bdc1f6b8a980df
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:02 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: MiddlewareQueue #481 (20-80).php
@@ -0,0 +1,61 @@
+use Cake\Core\ContainerInterface;
+use Cake\Http\Middleware\ClosureDecoratorMiddleware;
+use Closure;
+use Countable;
+use InvalidArgumentException;
+use LogicException;
+use OutOfBoundsException;
+use Psr\Http\Server\MiddlewareInterface;
+use SeekableIterator;
+
+/**
+ * Provides methods for creating and manipulating a "queue" of middlewares.
+ * This queue is used to process a request and generate response via \Cake\Http\Runner.
+ *
+ * @template-implements \SeekableIterator<int, \Psr\Http\Server\MiddlewareInterface>
+ */
+class MiddlewareQueue implements Countable, SeekableIterator
+{
+    /**
+     * Internal position for iterator.
+     *
+     * @var int
+     */
+    protected int $position = 0;
+
+    /**
+     * The queue of middlewares.
+     *
+     * @var array<int, mixed>
+     */
+    protected array $queue = [];
+
+    /**
+     * @var \Cake\Core\ContainerInterface|null
+     */
+    protected ?ContainerInterface $container;
+
+    /**
+     * Constructor
+     *
+     * @param array $middleware The list of middleware to append.
+     * @param \Cake\Core\ContainerInterface|null $container Container instance.
+     */
+    public function __construct(array $middleware = [], ?ContainerInterface $container = null)
+    {
+        $this->container = $container;
+        $this->queue = $middleware;
+    }
+
+    /**
+     * Resolve middleware name to a PSR 15 compliant middleware instance.
+     *
+     * @param \Psr\Http\Server\MiddlewareInterface|\Closure|string $middleware The middleware to resolve.
+     * @return \Psr\Http\Server\MiddlewareInterface
+     * @throws \InvalidArgumentException If Middleware not found.
+     */
+    protected function resolve(MiddlewareInterface|Closure|string $middleware): MiddlewareInterface
+    {
+        if (is_string($middleware)) {
+            if ($this->container && $this->container->has($middleware)) {
+                $middleware = $this->container->get($middleware);
