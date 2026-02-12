e28f202568a94f9a647a65d2b5e083c2936ff4ce
pmtrilio <pm@trilio.bg>
2026-02-12 15:43:31 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: EmailValidator #195 (30-130).php
@@ -0,0 +1,101 @@
+    /**
+     * @var string the regular expression used to validate email addresses with the name part.
+     * This property is used only when [[allowName]] is true.
+     * @see allowName
+     */
+    public $fullPattern = '/^[^@]*<[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?>$/';
+    /**
+     * @var string the regular expression used to validate the part before the @ symbol, used if ASCII conversion fails to validate the address.
+     * @see https://www.regular-expressions.info/email.html
+     * @since 2.0.42
+     */
+    public $patternASCII = '/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*$/';
+    /**
+     * @var string the regular expression used to validate email addresses with the name part before the @ symbol, used if ASCII conversion fails to validate the address.
+     * This property is used only when [[allowName]] is true.
+     * @see allowName
+     * @since 2.0.42
+     */
+    public $fullPatternASCII = '/^[^@]*<[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*$/';
+    /**
+     * @var bool whether to allow name in the email address (e.g. "John Smith <john.smith@example.com>"). Defaults to false.
+     * @see fullPattern
+     */
+    public $allowName = false;
+    /**
+     * @var bool whether to check whether the email's domain exists and has either an A or MX record.
+     * Be aware that this check can fail due to temporary DNS problems even if the email address is
+     * valid and an email would be deliverable. Defaults to false.
+     */
+    public $checkDNS = false;
+    /**
+     * @var bool whether validation process should take into account IDN (internationalized domain
+     * names). Defaults to false meaning that validation of emails containing IDN will always fail.
+     * Note that in order to use IDN validation you have to install and enable `intl` PHP extension,
+     * otherwise an exception would be thrown.
+     */
+    public $enableIDN = false;
+    /**
+     * @var bool whether [[enableIDN]] should apply to the local part of the email (left side
+     * of the `@`). Only applies if [[enableIDN]] is `true`.
+     * @since 2.0.43
+     */
+    public $enableLocalIDN = true;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->enableIDN && !function_exists('idn_to_ascii')) {
+            throw new InvalidConfigException('In order to use IDN validation intl extension must be installed and enabled.');
+        }
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', '{attribute} is not a valid email address.');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function validateValue($value)
+    {
+        if (!is_string($value)) {
+            $valid = false;
+        } elseif (!preg_match('/^(?P<name>(?:"?([^"]*)"?\s)?)(?:\s+)?(?:(?P<open><?)((?P<local>.+)@(?P<domain>[^>]+))(?P<close>>?))$/i', $value, $matches)) {
+            $valid = false;
+        } else {
+            if ($this->enableIDN) {
+                if ($this->enableLocalIDN) {
+                    $matches['local'] = $this->idnToAsciiWithFallback($matches['local']);
+                }
+                $matches['domain'] = $this->idnToAscii($matches['domain']);
+                $value = $matches['name'] . $matches['open'] . $matches['local'] . '@' . $matches['domain'] . $matches['close'];
+            }
+
+            if (strlen($matches['local']) > 64) {
+                // The maximum total length of a user name or other local-part is 64 octets. RFC 5322 section 4.5.3.1.1
+                // https://datatracker.ietf.org/doc/html/rfc5321#section-4.5.3.1.1
+                $valid = false;
+            } elseif (strlen($matches['local'] . '@' . $matches['domain']) > 254) {
+                // There is a restriction in RFC 2821 on the length of an address in MAIL and RCPT commands
+                // of 254 characters. Since addresses that do not fit in those fields are not normally useful, the
+                // upper limit on address lengths should normally be considered to be 254.
+                //
+                // Dominic Sayers, RFC 3696 erratum 1690
+                // https://www.rfc-editor.org/errata_search.php?eid=1690
+                $valid = false;
+            } else {
+                $valid = preg_match($this->pattern, $value) || ($this->allowName && preg_match($this->fullPattern, $value));
+                if ($valid && $this->checkDNS) {
+                    $valid = $this->isDNSValid($matches['domain']);
+                }
+            }
+        }
+
+        return $valid ? null : [$this->message, []];
+    }
+
+    /**
