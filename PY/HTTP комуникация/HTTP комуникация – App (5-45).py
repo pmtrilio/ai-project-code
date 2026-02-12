f3732ce08431f6fe6f05c4e7a0e7d6213cbe6aab
pmtrilio <pm@trilio.bg>
2026-02-12 14:22:40 +0200

    Upload file via PHP cURL

---
+++ PY/HTTP комуникация/HTTP комуникация – App (5-45).py
@@ -0,0 +1,41 @@
+import os
+import sys
+import typing as t
+import weakref
+from datetime import timedelta
+from functools import update_wrapper
+from inspect import iscoroutinefunction
+from itertools import chain
+from types import TracebackType
+from urllib.parse import quote as _url_quote
+
+import click
+from werkzeug.datastructures import Headers
+from werkzeug.datastructures import ImmutableDict
+from werkzeug.exceptions import BadRequestKeyError
+from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import InternalServerError
+from werkzeug.routing import BuildError
+from werkzeug.routing import MapAdapter
+from werkzeug.routing import RequestRedirect
+from werkzeug.routing import RoutingException
+from werkzeug.routing import Rule
+from werkzeug.serving import is_running_from_reloader
+from werkzeug.wrappers import Response as BaseResponse
+from werkzeug.wsgi import get_host
+
+from . import cli
+from . import typing as ft
+from .ctx import AppContext
+from .globals import _cv_app
+from .globals import app_ctx
+from .globals import g
+from .globals import request
+from .globals import session
+from .helpers import get_debug_flag
+from .helpers import get_flashed_messages
+from .helpers import get_load_dotenv
+from .helpers import send_from_directory
+from .sansio.app import App
+from .sessions import SecureCookieSessionInterface
+from .sessions import SessionInterface
