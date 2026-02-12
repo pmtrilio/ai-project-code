1100b864810df2118bc122868d9902361426cca1
pmtrilio <pm@trilio.bg>
2026-02-12 19:10:28 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Database Seeder (45-89).php
@@ -0,0 +1,45 @@
+        $classes = Arr::wrap($class);
+
+        foreach ($classes as $class) {
+            $seeder = $this->resolve($class);
+
+            $name = get_class($seeder);
+
+            if ($silent === false && isset($this->command)) {
+                (new TwoColumnDetail($this->command->getOutput()))
+                    ->render($name, '<fg=yellow;options=bold>RUNNING</>');
+            }
+
+            $startTime = microtime(true);
+
+            $seeder->__invoke($parameters);
+
+            if ($silent === false && isset($this->command)) {
+                $runTime = number_format((microtime(true) - $startTime) * 1000);
+
+                (new TwoColumnDetail($this->command->getOutput()))
+                    ->render($name, "<fg=gray>$runTime ms</> <fg=green;options=bold>DONE</>");
+
+                $this->command->getOutput()->writeln('');
+            }
+
+            static::$called[] = $class;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Run the given seeder class.
+     *
+     * @param  array|string  $class
+     * @param  array  $parameters
+     * @return void
+     */
+    public function callWith($class, array $parameters = [])
+    {
+        $this->call($class, false, $parameters);
+    }
+
+    /**
+     * Silently run the given seeder class.
