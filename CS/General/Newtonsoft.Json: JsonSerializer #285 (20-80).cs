b83361c6b76b91f6922d0f7c1826905a61c66d8a
pmtrilio <pm@trilio.bg>
2026-02-12 15:06:59 +0200

    Upload file via PHP cURL

---
+++ CS/General/Newtonsoft.Json: JsonSerializer #285 (20-80).cs
@@ -0,0 +1,61 @@
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Utilities;
+using System.Runtime.Serialization;
+using ErrorEventArgs = Newtonsoft.Json.Serialization.ErrorEventArgs;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Newtonsoft.Json
+{
+    /// <summary>
+    /// Serializes and deserializes objects into and from the JSON format.
+    /// The <see cref="JsonSerializer"/> enables you to control how objects are encoded into JSON.
+    /// </summary>
+    [RequiresUnreferencedCode(MiscellaneousUtils.TrimWarning)]
+    [RequiresDynamicCode(MiscellaneousUtils.AotWarning)]
+    public class JsonSerializer
+    {
+        internal TypeNameHandling _typeNameHandling;
+        internal TypeNameAssemblyFormatHandling _typeNameAssemblyFormatHandling;
+        internal PreserveReferencesHandling _preserveReferencesHandling;
+        internal ReferenceLoopHandling _referenceLoopHandling;
+        internal MissingMemberHandling _missingMemberHandling;
+        internal ObjectCreationHandling _objectCreationHandling;
+        internal NullValueHandling _nullValueHandling;
+        internal DefaultValueHandling _defaultValueHandling;
+        internal ConstructorHandling _constructorHandling;
+        internal MetadataPropertyHandling _metadataPropertyHandling;
+        internal JsonConverterCollection? _converters;
+        internal IContractResolver _contractResolver;
+        internal ITraceWriter? _traceWriter;
+        internal IEqualityComparer? _equalityComparer;
+        internal ISerializationBinder _serializationBinder;
+        internal StreamingContext _context;
+        private IReferenceResolver? _referenceResolver;
+
+        private Formatting? _formatting;
+        private DateFormatHandling? _dateFormatHandling;
+        private DateTimeZoneHandling? _dateTimeZoneHandling;
+        private DateParseHandling? _dateParseHandling;
+        private FloatFormatHandling? _floatFormatHandling;
+        private FloatParseHandling? _floatParseHandling;
+        private StringEscapeHandling? _stringEscapeHandling;
+        private CultureInfo _culture;
+        private int? _maxDepth;
+        private bool _maxDepthSet;
+        private bool? _checkAdditionalContent;
+        private string? _dateFormatString;
