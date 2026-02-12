b0e750d056dfc5bd6e3e8941f9d0f1440d58b650
pmtrilio <pm@trilio.bg>
2026-02-12 20:10:48 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Pipe #633 (20-80).cs
@@ -0,0 +1,60 @@
+
+        // These callbacks all point to the same methods but are different delegate types
+        private static readonly ContextCallback s_executionContextRawCallback = ExecuteWithoutExecutionContext!;
+        private static readonly SendOrPostCallback s_syncContextExecutionContextCallback = ExecuteWithExecutionContext!;
+        private static readonly SendOrPostCallback s_syncContextExecuteWithoutExecutionContextCallback = ExecuteWithoutExecutionContext!;
+        private static readonly Action<object?> s_scheduleWithExecutionContextCallback = ExecuteWithExecutionContext!;
+
+        // Mutable struct! Don't make this readonly
+        private BufferSegmentStack _bufferSegmentPool;
+
+        private readonly DefaultPipeReader _reader;
+        private readonly DefaultPipeWriter _writer;
+
+        // The options instance
+        private readonly PipeOptions _options;
+        private readonly object _sync = new object();
+
+        // Computed state from the options instance
+        private bool UseSynchronizationContext => _options.UseSynchronizationContext;
+        private int MinimumSegmentSize => _options.MinimumSegmentSize;
+        private long PauseWriterThreshold => _options.PauseWriterThreshold;
+        private long ResumeWriterThreshold => _options.ResumeWriterThreshold;
+
+        private PipeScheduler ReaderScheduler => _options.ReaderScheduler;
+        private PipeScheduler WriterScheduler => _options.WriterScheduler;
+
+        // This sync objects protects the shared state between the writer and reader (most of this class)
+        private object SyncObj => _sync;
+
+        // The number of bytes flushed but not consumed by the reader
+        private long _unconsumedBytes;
+
+        // The number of bytes written but not flushed
+        private long _unflushedBytes;
+
+        private PipeAwaitable _readerAwaitable;
+        private PipeAwaitable _writerAwaitable;
+
+        private PipeCompletion _writerCompletion;
+        private PipeCompletion _readerCompletion;
+
+        // Stores the last examined position, used to calculate how many bytes were to release
+        // for back pressure management
+        private long _lastExaminedIndex = -1;
+
+        // The read head which is the start of the PipeReader's consumed bytes
+        private BufferSegment? _readHead;
+        private int _readHeadIndex;
+
+        private bool _disposed;
+
+        // The extent of the bytes available to the PipeReader to consume
+        private BufferSegment? _readTail;
+        private int _readTailIndex;
+        private int _minimumReadBytes;
+
+        // The write head which is the extent of the PipeWriter's written bytes
+        private BufferSegment? _writingHead;
+        private Memory<byte> _writingHeadMemory;
+        private int _writingHeadBytesBuffered;
