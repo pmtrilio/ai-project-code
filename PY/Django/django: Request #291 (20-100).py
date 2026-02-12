8a4dba6f148c3ffc4bf79e03559e3a50be58ffc4
pmtrilio <pm@trilio.bg>
2026-02-12 19:36:19 +0200

    Upload file via PHP cURL

---
+++ PY/Django/django: Request #291 (20-100).py
@@ -0,0 +1,81 @@
+    MultiPartParserError,
+    TooManyFilesSent,
+)
+from django.utils.datastructures import (
+    CaseInsensitiveMapping,
+    ImmutableList,
+    MultiValueDict,
+)
+from django.utils.encoding import escape_uri_path, iri_to_uri
+from django.utils.functional import cached_property
+from django.utils.http import is_same_domain, parse_header_parameters
+from django.utils.regex_helper import _lazy_re_compile
+
+RAISE_ERROR = object()
+host_validation_re = _lazy_re_compile(
+    r"^([a-z0-9.-]+|\[[a-f0-9]*:[a-f0-9.:]+\])(?::([0-9]+))?$"
+)
+
+
+class UnreadablePostError(OSError):
+    pass
+
+
+class RawPostDataException(Exception):
+    """
+    You cannot access raw_post_data from a request that has
+    multipart/* POST data if it has been accessed via POST,
+    FILES, etc..
+    """
+
+    pass
+
+
+class HttpRequest:
+    """A basic HTTP request."""
+
+    # The encoding used in GET/POST dicts. None means use default setting.
+    _encoding = None
+    _upload_handlers = []
+
+    def __init__(self):
+        # WARNING: The `WSGIRequest` subclass doesn't call `super`.
+        # Any variable assignment made here should also happen in
+        # `WSGIRequest.__init__()`.
+
+        self.GET = QueryDict(mutable=True)
+        self.POST = QueryDict(mutable=True)
+        self.COOKIES = {}
+        self.META = {}
+        self.FILES = MultiValueDict()
+
+        self.path = ""
+        self.path_info = ""
+        self.method = None
+        self.resolver_match = None
+        self.content_type = None
+        self.content_params = None
+
+    def __repr__(self):
+        if self.method is None or not self.get_full_path():
+            return "<%s>" % self.__class__.__name__
+        return "<%s: %s %r>" % (
+            self.__class__.__name__,
+            self.method,
+            self.get_full_path(),
+        )
+
+    @cached_property
+    def headers(self):
+        return HttpHeaders(self.META)
+
+    @cached_property
+    def accepted_types(self):
+        """
+        Return a list of MediaType instances, in order of preference (quality).
+        """
+        header_value = self.headers.get("Accept", "*/*")
+        return sorted(
+            (
+                media_type
+                for token in header_value.split(",")
