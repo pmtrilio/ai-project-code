9dd3509575e2c17e33f4f90860d912e1482fc7b6
pmtrilio <pm@trilio.bg>
2026-02-12 14:11:38 +0200

    Upload file via PHP cURL

---
+++ CS/Entity Framework Core/Entity Framework Core: SQL изрази и моделиране – Sqlbinaryexpression (1-61).cs
@@ -0,0 +1,60 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+/// <summary>
+///     <para>
+///         An expression that represents a binary operation in a SQL tree.
+///     </para>
+///     <para>
+///         This type is typically used by database providers (and other extensions). It is generally
+///         not used in application code.
+///     </para>
+/// </summary>
+public class SqlBinaryExpression : SqlExpression
+{
+    private static ConstructorInfo? _quotingConstructor;
+
+    /// <summary>
+    ///     Creates a new instance of the <see cref="SqlBinaryExpression" /> class.
+    /// </summary>
+    /// <param name="operatorType">The operator to apply.</param>
+    /// <param name="left">An expression which is left operand.</param>
+    /// <param name="right">An expression which is right operand.</param>
+    /// <param name="type">The <see cref="Type" /> of the expression.</param>
+    /// <param name="typeMapping">The <see cref="RelationalTypeMapping" /> associated with the expression.</param>
+    public SqlBinaryExpression(
+        ExpressionType operatorType,
+        SqlExpression left,
+        SqlExpression right,
+        Type type,
+        RelationalTypeMapping? typeMapping)
+        : base(type, typeMapping)
+    {
+        if (!IsValidOperator(operatorType))
+        {
+            throw new InvalidOperationException(
+                RelationalStrings.UnsupportedOperatorForSqlExpression(
+                    operatorType, typeof(SqlBinaryExpression).ShortDisplayName()));
+        }
+
+        OperatorType = operatorType;
+        Left = left;
+        Right = right;
+    }
+
+    /// <summary>
+    ///     The operator of this SQL binary operation.
+    /// </summary>
+    public virtual ExpressionType OperatorType { get; }
+
+    /// <summary>
+    ///     The left operand.
+    /// </summary>
+    public virtual SqlExpression Left { get; }
+
+    /// <summary>
+    ///     The right operand.
+    /// </summary>
+    public virtual SqlExpression Right { get; }
