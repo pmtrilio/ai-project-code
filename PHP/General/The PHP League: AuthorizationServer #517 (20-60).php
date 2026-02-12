3820f8728bcdd9ed9b344e31649835126d92ebf3
pmtrilio <pm@trilio.bg>
2026-02-12 20:00:19 +0200

    Upload file via PHP cURL

---
+++ PHP/General/The PHP League: AuthorizationServer #517 (20-60).php
@@ -0,0 +1,41 @@
+use League\OAuth2\Server\Grant\GrantTypeInterface;
+use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
+use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+use League\OAuth2\Server\RequestTypes\AuthorizationRequestInterface;
+use League\OAuth2\Server\ResponseTypes\AbstractResponseType;
+use League\OAuth2\Server\ResponseTypes\BearerTokenResponse;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use SensitiveParameter;
+
+class AuthorizationServer implements EmitterAwareInterface
+{
+    use EmitterAwarePolyfill;
+
+    /**
+     * @var GrantTypeInterface[]
+     */
+    protected array $enabledGrantTypes = [];
+
+    /**
+     * @var DateInterval[]
+     */
+    protected array $grantTypeAccessTokenTTL = [];
+
+    protected CryptKeyInterface $privateKey;
+
+    protected CryptKeyInterface $publicKey;
+
+    protected ResponseTypeInterface $responseType;
+
+    private string|Key $encryptionKey;
+
+    private string $defaultScope = '';
+
+    private bool $revokeRefreshTokens = true;
+
+    /**
+     * New server instance
+     */
