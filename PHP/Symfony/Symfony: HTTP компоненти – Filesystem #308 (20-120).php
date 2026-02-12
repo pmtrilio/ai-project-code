30a0d478207156f93380c840301570d3241b5560
pmtrilio <pm@trilio.bg>
2026-02-12 19:01:56 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: HTTP компоненти – Filesystem #308 (20-120).php
@@ -0,0 +1,101 @@
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class Filesystem
+{
+    private static ?string $lastError = null;
+
+    /**
+     * Copies a file.
+     *
+     * If the target file is older than the origin file, it's always overwritten.
+     * If the target file is newer, it is overwritten only when the
+     * $overwriteNewerFiles option is set to true.
+     *
+     * @return void
+     *
+     * @throws FileNotFoundException When originFile doesn't exist
+     * @throws IOException           When copy fails
+     */
+    public function copy(string $originFile, string $targetFile, bool $overwriteNewerFiles = false)
+    {
+        $originIsLocal = stream_is_local($originFile) || 0 === stripos($originFile, 'file://');
+        if ($originIsLocal && !is_file($originFile)) {
+            throw new FileNotFoundException(\sprintf('Failed to copy "%s" because file does not exist.', $originFile), 0, null, $originFile);
+        }
+
+        $this->mkdir(\dirname($targetFile));
+
+        $doCopy = true;
+        if (!$overwriteNewerFiles && !parse_url($originFile, \PHP_URL_HOST) && is_file($targetFile)) {
+            $doCopy = filemtime($originFile) > filemtime($targetFile);
+        }
+
+        if ($doCopy) {
+            // https://bugs.php.net/64634
+            if (!$source = self::box('fopen', $originFile, 'r')) {
+                throw new IOException(\sprintf('Failed to copy "%s" to "%s" because source file could not be opened for reading: ', $originFile, $targetFile).self::$lastError, 0, null, $originFile);
+            }
+
+            // Stream context created to allow files overwrite when using FTP stream wrapper - disabled by default
+            if (!$target = self::box('fopen', $targetFile, 'w', false, stream_context_create(['ftp' => ['overwrite' => true]]))) {
+                throw new IOException(\sprintf('Failed to copy "%s" to "%s" because target file could not be opened for writing: ', $originFile, $targetFile).self::$lastError, 0, null, $originFile);
+            }
+
+            $bytesCopied = stream_copy_to_stream($source, $target);
+            fclose($source);
+            fclose($target);
+            unset($source, $target);
+
+            if (!is_file($targetFile)) {
+                throw new IOException(\sprintf('Failed to copy "%s" to "%s".', $originFile, $targetFile), 0, null, $originFile);
+            }
+
+            if ($originIsLocal) {
+                // Like `cp`, preserve executable permission bits
+                self::box('chmod', $targetFile, fileperms($targetFile) | (fileperms($originFile) & 0111));
+
+                // Like `cp`, preserve the file modification time
+                self::box('touch', $targetFile, filemtime($originFile));
+
+                if ($bytesCopied !== $bytesOrigin = filesize($originFile)) {
+                    throw new IOException(\sprintf('Failed to copy the whole content of "%s" to "%s" (%g of %g bytes copied).', $originFile, $targetFile, $bytesCopied, $bytesOrigin), 0, null, $originFile);
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a directory recursively.
+     *
+     * @return void
+     *
+     * @throws IOException On any directory creation failure
+     */
+    public function mkdir(string|iterable $dirs, int $mode = 0777)
+    {
+        foreach ($this->toIterable($dirs) as $dir) {
+            if (is_dir($dir)) {
+                continue;
+            }
+
+            if (!self::box('mkdir', $dir, $mode, true) && !is_dir($dir)) {
+                throw new IOException(\sprintf('Failed to create "%s": ', $dir).self::$lastError, 0, null, $dir);
+            }
+        }
+    }
+
+    /**
+     * Checks the existence of files or directories.
+     */
+    public function exists(string|iterable $files): bool
+    {
+        $maxPathLength = \PHP_MAXPATHLEN - 2;
+
+        foreach ($this->toIterable($files) as $file) {
+            if (\strlen($file) > $maxPathLength) {
+                throw new IOException(\sprintf('Could not check if file exist because path length exceeds %d characters.', $maxPathLength), 0, null, $file);
+            }
+
+            if (!file_exists($file)) {
+                return false;
