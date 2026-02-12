bd5f3cb7741a0e4b7f4825d9a346c1b9857872de
pmtrilio <pm@trilio.bg>
2026-02-12 19:05:30 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Container #488 (30-130).php
@@ -0,0 +1,101 @@
+     * The current globally available container (if any).
+     *
+     * @var static
+     */
+    protected static $instance;
+
+    /**
+     * An array of the types that have been resolved.
+     *
+     * @var bool[]
+     */
+    protected $resolved = [];
+
+    /**
+     * The container's bindings.
+     *
+     * @var array[]
+     */
+    protected $bindings = [];
+
+    /**
+     * The container's method bindings.
+     *
+     * @var \Closure[]
+     */
+    protected $methodBindings = [];
+
+    /**
+     * The container's shared instances.
+     *
+     * @var object[]
+     */
+    protected $instances = [];
+
+    /**
+     * The container's scoped instances.
+     *
+     * @var array
+     */
+    protected $scopedInstances = [];
+
+    /**
+     * The registered type aliases.
+     *
+     * @var string[]
+     */
+    protected $aliases = [];
+
+    /**
+     * The registered aliases keyed by the abstract name.
+     *
+     * @var array[]
+     */
+    protected $abstractAliases = [];
+
+    /**
+     * The extension closures for services.
+     *
+     * @var array[]
+     */
+    protected $extenders = [];
+
+    /**
+     * All of the registered tags.
+     *
+     * @var array[]
+     */
+    protected $tags = [];
+
+    /**
+     * The stack of concretions currently being built.
+     *
+     * @var array[]
+     */
+    protected $buildStack = [];
+
+    /**
+     * The parameter override stack.
+     *
+     * @var array[]
+     */
+    protected $with = [];
+
+    /**
+     * The contextual binding map.
+     *
+     * @var array[]
+     */
+    public $contextual = [];
+
+    /**
+     * The contextual attribute handlers.
+     *
+     * @var array[]
+     */
+    public $contextualAttributes = [];
+
+    /**
+     * Whether an abstract class has already had its attributes checked for bindings.
+     *
+     * @var array<class-string, true>
