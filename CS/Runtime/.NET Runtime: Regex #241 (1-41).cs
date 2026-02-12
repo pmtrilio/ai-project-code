5b4d7b12143ec9a22c3f2d7d786eb783eed647ec
pmtrilio <pm@trilio.bg>
2026-02-12 20:06:36 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Regex #241 (1-41).cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions.Symbolic;
+using System.Threading;
+
+namespace System.Text.RegularExpressions
+{
+    /// <summary>
+    /// Represents an immutable regular expression. Also contains static methods that
+    /// allow use of regular expressions without instantiating a Regex explicitly.
+    /// </summary>
+    public partial class Regex : ISerializable
+    {
+        [StringSyntax(StringSyntaxAttribute.Regex)]
+        protected internal string? pattern;                   // The string pattern provided
+        protected internal RegexOptions roptions;             // the top-level options from the options string
+        protected internal RegexRunnerFactory? factory;       // Factory used to create runner instances for executing the regex
+        protected internal Hashtable? caps;                   // if captures are sparse, this is the hashtable capnum->index
+        protected internal Hashtable? capnames;               // if named captures are used, this maps names->index
+        protected internal string[]? capslist;                // if captures are sparse or named captures are used, this is the sorted list of names
+        protected internal int capsize;                       // the size of the capture array
+
+        private volatile RegexRunner? _runner;                // cached runner
+
+#if DEBUG
+        // These members aren't used from Regex(), but we want to keep them in debug builds for now,
+        // so this is a convenient place to include them rather than needing a debug-only illink file.
+        [DynamicDependency(nameof(SaveDGML))]
+        [DynamicDependency(nameof(GenerateUnicodeTables))]
+        [DynamicDependency(nameof(SampleMatches))]
+        [DynamicDependency(nameof(Explore))]
