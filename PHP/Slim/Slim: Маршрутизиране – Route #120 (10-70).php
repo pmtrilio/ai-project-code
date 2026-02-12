355462f4bc785b3f73147883f4fa8920e889db14
pmtrilio <pm@trilio.bg>
2026-02-12 14:30:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: Маршрутизиране – Route #120 (10-70).php
@@ -0,0 +1,61 @@
+
+namespace Slim\Routing;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Handlers\Strategies\RequestHandler;
+use Slim\Handlers\Strategies\RequestResponse;
+use Slim\Interfaces\AdvancedCallableResolverInterface;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\InvocationStrategyInterface;
+use Slim\Interfaces\RequestHandlerInvocationStrategyInterface;
+use Slim\Interfaces\RouteGroupInterface;
+use Slim\Interfaces\RouteInterface;
+use Slim\MiddlewareDispatcher;
+
+use function array_key_exists;
+use function array_replace;
+use function array_reverse;
+use function class_implements;
+use function in_array;
+use function is_array;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ */
+class Route implements RouteInterface, RequestHandlerInterface
+{
+    /**
+     * HTTP methods supported by this route
+     *
+     * @var string[]
+     */
+    protected array $methods = [];
+
+    /**
+     * Route identifier
+     */
+    protected string $identifier;
+
+    /**
+     * Route name
+     */
+    protected ?string $name = null;
+
+    /**
+     * Parent route groups
+     *
+     * @var RouteGroupInterface[]
+     */
+    protected array $groups;
+
+    protected InvocationStrategyInterface $invocationStrategy;
+
+    /**
+     * Route parameters
+     *
