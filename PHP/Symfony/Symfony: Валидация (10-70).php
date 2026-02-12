e7bb4aba72912dec534f29073acb3e8092f85f38
pmtrilio <pm@trilio.bg>
2026-02-12 14:26:52 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: Валидация (10-70).php
@@ -0,0 +1,61 @@
+ */
+
+namespace Symfony\Component\Validator\Validator;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\GroupSequence;
+use Symfony\Component\Validator\ConstraintValidatorFactoryInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Context\ExecutionContextFactoryInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Mapping\Factory\MetadataFactoryInterface;
+use Symfony\Component\Validator\Mapping\MetadataInterface;
+use Symfony\Component\Validator\ObjectInitializerInterface;
+
+/**
+ * Recursive implementation of {@link ValidatorInterface}.
+ *
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class RecursiveValidator implements ValidatorInterface
+{
+    protected $contextFactory;
+    protected $metadataFactory;
+    protected $validatorFactory;
+    protected $objectInitializers;
+    protected ?ContainerInterface $groupProviderLocator;
+
+    /**
+     * Creates a new validator.
+     *
+     * @param ObjectInitializerInterface[] $objectInitializers The object initializers
+     */
+    public function __construct(ExecutionContextFactoryInterface $contextFactory, MetadataFactoryInterface $metadataFactory, ConstraintValidatorFactoryInterface $validatorFactory, array $objectInitializers = [], ?ContainerInterface $groupProviderLocator = null)
+    {
+        $this->contextFactory = $contextFactory;
+        $this->metadataFactory = $metadataFactory;
+        $this->validatorFactory = $validatorFactory;
+        $this->objectInitializers = $objectInitializers;
+        $this->groupProviderLocator = $groupProviderLocator;
+    }
+
+    public function startContext(mixed $root = null): ContextualValidatorInterface
+    {
+        return new RecursiveContextualValidator(
+            $this->contextFactory->createContext($this, $root),
+            $this->metadataFactory,
+            $this->validatorFactory,
+            $this->objectInitializers,
+            $this->groupProviderLocator,
+        );
+    }
+
+    public function inContext(ExecutionContextInterface $context): ContextualValidatorInterface
+    {
+        return new RecursiveContextualValidator(
+            $context,
+            $this->metadataFactory,
+            $this->validatorFactory,
+            $this->objectInitializers,
+            $this->groupProviderLocator,
