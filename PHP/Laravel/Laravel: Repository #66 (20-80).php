734318422d0cca8717fcddc5caed04fabf3c9434
pmtrilio <pm@trilio.bg>
2026-02-12 20:26:35 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Repository #66 (20-80).php
@@ -0,0 +1,61 @@
+use Illuminate\Cache\Events\RetrievingManyKeys;
+use Illuminate\Cache\Events\WritingKey;
+use Illuminate\Cache\Events\WritingManyKeys;
+use Illuminate\Contracts\Cache\Repository as CacheContract;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Support\InteractsWithTime;
+use Illuminate\Support\Traits\Macroable;
+
+use function Illuminate\Support\defer;
+use function Illuminate\Support\enum_value;
+
+/**
+ * @mixin \Illuminate\Contracts\Cache\Store
+ */
+class Repository implements ArrayAccess, CacheContract
+{
+    use InteractsWithTime, Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The cache store implementation.
+     *
+     * @var \Illuminate\Contracts\Cache\Store
+     */
+    protected $store;
+
+    /**
+     * The event dispatcher implementation.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher|null
+     */
+    protected $events;
+
+    /**
+     * The default number of seconds to store items.
+     *
+     * @var int|null
+     */
+    protected $default = 3600;
+
+    /**
+     * The cache store configuration options.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Create a new cache repository instance.
+     *
+     * @param  \Illuminate\Contracts\Cache\Store  $store
+     * @param  array  $config
+     */
+    public function __construct(Store $store, array $config = [])
+    {
+        $this->store = $store;
+        $this->config = $config;
