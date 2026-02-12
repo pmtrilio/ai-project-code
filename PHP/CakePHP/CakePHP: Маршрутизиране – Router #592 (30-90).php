a871b073fd1da2553cac0fc177498bd663bd3a7a
pmtrilio <pm@trilio.bg>
2026-02-12 19:07:37 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Маршрутизиране – Router #592 (30-90).php
@@ -0,0 +1,61 @@
+/**
+ * Parses the request URL into controller, action, and parameters. Uses the connected routes
+ * to match the incoming URL string to parameters that will allow the request to be dispatched. Also
+ * handles converting parameter lists into URL strings, using the connected routes. Routing allows you to decouple
+ * the way the world interacts with your application (URLs) and the implementation (controllers and actions).
+ */
+class Router
+{
+    /**
+     * Default route class.
+     *
+     * @var string
+     */
+    protected static string $_defaultRouteClass = Route::class;
+
+    /**
+     * Contains the base string that will be applied to all generated URLs
+     * For example `https://example.com`
+     *
+     * @var string|null
+     */
+    protected static ?string $_fullBaseUrl = null;
+
+    /**
+     * Regular expression for action names
+     *
+     * @var string
+     */
+    public const ACTION = 'index|show|add|create|edit|update|remove|del|delete|view|item';
+
+    /**
+     * Regular expression for years
+     *
+     * @var string
+     */
+    public const YEAR = '[12][0-9]{3}';
+
+    /**
+     * Regular expression for months
+     *
+     * @var string
+     */
+    public const MONTH = '0[1-9]|1[012]';
+
+    /**
+     * Regular expression for days
+     *
+     * @var string
+     */
+    public const DAY = '0[1-9]|[12][0-9]|3[01]';
+
+    /**
+     * Regular expression for auto increment IDs
+     *
+     * @var string
+     */
+    public const ID = '[0-9]+';
+
+    /**
+     * Regular expression for UUIDs
+     *
