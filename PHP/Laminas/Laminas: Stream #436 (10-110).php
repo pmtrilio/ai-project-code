a3f9afe99f3bdb7952cd1514e133edc50aa42f98
pmtrilio <pm@trilio.bg>
2026-02-12 20:34:07 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Stream #436 (10-110).php
@@ -0,0 +1,101 @@
+use Traversable;
+
+use function chmod;
+use function dirname;
+use function fclose;
+use function file_exists;
+use function fopen;
+use function fwrite;
+use function get_resource_type;
+use function gettype;
+use function is_array;
+use function is_resource;
+use function is_string;
+use function is_writable;
+use function iterator_to_array;
+use function sprintf;
+use function touch;
+
+use const PHP_EOL;
+
+class Stream extends AbstractWriter
+{
+    /**
+     * Separator between log entries
+     *
+     * @var string
+     */
+    protected $logSeparator = PHP_EOL;
+
+    /**
+     * Holds the PHP stream to log to.
+     *
+     * @var null|Stream
+     */
+    protected $stream;
+
+    /**
+     * Constructor
+     *
+     * @param  string|resource|array|Traversable $streamOrUrl Stream or URL to open as a stream
+     * @param  string|null $mode Mode, only applicable if a URL is given
+     * @param  null|string $logSeparator Log separator string
+     * @param  null|int $filePermissions Permissions value, only applicable if a filename is given;
+     *     when $streamOrUrl is an array of options, use the 'chmod' key to specify this.
+     * @throws Exception\InvalidArgumentException
+     * @throws Exception\RuntimeException
+     */
+    public function __construct($streamOrUrl, $mode = null, $logSeparator = null, $filePermissions = null)
+    {
+        if ($streamOrUrl instanceof Traversable) {
+            $streamOrUrl = iterator_to_array($streamOrUrl);
+        }
+
+        if (is_array($streamOrUrl)) {
+            parent::__construct($streamOrUrl);
+            $mode            = $streamOrUrl['mode'] ?? null;
+            $logSeparator    = $streamOrUrl['log_separator'] ?? null;
+            $filePermissions = $streamOrUrl['chmod'] ?? $filePermissions;
+            $streamOrUrl     = $streamOrUrl['stream'] ?? null;
+        }
+
+        // Setting the default mode
+        if (null === $mode) {
+            $mode = 'a';
+        }
+
+        if (! is_string($streamOrUrl) && ! is_resource($streamOrUrl)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Resource is not a stream nor a string; received "%s',
+                gettype($streamOrUrl)
+            ));
+        }
+
+        if (is_resource($streamOrUrl)) {
+            if ('stream' !== get_resource_type($streamOrUrl)) {
+                throw new Exception\InvalidArgumentException(sprintf(
+                    'Resource is not a stream; received "%s',
+                    get_resource_type($streamOrUrl)
+                ));
+            }
+
+            if ('a' !== $mode) {
+                throw new Exception\InvalidArgumentException(sprintf(
+                    'Mode must be "a" on existing streams; received "%s"',
+                    $mode
+                ));
+            }
+
+            $this->stream = $streamOrUrl;
+        } else {
+            ErrorHandler::start();
+            if (isset($filePermissions) && ! file_exists($streamOrUrl) && is_writable(dirname($streamOrUrl))) {
+                touch($streamOrUrl);
+                chmod($streamOrUrl, $filePermissions);
+            }
+            $this->stream = fopen($streamOrUrl, $mode, false);
+            $error        = ErrorHandler::stop();
+        }
+
+        if (! $this->stream) {
+            throw new Exception\RuntimeException(sprintf(
