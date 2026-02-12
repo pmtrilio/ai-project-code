3e56446ff9c2cc47076db331d47bb8080ea1b9c8
pmtrilio <pm@trilio.bg>
2026-02-12 19:03:41 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: HTTP компоненти – Validator #394 (30-70).php
@@ -0,0 +1,41 @@
+/**
+ * Validator object encapsulates all methods related to data validations for a model
+ * It also provides an API to dynamically change validation rules for each model field.
+ *
+ * Implements ArrayAccess to easily modify rules in the set
+ *
+ * @link https://book.cakephp.org/5/en/core-libraries/validation.html
+ * @template-implements \ArrayAccess<string, \Cake\Validation\ValidationSet>
+ * @template-implements \IteratorAggregate<string, \Cake\Validation\ValidationSet>
+ */
+class Validator implements ArrayAccess, IteratorAggregate, Countable
+{
+    /**
+     * By using 'create' you can make fields required when records are first created.
+     *
+     * @var string
+     */
+    public const WHEN_CREATE = 'create';
+
+    /**
+     * By using 'update', you can make fields required when they are updated.
+     *
+     * @var string
+     */
+    public const WHEN_UPDATE = 'update';
+
+    /**
+     * Used to flag nested rules created with addNested() and addNestedMany()
+     *
+     * @var string
+     */
+    public const NESTED = '_nested';
+
+    /**
+     * A flag for allowEmptyFor()
+     *
+     * When `null` is given, it will be recognized as empty.
+     *
+     * @var int
+     */
+    public const EMPTY_NULL = 0;
