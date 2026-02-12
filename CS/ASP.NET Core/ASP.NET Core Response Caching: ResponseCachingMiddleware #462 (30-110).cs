307728dd4ab90f18fe5bd4861667ba4bb6aaa2b9
pmtrilio <pm@trilio.bg>
2026-02-12 19:18:41 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Response Caching: ResponseCachingMiddleware #462 (30-110).cs
@@ -0,0 +1,80 @@
+    private readonly IResponseCachingKeyProvider _keyProvider;
+
+    /// <summary>
+    /// Creates a new <see cref="ResponseCachingMiddleware"/>.
+    /// </summary>
+    /// <param name="next">The <see cref="RequestDelegate"/> representing the next middleware in the pipeline.</param>
+    /// <param name="options">The options for this middleware.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> used for logging.</param>
+    /// <param name="poolProvider">The <see cref="ObjectPoolProvider"/> used for creating <see cref="ObjectPool"/> instances.</param>
+    public ResponseCachingMiddleware(
+        RequestDelegate next,
+        IOptions<ResponseCachingOptions> options,
+        ILoggerFactory loggerFactory,
+        ObjectPoolProvider poolProvider)
+        : this(
+            next,
+            options,
+            loggerFactory,
+            new ResponseCachingPolicyProvider(),
+            new MemoryResponseCache(new MemoryCache(new MemoryCacheOptions
+            {
+                SizeLimit = options.Value.SizeLimit
+            })),
+            new ResponseCachingKeyProvider(poolProvider, options))
+    { }
+
+    // for testing
+    internal ResponseCachingMiddleware(
+        RequestDelegate next,
+        IOptions<ResponseCachingOptions> options,
+        ILoggerFactory loggerFactory,
+        IResponseCachingPolicyProvider policyProvider,
+        IResponseCache cache,
+        IResponseCachingKeyProvider keyProvider)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        ArgumentNullException.ThrowIfNull(policyProvider);
+        ArgumentNullException.ThrowIfNull(cache);
+        ArgumentNullException.ThrowIfNull(keyProvider);
+
+        _next = next;
+        _options = options.Value;
+        _logger = loggerFactory.CreateLogger<ResponseCachingMiddleware>();
+        _policyProvider = policyProvider;
+        _cache = cache;
+        _keyProvider = keyProvider;
+    }
+
+    /// <summary>
+    /// Invokes the logic of the middleware.
+    /// </summary>
+    /// <param name="httpContext">The <see cref="HttpContext"/>.</param>
+    /// <returns>A <see cref="Task"/> that completes when the middleware has completed processing.</returns>
+    public async Task Invoke(HttpContext httpContext)
+    {
+        var context = new ResponseCachingContext(httpContext, _logger);
+
+        // Should we attempt any caching logic?
+        if (_policyProvider.AttemptResponseCaching(context))
+        {
+            // Can this request be served from cache?
+            if (_policyProvider.AllowCacheLookup(context) && await TryServeFromCacheAsync(context))
+            {
+                return;
+            }
+
+            // Should we store the response to this request?
+            if (_policyProvider.AllowCacheStorage(context))
+            {
+                // Hook up to listen to the response stream
+                ShimResponseStream(context);
+
+                try
+                {
+                    await _next(httpContext);
+
+                    // If there was no response body, check the response headers now. We can cache things like redirects.
+                    StartResponse(context);
