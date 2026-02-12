be0ce78315aea4a68b61b7ab3f05dffb9145ced5
pmtrilio <pm@trilio.bg>
2026-02-12 20:29:00 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: MvcEvent #185 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Laminas\Mvc;
+
+use Laminas\EventManager\Event;
+use Laminas\Router\RouteMatch;
+use Laminas\Router\RouteStackInterface;
+use Laminas\Stdlib\RequestInterface as Request;
+use Laminas\Stdlib\ResponseInterface as Response;
+use Laminas\View\Model\ModelInterface as Model;
+use Laminas\View\Model\ViewModel;
+
+class MvcEvent extends Event
+{
+    /**#@+
+     * Mvc events triggered by eventmanager
+     */
+    public const EVENT_BOOTSTRAP      = 'bootstrap';
+    public const EVENT_DISPATCH       = 'dispatch';
+    public const EVENT_DISPATCH_ERROR = 'dispatch.error';
+    public const EVENT_FINISH         = 'finish';
+    public const EVENT_RENDER         = 'render';
+    public const EVENT_RENDER_ERROR   = 'render.error';
+    public const EVENT_ROUTE          = 'route';
+    /** @var ApplicationInterface|null */
+    protected $application;
+
+    /** @var Request */
+    protected $request;
+
+    /** @var Response */
+    protected $response;
+
+    /** @var mixed */
+    protected $result;
+
+    /** @var RouteStackInterface */
+    protected $router;
+
+    /** @var null|RouteMatch */
+    protected $routeMatch;
