2680e64e84b2f5ab6870c4f647617f37aae8d9f3
pmtrilio <pm@trilio.bg>
2026-02-12 20:31:47 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Command #321 (5-45).php
@@ -0,0 +1,41 @@
+use Illuminate\Console\View\Components\Factory;
+use Illuminate\Contracts\Console\Isolatable;
+use Illuminate\Support\Traits\Macroable;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class Command extends SymfonyCommand
+{
+    use Concerns\CallsCommands,
+        Concerns\ConfiguresPrompts,
+        Concerns\HasParameters,
+        Concerns\InteractsWithIO,
+        Concerns\InteractsWithSignals,
+        Concerns\PromptsForMissingInput,
+        Macroable;
+
+    /**
+     * The Laravel application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $laravel;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
