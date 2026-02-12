dc66cded29c21f821e815768ce8f0ad032675616
pmtrilio <pm@trilio.bg>
2026-02-12 19:01:30 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Container #288 (10-50).php
@@ -0,0 +1,41 @@
+use Illuminate\Container\Attributes\Singleton;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Container\CircularDependencyException;
+use Illuminate\Contracts\Container\Container as ContainerContract;
+use Illuminate\Contracts\Container\ContextualAttribute;
+use Illuminate\Contracts\Container\SelfBuilding;
+use Illuminate\Support\Traits\ReflectsClosures;
+use LogicException;
+use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionFunction;
+use ReflectionParameter;
+use TypeError;
+
+class Container implements ArrayAccess, ContainerContract
+{
+    use ReflectsClosures;
+
+    /**
+     * The current globally available container (if any).
+     *
+     * @var static
+     */
+    protected static $instance;
+
+    /**
+     * An array of the types that have been resolved.
+     *
+     * @var bool[]
+     */
+    protected $resolved = [];
+
+    /**
+     * The container's bindings.
+     *
+     * @var array[]
+     */
+    protected $bindings = [];
+
+    /**
