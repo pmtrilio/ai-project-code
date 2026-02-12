dfca9db8b2a18149e1f7d51c4854c3999d1bc73d
pmtrilio <pm@trilio.bg>
2026-02-12 14:39:12 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Optional #2 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Illuminate\Support;
+
+use ArrayAccess;
+use ArrayObject;
+use Illuminate\Support\Traits\Macroable;
+
+class Optional implements ArrayAccess
+{
+    use Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The underlying object.
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * Create a new optional instance.
+     *
+     * @param  mixed  $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Dynamically access a property on the underlying object.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        if (is_object($this->value)) {
+            return $this->value->{$key} ?? null;
+        }
+    }
+
+    /**
+     * Dynamically check a property exists on the underlying object.
+     *
+     * @param  mixed  $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        if (is_object($this->value)) {
+            return isset($this->value->{$name});
+        }
+
+        if (is_array($this->value) || $this->value instanceof ArrayObject) {
+            return isset($this->value[$name]);
+        }
+
+        return false;
