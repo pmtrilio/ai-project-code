9a6b102137f52b4e4f88a01ad8144a7b8df9e1c3
pmtrilio <pm@trilio.bg>
2026-02-12 19:19:59 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: IRule #535 (10-50).cs
@@ -0,0 +1,10 @@
+{
+    /// <summary>
+    /// Applies the rule.
+    /// Implementations of ApplyRule should set the value for <see cref="RewriteContext.Result"/>
+    /// (defaults to RuleResult.ContinueRules)
+    /// </summary>
+    /// <param name="context"></param>
+    void ApplyRule(RewriteContext context);
+}
+
