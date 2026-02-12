0b7f688befa709d3d23163207471be58ceec6a80
pmtrilio <pm@trilio.bg>
2026-02-12 20:11:00 +0200

    Upload file via PHP cURL

---
+++ CS/General/Newtonsoft.Json: JsonSerializerInternalReader #651 (20-80).cs
@@ -0,0 +1,61 @@
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+#if HAVE_DYNAMIC
+using System.ComponentModel;
+using System.Dynamic;
+#endif
+using System.Diagnostics;
+using System.Globalization;
+#if HAVE_BIG_INTEGER
+using System.Numerics;
+#endif
+using System.Reflection;
+using System.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Utilities;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+#if !HAVE_LINQ
+using Newtonsoft.Json.Utilities.LinqBridge;
+#else
+using System.Linq;
+#endif
+
+namespace Newtonsoft.Json.Serialization
+{
+    internal class JsonSerializerInternalReader : JsonSerializerInternalBase
+    {
+        internal enum PropertyPresence
+        {
+            None = 0,
+            Null = 1,
+            Value = 2
+        }
+
+        public JsonSerializerInternalReader(JsonSerializer serializer)
+            : base(serializer)
+        {
+        }
+
+        [RequiresUnreferencedCode(MiscellaneousUtils.TrimWarning)]
+        [RequiresDynamicCode(MiscellaneousUtils.AotWarning)]
+        public void Populate(JsonReader reader, object target)
+        {
+            ValidationUtils.ArgumentNotNull(target, nameof(target));
+
+            Type objectType = target.GetType();
+
+            JsonContract contract = Serializer._contractResolver.ResolveContract(objectType);
+
+            if (!reader.MoveToContent())
+            {
+                throw JsonSerializationException.Create(reader, "No JSON content found.");
+            }
