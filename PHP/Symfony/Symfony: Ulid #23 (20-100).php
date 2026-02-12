46994cc18db38a378abb0ee69e79aacc8601ef51
pmtrilio <pm@trilio.bg>
2026-02-12 15:40:23 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony: Ulid #23 (20-100).php
@@ -0,0 +1,81 @@
+ */
+class Ulid extends AbstractUid implements TimeBasedUidInterface
+{
+    protected const NIL = '00000000000000000000000000';
+    protected const MAX = '7ZZZZZZZZZZZZZZZZZZZZZZZZZ';
+
+    private static string $time = '';
+    private static array $rand = [];
+
+    public function __construct(?string $ulid = null)
+    {
+        if (null === $ulid) {
+            $this->uid = static::generate();
+        } elseif (self::NIL === $ulid) {
+            $this->uid = $ulid;
+        } elseif (self::MAX === strtr($ulid, 'z', 'Z')) {
+            $this->uid = $ulid;
+        } else {
+            if (!self::isValid($ulid)) {
+                throw new \InvalidArgumentException('Invalid ULID.');
+            }
+
+            $this->uid = strtoupper($ulid);
+        }
+    }
+
+    public static function isValid(string $ulid): bool
+    {
+        if (26 !== \strlen($ulid)) {
+            return false;
+        }
+
+        if (26 !== strspn($ulid, '0123456789ABCDEFGHJKMNPQRSTVWXYZabcdefghjkmnpqrstvwxyz')) {
+            return false;
+        }
+
+        return $ulid[0] <= '7';
+    }
+
+    public static function fromString(string $ulid): static
+    {
+        if (36 === \strlen($ulid) && preg_match('{^[0-9a-f]{8}(?:-[0-9a-f]{4}){3}-[0-9a-f]{12}$}Di', $ulid)) {
+            $ulid = uuid_parse($ulid);
+        } elseif (22 === \strlen($ulid) && 22 === strspn($ulid, BinaryUtil::BASE58[''])) {
+            $ulid = str_pad(BinaryUtil::fromBase($ulid, BinaryUtil::BASE58), 16, "\0", \STR_PAD_LEFT);
+        }
+
+        if (16 !== \strlen($ulid)) {
+            return match (strtr($ulid, 'z', 'Z')) {
+                self::NIL => new NilUlid(),
+                self::MAX => new MaxUlid(),
+                default => new static($ulid),
+            };
+        }
+
+        $ulid = bin2hex($ulid);
+        $ulid = \sprintf('%02s%04s%04s%04s%04s%04s%04s',
+            base_convert(substr($ulid, 0, 2), 16, 32),
+            base_convert(substr($ulid, 2, 5), 16, 32),
+            base_convert(substr($ulid, 7, 5), 16, 32),
+            base_convert(substr($ulid, 12, 5), 16, 32),
+            base_convert(substr($ulid, 17, 5), 16, 32),
+            base_convert(substr($ulid, 22, 5), 16, 32),
+            base_convert(substr($ulid, 27, 5), 16, 32)
+        );
+
+        if (self::NIL === $ulid) {
+            return new NilUlid();
+        }
+
+        if (self::MAX === $ulid = strtr($ulid, 'abcdefghijklmnopqrstuv', 'ABCDEFGHJKMNPQRSTVWXYZ')) {
+            return new MaxUlid();
+        }
+
+        $u = new static(self::NIL);
+        $u->uid = $ulid;
+
+        return $u;
+    }
+
+    public function toBinary(): string
