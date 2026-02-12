0dc05035c999655e81ea6e429bd8a8b0a5b70a5f
pmtrilio <pm@trilio.bg>
2026-02-12 14:47:34 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Eventfake #471 (20-60).php
@@ -0,0 +1,41 @@
+
+    /**
+     * The original event dispatcher.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    public $dispatcher;
+
+    /**
+     * The event types that should be intercepted instead of dispatched.
+     *
+     * @var array
+     */
+    protected $eventsToFake = [];
+
+    /**
+     * The event types that should be dispatched instead of intercepted.
+     *
+     * @var array
+     */
+    protected $eventsToDispatch = [];
+
+    /**
+     * All of the events that have been intercepted keyed by type.
+     *
+     * @var array
+     */
+    protected $events = [];
+
+    /**
+     * Create a new event fake instance.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $dispatcher
+     * @param  array|string  $eventsToFake
+     */
+    public function __construct(Dispatcher $dispatcher, $eventsToFake = [])
+    {
+        $this->dispatcher = $dispatcher;
+
+        $this->eventsToFake = Arr::wrap($eventsToFake);
+    }
