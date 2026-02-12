9a8d953042728c768de7bed6d7f40c05d4409a20
pmtrilio <pm@trilio.bg>
2026-02-12 19:13:12 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Localization: RequestCultureProvider #146 (10-110).cs
@@ -0,0 +1,18 @@
+/// An abstract base class provider for determining the culture information of an <see cref="HttpRequest"/>.
+/// </summary>
+public abstract class RequestCultureProvider : IRequestCultureProvider
+{
+    /// <summary>
+    /// Result that indicates that this instance of <see cref="RequestCultureProvider" /> could not determine the
+    /// request culture.
+    /// </summary>
+    protected static readonly Task<ProviderCultureResult?> NullProviderCultureResult = Task.FromResult(default(ProviderCultureResult));
+
+    /// <summary>
+    /// The current options for the <see cref="RequestLocalizationMiddleware"/>.
+    /// </summary>
+    public RequestLocalizationOptions? Options { get; set; }
+
+    /// <inheritdoc />
+    public abstract Task<ProviderCultureResult?> DetermineProviderCultureResult(HttpContext httpContext);
+}
