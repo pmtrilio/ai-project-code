25715a57fdd8bbdd4d8c36dd6f8b705b8fc02ad4
pmtrilio <pm@trilio.bg>
2026-02-12 19:03:48 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: Маршрутизиране – Route #400 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @license https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+
+declare(strict_types=1);
+
+namespace Slim\Routing;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Handlers\Strategies\RequestHandler;
+use Slim\Handlers\Strategies\RequestResponse;
+use Slim\Interfaces\AdvancedCallableResolverInterface;
+use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\InvocationStrategyInterface;
+use Slim\Interfaces\RequestHandlerInvocationStrategyInterface;
+use Slim\Interfaces\RouteGroupInterface;
+use Slim\Interfaces\RouteInterface;
+use Slim\MiddlewareDispatcher;
+
+use function array_key_exists;
+use function array_replace;
+use function array_reverse;
+use function class_implements;
+use function in_array;
+use function is_array;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ */
+class Route implements RouteInterface, RequestHandlerInterface
+{
