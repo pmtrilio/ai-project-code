054d1e06916fa7854cd4117573a5424f19dfac34
pmtrilio <pm@trilio.bg>
2026-02-12 21:25:18 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: Session #266 (1-81).php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidArgumentException;
+use yii\base\InvalidConfigException;
+
+/**
+ * Session provides session data management and the related configurations.
+ *
+ * Session is a Web application component that can be accessed via `Yii::$app->session`.
+ *
+ * To start the session, call [[open()]]; To complete and send out session data, call [[close()]];
+ * To destroy the session, call [[destroy()]].
+ *
+ * Session can be used like an array to set and get session data. For example,
+ *
+ * ```
+ * $session = new Session;
+ * $session->open();
+ * $value1 = $session['name1'];  // get session variable 'name1'
+ * $value2 = $session['name2'];  // get session variable 'name2'
+ * foreach ($session as $name => $value) // traverse all session variables
+ * $session['name3'] = $value3;  // set session variable 'name3'
+ * ```
+ *
+ * Session can be extended to support customized session storage.
+ * To do so, override [[useCustomStorage]] so that it returns true, and
+ * override these methods with the actual logic about using custom storage:
+ * [[openSession()]], [[closeSession()]], [[readSession()]], [[writeSession()]],
+ * [[destroySession()]] and [[gcSession()]].
+ *
+ * Session also supports a special type of session data, called *flash messages*.
+ * A flash message is available only in the current request and the next request.
+ * After that, it will be deleted automatically. Flash messages are particularly
+ * useful for displaying confirmation messages. To use flash messages, simply
+ * call methods such as [[setFlash()]], [[getFlash()]].
+ *
+ * For more details and usage information on Session, see the [guide article on sessions](guide:runtime-sessions-cookies).
+ *
+ * @property-read array $allFlashes Flash messages (key => message or key => [message1, message2]).
+ * @property string $cacheLimiter Current cache limiter.
+ * @property array $cookieParams The session cookie parameters.
+ * @property-read int $count The number of session variables.
+ * @property-write string $flash The key identifying the flash message. Note that flash messages and normal
+ * session variables share the same name space. If you have a normal session variable using the same name, its
+ * value will be overwritten by this method.
+ * @property float $gCProbability The probability (percentage) that the GC (garbage collection) process is
+ * started on every session initialization.
+ * @property bool $hasSessionId Whether the current request has sent the session ID.
+ * @property string $id The current session ID.
+ * @property-read bool $isActive Whether the session has started.
+ * @property-read SessionIterator $iterator An iterator for traversing the session variables.
+ * @property string $name The current session name.
+ * @property string $savePath The current session save path, defaults to '/tmp'.
+ * @property int $timeout The number of seconds after which data will be seen as 'garbage' and cleaned up. The
+ * default value is 1440 seconds (or the value of "session.gc_maxlifetime" set in php.ini).
+ * @property bool|null $useCookies The value indicating whether cookies should be used to store session IDs.
+ * @property-read bool $useCustomStorage Whether to use custom storage.
+ * @property bool $useStrictMode Whether strict mode is enabled or not.
+ * @property bool $useTransparentSessionID Whether transparent sid support is enabled or not, defaults to
+ * false.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ *
+ * @implements \IteratorAggregate<array-key, mixed>
+ * @implements \ArrayAccess<array-key, mixed>
+ */
+class Session extends Component implements \IteratorAggregate, \ArrayAccess, \Countable
+{
+    /**
+     * @var string|null Holds the original session module (before a custom handler is registered) so that it can be
