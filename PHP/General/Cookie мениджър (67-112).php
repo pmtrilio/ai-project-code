65cff6b6c9dca6031b4a8044c42b717b94d6cce0
pmtrilio <pm@trilio.bg>
2026-02-12 19:10:05 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Cookie мениджър (67-112).php
@@ -0,0 +1,46 @@
+
+        $time = ($minutes == 0) ? 0 : $this->availableAt($minutes * 60);
+
+        return new Cookie($name, $value, $time, $path, $domain, $secure, $httpOnly, $raw, $sameSite);
+    }
+
+    /**
+     * Create a cookie that lasts "forever" (400 days).
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  string|null  $path
+     * @param  string|null  $domain
+     * @param  bool|null  $secure
+     * @param  bool  $httpOnly
+     * @param  bool  $raw
+     * @param  string|null  $sameSite
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
+    public function forever($name, $value, $path = null, $domain = null, $secure = null, $httpOnly = true, $raw = false, $sameSite = null)
+    {
+        return $this->make($name, $value, 576000, $path, $domain, $secure, $httpOnly, $raw, $sameSite);
+    }
+
+    /**
+     * Expire the given cookie.
+     *
+     * @param  string  $name
+     * @param  string|null  $path
+     * @param  string|null  $domain
+     * @return \Symfony\Component\HttpFoundation\Cookie
+     */
+    public function forget($name, $path = null, $domain = null)
+    {
+        return $this->make($name, null, -2628000, $path, $domain);
+    }
+
+    /**
+     * Determine if a cookie has been queued.
+     *
+     * @param  string  $key
+     * @param  string|null  $path
+     * @return bool
+     */
+    public function hasQueued($key, $path = null)
+    {
