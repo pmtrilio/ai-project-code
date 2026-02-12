def36b22290bf2f28fd1c23e7c9a9087d7529cdc
pmtrilio <pm@trilio.bg>
2026-02-12 14:18:49 +0200

    Upload file via PHP cURL

---
+++ PY/HTTP комуникация/HTTP комуникация – Resolvers (30-70).py
@@ -0,0 +1,41 @@
+from .exceptions import NoReverseMatch, Resolver404
+from .utils import get_callable
+
+
+class ResolverMatch:
+    def __init__(
+        self,
+        func,
+        args,
+        kwargs,
+        url_name=None,
+        app_names=None,
+        namespaces=None,
+        route=None,
+        tried=None,
+        captured_kwargs=None,
+        extra_kwargs=None,
+    ):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.url_name = url_name
+        self.route = route
+        self.tried = tried
+        self.captured_kwargs = captured_kwargs
+        self.extra_kwargs = extra_kwargs
+
+        # If a URLRegexResolver doesn't have a namespace or app_name, it passes
+        # in an empty value.
+        self.app_names = [x for x in app_names if x] if app_names else []
+        self.app_name = ":".join(self.app_names)
+        self.namespaces = [x for x in namespaces if x] if namespaces else []
+        self.namespace = ":".join(self.namespaces)
+
+        if hasattr(func, "view_class"):
+            func = func.view_class
+        if not hasattr(func, "__name__"):
+            # A class-based view
+            self._func_path = func.__class__.__module__ + "." + func.__class__.__name__
+        else:
+            # A function-based view
