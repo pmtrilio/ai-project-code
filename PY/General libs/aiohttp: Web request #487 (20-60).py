5e141dff7e01ae5d8b5178cab7b3714f6c4ee17f
pmtrilio <pm@trilio.bg>
2026-02-12 20:21:27 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/aiohttp: Web request #487 (20-60).py
@@ -0,0 +1,41 @@
+from ._cookie_helpers import parse_cookie_header
+from .abc import AbstractStreamWriter
+from .helpers import (
+    _SENTINEL,
+    ETAG_ANY,
+    LIST_QUOTED_ETAG_RE,
+    ChainMapProxy,
+    ETag,
+    HeadersMixin,
+    RequestKey,
+    frozen_dataclass_decorator,
+    is_expected_content_type,
+    parse_http_date,
+    reify,
+    sentinel,
+    set_exception,
+)
+from .http_parser import RawRequestMessage
+from .http_writer import HttpVersion
+from .multipart import BodyPartReader, MultipartReader
+from .streams import EmptyStreamReader, StreamReader
+from .typedefs import (
+    DEFAULT_JSON_DECODER,
+    JSONDecoder,
+    LooseHeaders,
+    RawHeaders,
+    StrOrURL,
+)
+from .web_exceptions import (
+    HTTPBadRequest,
+    HTTPRequestEntityTooLarge,
+    HTTPUnsupportedMediaType,
+)
+from .web_response import StreamResponse
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    Self = Any
+
+__all__ = ("BaseRequest", "FileField", "Request")
