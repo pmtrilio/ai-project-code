86347f7b154b92a39240fb8479f41f0066cb0e11
pmtrilio <pm@trilio.bg>
2026-02-12 19:17:28 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteOptionsExtensions #390 (5-85).cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Rewrite;
+
+/// <summary>
+/// The builder to a list of rules for <see cref="RewriteOptions"/> and <see cref="RewriteMiddleware"/>
+/// </summary>
+public static class RewriteOptionsExtensions
+{
+    /// <summary>
+    /// Adds a rule to the current rules.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="rule">A rule to be added to the current rules.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions Add(this RewriteOptions options, IRule rule)
+    {
+        options.Rules.Add(rule);
+        return options;
+    }
+
+    /// <summary>
+    /// Adds a rule to the current rules.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="applyRule">A Func that checks and applies the rule.</param>
+    /// <returns></returns>
+    public static RewriteOptions Add(this RewriteOptions options, Action<RewriteContext> applyRule)
+    {
+        options.Rules.Add(new DelegateRule(applyRule));
+        return options;
+    }
+
+    /// <summary>
+    /// Adds a rule that rewrites the path if the regex matches the HttpContext's PathString.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <param name="skipRemainingRules">If the regex matches, conditionally stop processing other rules.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRewrite(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement, bool skipRemainingRules)
+    {
+        options.Rules.Add(new RewriteRule(regex, replacement, skipRemainingRules));
+        return options;
+    }
+
+    /// <summary>
+    /// Redirect the request if the regex matches the HttpContext's PathString, with returning a 302
+    /// status code for found.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRedirect(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement)
+    {
+        return AddRedirect(options, regex, replacement, statusCode: StatusCodes.Status302Found);
+    }
+
+    /// <summary>
+    /// Redirect the request if the regex matches the HttpContext's PathString
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <param name="regex">The regex string to compare with.</param>
+    /// <param name="replacement">If the regex matches, what to replace the uri with.</param>
+    /// <param name="statusCode">The status code to add to the response.</param>
+    /// <returns>The Rewrite options.</returns>
+    public static RewriteOptions AddRedirect(this RewriteOptions options, [StringSyntax(StringSyntaxAttribute.Regex)] string regex, string replacement, int statusCode)
+    {
+        options.Rules.Add(new RedirectRule(regex, replacement, statusCode));
+        return options;
+    }
+
+    /// <summary>
+    /// Redirect a request to https if the incoming request is http, with returning a 301
+    /// status code for permanently redirected.
+    /// </summary>
+    /// <param name="options">The <see cref="RewriteOptions"/>.</param>
+    /// <returns></returns>
+    public static RewriteOptions AddRedirectToHttpsPermanent(this RewriteOptions options)
