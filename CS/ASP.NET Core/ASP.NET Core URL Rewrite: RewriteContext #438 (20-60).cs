57cf6458f7e37c765790dcd5e779f9906c9493ad
pmtrilio <pm@trilio.bg>
2026-02-12 19:18:19 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core URL Rewrite: RewriteContext #438 (20-60).cs
@@ -0,0 +1,20 @@
+    public HttpContext HttpContext { get; set; } = default!;
+
+    /// <summary>
+    /// Gets and sets the File Provider for file and directory checks.
+    /// </summary>
+    public IFileProvider StaticFileProvider { get; set; } = default!;
+
+    /// <summary>
+    /// Gets and sets the logger
+    /// </summary>
+    public ILogger Logger { get; set; } = NullLogger.Instance;
+
+    /// <summary>
+    /// A shared result that is set appropriately by each rule for the next action that
+    /// should be taken. See <see cref="RuleResult"/>
+    /// </summary>
+    public RuleResult Result { get; set; }
+
+    internal StringBuilder Builder { get; set; } = new StringBuilder(64);
+}
