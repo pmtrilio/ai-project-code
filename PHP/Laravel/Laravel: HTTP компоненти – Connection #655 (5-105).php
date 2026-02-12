4943bc0878c88efc79acdce3d63037945857f6f2
pmtrilio <pm@trilio.bg>
2026-02-12 19:08:49 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Connection #655 (5-105).php
@@ -0,0 +1,101 @@
+use Carbon\CarbonInterval;
+use Closure;
+use DateTimeInterface;
+use Exception;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Events\StatementPrepared;
+use Illuminate\Database\Events\TransactionBeginning;
+use Illuminate\Database\Events\TransactionCommitted;
+use Illuminate\Database\Events\TransactionCommitting;
+use Illuminate\Database\Events\TransactionRolledBack;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Query\Grammars\Grammar as QueryGrammar;
+use Illuminate\Database\Query\Processors\Processor;
+use Illuminate\Database\Schema\Builder as SchemaBuilder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\InteractsWithTime;
+use Illuminate\Support\Traits\Macroable;
+use PDO;
+use PDOStatement;
+use RuntimeException;
+
+use function Illuminate\Support\enum_value;
+
+class Connection implements ConnectionInterface
+{
+    use DetectsConcurrencyErrors,
+        DetectsLostConnections,
+        Concerns\ManagesTransactions,
+        InteractsWithTime,
+        Macroable;
+
+    /**
+     * The active PDO connection.
+     *
+     * @var \PDO|(\Closure(): \PDO)
+     */
+    protected $pdo;
+
+    /**
+     * The active PDO connection used for reads.
+     *
+     * @var \PDO|(\Closure(): \PDO)
+     */
+    protected $readPdo;
+
+    /**
+     * The database connection configuration options for reading.
+     *
+     * @var array
+     */
+    protected $readPdoConfig = [];
+
+    /**
+     * The name of the connected database.
+     *
+     * @var string
+     */
+    protected $database;
+
+    /**
+     * The type of the connection.
+     *
+     * @var string|null
+     */
+    protected $readWriteType;
+
+    /**
+     * The table prefix for the connection.
+     *
+     * @var string
+     */
+    protected $tablePrefix = '';
+
+    /**
+     * The database connection configuration options.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * The reconnector instance for the connection.
+     *
+     * @var (callable(\Illuminate\Database\Connection): mixed)
+     */
+    protected $reconnector;
+
+    /**
+     * The query grammar implementation.
+     *
+     * @var \Illuminate\Database\Query\Grammars\Grammar
+     */
+    protected $queryGrammar;
+
+    /**
+     * The schema grammar implementation.
+     *
+     * @var \Illuminate\Database\Schema\Grammars\Grammar
+     */
