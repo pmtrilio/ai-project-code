f62965843d167bcece565d3d4a5446f7abde5af9
pmtrilio <pm@trilio.bg>
2026-02-12 14:46:47 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Blueprint #426 (20-60).php
@@ -0,0 +1,41 @@
+    use Macroable;
+
+    /**
+     * The database connection instance.
+     */
+    protected Connection $connection;
+
+    /**
+     * The schema grammar instance.
+     */
+    protected Grammar $grammar;
+
+    /**
+     * The table the blueprint describes.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * The columns that should be added to the table.
+     *
+     * @var \Illuminate\Database\Schema\ColumnDefinition[]
+     */
+    protected $columns = [];
+
+    /**
+     * The commands that should be run for the table.
+     *
+     * @var \Illuminate\Support\Fluent[]
+     */
+    protected $commands = [];
+
+    /**
+     * The storage engine that should be used for the table.
+     *
+     * @var string
+     */
+    public $engine;
+
+    /**
