d0a90ccf1060079674b853659f467360466696e8
pmtrilio <pm@trilio.bg>
2026-02-12 14:26:12 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Конфигуриране (10-70).php
@@ -0,0 +1,61 @@
+use InvalidArgumentException;
+
+class Repository implements ArrayAccess, ConfigContract
+{
+    use Macroable;
+
+    /**
+     * All of the configuration items.
+     *
+     * @var array<string,mixed>
+     */
+    protected $items = [];
+
+    /**
+     * Create a new configuration repository.
+     *
+     * @param  array  $items
+     */
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Determine if the given configuration value exists.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return Arr::has($this->items, $key);
+    }
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param  array|string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        if (is_array($key)) {
+            return $this->getMany($key);
+        }
+
+        return Arr::get($this->items, $key, $default);
+    }
+
+    /**
+     * Get many configuration values.
+     *
+     * @param  array<string|int,mixed>  $keys
+     * @return array<string,mixed>
+     */
+    public function getMany($keys)
+    {
+        $config = [];
+
+        foreach ($keys as $key => $default) {
