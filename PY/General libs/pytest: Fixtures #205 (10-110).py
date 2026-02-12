7cab2c3518791ab372fbc97a6ff61beafdc189e0
pmtrilio <pm@trilio.bg>
2026-02-12 19:34:28 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/pytest: Fixtures #205 (10-110).py
@@ -0,0 +1,100 @@
+from collections.abc import Iterable
+from collections.abc import Iterator
+from collections.abc import Mapping
+from collections.abc import MutableMapping
+from collections.abc import Sequence
+from collections.abc import Set as AbstractSet
+import dataclasses
+import functools
+import inspect
+import os
+from pathlib import Path
+import sys
+import types
+from typing import Any
+from typing import cast
+from typing import Final
+from typing import final
+from typing import Generic
+from typing import Literal
+from typing import NoReturn
+from typing import overload
+from typing import TYPE_CHECKING
+from typing import TypeVar
+import warnings
+
+import _pytest
+from _pytest import nodes
+from _pytest._code import getfslineno
+from _pytest._code import Source
+from _pytest._code.code import FormattedExcinfo
+from _pytest._code.code import TerminalRepr
+from _pytest._io import TerminalWriter
+from _pytest.compat import assert_never
+from _pytest.compat import get_real_func
+from _pytest.compat import getfuncargnames
+from _pytest.compat import getimfunc
+from _pytest.compat import getlocation
+from _pytest.compat import NOTSET
+from _pytest.compat import NotSetType
+from _pytest.compat import safe_getattr
+from _pytest.compat import safe_isclass
+from _pytest.compat import signature
+from _pytest.config import _PluggyPlugin
+from _pytest.config import Config
+from _pytest.config import ExitCode
+from _pytest.config.argparsing import Parser
+from _pytest.deprecated import check_ispytest
+from _pytest.deprecated import YIELD_FIXTURE
+from _pytest.main import Session
+from _pytest.mark import ParameterSet
+from _pytest.mark.structures import MarkDecorator
+from _pytest.outcomes import fail
+from _pytest.outcomes import skip
+from _pytest.outcomes import TEST_OUTCOME
+from _pytest.pathlib import absolutepath
+from _pytest.pathlib import bestrelpath
+from _pytest.scope import _ScopeName
+from _pytest.scope import HIGH_SCOPES
+from _pytest.scope import Scope
+from _pytest.warning_types import PytestWarning
+
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import BaseExceptionGroup
+
+
+if TYPE_CHECKING:
+    from _pytest.python import CallSpec2
+    from _pytest.python import Function
+    from _pytest.python import Metafunc
+
+
+# The value of the fixture -- return/yield of the fixture function (type variable).
+FixtureValue = TypeVar("FixtureValue", covariant=True)
+# The type of the fixture function (type variable).
+FixtureFunction = TypeVar("FixtureFunction", bound=Callable[..., object])
+# The type of a fixture function (type alias generic in fixture value).
+_FixtureFunc = Callable[..., FixtureValue] | Callable[..., Generator[FixtureValue]]
+# The type of FixtureDef.cached_result (type alias generic in fixture value).
+_FixtureCachedResult = (
+    tuple[
+        # The result.
+        FixtureValue,
+        # Cache key.
+        object,
+        None,
+    ]
+    | tuple[
+        None,
+        # Cache key.
+        object,
+        # The exception and the original traceback.
+        tuple[BaseException, types.TracebackType | None],
+    ]
+)
+
+
+def pytest_sessionstart(session: Session) -> None:
+    session._fixturemanager = FixtureManager(session)
+
