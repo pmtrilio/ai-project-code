3c05441b1dfed6d66ae5ac652a8a319639695751
pmtrilio <pm@trilio.bg>
2026-02-12 19:12:07 +0200

    Upload file via PHP cURL

---
+++ PHP/General/Listener Provider (56-112).php
@@ -0,0 +1,11 @@
+     * Gets the listener priority for a specific event.
+     *
+     * Returns null if the event or the listener does not exist.
+     */
+    public function getListenerPriority(string $eventName, callable $listener): ?int;
+
+    /**
+     * Checks whether an event has any registered listeners.
+     */
+    public function hasListeners(?string $eventName = null): bool;
+}
