9d48182bfbc6a95a2c0833b3f154f651a1c58cff
pmtrilio <pm@trilio.bg>
2026-02-12 14:43:28 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Eventmanager #239 (1-81).php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         2.1.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Event;
+
+use Cake\Core\Exception\CakeException;
+use Closure;
+use InvalidArgumentException;
+use ReflectionFunction;
+use function Cake\Core\deprecationWarning;
+
+/**
+ * The event manager is responsible for keeping track of event listeners, passing the correct
+ * data to them, and firing them in the correct order, when associated events are triggered. You
+ * can create multiple instances of this object to manage local events or keep a single instance
+ * and pass it around to manage all events in your app.
+ */
+class EventManager implements EventManagerInterface
+{
+    /**
+     * The default priority queue value for new, attached listeners
+     *
+     * @var int
+     */
+    public static int $defaultPriority = 10;
+
+    /**
+     * The globally available instance, used for dispatching events attached from any scope
+     *
+     * @var \Cake\Event\EventManager|null
+     */
+    protected static ?EventManager $_generalManager = null;
+
+    /**
+     * List of listener callbacks associated to
+     *
+     * @var array
+     */
+    protected array $_listeners = [];
+
+    /**
+     * Internal flag to distinguish a common manager from the singleton
+     *
+     * @var bool
+     */
+    protected bool $_isGlobal = false;
+
+    /**
+     * The event list object.
+     *
+     * @var \Cake\Event\EventList|null
+     */
+    protected ?EventList $_eventList = null;
+
+    /**
+     * Enables automatic adding of events to the event list object if it is present.
+     *
+     * @var bool
+     */
+    protected bool $_trackEvents = false;
+
+    /**
+     * Returns the globally available instance of a Cake\Event\EventManager
+     * this is used for dispatching events attached from outside the scope
+     * other managers were created. Usually for creating hook systems or inter-class
+     * communication
+     *
+     * If called with the first parameter, it will be set as the globally available instance
