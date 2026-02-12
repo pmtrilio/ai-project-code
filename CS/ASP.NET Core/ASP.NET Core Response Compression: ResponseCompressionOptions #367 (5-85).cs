f7a622425f1f59911ebd446d13421ba3965601dd
pmtrilio <pm@trilio.bg>
2026-02-12 19:17:01 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Response Compression: ResponseCompressionOptions #367 (5-85).cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Microsoft.AspNetCore.ResponseCompression;
+
+/// <summary>
+/// Options for the HTTP response compression middleware.
+/// </summary>
+public class ResponseCompressionOptions
+{
+    /// <summary>
+    /// Response Content-Type MIME types to compress.
+    /// </summary>
+    public IEnumerable<string> MimeTypes { get; set; } = Enumerable.Empty<string>();
+
+    /// <summary>
+    /// Response Content-Type MIME types to not compress.
+    /// </summary>
+    public IEnumerable<string> ExcludedMimeTypes { get; set; } = Enumerable.Empty<string>();
+
+    /// <summary>
+    /// Indicates if responses over HTTPS connections should be compressed. The default is 'false'.
+    /// Enabling compression on HTTPS requests for remotely manipulable content may expose security problems.
+    /// </summary>
+    /// <remarks>
+    /// This can be overridden per request using <see cref="IHttpsCompressionFeature"/>.
+    /// </remarks>
+    public bool EnableForHttps { get; set; }
+
+    /// <summary>
+    /// The <see cref="ICompressionProvider"/> types to use for responses.
+    /// Providers are prioritized based on the order they are added.
+    /// </summary>
+    public CompressionProviderCollection Providers { get; } = new CompressionProviderCollection();
+}
