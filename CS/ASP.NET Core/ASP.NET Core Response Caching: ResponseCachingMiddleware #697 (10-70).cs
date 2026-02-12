bcd744e9d891b5175709ba90a9752f3009f0b657
pmtrilio <pm@trilio.bg>
2026-02-12 19:29:41 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Response Caching: ResponseCachingMiddleware #697 (10-70).cs
@@ -0,0 +1,61 @@
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.AspNetCore.ResponseCaching;
+
+/// <summary>
+/// Enable HTTP response caching.
+/// </summary>
+public class ResponseCachingMiddleware
+{
+    private static readonly TimeSpan DefaultExpirationTimeSpan = TimeSpan.FromSeconds(10);
+
+    // see https://tools.ietf.org/html/rfc7232#section-4.1
+    private static readonly string[] HeadersToIncludeIn304 =
+        new[] { "Cache-Control", "Content-Location", "Date", "ETag", "Expires", "Vary" };
+
+    private readonly RequestDelegate _next;
+    private readonly ResponseCachingOptions _options;
+    private readonly ILogger _logger;
+    private readonly IResponseCachingPolicyProvider _policyProvider;
+    private readonly IResponseCache _cache;
+    private readonly IResponseCachingKeyProvider _keyProvider;
+
+    /// <summary>
+    /// Creates a new <see cref="ResponseCachingMiddleware"/>.
+    /// </summary>
+    /// <param name="next">The <see cref="RequestDelegate"/> representing the next middleware in the pipeline.</param>
+    /// <param name="options">The options for this middleware.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> used for logging.</param>
+    /// <param name="poolProvider">The <see cref="ObjectPoolProvider"/> used for creating <see cref="ObjectPool"/> instances.</param>
+    public ResponseCachingMiddleware(
+        RequestDelegate next,
+        IOptions<ResponseCachingOptions> options,
+        ILoggerFactory loggerFactory,
+        ObjectPoolProvider poolProvider)
+        : this(
+            next,
+            options,
+            loggerFactory,
+            new ResponseCachingPolicyProvider(),
+            new MemoryResponseCache(new MemoryCache(new MemoryCacheOptions
+            {
+                SizeLimit = options.Value.SizeLimit
+            })),
+            new ResponseCachingKeyProvider(poolProvider, options))
+    { }
+
+    // for testing
+    internal ResponseCachingMiddleware(
+        RequestDelegate next,
+        IOptions<ResponseCachingOptions> options,
+        ILoggerFactory loggerFactory,
+        IResponseCachingPolicyProvider policyProvider,
+        IResponseCache cache,
+        IResponseCachingKeyProvider keyProvider)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        ArgumentNullException.ThrowIfNull(policyProvider);
+        ArgumentNullException.ThrowIfNull(cache);
+        ArgumentNullException.ThrowIfNull(keyProvider);
