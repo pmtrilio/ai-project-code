92d84fc0bcb5b75797655157a5ecc08665bfef66
pmtrilio <pm@trilio.bg>
2026-02-12 20:35:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Pipeline #518 (5-45).php
@@ -0,0 +1,40 @@
+use Closure;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Pipeline\Pipeline as PipelineContract;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
+use RuntimeException;
+use Throwable;
+
+class Pipeline implements PipelineContract
+{
+    use Conditionable;
+    use Macroable;
+
+    /**
+     * The container implementation.
+     *
+     * @var \Illuminate\Contracts\Container\Container|null
+     */
+    protected $container;
+
+    /**
+     * The object being passed through the pipeline.
+     *
+     * @var mixed
+     */
+    protected $passable;
+
+    /**
+     * The array of class pipes.
+     *
+     * @var array
+     */
+    protected $pipes = [];
+
+    /**
+     * The method to call on each pipe.
+     *
+     * @var string
+     */
+    protected $method = 'handle';
