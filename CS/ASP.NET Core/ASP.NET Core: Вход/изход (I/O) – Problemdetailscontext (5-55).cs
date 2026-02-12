25a3a9c7a6d6bd308cff6f98a3f3b26a4e5596de
pmtrilio <pm@trilio.bg>
2026-02-12 13:43:57 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: Вход/изход (I/O) – Problemdetailscontext (5-55).cs
@@ -0,0 +1,35 @@
+
+namespace Microsoft.AspNetCore.Http;
+
+/// <summary>
+/// Represent the current problem details context for the request.
+/// </summary>
+public sealed class ProblemDetailsContext
+{
+    private ProblemDetails? _problemDetails;
+
+    /// <summary>
+    /// The <see cref="HttpContext"/> associated with the current request being processed by the filter.
+    /// </summary>
+    public required HttpContext HttpContext { get; init; }
+
+    /// <summary>
+    /// A collection of additional arbitrary metadata associated with the current request endpoint.
+    /// </summary>
+    public EndpointMetadataCollection? AdditionalMetadata { get; init; }
+
+    /// <summary>
+    /// An instance of <see cref="ProblemDetails"/> that will be
+    /// used during the response payload generation.
+    /// </summary>
+    public ProblemDetails ProblemDetails
+    {
+        get => _problemDetails ??= new ProblemDetails();
+        set => _problemDetails = value;
+    }
+
+    /// <summary>
+    /// The exception causing the problem or <c>null</c> if no exception information is available.
+    /// </summary>
+    public Exception? Exception { get; init; }
+}
