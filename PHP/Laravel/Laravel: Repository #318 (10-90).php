4790cd7d6acaaf268495ba3eaea13fe915c869a6
pmtrilio <pm@trilio.bg>
2026-02-12 20:31:42 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Repository #318 (10-90).php
@@ -0,0 +1,81 @@
+use Illuminate\Cache\Events\CacheFlushFailed;
+use Illuminate\Cache\Events\CacheFlushing;
+use Illuminate\Cache\Events\CacheHit;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Cache\Events\ForgettingKey;
+use Illuminate\Cache\Events\KeyForgetFailed;
+use Illuminate\Cache\Events\KeyForgotten;
+use Illuminate\Cache\Events\KeyWriteFailed;
+use Illuminate\Cache\Events\KeyWritten;
+use Illuminate\Cache\Events\RetrievingKey;
+use Illuminate\Cache\Events\RetrievingManyKeys;
+use Illuminate\Cache\Events\WritingKey;
+use Illuminate\Cache\Events\WritingManyKeys;
+use Illuminate\Contracts\Cache\Repository as CacheContract;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Support\InteractsWithTime;
+use Illuminate\Support\Traits\Macroable;
+
+use function Illuminate\Support\defer;
+use function Illuminate\Support\enum_value;
+
+/**
+ * @mixin \Illuminate\Contracts\Cache\Store
+ */
+class Repository implements ArrayAccess, CacheContract
+{
+    use InteractsWithTime, Macroable {
+        __call as macroCall;
+    }
+
+    /**
+     * The cache store implementation.
+     *
+     * @var \Illuminate\Contracts\Cache\Store
+     */
+    protected $store;
+
+    /**
+     * The event dispatcher implementation.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher|null
+     */
+    protected $events;
+
+    /**
+     * The default number of seconds to store items.
+     *
+     * @var int|null
+     */
+    protected $default = 3600;
+
+    /**
+     * The cache store configuration options.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Create a new cache repository instance.
+     *
+     * @param  \Illuminate\Contracts\Cache\Store  $store
+     * @param  array  $config
+     */
+    public function __construct(Store $store, array $config = [])
+    {
+        $this->store = $store;
+        $this->config = $config;
+    }
+
+    /**
+     * Determine if an item exists in the cache.
+     *
+     * @param  \BackedEnum|\UnitEnum|array|string  $key
+     * @return bool
+     */
+    public function has($key): bool
+    {
