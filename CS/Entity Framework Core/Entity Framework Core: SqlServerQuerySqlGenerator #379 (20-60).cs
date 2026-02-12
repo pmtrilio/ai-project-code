c3da623636a6ea391c9518a9ce981f6c618f94f5
pmtrilio <pm@trilio.bg>
2026-02-12 15:08:19 +0200

    Upload file via PHP cURL

---
+++ CS/Entity Framework Core/Entity Framework Core: SqlServerQuerySqlGenerator #379 (20-60).cs
@@ -0,0 +1,41 @@
+public class SqlServerQuerySqlGenerator : QuerySqlGenerator
+{
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+    private readonly ISqlGenerationHelper _sqlGenerationHelper;
+    private readonly ISqlServerSingletonOptions _sqlServerSingletonOptions;
+
+    private bool _withinTable;
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqlServerQuerySqlGenerator(
+        QuerySqlGeneratorDependencies dependencies,
+        IRelationalTypeMappingSource typeMappingSource,
+        ISqlServerSingletonOptions sqlServerSingletonOptions)
+        : base(dependencies)
+    {
+        _typeMappingSource = typeMappingSource;
+        _sqlGenerationHelper = dependencies.SqlGenerationHelper;
+        _sqlServerSingletonOptions = sqlServerSingletonOptions;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    protected override bool TryGenerateWithoutWrappingSelect(SelectExpression selectExpression)
+        // SQL Server doesn't support VALUES as a top-level statement, so we need to wrap the VALUES in a SELECT:
+        // SELECT 1 AS x UNION VALUES (2), (3) -- simple
+        // SELECT 1 AS x UNION SELECT * FROM (VALUES (2), (3)) AS f(x) -- SQL Server
+        => selectExpression.Tables is not [ValuesExpression]
+            && base.TryGenerateWithoutWrappingSelect(selectExpression);
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
