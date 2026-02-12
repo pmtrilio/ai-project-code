ef3507f46de7a87ae06e4ce8fb7f73522381e167
pmtrilio <pm@trilio.bg>
2026-02-12 19:06:16 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Facade #527 (5-45).php
@@ -0,0 +1,41 @@
+use Closure;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Js;
+use Illuminate\Support\Number;
+use Illuminate\Support\Str;
+use Illuminate\Support\Testing\Fakes\Fake;
+use Illuminate\Support\Uri;
+use Mockery;
+use Mockery\LegacyMockInterface;
+use RuntimeException;
+
+abstract class Facade
+{
+    /**
+     * The application instance being facaded.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application|null
+     */
+    protected static $app;
+
+    /**
+     * The resolved object instances.
+     *
+     * @var array
+     */
+    protected static $resolvedInstance;
+
+    /**
+     * Indicates if the resolved instance should be cached.
+     *
+     * @var bool
+     */
+    protected static $cached = true;
+
+    /**
+     * Run a Closure when the facade has been resolved.
+     *
+     * @param  \Closure  $callback
+     * @return void
