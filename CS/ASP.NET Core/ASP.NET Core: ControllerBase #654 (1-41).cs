01297e2546e3cb6e611cc14f57a4c8f1791a519d
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:54 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: ControllerBase #654 (1-41).cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// A base class for an MVC controller without view support.
+/// </summary>
+[Controller]
+public abstract class ControllerBase
+{
+    private ControllerContext? _controllerContext;
+    private IModelMetadataProvider? _metadataProvider;
+    private IModelBinderFactory? _modelBinderFactory;
+    private IObjectModelValidator? _objectValidator;
+    private IUrlHelper? _url;
+    private ProblemDetailsFactory? _problemDetailsFactory;
+
+    /// <summary>
+    /// Gets the <see cref="Http.HttpContext"/> for the executing action.
+    /// </summary>
+    public HttpContext HttpContext => ControllerContext.HttpContext;
+
+    /// <summary>
+    /// Gets the <see cref="HttpRequest"/> for the executing action.
+    /// </summary>
