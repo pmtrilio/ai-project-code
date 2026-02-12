fd4c453d62b1d98685cd627d766417df4f44f669
pmtrilio <pm@trilio.bg>
2026-02-12 14:30:58 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Boundmethod #129 (1-81).php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Illuminate\Container;
+
+use Closure;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use InvalidArgumentException;
+use ReflectionFunction;
+use ReflectionMethod;
+
+class BoundMethod
+{
+    /**
+     * Call the given Closure / class@method and inject its dependencies.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     * @param  callable|string  $callback
+     * @param  array  $parameters
+     * @param  string|null  $defaultMethod
+     * @return mixed
+     *
+     * @throws \ReflectionException
+     * @throws \InvalidArgumentException
+     */
+    public static function call($container, $callback, array $parameters = [], $defaultMethod = null)
+    {
+        if (is_string($callback) && ! $defaultMethod && method_exists($callback, '__invoke')) {
+            $defaultMethod = '__invoke';
+        }
+
+        if (static::isCallableWithAtSign($callback) || $defaultMethod) {
+            return static::callClass($container, $callback, $parameters, $defaultMethod);
+        }
+
+        return static::callBoundMethod($container, $callback, function () use ($container, $callback, $parameters) {
+            return $callback(...array_values(static::getMethodDependencies($container, $callback, $parameters)));
+        });
+    }
+
+    /**
+     * Call a string reference to a class using Class@method syntax.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     * @param  string  $target
+     * @param  array  $parameters
+     * @param  string|null  $defaultMethod
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected static function callClass($container, $target, array $parameters = [], $defaultMethod = null)
+    {
+        $segments = explode('@', $target);
+
+        // We will assume an @ sign is used to delimit the class name from the method
+        // name. We will split on this @ sign and then build a callable array that
+        // we can pass right back into the "call" method for dependency binding.
+        $method = count($segments) === 2
+            ? $segments[1]
+            : $defaultMethod;
+
+        if (is_null($method)) {
+            throw new InvalidArgumentException('Method not provided.');
+        }
+
+        return static::call(
+            $container,
+            [$container->make($segments[0]), $method],
+            $parameters
+        );
+    }
+
+    /**
+     * Call a method that has been bound to the container.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     * @param  callable  $callback
+     * @param  mixed  $default
+     * @return mixed
+     */
+    protected static function callBoundMethod($container, $callback, $default)
