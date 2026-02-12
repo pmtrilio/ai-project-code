a2a2199fcdb19c835565de914525b7d04e925360
pmtrilio <pm@trilio.bg>
2026-02-12 20:34:33 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: RedisManager #457 (30-110).php
@@ -0,0 +1,81 @@
+     */
+    protected $driver;
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * The Redis server configurations.
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * The Redis connections.
+     *
+     * @var mixed
+     */
+    protected $connections;
+
+    /**
+     * Indicates whether event dispatcher is set on connections.
+     *
+     * @var bool
+     */
+    protected $events = false;
+
+    /**
+     * Create a new Redis manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  string  $driver
+     * @param  array  $config
+     */
+    public function __construct($app, $driver, array $config)
+    {
+        $this->app = $app;
+        $this->driver = $driver;
+        $this->config = $config;
+    }
+
+    /**
+     * Get a Redis connection by name.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Redis\Connections\Connection
+     */
+    public function connection($name = null)
+    {
+        $name = $name ?: 'default';
+
+        if (isset($this->connections[$name])) {
+            return $this->connections[$name];
+        }
+
+        return $this->connections[$name] = $this->configure(
+            $this->resolve($name), $name
+        );
+    }
+
+    /**
+     * Resolve the given connection by name.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Redis\Connections\Connection
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function resolve($name = null)
+    {
+        $name = $name ?: 'default';
+
+        $options = $this->config['options'] ?? [];
+
+        if (isset($this->config[$name])) {
+            return $this->connector()->connect(
+                $this->parseConnectionConfiguration($this->config[$name]),
