2a6df449108c8a35845c113ac6e221ad2ebd24cc
pmtrilio <pm@trilio.bg>
2026-02-12 14:41:01 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Env #105 (5-85).php
@@ -0,0 +1,80 @@
+use Closure;
+use Dotenv\Repository\Adapter\PutenvAdapter;
+use Dotenv\Repository\RepositoryBuilder;
+use PhpOption\Option;
+use RuntimeException;
+
+class Env
+{
+    /**
+     * Indicates if the putenv adapter is enabled.
+     *
+     * @var bool
+     */
+    protected static $putenv = true;
+
+    /**
+     * The environment repository instance.
+     *
+     * @var \Dotenv\Repository\RepositoryInterface|null
+     */
+    protected static $repository;
+
+    /**
+     * The list of custom adapters for loading environment variables.
+     *
+     * @var array<Closure>
+     */
+    protected static $customAdapters = [];
+
+    /**
+     * Enable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function enablePutenv()
+    {
+        static::$putenv = true;
+        static::$repository = null;
+    }
+
+    /**
+     * Disable the putenv adapter.
+     *
+     * @return void
+     */
+    public static function disablePutenv()
+    {
+        static::$putenv = false;
+        static::$repository = null;
+    }
+
+    /**
+     * Register a custom adapter creator Closure.
+     */
+    public static function extend(Closure $callback, ?string $name = null): void
+    {
+        if (! is_null($name)) {
+            static::$customAdapters[$name] = $callback;
+        } else {
+            static::$customAdapters[] = $callback;
+        }
+    }
+
+    /**
+     * Get the environment repository instance.
+     *
+     * @return \Dotenv\Repository\RepositoryInterface
+     */
+    public static function getRepository()
+    {
+        if (static::$repository === null) {
+            $builder = RepositoryBuilder::createWithDefaultAdapters();
+
+            if (static::$putenv) {
+                $builder = $builder->addAdapter(PutenvAdapter::class);
+            }
+
+            foreach (static::$customAdapters as $adapter) {
+                $builder = $builder->addAdapter($adapter());
+            }
