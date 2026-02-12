88d8adf53f229ba4667614ecf8bac8907c6f14a1
pmtrilio <pm@trilio.bg>
2026-02-12 14:18:44 +0200

    Upload file via PHP cURL

---
+++ PY/HTTP комуникация/HTTP комуникация – Request (1-41).py
@@ -0,0 +1,40 @@
+import codecs
+import copy
+import operator
+from io import BytesIO
+from itertools import chain
+from urllib.parse import parse_qsl, quote, urlencode, urljoin, urlsplit
+
+from django.conf import settings
+from django.core import signing
+from django.core.exceptions import (
+    BadRequest,
+    DisallowedHost,
+    ImproperlyConfigured,
+    RequestDataTooBig,
+    TooManyFieldsSent,
+)
+from django.core.files import uploadhandler
+from django.http.multipartparser import (
+    MultiPartParser,
+    MultiPartParserError,
+    TooManyFilesSent,
+)
+from django.utils.datastructures import (
+    CaseInsensitiveMapping,
+    ImmutableList,
+    MultiValueDict,
+)
+from django.utils.encoding import escape_uri_path, iri_to_uri
+from django.utils.functional import cached_property
+from django.utils.http import is_same_domain, parse_header_parameters
+from django.utils.regex_helper import _lazy_re_compile
+
+RAISE_ERROR = object()
+host_validation_re = _lazy_re_compile(
+    r"^([a-z0-9.-]+|\[[a-f0-9]*:[a-f0-9.:]+\])(?::([0-9]+))?$"
+)
+
+
+class UnreadablePostError(OSError):
+    pass
