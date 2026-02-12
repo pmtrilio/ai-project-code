434e8c686b43cd8addeb5a348806c2713e4e25ac
pmtrilio <pm@trilio.bg>
2026-02-12 14:42:33 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Eventmanager #187 (20-120).php
@@ -0,0 +1,100 @@
+use Closure;
+use InvalidArgumentException;
+use ReflectionFunction;
+use function Cake\Core\deprecationWarning;
+
+/**
+ * The event manager is responsible for keeping track of event listeners, passing the correct
+ * data to them, and firing them in the correct order, when associated events are triggered. You
+ * can create multiple instances of this object to manage local events or keep a single instance
+ * and pass it around to manage all events in your app.
+ */
+class EventManager implements EventManagerInterface
+{
+    /**
+     * The default priority queue value for new, attached listeners
+     *
+     * @var int
+     */
+    public static int $defaultPriority = 10;
+
+    /**
+     * The globally available instance, used for dispatching events attached from any scope
+     *
+     * @var \Cake\Event\EventManager|null
+     */
+    protected static ?EventManager $_generalManager = null;
+
+    /**
+     * List of listener callbacks associated to
+     *
+     * @var array
+     */
+    protected array $_listeners = [];
+
+    /**
+     * Internal flag to distinguish a common manager from the singleton
+     *
+     * @var bool
+     */
+    protected bool $_isGlobal = false;
+
+    /**
+     * The event list object.
+     *
+     * @var \Cake\Event\EventList|null
+     */
+    protected ?EventList $_eventList = null;
+
+    /**
+     * Enables automatic adding of events to the event list object if it is present.
+     *
+     * @var bool
+     */
+    protected bool $_trackEvents = false;
+
+    /**
+     * Returns the globally available instance of a Cake\Event\EventManager
+     * this is used for dispatching events attached from outside the scope
+     * other managers were created. Usually for creating hook systems or inter-class
+     * communication
+     *
+     * If called with the first parameter, it will be set as the globally available instance
+     *
+     * @param \Cake\Event\EventManager|null $manager Event manager instance.
+     * @return \Cake\Event\EventManager The global event manager
+     */
+    public static function instance(?EventManager $manager = null): EventManager
+    {
+        if ($manager === null && static::$_generalManager) {
+            return static::$_generalManager;
+        }
+
+        if ($manager instanceof EventManager) {
+            static::$_generalManager = $manager;
+        }
+        static::$_generalManager ??= new static();
+        static::$_generalManager->_isGlobal = true;
+
+        return static::$_generalManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function on(
+        EventListenerInterface|string $eventKey,
+        callable|array $options = [],
+        ?callable $callable = null,
+    ) {
+        if ($eventKey instanceof EventListenerInterface) {
+            $this->_attachSubscriber($eventKey);
+
+            return $this;
+        }
+
+        if ($callable === null && !is_callable($options)) {
+            throw new InvalidArgumentException(
+                'Second argument of `EventManager::on()` must be a callable if `$callable` is null.',
+            );
+        }
