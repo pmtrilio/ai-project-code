640d6eb1dc30013fb4231231bcc3a0496cef04ae
pmtrilio <pm@trilio.bg>
2026-02-12 14:10:21 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: HTTP заявка – Ihttprequestfeature (5-45).cs
@@ -0,0 +1,41 @@
+
+/// <summary>
+/// Contains the details of a given request. These properties should all be mutable.
+/// None of these properties should ever be set to null.
+/// </summary>
+public interface IHttpRequestFeature
+{
+    /// <summary>
+    /// Gets or set the HTTP-version as defined in RFC 7230. E.g. "HTTP/1.1"
+    /// </summary>
+    string Protocol { get; set; }
+
+    /// <summary>
+    /// Gets or set the request uri scheme. E.g. "http" or "https".
+    /// <para>
+    /// Note this value is not included in the original request,
+    /// it is inferred by checking if the transport used a TLS
+    /// connection or not.
+    /// </para>
+    /// </summary>
+    string Scheme { get; set; }
+
+    /// <summary>
+    /// Gets or sets the request method as defined in RFC 7230. E.g. "GET", "HEAD", "POST", etc..
+    /// </summary>
+    string Method { get; set; }
+
+    /// <summary>
+    /// Gets or sets the first portion of the request path associated with application root.
+    /// <para>
+    /// The value is un-escaped. The value may be <see cref="string.Empty"/>.
+    /// </para>
+    /// </summary>
+    string PathBase { get; set; }
+
+    /// <summary>
+    /// Gets or sets the portion of the request path that identifies the requested resource.
+    /// <para>
+    /// The value may be <see cref="string.Empty"/> if <see cref="PathBase"/> contains the full path,
+    /// or for 'OPTIONS *' requests.
+    /// The path is fully decoded by the server except for '%2F', which would decode to '/' and
