e3d92a18ef85d23914904a453d8f95bfd9d16d93
pmtrilio <pm@trilio.bg>
2026-02-12 14:51:02 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Connectionmanager #659 (1-61).php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Datasource;
+
+use Cake\Core\StaticConfigTrait;
+use Cake\Database\Connection;
+use Cake\Database\Driver\Mysql;
+use Cake\Database\Driver\Postgres;
+use Cake\Database\Driver\Sqlite;
+use Cake\Database\Driver\Sqlserver;
+use Cake\Datasource\Exception\MissingDatasourceConfigException;
+use Closure;
+
+/**
+ * Manages and loads instances of Connection
+ *
+ * Provides an interface to loading and creating connection objects. Acts as
+ * a registry for the connections defined in an application.
+ *
+ * Provides an interface for loading and enumerating connections defined in
+ * config/app.php
+ */
+class ConnectionManager
+{
+    use StaticConfigTrait {
+        setConfig as protected _setConfig;
+        parseDsn as protected _parseDsn;
+    }
+
+    /**
+     * A map of connection aliases.
+     *
+     * @var array<string, string>
+     */
+    protected static array $_aliasMap = [];
+
+    /**
+     * An array mapping url schemes to fully qualified driver class names
+     *
+     * @var array<string, string>
+     * @phpstan-var array<string, class-string>
+     */
+    protected static array $_dsnClassMap = [
+        'mysql' => Mysql::class,
+        'postgres' => Postgres::class,
+        'sqlite' => Sqlite::class,
+        'sqlserver' => Sqlserver::class,
