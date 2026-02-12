09ab4c7a495f51bab7b7b133cacb779648822d2b
pmtrilio <pm@trilio.bg>
2026-02-12 19:13:35 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: DefaultFilesMiddleware #168 (1-41).cs
@@ -0,0 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.StaticFiles;
+
+/// <summary>
+/// This examines a directory path and determines if there is a default file present.
+/// If so the file name is appended to the path and execution continues.
+/// Note we don't just serve the file because it may require interpretation.
+/// </summary>
+public class DefaultFilesMiddleware
+{
+    private readonly DefaultFilesOptions _options;
+    private readonly PathString _matchUrl;
+    private readonly RequestDelegate _next;
+    private readonly IFileProvider _fileProvider;
+
+    /// <summary>
+    /// Creates a new instance of the DefaultFilesMiddleware.
+    /// </summary>
+    /// <param name="next">The next middleware in the pipeline.</param>
+    /// <param name="hostingEnv">The <see cref="IWebHostEnvironment"/> used by this middleware.</param>
+    /// <param name="options">The configuration options for this middleware.</param>
+    public DefaultFilesMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<DefaultFilesOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(hostingEnv);
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next;
+        _options = options.Value;
+        _fileProvider = _options.FileProvider ?? Helpers.ResolveFileProvider(hostingEnv);
+        _matchUrl = _options.RequestPath;
+    }
