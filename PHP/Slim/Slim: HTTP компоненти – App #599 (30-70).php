ceae078b33a22a6b1b5c0ae35dd694121306f88f
pmtrilio <pm@trilio.bg>
2026-02-12 19:07:44 +0200

    Upload file via PHP cURL

---
+++ PHP/Slim/Slim: HTTP компоненти – App #599 (30-70).php
@@ -0,0 +1,41 @@
+use Slim\Routing\RouteRunner;
+
+use function strtoupper;
+
+/**
+ * @api
+ * @template TContainerInterface of (ContainerInterface|null)
+ * @template-extends RouteCollectorProxy<TContainerInterface>
+ */
+class App extends RouteCollectorProxy implements RequestHandlerInterface
+{
+    /**
+     * Current version
+     *
+     * @var string
+     */
+    public const VERSION = '4.15.1';
+
+    protected RouteResolverInterface $routeResolver;
+
+    protected MiddlewareDispatcherInterface $middlewareDispatcher;
+
+    /**
+     * @param TContainerInterface $container
+     */
+    public function __construct(
+        ResponseFactoryInterface $responseFactory,
+        ?ContainerInterface $container = null,
+        ?CallableResolverInterface $callableResolver = null,
+        ?RouteCollectorInterface $routeCollector = null,
+        ?RouteResolverInterface $routeResolver = null,
+        ?MiddlewareDispatcherInterface $middlewareDispatcher = null
+    ) {
+        parent::__construct(
+            $responseFactory,
+            $callableResolver ?? new CallableResolver($container),
+            $container,
+            $routeCollector
+        );
+
+        $this->routeResolver = $routeResolver ?? new RouteResolver($this->routeCollector);
