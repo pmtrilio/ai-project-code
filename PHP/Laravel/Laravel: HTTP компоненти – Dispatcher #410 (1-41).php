6fd581a5d704741965f3c3745a9b46bd85291dd6
pmtrilio <pm@trilio.bg>
2026-02-12 19:03:57 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: HTTP компоненти – Dispatcher #410 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Illuminate\Events;
+
+use Closure;
+use Exception;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Broadcasting\Factory as BroadcastFactory;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Container\Container as ContainerContract;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueueAfterCommit;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Queue\Concerns\ResolvesQueueRoutes;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Support\Traits\ReflectsClosures;
+use ReflectionClass;
+
+use function Illuminate\Support\enum_value;
+
+class Dispatcher implements DispatcherContract
+{
+    use Macroable, ReflectsClosures, ResolvesQueueRoutes;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Contracts\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The registered event listeners.
+     *
+     * @var array<string, callable|array|class-string|null>
