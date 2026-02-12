fcbfa19c92b37ba975b31c9e37d0c7971f56f239
pmtrilio <pm@trilio.bg>
2026-02-12 20:17:20 +0200

    Upload file via PHP cURL

---
+++ PY/General libs/uvicorn: Config #264 (1-41).py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import asyncio
+import inspect
+import json
+import logging
+import logging.config
+import os
+import socket
+import ssl
+import sys
+from collections.abc import Awaitable, Callable
+from configparser import RawConfigParser
+from pathlib import Path
+from typing import IO, Any, Literal
+
+import click
+
+from uvicorn._compat import iscoroutinefunction
+from uvicorn._types import ASGIApplication
+from uvicorn.importer import ImportFromStringError, import_from_string
+from uvicorn.logging import TRACE_LOG_LEVEL
+from uvicorn.middleware.asgi2 import ASGI2Middleware
+from uvicorn.middleware.message_logger import MessageLoggerMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+from uvicorn.middleware.wsgi import WSGIMiddleware
+
+HTTPProtocolType = Literal["auto", "h11", "httptools"]
+WSProtocolType = Literal["auto", "none", "websockets", "websockets-sansio", "wsproto"]
+LifespanType = Literal["auto", "on", "off"]
+LoopFactoryType = Literal["none", "auto", "asyncio", "uvloop"]
+InterfaceType = Literal["auto", "asgi3", "asgi2", "wsgi"]
+
+LOG_LEVELS: dict[str, int] = {
+    "critical": logging.CRITICAL,
+    "error": logging.ERROR,
+    "warning": logging.WARNING,
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+    "trace": TRACE_LOG_LEVEL,
+}
