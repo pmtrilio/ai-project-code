e55a9a263186603cd66382819dec2504941f9328
pmtrilio <pm@trilio.bg>
2026-02-12 15:06:38 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: RazorView #258 (30-130).cs
@@ -0,0 +1,101 @@
+    /// <param name="viewEngine">The <see cref="IRazorViewEngine"/> used to locate Layout pages.</param>
+    /// <param name="pageActivator">The <see cref="IRazorPageActivator"/> used to activate pages.</param>
+    /// <param name="viewStartPages">The sequence of <see cref="IRazorPage" /> instances executed as _ViewStarts.
+    /// </param>
+    /// <param name="razorPage">The <see cref="IRazorPage"/> instance to execute.</param>
+    /// <param name="htmlEncoder">The HTML encoder.</param>
+    /// <param name="diagnosticListener">The <see cref="DiagnosticListener"/>.</param>
+    public RazorView(
+        IRazorViewEngine viewEngine,
+        IRazorPageActivator pageActivator,
+        IReadOnlyList<IRazorPage> viewStartPages,
+        IRazorPage razorPage,
+        HtmlEncoder htmlEncoder,
+        DiagnosticListener diagnosticListener)
+    {
+        ArgumentNullException.ThrowIfNull(viewEngine);
+        ArgumentNullException.ThrowIfNull(pageActivator);
+        ArgumentNullException.ThrowIfNull(viewStartPages);
+        ArgumentNullException.ThrowIfNull(razorPage);
+        ArgumentNullException.ThrowIfNull(htmlEncoder);
+        ArgumentNullException.ThrowIfNull(diagnosticListener);
+
+        _viewEngine = viewEngine;
+        _pageActivator = pageActivator;
+        ViewStartPages = viewStartPages;
+        RazorPage = razorPage;
+        _htmlEncoder = htmlEncoder;
+        _diagnosticListener = diagnosticListener;
+    }
+
+    /// <inheritdoc />
+    public string Path => RazorPage.Path;
+
+    /// <summary>
+    /// Gets <see cref="IRazorPage"/> instance that the views executes on.
+    /// </summary>
+    public IRazorPage RazorPage { get; }
+
+    /// <summary>
+    /// Gets the sequence of _ViewStart <see cref="IRazorPage"/> instances that are executed by this view.
+    /// </summary>
+    public IReadOnlyList<IRazorPage> ViewStartPages { get; }
+
+    internal Action<IRazorPage, ViewContext>? OnAfterPageActivated { get; set; }
+
+    /// <inheritdoc />
+    public virtual async Task RenderAsync(ViewContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        // This GetRequiredService call is by design. ViewBufferScope is a scoped service, RazorViewEngine
+        // is the component responsible for creating RazorViews and it is a Singleton service. It doesn't
+        // have access to the RequestServices so requiring the service when we render the page is the best
+        // we can do.
+        _bufferScope = context.HttpContext.RequestServices.GetRequiredService<IViewBufferScope>();
+        var bodyWriter = await RenderPageAsync(RazorPage, context, invokeViewStarts: true);
+        await RenderLayoutAsync(context, bodyWriter);
+    }
+
+    private async Task<ViewBufferTextWriter> RenderPageAsync(
+        IRazorPage page,
+        ViewContext context,
+        bool invokeViewStarts)
+    {
+        var writer = context.Writer as ViewBufferTextWriter;
+        if (writer == null)
+        {
+            Debug.Assert(_bufferScope != null);
+
+            // If we get here, this is likely the top-level page (not a partial) - this means
+            // that context.Writer is wrapping the output stream. We need to buffer, so create a buffered writer.
+            var buffer = new ViewBuffer(_bufferScope, page.Path, ViewBuffer.ViewPageSize);
+            writer = new ViewBufferTextWriter(buffer, context.Writer.Encoding, _htmlEncoder, context.Writer);
+        }
+        else
+        {
+            // This means we're writing something like a partial, where the output needs to be buffered.
+            // Create a new buffer, but without the ability to flush.
+            var buffer = new ViewBuffer(_bufferScope, page.Path, ViewBuffer.ViewPageSize);
+            writer = new ViewBufferTextWriter(buffer, context.Writer.Encoding);
+        }
+
+        // The writer for the body is passed through the ViewContext, allowing things like HtmlHelpers
+        // and ViewComponents to reference it.
+        var oldWriter = context.Writer;
+        var oldFilePath = context.ExecutingFilePath;
+
+        context.Writer = writer;
+        context.ExecutingFilePath = page.Path;
+
+        try
+        {
+            if (invokeViewStarts)
+            {
+                // Execute view starts using the same context + writer as the page to render.
+                await RenderViewStartsAsync(context);
+            }
+
+            await RenderPageCoreAsync(page, context);
+            return writer;
+        }
