18755874228506ae4398aa5bac2600464dd0110d
pmtrilio <pm@trilio.bg>
2026-02-12 15:13:56 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: StaticFileMiddleware #73 (30-150).cs
@@ -0,0 +1,98 @@
+    /// <param name="options">The configuration options.</param>
+    /// <param name="loggerFactory">An <see cref="ILoggerFactory"/> instance used to create loggers.</param>
+    public StaticFileMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<StaticFileOptions> options, ILoggerFactory loggerFactory)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(hostingEnv);
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+
+        _next = next;
+        _options = options.Value;
+        _contentTypeProvider = _options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+        _fileProvider = _options.FileProvider ?? Helpers.ResolveFileProvider(hostingEnv);
+        _matchUrl = _options.RequestPath;
+        _logger = loggerFactory.CreateLogger<StaticFileMiddleware>();
+
+        // See HostingEnvironmentExtensions.Initialize
+        if (_fileProvider is NullFileProvider && _fileProvider == hostingEnv.WebRootFileProvider)
+        {
+            _logger.WebRootPathNotFound(Path.GetFullPath(Path.Combine(hostingEnv.ContentRootPath, hostingEnv.WebRootPath ?? "wwwroot")));
+        }
+    }
+
+    /// <summary>
+    /// Processes a request to determine if it matches a known file, and if so, serves it.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public Task Invoke(HttpContext context)
+    {
+        if (!ValidateNoEndpointDelegate(context))
+        {
+            _logger.EndpointMatched();
+        }
+        else if (!ValidateMethod(context))
+        {
+            _logger.RequestMethodNotSupported(context.Request.Method);
+        }
+        else if (!ValidatePath(context, _matchUrl, out var subPath))
+        {
+            _logger.PathMismatch(subPath);
+        }
+        else if (!LookupContentType(_contentTypeProvider, _options, subPath, out var contentType))
+        {
+            _logger.FileTypeNotSupported(subPath);
+        }
+        else
+        {
+            // If we get here, we can try to serve the file
+            return TryServeStaticFile(context, contentType, subPath);
+        }
+
+        return _next(context);
+    }
+
+    // Return true because we only want to run if there is no endpoint delegate.
+    private static bool ValidateNoEndpointDelegate(HttpContext context) => context.GetEndpoint()?.RequestDelegate is null;
+
+    private static bool ValidateMethod(HttpContext context)
+    {
+        return Helpers.IsGetOrHeadMethod(context.Request.Method);
+    }
+
+    internal static bool ValidatePath(HttpContext context, PathString matchUrl, out PathString subPath) => Helpers.TryMatchPath(context, matchUrl, forDirectory: false, out subPath);
+
+    internal static bool LookupContentType(IContentTypeProvider contentTypeProvider, StaticFileOptions options, PathString subPath, out string? contentType)
+    {
+        if (contentTypeProvider.TryGetContentType(subPath.Value!, out contentType))
+        {
+            return true;
+        }
+
+        if (options.ServeUnknownFileTypes)
+        {
+            contentType = options.DefaultContentType;
+            return true;
+        }
+
+        return false;
+    }
+
+    private Task TryServeStaticFile(HttpContext context, string? contentType, PathString subPath)
+    {
+        var fileContext = new StaticFileContext(context, _options, _logger, _fileProvider, contentType, subPath);
+
+        if (!fileContext.LookupFileInfo())
+        {
+            _logger.FileNotFound(fileContext.SubPath);
+        }
+        else
+        {
+            // If we get here, we can try to serve the file
+            return fileContext.ServeStaticFile(context, _next);
+        }
+
+        return _next(context);
+    }
+}
