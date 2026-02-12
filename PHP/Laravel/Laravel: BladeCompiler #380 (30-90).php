ad084a4a5a82315d89e2c73fc2ebbb6ea411d7f6
pmtrilio <pm@trilio.bg>
2026-02-12 21:27:23 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: BladeCompiler #380 (30-90).php
@@ -0,0 +1,61 @@
+        Concerns\CompilesJson,
+        Concerns\CompilesJs,
+        Concerns\CompilesLayouts,
+        Concerns\CompilesLoops,
+        Concerns\CompilesRawPhp,
+        Concerns\CompilesSessions,
+        Concerns\CompilesStacks,
+        Concerns\CompilesStyles,
+        Concerns\CompilesTranslations,
+        Concerns\CompilesUseStatements,
+        ReflectsClosures;
+
+    /**
+     * All of the registered extensions.
+     *
+     * @var array
+     */
+    protected $extensions = [];
+
+    /**
+     * All custom "directive" handlers.
+     *
+     * @var array
+     */
+    protected $customDirectives = [];
+
+    /**
+     * All custom "condition" handlers.
+     *
+     * @var array
+     */
+    protected $conditions = [];
+
+    /**
+     * The registered string preparation callbacks.
+     *
+     * @var array
+     */
+    protected $prepareStringsForCompilationUsing = [];
+
+    /**
+     * All of the registered precompilers.
+     *
+     * @var array
+     */
+    protected $precompilers = [];
+
+    /**
+     * The file currently being compiled.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * All of the available compiler functions.
+     *
+     * @var string[]
+     */
+    protected $compilers = [
+        // 'Comments',
