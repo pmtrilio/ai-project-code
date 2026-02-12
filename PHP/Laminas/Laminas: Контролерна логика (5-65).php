bc5b31e3324b5e4672e90f6be7f8f0bbffe3bfb4
pmtrilio <pm@trilio.bg>
2026-02-12 14:27:29 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Контролерна логика (5-65).php
@@ -0,0 +1,60 @@
+use Laminas\Mvc\Exception;
+use Laminas\Mvc\Exception\DomainException;
+use Laminas\Mvc\MvcEvent;
+use Laminas\View\Model\ViewModel;
+
+use function method_exists;
+
+/**
+ * Basic action controller
+ */
+abstract class AbstractActionController extends AbstractController
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $eventIdentifier = self::class;
+
+    /**
+     * Default action if none provided
+     *
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        return new ViewModel([
+            'content' => 'Placeholder page',
+        ]);
+    }
+
+    /**
+     * Action called if matched action does not exist
+     *
+     * @return ViewModel
+     */
+    public function notFoundAction()
+    {
+        $event      = $this->getEvent();
+        $routeMatch = $event->getRouteMatch();
+        $routeMatch->setParam('action', 'not-found');
+
+        $helper = $this->plugin('createHttpNotFoundModel');
+        return $helper($event->getResponse());
+    }
+
+    /**
+     * Execute the request
+     *
+     * @return mixed
+     * @throws Exception\DomainException
+     */
+    public function onDispatch(MvcEvent $e)
+    {
+        $routeMatch = $e->getRouteMatch();
+        if (! $routeMatch) {
+            /**
+             * @todo Determine requirements for when route match is missing.
+             *       Potentially allow pulling directly from request metadata?
+             */
+            throw new DomainException('Missing route matches; unsure how to retrieve action');
+        }
