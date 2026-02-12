877142f18604b428c2cac5fdabad2111d1413bbc
pmtrilio <pm@trilio.bg>
2026-02-12 15:05:37 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: SessionMiddleware #184 (20-60).cs
@@ -0,0 +1,41 @@
+    private static readonly Func<bool> ReturnTrue = () => true;
+    private readonly RequestDelegate _next;
+    private readonly SessionOptions _options;
+    private readonly ILogger _logger;
+    private readonly ISessionStore _sessionStore;
+    private readonly IDataProtector _dataProtector;
+
+    /// <summary>
+    /// Creates a new <see cref="SessionMiddleware"/>.
+    /// </summary>
+    /// <param name="next">The <see cref="RequestDelegate"/> representing the next middleware in the pipeline.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> representing the factory that used to create logger instances.</param>
+    /// <param name="dataProtectionProvider">The <see cref="IDataProtectionProvider"/> used to protect and verify the cookie.</param>
+    /// <param name="sessionStore">The <see cref="ISessionStore"/> representing the session store.</param>
+    /// <param name="options">The session configuration options.</param>
+    public SessionMiddleware(
+        RequestDelegate next,
+        ILoggerFactory loggerFactory,
+        IDataProtectionProvider dataProtectionProvider,
+        ISessionStore sessionStore,
+        IOptions<SessionOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        ArgumentNullException.ThrowIfNull(dataProtectionProvider);
+        ArgumentNullException.ThrowIfNull(sessionStore);
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next;
+        _logger = loggerFactory.CreateLogger<SessionMiddleware>();
+        _dataProtector = dataProtectionProvider.CreateProtector(nameof(SessionMiddleware));
+        _options = options.Value;
+        _sessionStore = sessionStore;
+    }
+
+    /// <summary>
+    /// Invokes the logic of the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/>.</param>
+    /// <returns>A <see cref="Task"/> that completes when the middleware has completed processing.</returns>
+    public async Task Invoke(HttpContext context)
