51d1d040b8c5d5a566b132520d35a15c4b167fb1
pmtrilio <pm@trilio.bg>
2026-02-12 19:39:53 +0200

    Upload file via PHP cURL

---
+++ PY/Scikit-learn/scikit-learn: Forest #467 (30-70).py
@@ -0,0 +1,41 @@
+  sub-estimator implementations.
+
+Single and multi-output problems are both handled.
+"""
+
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+import threading
+from abc import ABCMeta, abstractmethod
+from numbers import Integral
+from warnings import warn
+
+import numpy as np
+from scipy.sparse import hstack as sparse_hstack
+from scipy.sparse import issparse
+
+from sklearn.base import (
+    ClassifierMixin,
+    MultiOutputMixin,
+    RegressorMixin,
+    TransformerMixin,
+    _fit_context,
+    is_classifier,
+)
+from sklearn.ensemble._base import BaseEnsemble, _partition_estimators
+from sklearn.ensemble._bootstrap import _get_n_samples_bootstrap
+from sklearn.exceptions import DataConversionWarning
+from sklearn.metrics import accuracy_score, r2_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import (
+    BaseDecisionTree,
+    DecisionTreeClassifier,
+    DecisionTreeRegressor,
+    ExtraTreeClassifier,
+    ExtraTreeRegressor,
+)
+from sklearn.tree._tree import DOUBLE, DTYPE
+from sklearn.utils import (
+    check_random_state,
+    compute_class_weight,
