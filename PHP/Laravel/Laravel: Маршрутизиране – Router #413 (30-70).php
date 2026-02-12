bd63f1ed725af756823f06f9e7c7ef2a522ec68b
pmtrilio <pm@trilio.bg>
2026-02-12 19:04:01 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Маршрутизиране – Router #413 (30-70).php
@@ -0,0 +1,41 @@
+use ReflectionClass;
+use stdClass;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+/**
+ * @mixin \Illuminate\Routing\RouteRegistrar
+ */
+class Router implements BindingRegistrar, RegistrarContract
+{
+    use Macroable {
+        __call as macroCall;
+    }
+    use Tappable;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The route collection instance.
+     *
+     * @var \Illuminate\Routing\RouteCollectionInterface
+     */
+    protected $routes;
+
+    /**
+     * The currently dispatched route instance.
+     *
+     * @var \Illuminate\Routing\Route|null
+     */
