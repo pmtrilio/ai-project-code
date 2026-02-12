023aa2f04c683bc7a2589986627f353b8737e818
pmtrilio <pm@trilio.bg>
2026-02-12 19:14:21 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core Static Files: DefaultFilesMiddleware #215 (30-90).cs
@@ -0,0 +1,55 @@
+    public DefaultFilesMiddleware(RequestDelegate next, IWebHostEnvironment hostingEnv, IOptions<DefaultFilesOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(next);
+        ArgumentNullException.ThrowIfNull(hostingEnv);
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next;
+        _options = options.Value;
+        _fileProvider = _options.FileProvider ?? Helpers.ResolveFileProvider(hostingEnv);
+        _matchUrl = _options.RequestPath;
+    }
+
+    /// <summary>
+    /// This examines the request to see if it matches a configured directory, and if there are any files with the
+    /// configured default names in that directory.  If so this will append the corresponding file name to the request
+    /// path for a later middleware to handle.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public Task Invoke(HttpContext context)
+    {
+        if (context.GetEndpoint()?.RequestDelegate is null
+            && Helpers.IsGetOrHeadMethod(context.Request.Method)
+            && Helpers.TryMatchPath(context, _matchUrl, forDirectory: true, subpath: out var subpath))
+        {
+            // TryMatchPath will not output an empty subpath when it returns true.
+            var dirContents = _fileProvider.GetDirectoryContents(subpath.Value!);
+            if (dirContents.Exists)
+            {
+                // Check if any of our default files exist.
+                for (int matchIndex = 0; matchIndex < _options.DefaultFileNames.Count; matchIndex++)
+                {
+                    string defaultFile = _options.DefaultFileNames[matchIndex];
+                    var file = _fileProvider.GetFileInfo(subpath.Value + defaultFile);
+                    // TryMatchPath will make sure subpath always ends with a "/" by adding it if needed.
+                    if (file.Exists)
+                    {
+                        // If the path matches a directory but does not end in a slash, redirect to add the slash.
+                        // This prevents relative links from breaking.
+                        if (_options.RedirectToAppendTrailingSlash && !Helpers.PathEndsInSlash(context.Request.Path))
+                        {
+                            Helpers.RedirectToPathWithSlash(context);
+                            return Task.CompletedTask;
+                        }
+                        // Match found, re-write the url. A later middleware will actually serve the file.
+                        context.Request.Path = new PathString(Helpers.GetPathValueWithSlash(context.Request.Path) + defaultFile);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return _next(context);
+    }
+}
