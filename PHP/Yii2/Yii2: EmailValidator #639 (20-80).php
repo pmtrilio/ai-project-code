7c58c7f151dc077658203e47e91e5a31a8f9bc4d
pmtrilio <pm@trilio.bg>
2026-02-12 20:02:50 +0200

    Upload file via PHP cURL

---
+++ PHP/Yii2/Yii2: EmailValidator #639 (20-80).php
@@ -0,0 +1,61 @@
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class EmailValidator extends Validator
+{
+    /**
+     * @var string the regular expression used to validate the attribute value.
+     * @see https://www.regular-expressions.info/email.html
+     */
+    public $pattern = '/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/';
+    /**
+     * @var string the regular expression used to validate email addresses with the name part.
+     * This property is used only when [[allowName]] is true.
+     * @see allowName
+     */
+    public $fullPattern = '/^[^@]*<[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?>$/';
+    /**
+     * @var string the regular expression used to validate the part before the @ symbol, used if ASCII conversion fails to validate the address.
+     * @see https://www.regular-expressions.info/email.html
+     * @since 2.0.42
+     */
+    public $patternASCII = '/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*$/';
+    /**
+     * @var string the regular expression used to validate email addresses with the name part before the @ symbol, used if ASCII conversion fails to validate the address.
+     * This property is used only when [[allowName]] is true.
+     * @see allowName
+     * @since 2.0.42
+     */
+    public $fullPatternASCII = '/^[^@]*<[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*$/';
+    /**
+     * @var bool whether to allow name in the email address (e.g. "John Smith <john.smith@example.com>"). Defaults to false.
+     * @see fullPattern
+     */
+    public $allowName = false;
+    /**
+     * @var bool whether to check whether the email's domain exists and has either an A or MX record.
+     * Be aware that this check can fail due to temporary DNS problems even if the email address is
+     * valid and an email would be deliverable. Defaults to false.
+     */
+    public $checkDNS = false;
+    /**
+     * @var bool whether validation process should take into account IDN (internationalized domain
+     * names). Defaults to false meaning that validation of emails containing IDN will always fail.
+     * Note that in order to use IDN validation you have to install and enable `intl` PHP extension,
+     * otherwise an exception would be thrown.
+     */
+    public $enableIDN = false;
+    /**
+     * @var bool whether [[enableIDN]] should apply to the local part of the email (left side
+     * of the `@`). Only applies if [[enableIDN]] is `true`.
+     * @since 2.0.43
+     */
+    public $enableLocalIDN = true;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
