a82ce3ee1e0db605dea5c8ef7fd3821ba3dbc127
pmtrilio <pm@trilio.bg>
2026-02-12 15:11:02 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: ViewDataDictionary #581 (30-110).cs
@@ -0,0 +1,81 @@
+    /// <param name="metadataProvider">
+    /// <see cref="IModelMetadataProvider"/> instance used to create <see cref="ViewFeatures.ModelExplorer"/>
+    /// instances.
+    /// </param>
+    /// <param name="modelState"><see cref="ModelStateDictionary"/> instance for this scope.</param>
+    /// <remarks>For use when creating a <see cref="ViewDataDictionary"/> for a new top-level scope.</remarks>
+    public ViewDataDictionary(
+        IModelMetadataProvider metadataProvider,
+        ModelStateDictionary modelState)
+        : this(metadataProvider, modelState, declaredModelType: typeof(object))
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class based entirely on an existing
+    /// instance.
+    /// </summary>
+    /// <param name="source"><see cref="ViewDataDictionary"/> instance to copy initial values from.</param>
+    /// <remarks>
+    /// <para>
+    /// For use when copying a <see cref="ViewDataDictionary"/> instance and the declared <see cref="Model"/>
+    /// <see cref="Type"/> will not change e.g. when copying from a <see cref="ViewDataDictionary{TModel}"/>
+    /// instance to a base <see cref="ViewDataDictionary"/> instance.
+    /// </para>
+    /// <para>
+    /// This constructor should not be used in any context where <see cref="Model"/> may be set to a value
+    /// incompatible with the declared type of <paramref name="source"/>.
+    /// </para>
+    /// </remarks>
+    public ViewDataDictionary(ViewDataDictionary source)
+        : this(source, source.Model, source._declaredModelType)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class.
+    /// </summary>
+    /// <param name="metadataProvider">
+    /// <see cref="IModelMetadataProvider"/> instance used to create <see cref="ViewFeatures.ModelExplorer"/>
+    /// instances.
+    /// </param>
+    /// <remarks>Internal for testing.</remarks>
+    internal ViewDataDictionary(IModelMetadataProvider metadataProvider)
+        : this(metadataProvider, new ModelStateDictionary())
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class.
+    /// </summary>
+    /// <param name="metadataProvider">
+    /// <see cref="IModelMetadataProvider"/> instance used to create <see cref="ViewFeatures.ModelExplorer"/>
+    /// instances.
+    /// </param>
+    /// <param name="declaredModelType">
+    /// <see cref="Type"/> of <see cref="Model"/> values expected. Used to set <see cref="ModelMetadata"/>.
+    /// </param>
+    /// <remarks>
+    /// For use when creating a derived <see cref="ViewDataDictionary"/> for a new top-level scope.
+    /// </remarks>
+    protected ViewDataDictionary(
+        IModelMetadataProvider metadataProvider,
+        Type declaredModelType)
+        : this(metadataProvider, new ModelStateDictionary(), declaredModelType)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewDataDictionary"/> class.
+    /// </summary>
+    /// <param name="metadataProvider">
+    /// <see cref="IModelMetadataProvider"/> instance used to create <see cref="ViewFeatures.ModelExplorer"/>
+    /// instances.
+    /// </param>
+    /// <param name="modelState"><see cref="ModelStateDictionary"/> instance for this scope.</param>
+    /// <param name="declaredModelType">
+    /// <see cref="Type"/> of <see cref="Model"/> values expected. Used to set <see cref="ModelMetadata"/>.
+    /// </param>
+    /// <remarks>
+    /// For use when creating a derived <see cref="ViewDataDictionary"/> for a new top-level scope.
+    /// </remarks>
