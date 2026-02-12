5aa70e6add1959e40d114564bc5407c800792dbe
pmtrilio <pm@trilio.bg>
2026-02-12 15:13:25 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core HTTPS Policy: HstsMiddleware #44 (30-150).cs
@@ -0,0 +1,65 @@
+    /// <param name="next"></param>
+    /// <param name="options"></param>
+    /// <param name="loggerFactory"></param>
+    public HstsMiddleware(RequestDelegate next, IOptions<HstsOptions> options, ILoggerFactory loggerFactory)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+
+        var hstsOptions = options.Value;
+        var maxAge = Convert.ToInt64(Math.Floor(hstsOptions.MaxAge.TotalSeconds))
+                        .ToString(CultureInfo.InvariantCulture);
+        var includeSubdomains = hstsOptions.IncludeSubDomains ? IncludeSubDomains : StringSegment.Empty;
+        var preload = hstsOptions.Preload ? Preload : StringSegment.Empty;
+        _strictTransportSecurityValue = new StringValues($"max-age={maxAge}{includeSubdomains}{preload}");
+        _excludedHosts = hstsOptions.ExcludedHosts;
+        _logger = loggerFactory.CreateLogger<HstsMiddleware>();
+    }
+
+    /// <summary>
+    /// Initialize the HSTS middleware.
+    /// </summary>
+    /// <param name="next"></param>
+    /// <param name="options"></param>
+    public HstsMiddleware(RequestDelegate next, IOptions<HstsOptions> options)
+        : this(next, options, NullLoggerFactory.Instance) { }
+
+    /// <summary>
+    /// Invoke the middleware.
+    /// </summary>
+    /// <param name="context">The <see cref="HttpContext"/>.</param>
+    /// <returns></returns>
+    public Task Invoke(HttpContext context)
+    {
+        if (!context.Request.IsHttps)
+        {
+            _logger.SkippingInsecure();
+            return _next(context);
+        }
+
+        if (IsHostExcluded(context.Request.Host.Host))
+        {
+            _logger.SkippingExcludedHost(context.Request.Host.Host);
+            return _next(context);
+        }
+
+        context.Response.Headers.StrictTransportSecurity = _strictTransportSecurityValue;
+        _logger.AddingHstsHeader();
+
+        return _next(context);
+    }
+
+    private bool IsHostExcluded(string host)
+    {
+        for (var i = 0; i < _excludedHosts.Count; i++)
+        {
+            if (string.Equals(host, _excludedHosts[i], StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
