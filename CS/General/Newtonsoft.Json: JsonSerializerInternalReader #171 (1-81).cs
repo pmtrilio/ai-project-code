848e72283a6b453371736a9863314aa37ff56d16
pmtrilio <pm@trilio.bg>
2026-02-12 20:05:49 +0200

    Upload file via PHP cURL

---
+++ CS/General/Newtonsoft.Json: JsonSerializerInternalReader #171 (1-81).cs
@@ -0,0 +1,80 @@
+#region License
+// Copyright (c) 2007 James Newton-King
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+#if HAVE_DYNAMIC
+using System.ComponentModel;
+using System.Dynamic;
+#endif
+using System.Diagnostics;
+using System.Globalization;
+#if HAVE_BIG_INTEGER
+using System.Numerics;
+#endif
+using System.Reflection;
+using System.Runtime.Serialization;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Utilities;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+#if !HAVE_LINQ
+using Newtonsoft.Json.Utilities.LinqBridge;
+#else
+using System.Linq;
+#endif
+
+namespace Newtonsoft.Json.Serialization
+{
+    internal class JsonSerializerInternalReader : JsonSerializerInternalBase
+    {
+        internal enum PropertyPresence
+        {
+            None = 0,
+            Null = 1,
+            Value = 2
+        }
+
+        public JsonSerializerInternalReader(JsonSerializer serializer)
+            : base(serializer)
+        {
+        }
+
+        [RequiresUnreferencedCode(MiscellaneousUtils.TrimWarning)]
+        [RequiresDynamicCode(MiscellaneousUtils.AotWarning)]
+        public void Populate(JsonReader reader, object target)
+        {
+            ValidationUtils.ArgumentNotNull(target, nameof(target));
+
+            Type objectType = target.GetType();
+
+            JsonContract contract = Serializer._contractResolver.ResolveContract(objectType);
+
+            if (!reader.MoveToContent())
+            {
+                throw JsonSerializationException.Create(reader, "No JSON content found.");
+            }
