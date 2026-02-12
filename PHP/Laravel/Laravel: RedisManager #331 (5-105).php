f89b1d7392ab1ba5d6849238f18bf410e4f812d5
pmtrilio <pm@trilio.bg>
2026-02-12 20:31:59 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: RedisManager #331 (5-105).php
@@ -0,0 +1,100 @@
+use Closure;
+use Illuminate\Contracts\Redis\Factory;
+use Illuminate\Redis\Connections\Connection;
+use Illuminate\Redis\Connectors\PhpRedisConnector;
+use Illuminate\Redis\Connectors\PredisConnector;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ConfigurationUrlParser;
+use InvalidArgumentException;
+
+/**
+ * @mixin \Illuminate\Redis\Connections\Connection
+ */
+class RedisManager implements Factory
+{
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The name of the default driver.
+     *
+     * @var string
+     */
+    protected $driver;
+
+    /**
+     * The registered custom driver creators.
+     *
+     * @var array
+     */
+    protected $customCreators = [];
+
+    /**
+     * The Redis server configurations.
+     *
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * The Redis connections.
+     *
+     * @var mixed
+     */
+    protected $connections;
+
+    /**
+     * Indicates whether event dispatcher is set on connections.
+     *
+     * @var bool
+     */
+    protected $events = false;
+
+    /**
+     * Create a new Redis manager instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  string  $driver
+     * @param  array  $config
+     */
+    public function __construct($app, $driver, array $config)
+    {
+        $this->app = $app;
+        $this->driver = $driver;
+        $this->config = $config;
+    }
+
+    /**
+     * Get a Redis connection by name.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Redis\Connections\Connection
+     */
+    public function connection($name = null)
+    {
+        $name = $name ?: 'default';
+
+        if (isset($this->connections[$name])) {
+            return $this->connections[$name];
+        }
+
+        return $this->connections[$name] = $this->configure(
+            $this->resolve($name), $name
+        );
+    }
+
+    /**
+     * Resolve the given connection by name.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Redis\Connections\Connection
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function resolve($name = null)
+    {
+        $name = $name ?: 'default';
