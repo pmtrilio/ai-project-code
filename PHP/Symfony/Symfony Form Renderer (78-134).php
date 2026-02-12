dc3e3f84b812c6ac36ec96497698bfa17933c207
pmtrilio <pm@trilio.bg>
2026-02-12 15:02:48 +0200

    Upload file via PHP cURL

---
+++ PHP/Symfony/Symfony Form Renderer (78-134).php
@@ -0,0 +1,8 @@
+     * Makes a technical name human readable.
+     *
+     * Sequences of underscores are replaced by single spaces. The first letter
+     * of the resulting string is capitalized, while all other letters are
+     * turned to lowercase.
+     */
+    public function humanize(string $text): string;
+}
