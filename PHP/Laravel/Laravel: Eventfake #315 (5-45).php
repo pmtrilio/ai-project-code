9f3708aaa39e72a8b7012550f01fe343dd29ccdf
pmtrilio <pm@trilio.bg>
2026-02-12 14:44:46 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: Eventfake #315 (5-45).php
@@ -0,0 +1,41 @@
+use Closure;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\ForwardsCalls;
+use Illuminate\Support\Traits\ReflectsClosures;
+use PHPUnit\Framework\Assert as PHPUnit;
+use ReflectionFunction;
+
+class EventFake implements Dispatcher, Fake
+{
+    use ForwardsCalls, ReflectsClosures;
+
+    /**
+     * The original event dispatcher.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    public $dispatcher;
+
+    /**
+     * The event types that should be intercepted instead of dispatched.
+     *
+     * @var array
+     */
+    protected $eventsToFake = [];
+
+    /**
+     * The event types that should be dispatched instead of intercepted.
+     *
+     * @var array
+     */
+    protected $eventsToDispatch = [];
+
+    /**
+     * All of the events that have been intercepted keyed by type.
+     *
+     * @var array
