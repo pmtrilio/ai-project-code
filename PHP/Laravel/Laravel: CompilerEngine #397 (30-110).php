fc31bb3210d51d6bdb9889671f92d84be4f9d2ab
pmtrilio <pm@trilio.bg>
2026-02-12 20:33:21 +0200

    Upload file via PHP cURL

---
+++ PHP/Laravel/Laravel: CompilerEngine #397 (30-110).php
@@ -0,0 +1,81 @@
+
+    /**
+     * The view paths that were compiled or are not expired, keyed by the path.
+     *
+     * @var array<string, true>
+     */
+    protected $compiledOrNotExpired = [];
+
+    /**
+     * Create a new compiler engine instance.
+     *
+     * @param  \Illuminate\View\Compilers\CompilerInterface  $compiler
+     * @param  \Illuminate\Filesystem\Filesystem|null  $files
+     */
+    public function __construct(CompilerInterface $compiler, ?Filesystem $files = null)
+    {
+        parent::__construct($files ?: new Filesystem);
+
+        $this->compiler = $compiler;
+    }
+
+    /**
+     * Get the evaluated contents of the view.
+     *
+     * @param  string  $path
+     * @param  array  $data
+     * @return string
+     */
+    public function get($path, array $data = [])
+    {
+        $this->lastCompiled[] = $path;
+
+        // If this given view has expired, which means it has simply been edited since
+        // it was last compiled, we will re-compile the views so we can evaluate a
+        // fresh copy of the view. We'll pass the compiler the path of the view.
+        if (! isset($this->compiledOrNotExpired[$path]) && $this->compiler->isExpired($path)) {
+            $this->compiler->compile($path);
+        }
+
+        // Once we have the path to the compiled file, we will evaluate the paths with
+        // typical PHP just like any other templates. We also keep a stack of views
+        // which have been rendered for right exception messages to be generated.
+
+        try {
+            $results = $this->evaluatePath($this->compiler->getCompiledPath($path), $data);
+        } catch (ViewException $e) {
+            if (! Str::of($e->getMessage())->contains(['No such file or directory', 'File does not exist at path'])) {
+                throw $e;
+            }
+
+            if (! isset($this->compiledOrNotExpired[$path])) {
+                throw $e;
+            }
+
+            $this->compiler->compile($path);
+
+            $results = $this->evaluatePath($this->compiler->getCompiledPath($path), $data);
+        }
+
+        $this->compiledOrNotExpired[$path] = true;
+
+        array_pop($this->lastCompiled);
+
+        return $results;
+    }
+
+    /**
+     * Handle a view exception.
+     *
+     * @param  \Throwable  $e
+     * @param  int  $obLevel
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    protected function handleViewException(Throwable $e, $obLevel)
+    {
+        if ($e instanceof HttpException ||
+            $e instanceof HttpResponseException ||
+            $e instanceof RecordNotFoundException ||
+            $e instanceof RecordsNotFoundException) {
