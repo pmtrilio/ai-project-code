7ec84718beac57e26d1d892634718865a2c7498f
pmtrilio <pm@trilio.bg>
2026-02-12 15:00:31 +0200

    Upload file via PHP cURL

---
+++ CS/Entity Framework Core/Entity Framework Core: DbContext #625 (5-105).cs
@@ -0,0 +1,101 @@
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+///     A DbContext instance represents a session with the database and can be used to query and save
+///     instances of your entities. DbContext is a combination of the Unit Of Work and Repository patterns.
+/// </summary>
+/// <remarks>
+///     <para>
+///         Entity Framework Core does not support multiple parallel operations being run on the same DbContext instance. This
+///         includes both parallel execution of async queries and any explicit concurrent use from multiple threads.
+///         Therefore, always await async calls immediately, or use separate DbContext instances for operations that execute
+///         in parallel. See <see href="https://aka.ms/efcore-docs-threading">Avoiding DbContext threading issues</see> for more information
+///         and examples.
+///     </para>
+///     <para>
+///         Typically you create a class that derives from DbContext and contains <see cref="DbSet{TEntity}" />
+///         properties for each entity in the model. If the <see cref="DbSet{TEntity}" /> properties have a public setter,
+///         they are automatically initialized when the instance of the derived context is created.
+///     </para>
+///     <para>
+///         Override the <see cref="OnConfiguring(DbContextOptionsBuilder)" /> method to configure the database (and
+///         other options) to be used for the context. Alternatively, if you would rather perform configuration externally
+///         instead of inline in your context, you can use <see cref="DbContextOptionsBuilder{TContext}" />
+///         (or <see cref="DbContextOptionsBuilder" />) to externally create an instance of <see cref="DbContextOptions{TContext}" />
+///         (or <see cref="DbContextOptions" />) and pass it to a base constructor of <see cref="DbContext" />.
+///     </para>
+///     <para>
+///         The model is discovered by running a set of conventions over the entity classes found in the
+///         <see cref="DbSet{TEntity}" /> properties on the derived context. To further configure the model that
+///         is discovered by convention, you can override the <see cref="OnModelCreating(ModelBuilder)" /> method.
+///     </para>
+///     <para>
+///         See <see href="https://aka.ms/efcore-docs-dbcontext">DbContext lifetime, configuration, and initialization</see>,
+///         <see href="https://aka.ms/efcore-docs-query">Querying data with EF Core</see>,
+///         <see href="https://aka.ms/efcore-docs-change-tracking">Changing tracking</see>, and
+///         <see href="https://aka.ms/efcore-docs-saving-data">Saving data with EF Core</see> for more information and examples.
+///     </para>
+/// </remarks>
+public class DbContext :
+    IInfrastructure<IServiceProvider>,
+    IDbContextDependencies,
+    IDbSetCache,
+    IDbContextPoolable
+{
+    private readonly DbContextOptions _options;
+
+    private Dictionary<(Type Type, string? Name), object>? _sets;
+    private IDbContextServices? _contextServices;
+    private IDbContextDependencies? _dbContextDependencies;
+    private DatabaseFacade? _database;
+    private ChangeTracker? _changeTracker;
+
+    private IServiceScope? _serviceScope;
+    private DbContextLease _lease = DbContextLease.InactiveLease;
+    private DbContextPoolConfigurationSnapshot? _configurationSnapshot;
+    private List<IResettableService>? _cachedResettableServices;
+    private bool _initializing;
+    private bool _disposed;
+
+    private readonly Guid _contextId = Guid.NewGuid();
+    private int _leaseCount;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="DbContext" /> class. The
+    ///     <see cref="OnConfiguring(DbContextOptionsBuilder)" />
+    ///     method will be called to configure the database (and other options) to be used for this context.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext">DbContext lifetime, configuration, and initialization</see>
+    ///     for more information and examples.
+    /// </remarks>
+    [RequiresUnreferencedCode(
+         "EF Core isn't fully compatible with trimming, and running the application may generate unexpected runtime failures. "
+         + "Some specific coding pattern are usually required to make trimming work properly, see https://aka.ms/efcore-docs-trimming for "
+         + "more details."), RequiresDynamicCode(
+         "EF Core isn't fully compatible with NativeAOT, and running the application may generate unexpected runtime failures.")]
+    protected DbContext()
+        : this(new DbContextOptions<DbContext>())
+    {
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="DbContext" /> class using the specified options.
+    ///     The <see cref="OnConfiguring(DbContextOptionsBuilder)" /> method will still be called to allow further
+    ///     configuration of the options.
+    /// </summary>
+    /// <remarks>
+    ///     See <see href="https://aka.ms/efcore-docs-dbcontext">DbContext lifetime, configuration, and initialization</see> and
+    ///     <see href="https://aka.ms/efcore-docs-dbcontext-options">Using DbContextOptions</see> for more information and examples.
+    /// </remarks>
+    /// <param name="options">The options for this context.</param>
+    [RequiresUnreferencedCode(
+         "EF Core isn't fully compatible with trimming, and running the application may generate unexpected runtime failures. "
+         + "Some specific coding pattern are usually required to make trimming work properly, see https://aka.ms/efcore-docs-trimming for "
+         + "more details."), RequiresDynamicCode(
+         "EF Core isn't fully compatible with NativeAOT, and running the application may generate unexpected runtime failures.")]
