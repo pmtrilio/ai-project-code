8cb580400880a29fef3d0814fd250ecc14405683
pmtrilio <pm@trilio.bg>
2026-02-12 14:55:10 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: HttpContext #233 (30-70).cs
@@ -0,0 +1,41 @@
+    /// Gets the <see cref="HttpResponse"/> object for this request.
+    /// </summary>
+    public abstract HttpResponse Response { get; }
+
+    /// <summary>
+    /// Gets information about the underlying connection for this request.
+    /// </summary>
+    public abstract ConnectionInfo Connection { get; }
+
+    /// <summary>
+    /// Gets an object that manages the establishment of WebSocket connections for this request.
+    /// </summary>
+    public abstract WebSocketManager WebSockets { get; }
+
+    /// <summary>
+    /// Gets or sets the user for this request.
+    /// </summary>
+    public abstract ClaimsPrincipal User { get; set; }
+
+    /// <summary>
+    /// Gets or sets a key/value collection that can be used to share data within the scope of this request.
+    /// </summary>
+    public abstract IDictionary<object, object?> Items { get; set; }
+
+    /// <summary>
+    /// Gets or sets the <see cref="IServiceProvider"/> that provides access to the request's service container.
+    /// </summary>
+    public abstract IServiceProvider RequestServices { get; set; }
+
+    /// <summary>
+    /// Notifies when the connection underlying this request is aborted and thus request operations should be
+    /// cancelled.
+    /// </summary>
+    public abstract CancellationToken RequestAborted { get; set; }
+
+    /// <summary>
+    /// Gets or sets a unique identifier to represent this request in trace logs.
+    /// </summary>
+    public abstract string TraceIdentifier { get; set; }
+
+    /// <summary>
