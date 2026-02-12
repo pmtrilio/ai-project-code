c42c8f29dd73a8d54b133829c372f56d6af806b3
pmtrilio <pm@trilio.bg>
2026-02-12 14:15:37 +0200

    Upload file via PHP cURL

---
+++ CS/Runtime/.NET Runtime: Вход|изход (I|O) – Dictionary (20-60).cs
@@ -0,0 +1,41 @@
+        // constants for serialization
+        private const string VersionName = "Version"; // Do not rename (binary serialization)
+        private const string HashSizeName = "HashSize"; // Do not rename (binary serialization). Must save buckets.Length
+        private const string KeyValuePairsName = "KeyValuePairs"; // Do not rename (binary serialization)
+        private const string ComparerName = "Comparer"; // Do not rename (binary serialization)
+
+        private int[]? _buckets;
+        private Entry[]? _entries;
+#if TARGET_64BIT
+        private ulong _fastModMultiplier;
+#endif
+        private int _count;
+        private int _freeList;
+        private int _freeCount;
+        private int _version;
+        private IEqualityComparer<TKey>? _comparer;
+        private KeyCollection? _keys;
+        private ValueCollection? _values;
+        private const int StartOfFreeList = -3;
+
+        public Dictionary() : this(0, null) { }
+
+        public Dictionary(int capacity) : this(capacity, null) { }
+
+        public Dictionary(IEqualityComparer<TKey>? comparer) : this(0, comparer) { }
+
+        public Dictionary(int capacity, IEqualityComparer<TKey>? comparer)
+        {
+            if (capacity < 0)
+            {
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.capacity);
+            }
+
+            if (capacity > 0)
+            {
+                Initialize(capacity);
+            }
+
+            // For reference types, we always want to store a comparer instance, either
+            // the one provided, or if one wasn't provided, the default (accessing
+            // EqualityComparer<TKey>.Default with shared generics on every dictionary
