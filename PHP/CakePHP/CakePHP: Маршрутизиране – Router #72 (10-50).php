f330fd6cd0dcd664fcd1f044ba78e8613209232d
pmtrilio <pm@trilio.bg>
2026-02-12 14:29:55 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Маршрутизиране – Router #72 (10-50).php
@@ -0,0 +1,41 @@
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Routing;
+
+use Cake\Core\Configure;
+use Cake\Core\Exception\CakeException;
+use Cake\Http\ServerRequest;
+use Cake\Routing\Exception\MissingRouteException;
+use Cake\Routing\Route\Route;
+use Closure;
+use InvalidArgumentException;
+use Psr\Http\Message\UriInterface;
+use ReflectionFunction;
+use Throwable;
+
+/**
+ * Parses the request URL into controller, action, and parameters. Uses the connected routes
+ * to match the incoming URL string to parameters that will allow the request to be dispatched. Also
+ * handles converting parameter lists into URL strings, using the connected routes. Routing allows you to decouple
+ * the way the world interacts with your application (URLs) and the implementation (controllers and actions).
+ */
+class Router
+{
+    /**
+     * Default route class.
+     *
+     * @var string
+     */
+    protected static string $_defaultRouteClass = Route::class;
+
+    /**
+     * Contains the base string that will be applied to all generated URLs
+     * For example `https://example.com`
+     *
+     * @var string|null
+     */
