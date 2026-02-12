21e8b95897efecb46cfa7596a4594bf5c9d0f72a
pmtrilio <pm@trilio.bg>
2026-02-12 14:54:46 +0200

    Upload file via PHP cURL

---
+++ CS/ASP.NET Core/ASP.NET Core: HttpResponse #203 (20-80).cs
@@ -0,0 +1,61 @@
+    private static readonly Func<object, Task> _disposeDelegate = state =>
+    {
+        // Prefer async dispose over dispose
+        if (state is IAsyncDisposable asyncDisposable)
+        {
+            return asyncDisposable.DisposeAsync().AsTask();
+        }
+        else if (state is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+        return Task.CompletedTask;
+    };
+
+    /// <summary>
+    /// Gets the <see cref="HttpContext"/> for this response.
+    /// </summary>
+    public abstract HttpContext HttpContext { get; }
+
+    /// <summary>
+    /// Gets or sets the HTTP response code.
+    /// </summary>
+    public abstract int StatusCode { get; set; }
+
+    /// <summary>
+    /// Gets the response headers.
+    /// </summary>
+    public abstract IHeaderDictionary Headers { get; }
+
+    /// <summary>
+    /// Gets or sets the response body <see cref="Stream"/>.
+    /// </summary>
+    public abstract Stream Body { get; set; }
+
+    /// <summary>
+    /// Gets the response body <see cref="PipeWriter"/>
+    /// </summary>
+    /// <value>The response body <see cref="PipeWriter"/>.</value>
+    public virtual PipeWriter BodyWriter { get => throw new NotImplementedException(); }
+
+    /// <summary>
+    /// Gets or sets the value for the <c>Content-Length</c> response header.
+    /// </summary>
+    public abstract long? ContentLength { get; set; }
+
+    /// <summary>
+    /// Gets or sets the value for the <c>Content-Type</c> response header.
+    /// </summary>
+    public abstract string? ContentType { get; set; }
+
+    /// <summary>
+    /// Gets an object that can be used to manage cookies for this response.
+    /// </summary>
+    public abstract IResponseCookies Cookies { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether response headers have been sent to the client.
+    /// </summary>
+    public abstract bool HasStarted { get; }
+
+    /// <summary>
