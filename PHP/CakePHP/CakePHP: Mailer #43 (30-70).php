9c04b9b6dedd382a5ca37639362a3e408a8209a1
pmtrilio <pm@trilio.bg>
2026-02-12 20:26:05 +0200

    Upload file via PHP cURL

---
+++ PHP/CakePHP/CakePHP: Mailer #43 (30-70).php
@@ -0,0 +1,41 @@
+ * Mailer classes let you encapsulate related Email logic into a reusable
+ * and testable class.
+ *
+ * ## Defining Messages
+ *
+ * Mailers make it easy for you to define methods that handle email formatting
+ * logic. For example:
+ *
+ * ```
+ * class UserMailer extends Mailer
+ * {
+ *     public function resetPassword($user)
+ *     {
+ *         $this
+ *             ->setSubject('Reset Password')
+ *             ->setTo($user->email)
+ *             ->set(['token' => $user->token]);
+ *     }
+ * }
+ * ```
+ *
+ * Is a trivial example but shows how a mailer could be declared.
+ *
+ * ## Sending Messages
+ *
+ * After you have defined some messages you will want to send them:
+ *
+ * ```
+ * $mailer = new UserMailer();
+ * $mailer->send('resetPassword', $user);
+ * ```
+ *
+ * ## Event Listener
+ *
+ * Mailers can also subscribe to application event allowing you to
+ * decouple email delivery from your application code. By re-declaring the
+ * `implementedEvents()` method you can define event handlers that can
+ * convert events into email. For example, if your application had a user
+ * registration event:
+ *
+ * ```
