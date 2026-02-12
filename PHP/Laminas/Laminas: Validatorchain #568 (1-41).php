fc9f4ebda79e36ea4a682ca370527b24d2f76f2e
pmtrilio <pm@trilio.bg>
2026-02-12 14:49:22 +0200

    Upload file via PHP cURL

---
+++ PHP/Laminas/Laminas: Validatorchain #568 (1-41).php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\Validator;
+
+use Countable;
+use IteratorAggregate;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Stdlib\PriorityQueue;
+use Laminas\Validator\Exception\InvalidSpecificationArrayException;
+use Traversable;
+
+use function array_replace;
+use function assert;
+use function count;
+use function is_array;
+use function is_bool;
+use function is_int;
+use function is_string;
+use function rsort;
+
+use const SORT_NUMERIC;
+
+/**
+ * @psalm-type QueueElement = array{instance: ValidatorInterface, breakChainOnFailure: bool}
+ * @implements IteratorAggregate<array-key, QueueElement>
+ * @psalm-type ValidatorSpecification = array{
+ *     name: non-empty-string|class-string<ValidatorInterface>,
+ *     options?: array<string, mixed>,
+ *     break_chain_on_failure?: bool,
+ *     priority?: int,
+ * }
+ * @psalm-type ValidatorChainSpecification = array<array-key, ValidatorSpecification|ValidatorInterface>
+ */
+final class ValidatorChain implements Countable, IteratorAggregate, ValidatorChainInterface
+{
+    /**
+     * Default priority at which validators are added
+     *
+     * @deprecated Use ValidatorChainInterface::DEFAULT_PRIORITY instead.
